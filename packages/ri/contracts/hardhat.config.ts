365479483d571490a58778a076f786e3413e025d
alvrs <alvarius@lattice.xyz>
2022-05-17 14:25:34 +0100

    build(ri-contracts): add automine to hardhat config (keep hardhat as fallback for anvil)

--- packages/ri/contracts/hardhat.config.ts
+++ packages/ri/contracts/hardhat.config.ts
@@ -33,6 +33,43 @@ const config: HardhatUserConfig = {
     flat: true,
     only: [":Ember$", ":World", "Component"],
   },
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545/",
+      accounts: [
+        "0x044C7963E9A89D4F8B64AB23E02E97B2E00DD57FCB60F316AC69B77135003AEF",
+        "0x523170AAE57904F24FFE1F61B7E4FF9E9A0CE7557987C2FC034EACB1C267B4AE",
+        "0x67195c963ff445314e667112ab22f4a7404bad7f9746564eb409b9bb8c6aed32",
+      ],
+      chainId: 31337,
+    },
+    hardhat: {
+      mining: {
+        auto: true,
+      },
+      gasPrice: 0,
+      initialBaseFeePerGas: 0,
+      blockGasLimit: 999999999999999,
+      accounts: [
+        // from/deployer is default the first address in accounts
+        {
+          privateKey: "0x044C7963E9A89D4F8B64AB23E02E97B2E00DD57FCB60F316AC69B77135003AEF",
+          balance: "100000000000000000000",
+        },
+        // user1 in tests
+        {
+          privateKey: "0x523170AAE57904F24FFE1F61B7E4FF9E9A0CE7557987C2FC034EACB1C267B4AE",
+          balance: "100000000000000000000",
+        },
+        // user2 in tests
+        {
+          privateKey: "0x67195c963ff445314e667112ab22f4a7404bad7f9746564eb409b9bb8c6aed32",
+          balance: "100000000000000000000",
+        },
+      ],
+    },
+  },
 };
 
 export default config;
