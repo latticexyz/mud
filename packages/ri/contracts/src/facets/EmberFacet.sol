c41120015c9b17611eaabb8c51f33354e59f3040
alvrs <alvarius@lattice.xyz>
2022-05-17 14:28:08 +0100

    style(ri-contracts): remove unreachable code

--- packages/ri/contracts/src/facets/EmberFacet.sol
+++ packages/ri/contracts/src/facets/EmberFacet.sol
@@ -14,7 +14,6 @@ contract EmberFacet is UsingDiamondOwner, UsingAccessControl {
 
   function world() external view returns (address) {
     return address(s.world);
-    return address(0);
   }
 
   // Debugging
