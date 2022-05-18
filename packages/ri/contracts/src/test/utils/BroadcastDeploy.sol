5b9f7907f23b6575dd5b3e23298a8864a556cd29
alvrs <alvarius@lattice.xyz>
2022-05-18 11:33:49 +0100

    build(ri-contracts): let deploy script accept deployer address as input arg

--- packages/ri/contracts/src/test/utils/BroadcastDeploy.sol
+++ packages/ri/contracts/src/test/utils/BroadcastDeploy.sol
@@ -72,10 +72,10 @@ contract Deploy is DSTest {
   PersonaMirror public personaMirror;
   MockConsumer public consumer;
 
-  function deployEmber() public returns (address) {
-    deployer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // 1st anvil address
+  function deployEmber(address deployer) public returns (address) {
+    // deployer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; // 1st anvil address
     // deployer = 0x1c0f0Af3262A7213E59Be7f1440282279D788335; // 1st hardhat address
-    deployer = 0xDdaa406fe1D2d3Af4B9cB3dA1B3C9E9fa3B7f427; // degen chain
+    // deployer = 0xDdaa406fe1D2d3Af4B9cB3dA1B3C9E9fa3B7f427; // degen chain
 
     vm.startBroadcast(deployer);
 
