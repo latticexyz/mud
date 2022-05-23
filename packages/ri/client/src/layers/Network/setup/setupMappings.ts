88726ec559ff5707b84f2eae1d570510fb3ae358
alvrs <alvarius@lattice.xyz>
2022-05-23 14:38:45 +0100

    perf(ri-client): throttle ECS event processing to 12000 per second
    to reduce client renderer lags

--- packages/ri/client/src/layers/Network/setup/setupMappings.ts
+++ packages/ri/client/src/layers/Network/setup/setupMappings.ts
@@ -2,8 +2,9 @@ import { World as WorldContract } from "ri-contracts/types/ethers-contracts/Worl
 import { CombinedFacets } from "ri-contracts/types/ethers-contracts/CombinedFacets";
 import { setComponent, World, Component, Schema } from "@mud/recs";
 import { createECSStream, Mappings, ContractEvent } from "@mud/network";
-import { mergeMap, Observable, Subject } from "rxjs";
+import { bufferCount, filter, mergeMap, Observable, Subject } from "rxjs";
 import { NetworkLayer } from "../types";
+import { stretch } from "@mud/utils";
 
 /**
  * Sets up syncronization between contract components and client components by listening to World contract events
@@ -36,13 +37,28 @@ export function setupMappings(
 
   const txReduced$ = new Subject<string>();
 
-  const ecsEventSub = ecsEventStream$.subscribe((update) => {
-    if (!world.entities.has(update.entity)) {
-      world.registerEntity(update.entity);
-    }
-    setComponent(components[update.component] as Component<Schema>, update.entity, update.value);
-    if (update.lastEventInTx) txReduced$.next(update.txHash);
-  });
+  const ecsEventSub = ecsEventStream$
+    .pipe(
+      // We throttle the client side event processing to 200 events every 16ms, so 12.000 events per second.
+      // This means if the chain would emit more than 12.000 events per second, the client couldn't keep up.
+      // (We're not close to 12.000 events per second on the chain yet)
+      bufferCount(200, 16),
+      filter((updates) => updates.length > 0),
+      stretch(16)
+    )
+    .subscribe((updates) => {
+      // Running this in a mobx action would result in only one system update per frame (should increase performance)
+      // but it currently breaks defineUpdateAction (https://linear.app/latticexyz/issue/LAT-594/defineupdatequery-does-not-work-when-running-multiple-component)
+      // runInAction(() => {
+      for (const update of updates) {
+        if (!world.entities.has(update.entity)) {
+          world.registerEntity(update.entity);
+        }
+        setComponent(components[update.component] as Component<Schema>, update.entity, update.value);
+        if (update.lastEventInTx) txReduced$.next(update.txHash);
+      }
+      // });
+    });
 
   world.registerDisposer(() => ecsEventSub?.unsubscribe());
   return { txReduced$: txReduced$.asObservable() };
