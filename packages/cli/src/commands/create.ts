58ca193138db9fd1cd58eb4fc6eb5c624ad9a461
alvrs <alvarius@lattice.xyz>
2022-05-24 13:16:05 +0100

    build(@latticexyz/cli): improve mud create command

--- packages/cli/src/commands/create.ts
+++ packages/cli/src/commands/create.ts
@@ -6,7 +6,7 @@ type Options = {
 };
 
 export const command = "create <name>";
-export const desc = "Sets up a fresh mud project into <name>";
+export const desc = "Sets up a fresh mud project into <name>. Requires yarn.";
 
 export const builder: CommandBuilder<Options, Options> = (yargs) =>
   yargs.positional("name", { type: "string", demandOption: true });
@@ -17,15 +17,35 @@ export const handler = async (argv: Arguments<Options>): Promise<void> => {
 
   console.log("Cloning...");
   await exec(`git clone https://github.com/latticexyz/mud _mudtemp`);
-  console.log("Done cloning");
 
   console.log("Moving...");
   await exec(`cp -r _mudtemp/packages/ri ${name}`);
-  console.log("Done moving");
 
   console.log("Cleaning up...");
   await exec(`rm -rf _mudtemp`);
-  console.log("Done cleaning up");
+
+  console.log("Setting up package.json...");
+  await exec(`mv ${name}/packagejson.template ${name}/package.json`);
+
+  console.log("Installing dependencies using yarn...");
+  await exec(`cd ${name} && yarn install`);
+
+  console.log("Setting up foundry.toml...");
+  await exec(`rm ${name}/contracts/foundry.toml`);
+  await exec(`mv ${name}/contracts/foundrytoml.template contracts/foundry.toml`);
+
+  console.log("Setting up remappings...");
+  await exec(`rm ${name}/contracts/remappings.txt`);
+  await exec(`mv ${name}/contracts/remappingstxt.template contracts/remappings.txt`);
+
+  console.log("Setting up compile task...");
+  await exec(`rm ${name}/contracts/tasks/compile.ts`);
+  await exec(`mv ${name}/contracts/tasks/compilets.template contracts/tasks/compile.ts`);
+
+  console.log("Building contracts...");
+  await exec(`cd contracts && yarn build`);
+
+  console.log("Done setting up! Run `yarn start` to start client and chain, then head to localhost:3000 to explore.");
 
   process.exit(0);
 };
