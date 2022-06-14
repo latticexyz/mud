86c161d12b8f9852cd80ec6612cb1b914c4ef95b
alvrs <alvarius@lattice.xyz>
2022-06-14 11:09:02 -0400

    feat(network): generalize component value decoder function, add tests

--- packages/solecs/src/LibTypes.sol
+++ packages/solecs/src/LibTypes.sol
@@ -3,10 +3,37 @@ pragma solidity >=0.8.13;
 
 library LibTypes {
   enum SchemaValue {
-    UINT8,
-    UINT256,
+    BOOL,
+    INT8,
+    INT16,
+    INT32,
     INT64,
+    INT128,
+    INT256,
+    INT,
+    UINT8,
+    UINT16,
+    UINT32,
+    UINT64,
+    UINT128,
+    UINT256,
+    BYTES,
+    STRING,
+    BOOL_ARRAY,
+    INT8_ARRAY,
+    INT16_ARRAY,
+    INT32_ARRAY,
+    INT64_ARRAY,
+    INT128_ARRAY,
+    INT256_ARRAY,
+    INT_ARRAY,
+    UINT8_ARRAY,
+    UINT16_ARRAY,
+    UINT32_ARRAY,
+    UINT64_ARRAY,
+    UINT128_ARRAY,
     UINT256_ARRAY,
-    BYTES
+    BYTES_ARRAY,
+    STRING_ARRAY
   }
 }
