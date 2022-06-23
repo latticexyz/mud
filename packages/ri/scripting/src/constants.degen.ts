e5c80ba941b45b011993909b2a79bd9f183f9b41
andrii dobroshynski <24281657+andriidski@users.noreply.github.com>
2022-06-23 17:38:53 -0400

    chore: update constants for lattice chain

--- packages/ri/scripting/src/constants.degen.ts
+++ packages/ri/scripting/src/constants.degen.ts
@@ -1,4 +1,4 @@
-export const RPC_URL = "https://degen-chain.lattice.xyz/";
-export const RPC_WS_URL = "wss://degen-chain.lattice.xyz/ws/";
-export const DEV_PRIVATE_KEY = "0xb20486f2dadf532076b54c65b945f7df6728945d8bff5df06088dc1286bee1c3";
-export const DIAMOND_ADDRESS = "0x39903eA6F367D765001A26f5C64264BEfd42D26f";
+export const RPC_URL = "https://follower.super-degen-chain.lattice.xyz/";
+export const RPC_WS_URL = "wss://follower.super-degen-chain.lattice.xyz/";
+export const DEV_PRIVATE_KEY = "0x26e86e45f6fc45ec6e2ecd128cec80fa1d1505e5507dcd2ae58c3130a7a97b48";
+export const DIAMOND_ADDRESS = "0xD83B554d44cBcDD8d5d6a80597A7d2f87cB187eF";
