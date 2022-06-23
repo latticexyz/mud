e581017eba5b3d51fac91abc39c2cee801730925
alvrs <alvarius@lattice.xyz>
2022-06-23 13:17:51 -0400

    feat(ri-launcher): add boilerplate for ri launcher

---
+++ packages/ri/launcher/src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App as AppImport } from "./App";
+
+// Assign variables that can be overridden by HMR
+let App = AppImport;
+
+function boot() {
+  const rootElement = document.getElementById("react-root");
+  if (!rootElement) return console.warn("React root not found");
+  const root = ReactDOM.createRoot(rootElement);
+
+  function renderEngine() {
+    root.render(<App />);
+  }
+
+  renderEngine();
+
+  if (import.meta.hot) {
+    // HMR React
+    import.meta.hot.accept("./App.tsx", async (module) => {
+      App = module.App;
+      renderEngine();
+    });
+  }
+}
+
+boot();
