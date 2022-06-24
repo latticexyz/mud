98ddea9f14974954ed7927cc87aa668861a711dc
alvrs <alvarius@lattice.xyz>
2022-06-24 11:35:27 -0400

    feat(persona-js): add persona types and utils

---
+++ packages/persona-js/src/Persona.ts
@@ -0,0 +1,68 @@
+import { Contract, Signer, Wallet } from "ethers";
+import { ChainSpec } from "./types";
+import { abi as PersonaAbi } from "@latticexyz/persona/abi/Persona.json";
+import { abi as PersonaMirrorAbi } from "@latticexyz/persona/abi/PersonaMirror.json";
+import { abi as PersonaAllMinterAbi } from "@latticexyz/persona/abi/PersonaAllMinter.json";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { Persona as PersonaContract, PersonaAllMinter, PersonaMirror } from "./ethers-types";
+import { hexZeroPad } from "ethers/lib/utils";
+
+export function Persona({ personaAddress, personaAllMinterAddress, personaMirrorAddress, chainId, rpc }: ChainSpec) {
+  const provider = new JsonRpcProvider(rpc, chainId);
+  let persona = new Contract(personaAddress, PersonaAbi, provider) as PersonaContract;
+  let personaMirror = new Contract(personaMirrorAddress, PersonaMirrorAbi, provider) as PersonaMirror;
+  let personaAllMinter = new Contract(personaAllMinterAddress, PersonaAllMinterAbi, provider) as PersonaAllMinter;
+
+  function getPersonasOfAddress(address: string) {
+    // Iterate over all personas and filter by address
+    // TODO: make persona contract enumerable
+    return [];
+  }
+
+  function connectSigner(signer: Signer) {
+    persona = persona.connect(signer);
+    personaMirror = personaMirror.connect(signer);
+    personaAllMinter = personaAllMinter.connect(signer);
+  }
+
+  async function mintPersona(): Promise<number> {
+    if (!persona.signer) throw new Error("Signer is not connected");
+    const tx = await personaAllMinter.mintPersona(persona.signer.getAddress(), { gasLimit: 1000000, gasPrice: 0 });
+    const receipt = await tx.wait();
+    let id: number | undefined;
+    for (const log of receipt.logs) {
+      try {
+        const { args } = persona.interface.parseLog(log);
+        if (args.id) {
+          id = args.id.toNumber();
+          break;
+        }
+      } catch {
+        // Unknown event
+      }
+    }
+    if (id == null) throw new Error("Could not mint persona");
+    return id;
+  }
+
+  async function authorize(personaId: number, user: string, consumer: string) {
+    const isAuthorized = await personaMirror.isAuthorized(personaId, user, consumer, hexZeroPad("0x0", 4));
+    if (!isAuthorized) await personaMirror.authorize(personaId, user, consumer, [], { gasLimit: 1000000, gasPrice: 0 });
+    return true;
+  }
+
+  async function impersonate(user: Wallet, personaId: number, consumer: string) {
+    const tx = await personaMirror.connect(user).impersonate(personaId, consumer, { gasLimit: 1000000, gasPrice: 0 });
+    return tx.wait();
+  }
+
+  async function mintAndBurner(consumer: string): Promise<{ personaId: number; burnerWallet: Wallet }> {
+    const personaId = await mintPersona();
+    const burnerWallet = Wallet.createRandom().connect(provider);
+    await authorize(personaId, burnerWallet.address, consumer);
+    await impersonate(burnerWallet, personaId, consumer);
+    return { personaId, burnerWallet };
+  }
+
+  return { connectSigner, getPersonasOfAddress, mintPersona, authorize, impersonate, mintAndBurner };
+}
