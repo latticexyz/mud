f374c89eea8768502404db489b485b481a398d63
Andy Cernera <kooshaba@gmail.com>
2022-06-29 12:26:09 -0400

    fix(ri/client): properly update local stamina inside move action

--- packages/ri/client/src/layers/Headless/api/moveEntity.ts
+++ packages/ri/client/src/layers/Headless/api/moveEntity.ts
@@ -9,7 +9,6 @@ import {
   EntityID,
 } from "@latticexyz/recs";
 import { ActionSystem, HeadlessLayer } from "../types";
-import { Coord } from "@latticexyz/utils";
 import { Direction, Directions } from "../../../constants";
 
 export function moveEntity(layer: HeadlessLayer, actions: ActionSystem, entity: EntityIndex, direction: Direction) {
@@ -46,10 +45,6 @@ export function moveEntity(layer: HeadlessLayer, actions: ActionSystem, entity: 
   const playerEntityIndex = getPlayerEntity(Persona, personaId);
   if (movingEntityOwner !== world.entities[playerEntityIndex]) return;
 
-  // Entity must have sufficient stamina
-  const netStamina = getComponentValue(LocalCurrentStamina, entity)?.value;
-  if (netStamina == null || netStamina < 1) return;
-
   const actionID = `move ${Math.random()}` as EntityID;
   const delta = Directions[direction];
 
@@ -63,9 +58,24 @@ export function moveEntity(layer: HeadlessLayer, actions: ActionSystem, entity: 
       StaminaRegeneration,
       LocalCurrentStamina,
     },
-    requirement: ({ Position, Untraversable }) => {
+    requirement: ({ Position, Untraversable, LocalCurrentStamina }) => {
+      let netStamina = getComponentValue(LocalCurrentStamina, entity)?.value;
+      if (!netStamina) {
+        actions.cancel(actionID);
+        return null;
+      }
+
+      netStamina -= 1;
+      if (netStamina < 0) {
+        actions.cancel(actionID);
+        return null;
+      }
+
       const currentPosition = getComponentValue(Position, entity);
-      if (!currentPosition) return null;
+      if (!currentPosition) {
+        actions.cancel(actionID);
+        return null;
+      }
 
       const targetPosition = { x: currentPosition.x + delta.x, y: currentPosition.y + delta.y };
 
@@ -73,20 +83,29 @@ export function moveEntity(layer: HeadlessLayer, actions: ActionSystem, entity: 
       const entities = runQuery([HasValue(Position, targetPosition)]);
       for (const entity of entities) {
         if (hasComponent(Untraversable, entity)) {
+          actions.cancel(actionID);
           return null;
         }
       }
 
-      return targetPosition;
+      return {
+        targetPosition,
+        netStamina,
+      };
     },
-    updates: (_, targetPosition: Coord) => [
+    updates: (_, { targetPosition, netStamina }) => [
       {
         component: "Position",
         entity: entity,
         value: targetPosition,
       },
+      {
+        component: "LocalCurrentStamina",
+        entity,
+        value: { value: netStamina },
+      },
     ],
-    execute: async (targetPosition: Coord) => {
+    execute: async ({ targetPosition, netStamina }) => {
       const tx = await layer.parentLayers.network.api.moveEntity(world.entities[entity], targetPosition);
       await tx.wait();
       setComponent(LocalCurrentStamina, entity, { value: netStamina });
