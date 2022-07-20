92704e2db589150c0ddc9d604985ce32426d36c6
ludens <ludens@lattice.xyz>
2022-07-20 00:24:33 -0400

    feat(network): it compiles!

--- packages/network/src/createLocalEVM.ts
+++ packages/network/src/createLocalEVM.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { BaseContract, CallOverrides, Overrides } from "ethers";
+import { Address } from "@ethereumjs/util";
+import { Common } from "@ethereumjs/common";
+import { Transaction } from "@ethereumjs/tx";
+import { VM } from "@ethereumjs/vm";
 import { autorun, computed, IComputedValue, IObservableValue, observable, runInAction } from "mobx";
-import { mapObject, deferred, uuid, awaitValue, cacheUntilReady } from "@latticexyz/utils";
-import { Mutex } from "async-mutex";
-import { TransactionResponse } from "@ethersproject/providers";
+import { mapObject, deferred, uuid, awaitValue, cacheUntilReady, streamToComputed } from "@latticexyz/utils";
 import { Contracts, LocalEVM } from "./types";
 import { ConnectionState } from "./createProvider";
 import { Network } from "./createNetwork";
@@ -28,7 +30,14 @@ export function createLocalEVM<C extends Contracts>(
     return { contracts, signer, provider };
   });
 
-  const simulateCall = (...arg: any[]) => console.log(arg);
+  async function simulateCall<Contract extends C[keyof C]>(contract: Contract, key: keyof BaseContract, ...arg: any[]) {
+    const bytecode = (contract as any).bytecode as string;
+    const provider = network.providers.get().json;
+    console.log(contract);
+    console.log(key);
+    console.log(arg);
+    console.log(bytecode);
+  }
 
   function proxyContract<Contract extends C[keyof C]>(contract: Contract): Contract {
     return mapObject(contract as any, (value, key) => {
@@ -48,6 +57,23 @@ export function createLocalEVM<C extends Contracts>(
     if (!contracts) return undefined;
     return mapObject(contracts, proxyContract);
   });
+  const run = async () => {
+    const common = new Common({ chain: 1 });
+    const vm = await VM.create({ common });
+
+    const tx = Transaction.fromTxData({
+      gasLimit: BigInt(21000),
+      value: BigInt(1),
+      to: Address.zero(),
+      v: BigInt(37),
+      r: BigInt("62886504200765677832366398998081608852310526822767264927793100349258111544447"),
+      s: BigInt("21948396863567062449199529794141973192314514851405455194940751428901681436138"),
+    });
+    console.log("tx", tx);
+    const out = await vm.runTx({ tx, skipBalance: true });
+    console.log(out);
+  };
+  run();
 
   const cachedProxiedContracts = cacheUntilReady(proxiedContracts);
 
--- packages/ri/client/vite.config.ts
+++ packages/ri/client/vite.config.ts
@@ -1,17 +1,38 @@
 import { defineConfig } from "vite";
+import GlobalPolyFill from "@esbuild-plugins/node-globals-polyfill";
+import ModulesPolyFill from "@esbuild-plugins/node-modules-polyfill";
 
 export default defineConfig({
   root: "src",
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020",
+      define: {
+        global: "globalThis",
+      },
+      plugins: [
+        GlobalPolyFill({
+          process: true,
+          buffer: true,
+        }),
+        ModulesPolyFill({}),
+      ],
+    },
+  },
   build: {
     outDir: "../dist",
     emptyOutDir: true,
     sourcemap: true,
     assetsInlineLimit: 0,
+    target: "es2020",
   },
   preview: {
     port: 3000,
   },
   resolve: {
     dedupe: ["proxy-deep"],
+    alias: {
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+    },
   },
 });
