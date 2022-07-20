9824b6b0adbae8de4bfcba1595cbf6a5268a7401
alvrs <alvarius@lattice.xyz>
2022-07-19 20:06:41 -0400

    feat: modify input system for catching

--- packages/ri/client/src/layers/phaser/systems/createInputSystem.ts
+++ packages/ri/client/src/layers/phaser/systems/createInputSystem.ts
@@ -1,4 +1,5 @@
 import { pixelCoordToTileCoord } from "@latticexyz/phaserx";
+import { HasValue, runQuery } from "@latticexyz/recs";
 import { NetworkLayer } from "../../network";
 import { PhaserLayer } from "../types";
 
@@ -15,9 +16,20 @@ export function createInputSystem(network: NetworkLayer, phaser: PhaserLayer) {
     },
   } = phaser;
 
+  const {
+    components: { Position },
+  } = network;
+
   const clickSub = input.click$.subscribe((pointer) => {
     const tilePos = pixelCoordToTileCoord({ x: pointer.worldX, y: pointer.worldY }, tileWidth, tileHeight);
-    network.api.move(tilePos);
+
+    if (runQuery([HasValue(Position, tilePos)]).size > 0) {
+      // Catch if there is something to catch
+      network.api.kidnap(tilePos);
+    } else {
+      // otherwise move
+      network.api.move(tilePos);
+    }
   });
 
   world.registerDisposer(() => clickSub?.unsubscribe());
