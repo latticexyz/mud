a6721838208ed163a2ee0b6757255c503f23e839
Andy Cernera <kooshaba@gmail.com>
2022-07-25 17:54:28 -0400

    fix(scripting): fix world addresses for anvil/hardhat in scripts

--- packages/ri/scripting/src/constants.local.ts
+++ packages/ri/scripting/src/constants.local.ts
@@ -1,11 +1,11 @@
 export const RPC_URL = "http://localhost:8545/";
 export const RPC_WS_URL = "ws://localhost:8545/";
 // Anvil
-// export const DEV_PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-// export const WORLD_ADDRESS = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
+export const DEV_PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+export const WORLD_ADDRESS = "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9";
 
 // hardhat
-export const DEV_PRIVATE_KEY = "0x044C7963E9A89D4F8B64AB23E02E97B2E00DD57FCB60F316AC69B77135003AEF";
-export const WORLD_ADDRESS = "0x3163ed21c876e17206a441017515fba53963d8a9";
+// export const DEV_PRIVATE_KEY = "0x044C7963E9A89D4F8B64AB23E02E97B2E00DD57FCB60F316AC69B77135003AEF";
+// export const WORLD_ADDRESS = "0x3163ed21c876e17206a441017515fba53963d8a9";
 
 export const GAS_PRICE = 10;
