0d9985add7775befda01a4920d05d0180d135817
alvrs <alvarius@lattice.xyz>
2022-08-20 17:34:16 -0400

    docs: change typedoc theme

--- packages/mud.dev/src/components/Package.tsx
+++ packages/mud.dev/src/components/Package.tsx
@@ -3,20 +3,20 @@ import styled from "styled-components";
 import { Description, Headline } from "./styled";
 
 export const Package: React.FC<{ name: string; description: string; link: string }> = ({ name, description, link }) => {
-  function handleMouseEnter(e: any) {
+  function setRandomPrimaryColor() {
     const color = `hsl(${Math.floor(Math.random() * 360)} 100% 80%)`;
     document.documentElement.style.setProperty("--primary-color", color);
     document.documentElement.style.setProperty("--secondary-color", "white");
   }
 
-  function handleMouseLeave(e: any) {
+  function resetPrimaryColor() {
     document.documentElement.style.setProperty("--primary-color", "#eb4926");
     document.documentElement.style.setProperty("--secondary-color", "#eb4926");
   }
 
   return (
     <Link to={link}>
-      <Container onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <Container onMouseEnter={setRandomPrimaryColor} onClick={resetPrimaryColor} onMouseLeave={resetPrimaryColor}>
         <Name>{name}</Name>
         <Desc>{description}</Desc>
       </Container>
