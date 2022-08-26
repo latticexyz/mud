eda18b032ead593fe35783110fc8a22dcda811e2
alvrs <alvarius@lattice.xyz>
2022-08-26 17:27:43 +0200

    feat: wip 2 forge bulkupload script

--- packages/cli/src/contracts/BulkUpload.sol
+++ packages/cli/src/contracts/BulkUpload.sol
@@ -8,7 +8,19 @@ import {BulkSetStateSystem, ID as BulkSetStateSystemID, ECSEvent} from "std-cont
 import {World} from "solecs/World.sol";
 import {getAddressById} from "solecs/utils.sol";
 
-uint256 constant oldBulkSetStateSystemID = uint256(keccak256("ember.system.bulkSetState"));
+uint256 constant oldBulkSetStateSystemID = uint256(keccak256("mudwar.system.BulkSetState"));
+
+struct ParsedState {
+  string[] componentIds;
+  string[] entities;
+  ParsedECSEvent[] state;
+}
+
+struct ParsedECSEvent {
+  uint8 component;
+  uint32 entity;
+  string value;
+}
 
 struct State {
   uint256[] componentIds;
@@ -24,22 +36,94 @@ contract BulkUpload is DSTest {
   Cheats internal immutable vm = Cheats(HEVM_ADDRESS);
 
   function run(string memory path, address worldAddress) public {
-    // string memory path = "./src/contracts/testdata.json";
+    vm.startBroadcast();
+
+    // Read JSON
     console.log(path);
     string memory json = vm.readFile(path);
 
     console.log(json);
     console.log(worldAddress);
 
-    State memory state = abi.decode(vm.parseJson(json), (State));
+    // Parse JSON
+    ParsedState memory parsedState = abi.decode(vm.parseJson(json), (ParsedState));
+    console.log(hexToUint256(parsedState.entities[0]));
 
+    // Convert component ids
+    uint256[] memory componentIds = new uint256[](parsedState.componentIds.length);
+    for (uint256 i; i < componentIds.length; i++) {
+      componentIds[i] = hexToUint256(parsedState.componentIds[i]);
+    }
+
+    // Convert entitiy ids
+    uint256[] memory entities = new uint256[](parsedState.entities.length);
+    for (uint256 i; i < entities.length; i++) {
+      entities[i] = hexToUint256(parsedState.entities[i]);
+    }
+
+    // Convert state
+    ECSEvent[] memory state = new ECSEvent[](parsedState.state.length);
+    for (uint256 i; i < parsedState.state.length; i++) {
+      ParsedECSEvent memory p = parsedState.state[i];
+      // Convert value hex string to bytes
+      bytes memory value = hexToBytes(substring(p.value, 2, bytes(p.value).length));
+      state[i] = ECSEvent(p.component, p.entity, value);
+    }
+
+    // Set state
     World world = World(worldAddress);
-
     BulkSetStateSystem bulkSetStateSystem = BulkSetStateSystem(
       getAddressById(world.systems(), oldBulkSetStateSystemID)
     );
 
     // TODO: split up into chunks and call in a loop to support bigger states
-    bulkSetStateSystem.execute(abi.encode(state.componentIds, state.entities, state.state));
+    bulkSetStateSystem.execute(abi.encode(componentIds, entities, state));
+
+    vm.stopBroadcast();
   }
+}
+
+function hexToUint8(bytes1 b) pure returns (uint8 res) {
+  if (b >= "0" && b <= "9") {
+    return uint8(b) - uint8(bytes1("0"));
+  } else if (b >= "A" && b <= "F") {
+    return 10 + uint8(b) - uint8(bytes1("A"));
+  } else if (b >= "a" && b <= "f") {
+    return 10 + uint8(b) - uint8(bytes1("a"));
+  }
+  return uint8(b); // or return error ...
+}
+
+function hexToUint256(string memory str) pure returns (uint256 value) {
+  bytes memory b = bytes(str);
+  uint256 number = 0;
+  for (uint256 i = 0; i < b.length; i++) {
+    number = number << 4; // or number = number * 16
+    number |= hexToUint8(b[i]); // or number += numberFromAscII(b[i]);
+  }
+  return number;
+}
+
+// Convert an hexadecimal string to raw bytes
+function hexToBytes(string memory s) pure returns (bytes memory) {
+  bytes memory ss = bytes(s);
+  require(ss.length % 2 == 0); // length must be even
+  bytes memory r = new bytes(ss.length / 2);
+  for (uint256 i = 0; i < ss.length / 2; ++i) {
+    r[i] = bytes1(hexToUint8(ss[2 * i]) * 16 + hexToUint8(ss[2 * i + 1]));
+  }
+  return r;
+}
+
+function substring(
+  string memory str,
+  uint256 start,
+  uint256 end
+) pure returns (string memory) {
+  bytes memory strBytes = bytes(str);
+  bytes memory result = new bytes(end - start);
+  for (uint256 i = start; i < end; i++) {
+    result[i - start] = strBytes[i];
+  }
+  return string(result);
 }
