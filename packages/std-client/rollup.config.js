be86d243ff3d7efae7225933066246f2747ea759
alvarius <89248902+alvrs@users.noreply.github.com>
2022-09-20 15:11:59 +0100

    feat(std-client): add setupContracts (#154)
    * feat(std-client): add setupContracts
    
    * chore: remove duplicate dependency

--- packages/std-client/rollup.config.js
+++ packages/std-client/rollup.config.js
@@ -2,6 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import pluginJson from "@rollup/plugin-json"
 
 import { defineConfig } from "rollup";
 
@@ -12,5 +13,5 @@ export default defineConfig({
     dir: "dist",
     sourcemap: true,
   },
-  plugins: [nodeResolve(), typescript(), commonjs(), peerDepsExternal()],
+  plugins: [nodeResolve(), typescript(), commonjs(), peerDepsExternal(), pluginJson()],
 });
