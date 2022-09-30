25a63b23ad6f4a101f85928917f344d2fa74471f
alvrs <alvarius@lattice.xyz>
2022-09-30 20:46:11 +0100

    feat(network): add util for creating faucet service

---
+++ packages/network/src/createFaucetService.ts
@@ -0,0 +1,12 @@
+import { FaucetServiceClient } from "@latticexyz/services/protobuf/ts/faucet/faucet.client";
+import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+
+/**
+ * Create a FaucetServiceClient
+ * @param url FaucetService URL
+ * @returns FaucetServiceClient
+ */
+export function createFaucetService(url: string): FaucetServiceClient {
+  const transport = new GrpcWebFetchTransport({ baseUrl: url, format: "binary" });
+  return new FaucetServiceClient(transport);
+}
