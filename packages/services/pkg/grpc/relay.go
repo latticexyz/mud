e84ae445962d5f3e432cfb4a7229a813eb47b681
authcall <authcall@proton.me>
2022-10-06 05:17:51 -0400

    fix(relay): proper interval rate limiting and flag to control account balance check (#181)
    * fix(relay): proper interval rate limiting and flag to control account balance check
    
    * chore: only log disconnected client count if > 0
    
    Co-authored-by: alvarius <89248902+alvrs@users.noreply.github.com>

--- packages/services/pkg/grpc/relay.go
+++ packages/services/pkg/grpc/relay.go
@@ -54,7 +54,9 @@ func (server *ecsRelayServer) DisconnectIdleClientsWorker(ticker *time.Ticker, q
 		select {
 		case <-ticker.C:
 			countDisconnected := server.DisconnectIdleClients(server.config.IdleTimeoutTime)
-			server.logger.Info("done disconnecting idle clients", zap.Int("count", countDisconnected))
+			if countDisconnected > 0 {
+				server.logger.Info("done disconnecting idle clients", zap.Int("count", countDisconnected))
+			}
 		case <-quit:
 			ticker.Stop()
 			return
@@ -283,6 +285,11 @@ func (server *ecsRelayServer) VerifyMessage(message *pb.Message, identity *pb.Id
 }
 
 func (server *ecsRelayServer) VerifySufficientBalance(client *relay.Client, address string) error {
+	// If the flag to verify account balance is turned off, do nothing.
+	if !server.config.VerifyAccountBalance {
+		return nil
+	}
+
 	if client.ShouldCheckBalance() {
 		balance, err := eth.GetCurrentBalance(server.ethClient, address)
 		if err != nil {
@@ -333,7 +340,7 @@ func (server *ecsRelayServer) HandlePushRequest(request *pb.PushRequest) error {
 
 	// Rate limit the client, if necessary.
 	if !client.GetLimiter().Allow() {
-		return fmt.Errorf("client rate limited, max %.2f msg push / second allowed", server.config.MessageRateLimit)
+		return fmt.Errorf("client rate limited, max %d msg push / second allowed", server.config.MessageRateLimit)
 	}
 
 	// Get the message.
