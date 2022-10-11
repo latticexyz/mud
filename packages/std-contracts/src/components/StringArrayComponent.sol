d0b3a9b5fa281fb599ac4ae69aae98ef185e9fda
alvrs <alvarius@lattice.xyz>
2022-10-11 12:45:32 -0500

    chore: self-review

---
+++ packages/std-contracts/src/components/StringArrayComponent.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+import "solecs/Component.sol";
+
+contract StringArrayComponent is Component {
+  constructor(address world, uint256 id) Component(world, id) {}
+
+  function getSchema() public pure override returns (string[] memory keys, LibTypes.SchemaValue[] memory values) {
+    keys = new string[](1);
+    values = new LibTypes.SchemaValue[](1);
+
+    keys[0] = "value";
+    values[0] = LibTypes.SchemaValue.STRING_ARRAY;
+  }
+
+  function set(uint256 entity, string[] memory value) public {
+    set(entity, abi.encode(value));
+  }
+
+  function getValue(uint256 entity) public view returns (string[] memory) {
+    return abi.decode(getRawValue(entity), (string[]));
+  }
+
+  function getEntitiesWithValue(string[] memory value) public view returns (uint256[] memory) {
+    return getEntitiesWithValue(abi.encode(value));
+  }
+}
