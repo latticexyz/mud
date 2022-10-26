9b0dd5932c913a2b670a1f29938de0ad4c01a214
alvrs <alvarius@lattice.xyz>
2022-10-26 18:20:57 +0100

    feat: make UpgradableSystem payable

--- packages/std-contracts/src/systems/UpgradableSystem.sol
+++ packages/std-contracts/src/systems/UpgradableSystem.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.0;
-import "solecs/System.sol";
+import "solecs/PayableSystem.sol";
 import { IWorld } from "solecs/interfaces/IWorld.sol";
 import { IComponent } from "solecs/interfaces/IComponent.sol";
 import { getAddressById } from "solecs/utils.sol";
@@ -8,13 +8,15 @@ import { AddressBareComponent } from "../components/AddressBareComponent.sol";
 
 uint256 constant ID = uint256(keccak256("system.Upgradable"));
 
-contract UpgradableSystem is System {
+contract UpgradableSystem is PayableSystem {
   address implementation;
 
-  constructor(IWorld _world, address _components) System(_world, _components) {}
+  constructor(IWorld _world, address _components) PayableSystem(_world, _components) {}
 
-  function execute(bytes memory args) public returns (bytes memory) {
-    (bool success, bytes memory returndata) = implementation.delegatecall(bytes.concat(ISystem.execute.selector, args));
+  function execute(bytes memory args) public payable returns (bytes memory) {
+    (bool success, bytes memory returndata) = implementation.delegatecall(
+      bytes.concat(IPayableSystem.execute.selector, args)
+    );
     if (!success) revert("delegate call failed");
     return returndata;
   }
