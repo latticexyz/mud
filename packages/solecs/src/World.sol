32523982525ea845cc89be82c786dfd7dcabd185
dk1a <dk1a@protonmail.com>
2022-11-26 21:34:19 +0300

    feat(solecs): refactor access control, fix some import formatting

--- packages/solecs/src/World.sol
+++ packages/solecs/src/World.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
+
 import { Set } from "./Set.sol";
 import { LibQuery } from "./LibQuery.sol";
 import { IWorld, WorldQueryFragment } from "./interfaces/IWorld.sol";
