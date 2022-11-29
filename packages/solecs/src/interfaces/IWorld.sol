068ab0da17044284a0636b2b1b377c6cfa7b5c1d
yoyoismee <yoyoismee@gmail.com>
2022-11-30 03:33:50 +0700

    chore: update license declarations to MIT

--- packages/solecs/src/interfaces/IWorld.sol
+++ packages/solecs/src/interfaces/IWorld.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import { QueryType } from "./Query.sol";
 import { IUint256Component } from "./IUint256Component.sol";
