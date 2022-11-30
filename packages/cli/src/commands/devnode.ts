e82f7fd5df12c5072f701bc1b4f6edca4f3080dc
alvrs <alvarius@lattice.xyz>
2022-11-30 15:44:35 +0000

    feat(cli): add devnode command

---
+++ packages/cli/src/commands/devnode.ts
@@ -0,0 +1,31 @@
+import { rmSync } from "fs";
+import { homedir } from "os";
+import path from "path";
+import type { Arguments, CommandBuilder } from "yargs";
+import { execLog } from "../utils";
+
+type Options = {
+  blocktime: number;
+};
+
+export const command = "devnode";
+export const desc = "Start a local Ethereum node for development";
+
+export const builder: CommandBuilder<Options, Options> = (yargs) =>
+  yargs.options({
+    blocktime: { type: "number", default: 1, decs: "Interval in which new blocks are produced" },
+  });
+
+export const handler = async (argv: Arguments<Options>): void => {
+  const { blocktime } = argv;
+  console.log("Clearing devnode history");
+  const userHomeDir = homedir();
+  rmSync(path.join(userHomeDir, ".foundry", "anvil", "tmp"), { recursive: true, force: true });
+  const { child } = await execLog("anvil", ["-b", String(blocktime), "--block-base-fee-per-gas", "0"]);
+
+  process.on("SIGINT", function () {
+    console.log("\ngracefully shutting down from SIGINT (Crtl-C)");
+    child.kill();
+    process.exit();
+  });
+};
