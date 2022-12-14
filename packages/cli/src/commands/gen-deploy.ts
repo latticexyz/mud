c58650ba4f391363bc80d4f373ff0bbb10731b2f
yoyismee.eth <yoyoismee@users.noreply.github.com>
2022-12-14 15:56:42 +0700

    add waring for gen-deploy necessary write access

--- packages/cli/src/commands/gen-deploy.ts
+++ packages/cli/src/commands/gen-deploy.ts
@@ -2,6 +2,7 @@ import type { Arguments, CommandBuilder } from "yargs";
 import { spawnSync } from "node:child_process";
 import fs from 'fs';
 import path from 'path';
+import chalk from "chalk";
 
 import {readdirSync} from 'fs';
 type Options = {
@@ -36,6 +37,7 @@ export const handler = async (args: Arguments<Options>): Promise<void> => {
   const sys = readdirSync(systems).filter(c => c.includes(".sol"));
   const syss = sys.map(function(e) { 
     const route = path.resolve(systems, e);
+    // give write access to any components that each system touch. even if it not actaully write
     const out = spawnSync("forge",["flatten" ,route]);
     const flatCode = out.stdout.toString()
     const used = comps.filter(c => flatCode.includes(c))
@@ -49,6 +51,6 @@ export const handler = async (args: Arguments<Options>): Promise<void> => {
   const deployJsonStr = JSON.stringify(depployJson, undefined, 4);
   fs.writeFileSync(outdir, deployJsonStr, 'utf8');
   console.log("write output to", outdir)
-
+  console.warn(chalk.yellow.bold("Warning: this function may grant more write access than necessary. It is recommended to manually remove any unnecessary permissions."))
   process.exit(0);
 };
