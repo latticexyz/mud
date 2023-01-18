1afd1f67b920cb66deeb5f164258bd59604f3d83
Kevin Ingersoll <kingersoll@gmail.com>
2023-01-18 12:28:04 -0800

    fix(network): throw errors from txQueue calls
    closes #315

--- packages/network/src/createTxQueue.ts
+++ packages/network/src/createTxQueue.ts
@@ -35,7 +35,7 @@ export function createTxQueue<C extends Contracts>(
       gasLimit: BigNumberish
     ) => Promise<{ hash: string; wait: () => Promise<TransactionReceipt> }>;
     estimateGas: () => BigNumberish | Promise<BigNumberish>;
-    cancel: () => void;
+    cancel: (error: any) => void;
     stateMutability?: string;
   }>();
   const submissionMutex = new Mutex();
@@ -152,7 +152,12 @@ export function createTxQueue<C extends Contracts>(
     };
 
     // Queue the tx execution
-    queue.add(uuid(), { execute, estimateGas, cancel: () => reject(new Error("TX_CANCELLED")), stateMutability });
+    queue.add(uuid(), {
+      execute,
+      estimateGas,
+      cancel: (error?: any) => reject(error ?? new Error("TX_CANCELLED")),
+      stateMutability,
+    });
 
     // Start processing the queue
     processQueue();
@@ -190,7 +195,7 @@ export function createTxQueue<C extends Contracts>(
         gasLimit = await txRequest.estimateGas();
       } catch (e) {
         console.error("[TXQueue] GAS ESTIMATION ERROR", e);
-        return txRequest.cancel();
+        return txRequest.cancel(e);
       }
 
       // Wait if nonce is not ready
@@ -219,6 +224,8 @@ export function createTxQueue<C extends Contracts>(
         // Nonce handeling
         if (shouldIncreaseNonce) incNonce();
         if (shouldResetNonce) await resetNonce();
+        // Bubble up error
+        if (error) txRequest.cancel(error);
       }
     });
 
