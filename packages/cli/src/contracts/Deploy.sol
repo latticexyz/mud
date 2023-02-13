43ad11837ae280509be92737e8f86d749d4d48d8
dk1a <dk1a@protonmail.com>
2023-02-14 01:23:59 +0300

    feat: update forge-std, use some new features in cli (#311)

--- packages/cli/src/contracts/Deploy.sol
+++ packages/cli/src/contracts/Deploy.sol
@@ -2,26 +2,22 @@
 pragma solidity >=0.8.0;
 
 // Foundry
-import {DSTest} from "ds-test/test.sol";
-import {Vm} from "forge-std/Vm.sol";
+import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
-import {Cheats} from "./Cheats.sol";
 
 // Libraries
 import {LibDeploy, DeployResult} from "./LibDeploy.sol";
 
-contract Deploy is DSTest {
-  Cheats internal immutable vm = Cheats(HEVM_ADDRESS);
-
+contract Deploy is Script {
   function broadcastDeploy(
     address _deployer,
     address _world,
     bool _reuseComponents
   ) public returns (address world, uint256 initialBlockNumber) {
-    vm.startBroadcast(_deployer);
+    vmSafe.startBroadcast(_deployer);
     initialBlockNumber = block.number;
     DeployResult memory result = LibDeploy.deploy(_deployer, _world, _reuseComponents);
-    vm.stopBroadcast();
+    vmSafe.stopBroadcast();
     world = address(result.world);
   }
 }
