cae82e5781931f86d0bc53eb05306197fab3d7aa
dk1a <dk1a@protonmail.com>
2023-02-14 04:42:52 +0300

    fix(network): add explicit return type to createFaucetService (#399)

--- packages/network/src/createFaucetService.ts
+++ packages/network/src/createFaucetService.ts
@@ -1,11 +1,14 @@
 import { FaucetServiceDefinition } from "@latticexyz/services/protobuf/ts/faucet/faucet";
-import { createChannel, createClient } from "nice-grpc-web";
+import { createChannel, createClient, RawClient } from "nice-grpc-web";
+import { FromTsProtoServiceDefinition } from "nice-grpc-web/lib/service-definitions/ts-proto";
 
 /**
  * Create a FaucetServiceClient
  * @param url FaucetService URL
  * @returns FaucetServiceClient
  */
-export function createFaucetService(url: string) {
+export function createFaucetService(
+  url: string
+): RawClient<FromTsProtoServiceDefinition<typeof FaucetServiceDefinition>> {
   return createClient(FaucetServiceDefinition, createChannel(url));
 }
