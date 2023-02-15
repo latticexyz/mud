ee1ce703a2f1b32cf05a17c66e1701ecaee07229
alvrs <alvarius@lattice.xyz>
2023-02-15 17:03:38 -0500

    feat(world): add core tables

--- packages/world/src/World.sol
+++ packages/world/src/World.sol
@@ -2,3 +2,29 @@
 pragma solidity >=0.8.0;
 
 contract World {}
+
+// RouteKey: [routeId]
+struct RouteSchema {
+  string preImage;
+}
+
+// RouteAccessKey: [routeId, caller]
+struct RouteAccessSchema {
+  bool access;
+}
+
+// OwnerKey: [owned]
+struct OwnerSchema {
+  address owner;
+}
+
+// SystemKey: [routeId]
+struct SystemSchema {
+  bool openAccess;
+  address system;
+}
+
+// SystemToRouteKey: [systemAddress]
+struct SystemToRouteSchema {
+  bytes32 routeId;
+}
