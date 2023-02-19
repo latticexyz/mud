f81c3377076bc02dd2af5edf1d0f1be76adadae9
dk1a <dk1a@protonmail.com>
2023-02-19 04:42:25 +0300

    build(cli): add clarifications to tsup config

--- packages/cli/tsup.config.js
+++ packages/cli/tsup.config.js
@@ -9,5 +9,8 @@ export default defineConfig({
   clean: true,
   bundle: true,
   dts: "src/index.ts",
+  // both inlined imports are hacks to make esm+esbuild work
+  // @latticexyz/services: esm imports directly from ts files don't work properly from transpiled js
+  // @latticexyz/solecs: esm requires import assertions for json, but esbuild doesn't support those (they just get stripped)
   noExternal: ["@latticexyz/services", "@latticexyz/solecs"],
 });
