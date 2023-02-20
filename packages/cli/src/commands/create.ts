292119f4c0c40cbd3ad688ee567f4c05d957af7c
Kevin Ingersoll <kingersoll@gmail.com>
2023-02-20 17:13:25 -0500

    feat(cli): deprecate create command (#424)

--- packages/cli/src/commands/create.ts
+++ packages/cli/src/commands/create.ts
@@ -10,23 +10,21 @@ type Options = {
 export const command = "create <name>";
 export const desc = "Sets up a mud project into <name>. Requires yarn.";
 
+export const deprecated = true;
+
 export const builder: CommandBuilder<Options, Options> = (yargs) =>
   yargs
     .options({
-      template: { type: "string", desc: "Template to be used (available: [minimal])", default: "minimal" },
+      template: { type: "string", desc: "Template to be used (available: [minimal, react])", default: "minimal" },
     })
     .positional("name", { type: "string", default: "mud-app" });
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
   const { name, template } = argv;
-  console.log(chalk.yellow.bold("Creating new mud project in", name));
-  let result = await execLog("git", ["clone", `https://github.com/latticexyz/mud-template-${template}`, name]);
+  console.log(chalk.red.bold("⚠️  This command will be removed soon. Use `yarn create mud` instead."));
+
+  const result = await execLog("yarn", ["create", "mud", name, "--template", template]);
   if (result.exitCode != 0) process.exit(result.exitCode);
 
-  console.log(chalk.yellow.bold("Installing dependencies..."));
-  result = await execLog("yarn", ["--cwd", `./${name}`]);
-  if (result.exitCode != 0) process.exit(result.exitCode);
-
-  console.log(chalk.yellow.bold(`Done! Run \`yarn dev\` in ${name} to get started.`));
   process.exit(0);
 };
