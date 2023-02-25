5bae806f47ab761671a1588519ccbabc7392a3ce
dk1a <dk1a@protonmail.com>
2023-02-26 00:19:15 +0300

    refactor(cli): settle on pure ESM config, add .ts support

--- packages/cli/src/config/loadConfig.ts
+++ packages/cli/src/config/loadConfig.ts
@@ -1,28 +1,30 @@
-import { findUpSync } from "find-up";
+import { findUp } from "find-up";
 import path from "path";
 import { ERRORS, MUDError } from "../utils/errors.js";
-
-// TODO require may or may not be needed, finish exploring the various configurations
-import { createRequire } from "module";
-import { fileURLToPath } from "url";
-const require = createRequire(fileURLToPath(import.meta.url));
 
 // Based on hardhat's config (MIT)
 // https://github.com/NomicFoundation/hardhat/tree/main/packages/hardhat-core
 
-const TS_CONFIG_FILENAME = "mud.config.mts";
+// In order of preference files are checked
+const configFiles = ["mud.config.ts", "mud.config.mts"];
 
 export async function loadConfig(configPath?: string): Promise<unknown> {
-  configPath = resolveConfigPath(configPath);
-
-  const config = (await import(configPath)).default;
-  console.log("Config loaded:", config);
-  return config;
+  configPath = await resolveConfigPath(configPath);
+  try {
+    return (await import(configPath)).default;
+  } catch (error) {
+    if (error instanceof SyntaxError && error.message === "Cannot use import statement outside a module") {
+      // TODO custom error can be used here to instruct to use .mts or type="module"
+      throw error;
+    } else {
+      throw error;
+    }
+  }
 }
 
-function resolveConfigPath(configPath: string | undefined) {
+async function resolveConfigPath(configPath: string | undefined) {
   if (configPath === undefined) {
-    configPath = getUserConfigPath();
+    configPath = await getUserConfigPath();
   } else {
     if (!path.isAbsolute(configPath)) {
       configPath = path.join(process.cwd(), configPath);
@@ -32,8 +34,8 @@ function resolveConfigPath(configPath: string | undefined) {
   return configPath;
 }
 
-function getUserConfigPath() {
-  const tsConfigPath = findUpSync(TS_CONFIG_FILENAME);
+async function getUserConfigPath() {
+  const tsConfigPath = await findUp(configFiles);
   if (tsConfigPath === undefined) {
     throw new MUDError(ERRORS.NOT_INSIDE_PROJECT);
   }
