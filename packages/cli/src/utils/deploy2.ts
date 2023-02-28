534cf2dde788dedfd2f89b3bc47dfc4ebed3e6bc
alvrs <alvarius@lattice.xyz>
2023-02-28 13:13:25 -0500

    feat(cli): wip add deployment script

---
+++ packages/cli/src/utils/deploy2.ts
@@ -0,0 +1,6 @@
+import { WorldConfig } from "../config/loadWorldConfig";
+
+export async function deploy2(_config: WorldConfig) {
+  const config: WorldConfig = {} as WorldConfig;
+  console.log("config:", config);
+}
