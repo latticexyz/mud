a25466f91226984de364cc2c71375539ec916a26
alvrs <alvarius@lattice.xyz>
2023-03-01 13:06:19 -0500

    chore(cli): add todos

--- packages/cli/src/utils/deploy-v2.ts
+++ packages/cli/src/utils/deploy-v2.ts
@@ -1,5 +1,9 @@
 import { ResolvedWorldConfig } from "../config/loadWorldConfig.js";
 
 export async function deploy(config: ResolvedWorldConfig) {
-  console.log("TODO: implement");
+  // TODO: Deploy World (either vanilla or overridden)
+  // TODO: Register tables
+  // TODO: Deploy systems
+  // TODO: Grant access to systems
+  // TODO: Execute postDeploy forge script
 }
