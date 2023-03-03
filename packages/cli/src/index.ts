c4c11dddf17ac5421e62cdcc5309e35d8b1ba324
alvrs <alvarius@lattice.xyz>
2023-03-03 17:08:05 -0500

    feat(cli): expose functions to index

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,6 @@
 export { parseStoreConfig } from "./config/parseStoreConfig.js";
 export { loadStoreConfig } from "./config/loadStoreConfig.js";
+export { loadWorldConfig, resolveWorldConfig, parseWorldConfig } from "./config/loadWorldConfig.js";
 export { renderTablesFromConfig } from "./render-table/renderTablesFromConfig.js";
 export { renderTable } from "./render-table/renderTable.js";
 
@@ -13,3 +14,4 @@ export type {
 } from "./config/index.js";
 
 export * from "./constants.js";
+export * from "./utils/index.js";
