3090273883011f8e35ecd0e9454179a8e6495469
alvrs <alvarius@lattice.xyz>
2023-03-03 14:10:15 -0500

    refactor(cli): move foundry utils to foundry.ts, move execLog to own util

---
+++ packages/cli/src/utils/execLog.ts
@@ -0,0 +1,22 @@
+import chalk from "chalk";
+import { execa } from "execa";
+
+/**
+ * Executes the given command, returns the stdout, and logs the command to the console.
+ * Throws an error if the command fails.
+ * @param command The command to execute
+ * @param args The arguments to pass to the command
+ * @returns The stdout of the command
+ */
+export async function execLog(command: string, args: string[]): Promise<string> {
+  const commandString = `${command} ${args.join(" ")}`;
+  try {
+    console.log(chalk.gray(`running "${commandString}"`));
+    const { stdout } = await execa(command, args, { stdout: "pipe", stderr: "pipe" });
+    return stdout;
+  } catch (error: any) {
+    let errorMessage = error?.stderr || error?.message || "";
+    errorMessage += chalk.red(`\nError running "${commandString}"`);
+    throw new Error(errorMessage);
+  }
+}
--- packages/cli/src/utils/index.ts
+++ packages/cli/src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "./errors.js";
-export * from "./forgeConfig.js";
+export * from "./foundry.js";
 export * from "./format.js";
 export * from "./deploy-v2.js";
-export * from "./foundry.js";
