7a3ea8c8ad902caf61624f825d38c45ad628a3d4
alvrs <alvarius@lattice.xyz>
2023-03-03 17:09:48 -0500

    chore(cli): remove redundant exports

--- packages/cli/src/config/index.ts
+++ packages/cli/src/config/index.ts
@@ -1,7 +1,6 @@
 import { StoreUserConfig, StoreConfig } from "./parseStoreConfig.js";
 import { WorldUserConfig, ResolvedWorldConfig } from "./loadWorldConfig.js";
 
-export type { StoreUserConfig, StoreConfig, WorldUserConfig, ResolvedWorldConfig };
 export type MUDUserConfig = StoreUserConfig & WorldUserConfig;
 export type MUDConfig = StoreConfig & ResolvedWorldConfig;
 
@@ -9,4 +8,5 @@ export * from "./commonSchemas.js";
 export * from "./loadConfig.js";
 export * from "./loadStoreConfig.js";
 export * from "./parseStoreConfig.js";
+export * from "./loadWorldConfig.js";
 export * from "./validation.js";
