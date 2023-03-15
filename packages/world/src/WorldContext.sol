9e01f1d21b26bf7c944acc3bdf9a7287624c25c1
alvrs <alvarius@lattice.xyz>
2023-03-14 19:41:56 -0500

    feat(world): mark WorldContext as abstract

--- packages/world/src/WorldContext.sol
+++ packages/world/src/WorldContext.sol
@@ -5,7 +5,7 @@ import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
 // Similar to https://eips.ethereum.org/EIPS/eip-2771, but any contract can be the trusted forwarder.
 // This should only be used for contracts without own storage, like Systems.
-contract WorldContext {
+abstract contract WorldContext {
   // Extract the trusted msg.sender value appended to the calldata
   function _msgSender() internal view returns (address sender) {
     assembly {
