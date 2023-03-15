f69e3dce1b8e605f8fc6689fc5b9722114860a49
alvarius <89248902+alvrs@users.noreply.github.com>
2023-03-15 15:56:16 -0500

    feat(world): ReverseMapping: infer target table id from source table id, add getKeysWithValue util (#490)

--- packages/world/src/modules/reversemapping/ReverseMappingHook.sol
+++ packages/world/src/modules/reversemapping/ReverseMappingHook.sol
@@ -6,8 +6,11 @@ import { IStoreHook } from "@latticexyz/store/src/IStore.sol";
 import { Bytes } from "@latticexyz/store/src/Bytes.sol";
 import { IWorld } from "@latticexyz/world/src/interfaces/IWorld.sol";
 
+import { ResourceSelector } from "../../ResourceSelector.sol";
+
 import { ReverseMapping } from "./tables/ReverseMapping.sol";
 import { ArrayLib } from "./ArrayLib.sol";
+import { getTargetTableSelector } from "./getTargetTableSelector.sol";
 
 /**
  * This is a very naive and inefficient implementation for now.
@@ -17,20 +20,13 @@ import { ArrayLib } from "./ArrayLib.sol";
  */
 contract ReverseMappingHook is IStoreHook {
   using ArrayLib for bytes32[];
+  using ResourceSelector for bytes32;
 
   error MultipleKeysNotSupported();
-  error InvalidTable(uint256 received, uint256 expected);
 
-  uint256 public immutable sourceTableId;
-  uint256 public immutable targetTableId;
-
-  constructor(uint256 _sourceTableId, uint256 _targetTableId) {
-    sourceTableId = _sourceTableId;
-    targetTableId = _targetTableId;
-  }
-
-  function onSetRecord(uint256 table, bytes32[] memory key, bytes memory data) public {
-    _sanityChecks(table, key);
+  function onSetRecord(uint256 sourceTableId, bytes32[] memory key, bytes memory data) public {
+    _requireSingleKey(key);
+    uint256 targetTableId = getTargetTableSelector(sourceTableId).toTableId();
 
     // Get the previous value
     bytes32 previousValue = keccak256(IWorld(msg.sender).getRecord(sourceTableId, key));
@@ -39,42 +35,44 @@ contract ReverseMappingHook is IStoreHook {
     if (previousValue == keccak256(data)) return;
 
     // Remove the key from the list of keys with the previous value
-    _removeKeyFromList(key[0], previousValue);
+    _removeKeyFromList(targetTableId, key[0], previousValue);
 
     // Push the key to the list of keys with the new value
     ReverseMapping.push(targetTableId, keccak256(data), key[0]);
   }
 
-  function onBeforeSetField(uint256 table, bytes32[] memory key, uint8, bytes memory) public {
-    _sanityChecks(table, key);
+  function onBeforeSetField(uint256 sourceTableId, bytes32[] memory key, uint8, bytes memory) public {
+    _requireSingleKey(key);
 
     // Remove the key from the list of keys with the previous value
     bytes32 previousValue = keccak256(IWorld(msg.sender).getRecord(sourceTableId, key));
-    _removeKeyFromList(key[0], previousValue);
+    uint256 targetTableId = getTargetTableSelector(sourceTableId).toTableId();
+    _removeKeyFromList(targetTableId, key[0], previousValue);
   }
 
-  function onAfterSetField(uint256 table, bytes32[] memory key, uint8, bytes memory) public {
-    _sanityChecks(table, key);
+  function onAfterSetField(uint256 sourceTableId, bytes32[] memory key, uint8, bytes memory) public {
+    _requireSingleKey(key);
 
     // Add the key to the list of keys with the new value
     bytes32 newValue = keccak256(IWorld(msg.sender).getRecord(sourceTableId, key));
+    uint256 targetTableId = getTargetTableSelector(sourceTableId).toTableId();
     ReverseMapping.push(targetTableId, newValue, key[0]);
   }
 
-  function onDeleteRecord(uint256 table, bytes32[] memory key) public {
-    _sanityChecks(table, key);
+  function onDeleteRecord(uint256 sourceTableId, bytes32[] memory key) public {
+    _requireSingleKey(key);
 
     // Remove the key from the list of keys with the previous value
     bytes32 previousValue = keccak256(IWorld(msg.sender).getRecord(sourceTableId, key));
-    _removeKeyFromList(key[0], previousValue);
+    uint256 targetTableId = getTargetTableSelector(sourceTableId).toTableId();
+    _removeKeyFromList(targetTableId, key[0], previousValue);
   }
 
-  function _sanityChecks(uint256 table, bytes32[] memory key) internal view {
+  function _requireSingleKey(bytes32[] memory key) internal pure {
     if (key.length > 1) revert MultipleKeysNotSupported();
-    if (table != sourceTableId) revert InvalidTable(table, sourceTableId);
   }
 
-  function _removeKeyFromList(bytes32 key, bytes32 valueHash) internal {
+  function _removeKeyFromList(uint256 targetTableId, bytes32 key, bytes32 valueHash) internal {
     // Get the keys with the previous value excluding the current key
     bytes32[] memory keysWithPreviousValue = ReverseMapping.get(targetTableId, valueHash).filter(key);
 
