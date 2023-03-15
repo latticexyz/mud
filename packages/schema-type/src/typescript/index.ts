b1bf876eee91d783bbd050c1361bb3af1f651e66
Kevin Ingersoll <kingersoll@gmail.com>
2023-03-14 17:03:18 -0700

    feat(schema-type): add SchemaType -> primitive map, rearrange files (#488)
    * feat(schema-type): move types into their own files, add primitive map
    
    * fix(schema-type): missed js import

---
+++ packages/schema-type/src/typescript/index.ts
@@ -0,0 +1,10 @@
+export type { StaticSchemaType } from "./StaticSchemaType.js";
+export type { DynamicSchemaType } from "./DynamicSchemaType.js";
+export type { ArraySchemaType } from "./ArraySchemaType.js";
+export type { SchemaTypeToPrimitive } from "./SchemaTypeToPrimitive.js";
+
+export { SchemaType } from "./SchemaType.js";
+export { encodeSchema } from "./encodeSchema.js";
+export { getStaticByteLength } from "./getStaticByteLength.js";
+export { SchemaTypeArrayToElement } from "./SchemaTypeArrayToElement.js";
+export { SchemaTypeId } from "./SchemaTypeId.js";
