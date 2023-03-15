ebe4fef46ed551c014e185e34068210d3e153790
alvrs <alvarius@lattice.xyz>
2023-03-15 14:11:18 -0500

    chore: change module namespace constant

--- packages/world/src/modules/reversemapping/constants.sol
+++ packages/world/src/modules/reversemapping/constants.sol
@@ -4,4 +4,4 @@ pragma solidity >=0.8.0;
 // Limiting the module namespace to 12 bytes so the last 4 bytes
 // can be used for an identifier of the source table namespace to avoid
 // collisions between tables with the same name in different namespaces
-bytes8 constant MODULE_NAMESPACE = "vkeylist";
+bytes8 constant MODULE_NAMESPACE = "revmap";
