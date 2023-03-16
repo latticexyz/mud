f067cfba271303b05fcdcf8942595793c1f7339b
Kevin Ingersoll <kingersoll@gmail.com>
2023-03-15 18:34:42 -0700

    refactor(schema-type): rename SchemaTypeId -> SchemaTypeToAbiType

--- packages/cli/src/render-solidity/userType.ts
+++ packages/cli/src/render-solidity/userType.ts
@@ -1,4 +1,4 @@
-import { getStaticByteLength, SchemaType, SchemaTypeId } from "@latticexyz/schema-type";
+import { getStaticByteLength, SchemaType, SchemaTypeToAbiType } from "@latticexyz/schema-type";
 import { StoreConfig } from "../index.js";
 import { ImportDatum, RenderTableType } from "./types.js";
 
@@ -44,7 +44,7 @@ export function importForSchemaOrUserType(
 export function getSchemaTypeInfo(schemaType: SchemaType): RenderTableType {
   const staticByteLength = getStaticByteLength(schemaType);
   const isDynamic = staticByteLength === 0;
-  const typeId = SchemaTypeId[schemaType];
+  const typeId = SchemaTypeToAbiType[schemaType];
   return {
     typeId,
     typeWithLocation: isDynamic ? typeId + " memory" : typeId,
@@ -79,7 +79,7 @@ export function getUserTypeInfo(
         isDynamic,
         typeWrap: `${userType}`,
         typeUnwrap: `${userType}`,
-        internalTypeId: `${SchemaTypeId[schemaType]}`,
+        internalTypeId: `${SchemaTypeToAbiType[schemaType]}`,
       },
     };
   } else {
