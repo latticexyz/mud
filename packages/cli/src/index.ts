8a0e8323b2d468af22ed080bb50e0ce7d581f507
Kevin Ingersoll <kingersoll@gmail.com>
2023-03-19 13:07:22 -0700

    fix(cli): stop exporting execa and killing clients

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,8 +1,8 @@
 export { loadStoreConfig } from "./config/loadStoreConfig.js";
 export { parseStoreConfig } from "./config/parseStoreConfig.js";
 export { loadWorldConfig, resolveWorldConfig, parseWorldConfig } from "./config/world/index.js";
-export { getTableOptions } from "./render-solidity/tableOptions.js";
-export { renderTable } from "./render-solidity/renderTable.js";
+// export { getTableOptions } from "./render-solidity/tableOptions.js";
+// export { renderTable } from "./render-solidity/renderTable.js";
 // export { resolveTableId } from "./config/dynamicResolution.js";
 
 export type {
@@ -16,4 +16,4 @@ export type {
 export { storeConfig, mudConfig } from "./config/index.js";
 
 export * from "./constants.js";
-export * from "./utils/index.js";
+// export * from "./utils/index.js";
