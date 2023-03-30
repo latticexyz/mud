11c655a0f6e0b657548e1bfe7b0b67ce0f634e2b
alvrs <alvarius@lattice.xyz>
2023-03-30 23:46:36 +0200

    refactor: update get mode block number

--- packages/network/src/v2/mode/getModeBlockNumber.ts
+++ packages/network/src/v2/mode/getModeBlockNumber.ts
@@ -2,14 +2,12 @@ import { QueryLayerClient } from "@latticexyz/services/protobuf/ts/mode/mode";
 import { getBlockNumberFromModeTable } from "./getBlockNumberFromModeTable";
 
 export async function getModeBlockNumber(client: QueryLayerClient, chainId: number): Promise<number> {
-  const response = await client.getState({
-    chainTables: ["block_number"],
+  const response = await client.single__GetState({
+    table: "block_number",
     namespace: {
       chainId: chainId.toString(),
-      worldAddress: "unknown",
     },
   });
-  console.log("getModeBlockNumber response", response);
   const blockNumber = getBlockNumberFromModeTable(response.chainTables["block_number"]);
   console.log("MODE block number", blockNumber);
   return blockNumber;
