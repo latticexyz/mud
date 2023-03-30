76781fa247b21e7e84ddf2240f3f05e6284a4320
alvrs <alvarius@lattice.xyz>
2023-03-31 01:19:39 +0200

    feat: add sanity check to getBlockNumberFromModeTable

--- packages/network/src/v2/mode/getBlockNumberFromModeTable.ts
+++ packages/network/src/v2/mode/getBlockNumberFromModeTable.ts
@@ -3,6 +3,6 @@ import { SchemaType } from "@latticexyz/schema-type";
 import { decodeValue } from "../schemas/decodeValue";
 
 export function getBlockNumberFromModeTable(table: GenericTable): number {
-  // TODO: make this easier to pluck off of the rpc response? this feels complicated for a built-in, always-present thing
+  if (table.cols[0] !== "block_number") throw new Error("Table does not contain block_number column");
   return Number(decodeValue(SchemaType.UINT256, table.rows[0].values[0]));
 }
