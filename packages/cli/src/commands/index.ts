22c4c7e4d3b8abc42f5054733696f5cf7eeaf433
alvrs <alvarius@lattice.xyz>
2023-04-01 16:25:56 +0200

    feat(cli): add mud2 cli entrypoint without v2 commands

--- packages/cli/src/commands/index.ts
+++ packages/cli/src/commands/index.ts
@@ -1,14 +1,5 @@
 import { CommandModule } from "yargs";
 
-import bulkupload from "./deprecated/bulkupload.js";
-import callSystem from "./deprecated/call-system.js";
-import codegenLibdeploy from "./deprecated/codegen-libdeploy.js";
-import deployContracts from "./deprecated/deploy-contracts.js";
-import systemTypes from "./deprecated/system-types.js";
-import test from "./deprecated/test.js";
-import trace from "./deprecated/trace.js";
-import types from "./deprecated/types.js";
-
 import devnode from "./devnode.js";
 import faucet from "./faucet.js";
 import gasReport from "./gas-report.js";
@@ -22,21 +13,13 @@ import testV2 from "./test-v2.js";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Each command has different options
 export const commands: CommandModule<any, any>[] = [
-  bulkupload,
-  callSystem,
-  codegenLibdeploy,
-  deployContracts,
   deployV2,
   devnode,
   faucet,
   gasReport,
   hello,
-  systemTypes,
   tablegen,
   tsgen,
-  test,
-  trace,
-  types,
   worldgen,
   setVersion,
   testV2,
