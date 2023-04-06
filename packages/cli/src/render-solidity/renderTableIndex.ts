e20c34bfc5cf7173e289864357e0e72b79aa946b
Andy Cernera <kooshaba@gmail.com>
2023-04-06 08:37:41 -0400

    fix(cli): properly import `TableId`s in the table index

--- packages/cli/src/render-solidity/renderTableIndex.ts
+++ packages/cli/src/render-solidity/renderTableIndex.ts
@@ -4,9 +4,12 @@ import { TableOptions } from "./tableOptions.js";
 export function renderTableIndex(options: TableOptions[]) {
   return `${renderedSolidityHeader}
 
-${renderList(options, ({ outputPath, tableName, renderOptions: { structName } }) => {
-  const structImport = structName ? `, ${structName}` : "";
-  return `import { ${tableName}, ${tableName}TableId${structImport} } from "./${outputPath}";`;
+${renderList(options, ({ outputPath, tableName, renderOptions: { structName, staticResourceData } }) => {
+  const imports = [tableName];
+  if (structName) imports.push(structName);
+  if (staticResourceData) imports.push(`${tableName}TableId`);
+
+  return `import { ${imports.join(", ")} } from "./${outputPath}";`;
 })}
 `;
 }
