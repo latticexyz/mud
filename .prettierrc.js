631354319c409dc22cc35cfcb00c927ca6e9dbce
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-12 11:08:54 -0700

    refactor: consolidate prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: ["prettier-plugin-solidity"],
+  printWidth: 120,
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+};
