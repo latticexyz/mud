a7c81febb8fc1c48f93643bff4fb784f41de8f6a
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-12 11:45:44 -0700

    chore: comment about explicit plugin definition

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 /** @type {import('prettier').Config} */
 module.exports = {
+  // We need to explicitly define the plugin here for husky's lint-staged hook to work properly
   plugins: ["prettier-plugin-solidity"],
   printWidth: 120,
   semi: true,
