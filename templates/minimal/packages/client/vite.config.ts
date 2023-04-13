22dbf5a25f0c7b016bf5820a2fe8a8f398156ada
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-12 22:43:25 -0700

    bump vite, fix worker build issue
    thanks again gpt-4

--- templates/minimal/packages/client/vite.config.ts
+++ templates/minimal/packages/client/vite.config.ts
@@ -1,29 +1,10 @@
 import { defineConfig } from "vite";
+
 export default defineConfig({
   server: {
     port: 3000,
     fs: {
       strict: false,
     },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2020",
-    },
-    exclude: ["@latticexyz/network"],
-    include: [
-      "proxy-deep",
-      "ethers/lib/utils",
-      "bn.js",
-      "js-sha3",
-      "hash.js",
-      "bech32",
-      "long",
-      "protobufjs/minimal",
-      "debug",
-      "is-observable",
-      "nice-grpc-web",
-      "@improbable-eng/grpc-web",
-    ],
   },
 });
--- templates/react/packages/client/vite.config.ts
+++ templates/react/packages/client/vite.config.ts
@@ -9,24 +9,4 @@ export default defineConfig({
       strict: false,
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2020",
-    },
-    exclude: ["@latticexyz/network"],
-    include: [
-      "proxy-deep",
-      "ethers/lib/utils",
-      "bn.js",
-      "js-sha3",
-      "hash.js",
-      "bech32",
-      "long",
-      "protobufjs/minimal",
-      "debug",
-      "is-observable",
-      "nice-grpc-web",
-      "@improbable-eng/grpc-web",
-    ],
-  },
 });
