010740d09d40d4ff6d95538d498a513fbb65ca45
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-13 10:44:11 -0700

    feat(create-mud): use pnpm in templates, move to root so they can be installed/run (#599)
    * move templates
    
    * get templates running
    
    * get templates working with locally linked mud
    
    * copy templates into create-mud dist
    
    * add action for template builds
    
    * try perl via gpt-4
    
    * clarify script
    
    * bump vite, fix worker build issue
    
    thanks again gpt-4
    
    * ignore pnpm-lock for now
    
    * remove action comment
    
    * create projects with templates in tests
    
    * chore: fix .gitignore comment
    
    Co-authored-by: alvarius <89248902+alvrs@users.noreply.github.com>
    
    * test(create-mud): let em fail
    
    * chore(create-mud): ignore test project dir
    
    * feat(create-mud): default to pnpm
    
    ---------
    
    Co-authored-by: alvarius <89248902+alvrs@users.noreply.github.com>

--- packages/create-mud/src/cli.ts
+++ packages/create-mud/src/cli.ts
@@ -4,19 +4,23 @@ import { create } from "create-create-app";
 import { resolve } from "path";
 import packageJson from "../package.json";
 
-const templateRoot = resolve(__dirname, "..", "templates");
+const templateRoot = resolve(__dirname, "..", "dist", "templates");
 
 // See https://github.com/uetchy/create-create-app/blob/master/README.md for other options.
 
 create("create-mud", {
   templateRoot,
   defaultTemplate: "minimal",
+  // templates use pnpm workspaces, so default to that for now
+  // not sure if it's worth trying to support multiple kinds of package managers for monorepos, given the tooling is so different
+  defaultPackageManager: "pnpm",
   promptForDescription: false,
   promptForAuthor: false,
   promptForEmail: false,
   promptForLicense: false,
   promptForTemplate: true,
-  caveat: ({ answers }) => `Done! Play in the MUD with \`cd ${answers.name}\` and \`yarn dev\``,
+  caveat: ({ answers, packageManager }) =>
+    `Done! Play in the MUD with \`cd ${answers.name}\` and \`${packageManager} run dev\``,
   extra: {
     "mud-version": {
       type: "input",
