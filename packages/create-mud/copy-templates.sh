bc650481cc3a4616ed1eb3880180e65aeb337929
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-12 22:11:03 -0700

    try perl via gpt-4

--- packages/create-mud/copy-templates.sh
+++ packages/create-mud/copy-templates.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
 git ls-files ../../templates | rsync --files-from=- ../../ dist
-find ./dist/templates/* -name "package.json" -type f -exec sed -i '' 's/"\(@latticexyz\/.*\)": "link:.*"/"\1": "{{mud-version}}"/g' {} +
+
+find ./dist/templates/* -name "package.json" -type f | while read -r file; do
+  # GPT-4 recommended perl to edit-in-place rather than sed, because sed wasn't working on CI
+  perl -pi -e 's/"\(@latticexyz\/.*\)": "link:.*"/"\1": "{{mud-version}}"/g' "$file"
+done
