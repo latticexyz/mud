40962cbe590c7069ad89aa534df0b5876ef5443e
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-20 13:45:32 -0700

    feat: add examples (#648)
    * chore: pnpm create mud v2-minimal
    
    * chore: bring over v2sandbox code
    
    * chore: sort examples package.json
    
    * ci: add CI workflow for examples
    
    * chore: remove unneeded solmate dep
    
    * chore: package name
    
    * chore: prettier

---
+++ examples/v2-minimal/packages/client/src/mud/supportedChains/localhost.ts
@@ -0,0 +1,26 @@
+import { MudChain } from "./types";
+
+const localhost = {
+  id: 31337,
+  name: "Localhost",
+  network: "localhost",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["http://127.0.0.1:8545"],
+      webSocket: ["ws://127.0.0.1:8545"],
+    },
+    public: {
+      http: ["http://127.0.0.1:8545"],
+      webSocket: ["ws://127.0.0.1:8545"],
+    },
+  },
+  modeUrl: undefined,
+  faucetUrl: undefined,
+} as const satisfies MudChain;
+
+export default localhost;
