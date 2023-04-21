4bf2c577dbb6a9acf248cf1c3bfc0e1b431a9ec5
alvrs <alvarius@lattice.xyz>
2023-04-21 17:41:11 +0200

    build(store): replace vite with tsup

--- packages/store/vite.config.ts
+++ packages/store/vite.config.ts
@@ -1,21 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from "vitest/config";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 /**
  * @see https://vitejs.dev/config/
  */
-export default defineConfig({
-  build: {
-    ssr: true,
-    lib: {
-      entry: ["mud.config.ts", "ts/index.ts"],
-      formats: ["es"],
-    },
-    outDir: "dist",
-    minify: true,
-    sourcemap: true,
-    target: "esnext",
-  },
-  plugins: [peerDepsExternal()] as any,
-});
+export default defineConfig({});
