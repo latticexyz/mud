42ed7f7c38fc17b708ce5b42041d68d3e6978bac
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-24 13:36:15 -0700

    revert docs changes

--- docs/components/Logo.tsx
+++ docs/components/Logo.tsx
@@ -1,9 +1,6 @@
+import { useTheme } from "nextra-theme-docs";
+
 export default function Logo() {
-  // TODO: switch between dark and light logo depending on color theme
-  return (
-    <div style={{ display: "grid", gridAutoFlow: "column", alignItems: "center" }}>
-      <img src="/logo512.png" style={{ height: "calc(var(--nextra-navbar-height) - 25px)" }} />
-      <p style={{ fontWeight: "bold", fontSize: "25px", marginTop: "6px", paddingLeft: "4px" }}>MUD</p>
-    </div>
-  );
+  const { resolvedTheme } = useTheme();
+  return <img src={resolvedTheme === "light" ? "/logo512-black.png" : "logo512-white.png"} alt="MUD logo" />;
 }
