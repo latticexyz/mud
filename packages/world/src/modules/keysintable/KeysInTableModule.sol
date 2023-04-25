5859a183f4322aa5c31367a7277086922c762030
Fraser Scott <fraserdscott@gmail.com>
2023-04-25 19:39:29 +0200

    feat: add keys array length table

--- packages/world/src/modules/keysintable/KeysInTableModule.sol
+++ packages/world/src/modules/keysintable/KeysInTableModule.sol
@@ -9,9 +9,10 @@ import { IModule } from "../../interfaces/IModule.sol";
 import { WorldContext } from "../../WorldContext.sol";
 import { ResourceSelector } from "../../ResourceSelector.sol";
 
-import { MODULE_NAMESPACE, USED_KEYS_NAMESPACE } from "./constants.sol";
+import { MODULE_NAMESPACE, KEYS_LENGTH_NAMESPACE, USED_KEYS_NAMESPACE } from "./constants.sol";
 import { KeysInTableHook } from "./KeysInTableHook.sol";
 import { KeysInTable } from "./tables/KeysInTable.sol";
+import { KeysInTableLength } from "./tables/KeysInTableLength.sol";
 import { UsedKeysIndex } from "./tables/UsedKeysIndex.sol";
 import { getTargetTableSelector } from "../utils/getTargetTableSelector.sol";
 
@@ -37,23 +38,7 @@ contract KeysInTableModule is IModule, WorldContext {
     return bytes16("index");
   }
 
-  function setupNamespace(bytes8 moduleNamespace, bytes32 sourceTableId) internal {
-    bytes32 targetTableSelector = getTargetTableSelector(moduleNamespace, sourceTableId);
-
-    IBaseWorld world = IBaseWorld(_world());
-
-    // Register the target table
-    world.registerTable(
-      targetTableSelector.getNamespace(),
-      targetTableSelector.getName(),
-      KeysInTable.getSchema(),
-      KeysInTable.getKeySchema()
-    );
-
-    // Register metadata for the target table
-    (string memory tableName, string[] memory fieldNames) = KeysInTable.getMetadata();
-    world.setMetadata(targetTableSelector.getNamespace(), targetTableSelector.getName(), tableName, fieldNames);
-
+  function grantAccess(IBaseWorld world, bytes32 targetTableSelector) internal {
     // Grant the hook access to the target table
     world.grantAccess(targetTableSelector.getNamespace(), targetTableSelector.getName(), address(hook));
   }
@@ -62,8 +47,43 @@ contract KeysInTableModule is IModule, WorldContext {
     // Extract source table id from args
     bytes32 sourceTableId = abi.decode(args, (bytes32));
 
-    setupNamespace(MODULE_NAMESPACE, sourceTableId);
-    setupNamespace(USED_KEYS_NAMESPACE, sourceTableId);
+    bytes32 targetTableSelector1 = getTargetTableSelector(MODULE_NAMESPACE, sourceTableId);
+    bytes32 targetTableSelector2 = getTargetTableSelector(KEYS_LENGTH_NAMESPACE, sourceTableId);
+    bytes32 targetTableSelector3 = getTargetTableSelector(USED_KEYS_NAMESPACE, sourceTableId);
+
+    IBaseWorld world = IBaseWorld(_world());
+
+    // Register the target table
+    world.registerTable(
+      targetTableSelector1.getNamespace(),
+      targetTableSelector1.getName(),
+      KeysInTable.getSchema(),
+      KeysInTable.getKeySchema()
+    );
+    world.registerTable(
+      targetTableSelector2.getNamespace(),
+      targetTableSelector2.getName(),
+      KeysInTableLength.getSchema(),
+      KeysInTableLength.getKeySchema()
+    );
+    world.registerTable(
+      targetTableSelector3.getNamespace(),
+      targetTableSelector3.getName(),
+      UsedKeysIndex.getSchema(),
+      UsedKeysIndex.getKeySchema()
+    );
+
+    // Register metadata for the target table
+    (string memory tableName1, string[] memory fieldNames1) = KeysInTable.getMetadata();
+    world.setMetadata(targetTableSelector1.getNamespace(), targetTableSelector1.getName(), tableName1, fieldNames1);
+    (string memory tableName2, string[] memory fieldNames2) = KeysInTableLength.getMetadata();
+    world.setMetadata(targetTableSelector2.getNamespace(), targetTableSelector2.getName(), tableName2, fieldNames2);
+    (string memory tableName3, string[] memory fieldNames3) = UsedKeysIndex.getMetadata();
+    world.setMetadata(targetTableSelector3.getNamespace(), targetTableSelector3.getName(), tableName3, fieldNames3);
+
+    grantAccess(world, targetTableSelector1);
+    grantAccess(world, targetTableSelector2);
+    grantAccess(world, targetTableSelector3);
 
     // Register a hook that is called when a value is set in the source table
     StoreSwitch.registerStoreHook(sourceTableId, hook);
--- packages/world/src/modules/keysintable/constants.sol
+++ packages/world/src/modules/keysintable/constants.sol
@@ -4,5 +4,6 @@ pragma solidity >=0.8.0;
 // Limiting the module namespace to 8 bytes so the last 8 bytes
 // can be used for an identifier of the source table namespace to avoid
 // collisions between tables with the same name in different namespaces
-bytes8 constant MODULE_NAMESPACE = "keyswtab";
+bytes8 constant MODULE_NAMESPACE = "keystab";
+bytes8 constant KEYS_LENGTH_NAMESPACE = "keylen";
 bytes8 constant USED_KEYS_NAMESPACE = "usedkeys";
