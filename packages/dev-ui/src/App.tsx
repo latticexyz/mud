0824b9033e9ae099deaad37d5518e1ee17972c27
Kevin Ingersoll <kingersoll@gmail.com>
2023-04-27 16:00:26 -0700

    more tabs

--- packages/dev-ui/src/App.tsx
+++ packages/dev-ui/src/App.tsx
@@ -6,9 +6,18 @@ export function App() {
   return (
     <div className="fixed inset-0 pointer-events-none">
       <div className="pointer-events-auto w-full max-w-screen-sm h-full absolute right-0 bg-slate-800 text-slate-300 text-sm flex flex-col">
-        <div className="flex-none bg-slate-900">
-          <button type="button" className="py-1.5 px-3 bg-slate-800">
+        <div className="flex-none bg-slate-900 text-white/60 font-medium">
+          <button type="button" className="py-1.5 px-3 hover:bg-blue-800 hover:text-white">
+            Summary
+          </button>
+          <button type="button" className="py-1.5 px-3 hover:bg-blue-800 hover:text-white">
+            Actions
+          </button>
+          <button type="button" className="py-1.5 px-3 bg-slate-800 text-white">
             Store log
+          </button>
+          <button type="button" className="py-1.5 px-3 hover:bg-blue-800 hover:text-white">
+            Store data
           </button>
         </div>
         <div className="flex-1 overflow-auto">
