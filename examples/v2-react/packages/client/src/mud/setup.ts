9393fda6d8e74dd3f7f8be4470621d488aefdf3f
ludens <ludens@lattice.xyz>
2023-05-02 14:51:12 +0200

    feat: v2-react example

---
+++ examples/v2-react/packages/client/src/mud/setup.ts
@@ -0,0 +1,95 @@
+import { setupMUDV2Network } from "@latticexyz/std-client";
+import { createFastTxExecutor, createFaucetService } from "@latticexyz/network";
+import { getNetworkConfig } from "./getNetworkConfig";
+import { defineContractComponents } from "./contractComponents";
+import { clientComponents } from "./clientComponents";
+import { world } from "./world";
+import { Contract, Signer, utils } from "ethers";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { IWorld__factory } from "../../../contracts/types/ethers-contracts/factories/IWorld__factory";
+
+export type SetupResult = Awaited<ReturnType<typeof setup>>;
+
+export async function setup() {
+  const contractComponents = defineContractComponents(world);
+  const networkConfig = await getNetworkConfig();
+  const result = await setupMUDV2Network<typeof contractComponents>({
+    networkConfig,
+    world,
+    contractComponents,
+    syncThread: "main",
+  });
+
+  result.startSync();
+
+  // Request drip from faucet
+  const signer = result.network.signer.get();
+  if (!networkConfig.devMode && networkConfig.faucetServiceUrl && signer) {
+    const address = await signer.getAddress();
+    console.info("[Dev Faucet]: Player address -> ", address);
+
+    const faucet = createFaucetService(networkConfig.faucetServiceUrl);
+
+    const requestDrip = async () => {
+      const balance = await signer.getBalance();
+      console.info(`[Dev Faucet]: Player balance -> ${balance}`);
+      const lowBalance = balance?.lte(utils.parseEther("1"));
+      if (lowBalance) {
+        console.info("[Dev Faucet]: Balance is low, dripping funds to player");
+        // Double drip
+        await faucet.dripDev({ address });
+        await faucet.dripDev({ address });
+      }
+    };
+
+    requestDrip();
+    // Request a drip every 20 seconds
+    setInterval(requestDrip, 20000);
+  }
+
+  // Create a World contract instance
+  const worldContract = IWorld__factory.connect(
+    networkConfig.worldAddress,
+    signer ?? result.network.providers.get().json
+  );
+
+  // Create a fast tx executor
+  const fastTxExecutor =
+    signer?.provider instanceof JsonRpcProvider
+      ? await createFastTxExecutor(
+          signer as Signer & { provider: JsonRpcProvider }
+        )
+      : null;
+
+  // TODO: infer this from fastTxExecute signature?
+  type BoundFastTxExecuteFn<C extends Contract> = <F extends keyof C>(
+    func: F,
+    args: Parameters<C[F]>,
+    options?: {
+      retryCount?: number;
+    }
+  ) => Promise<ReturnType<C[F]>>;
+
+  function bindFastTxExecute<C extends Contract>(
+    contract: C
+  ): BoundFastTxExecuteFn<C> {
+    return async function (...args) {
+      if (!fastTxExecutor) {
+        throw new Error("no signer");
+      }
+      const { tx } = await fastTxExecutor.fastTxExecute(contract, ...args);
+      return await tx;
+    };
+  }
+
+  return {
+    ...result,
+    components: {
+      ...result.components,
+      ...clientComponents,
+    },
+    worldContract,
+    worldSend: bindFastTxExecute(worldContract),
+    fastTxExecutor,
+  };
+}
---
+++ examples/v2-react/packages/client/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    fs: {
+      strict: false,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2022",
+    },
+  },
+  build: {
+    rollupOptions: {
+      // TODO: revisit this config after splitting out mud config dependencies
+      // from the cli package so we don't need to bundle the cli package
+      external: ["chalk", "locate-path", "path-exists", "find-up"],
+    },
+    target: "es2022",
+  },
+  define: {
+    "process.env": {},
+  },
+});
