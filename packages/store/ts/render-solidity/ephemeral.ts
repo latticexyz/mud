362f4d21f8980ed9e667f6268735e3b8cbfe095c
Andy Cernera <kooshaba@gmail.com>
2023-05-02 15:35:13 -0400

    fix(store): use correct ephemeral function

--- packages/store/ts/render-solidity/ephemeral.ts
+++ packages/store/ts/render-solidity/ephemeral.ts
@@ -35,12 +35,12 @@ export function renderEphemeralMethods(options: RenderTableOptions) {
         _typedKeyArgs,
         `${structName} memory _table`,
       ])}) internal {
-        setEphemeralRecord(${renderArguments([
-          _untypedStore,
-          _tableId,
-          _keyArgs,
-          renderArguments(options.fields.map(({ name }) => `_table.${name}`)),
-        ])});
+       setEphemeral(${renderArguments([
+         _untypedStore,
+         _tableId,
+         _keyArgs,
+         renderArguments(options.fields.map(({ name }) => `_table.${name}`)),
+       ])});
       }
     `
     );
