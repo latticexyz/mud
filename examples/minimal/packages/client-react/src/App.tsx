a3508df4c1e6966478ba4239189d65e5a00cc640
Andy Cernera <kooshaba@gmail.com>
2023-05-03 16:10:24 -0400

    feat: add chat to react example

--- examples/minimal/packages/client-react/src/App.tsx
+++ examples/minimal/packages/client-react/src/App.tsx
@@ -1,15 +1,25 @@
 import { useComponentValue } from "@latticexyz/react";
 import { useMUD } from "./MUDContext";
+import { useEffect, useState } from "react";
 
 export const App = () => {
   const {
-    components: { CounterTable },
+    components: { CounterTable, MessageTable },
     singletonEntity,
-    network: { signer },
     worldSend,
   } = useMUD();
 
   const counter = useComponentValue(CounterTable, singletonEntity);
+
+  const [myMessage, setMyMessage] = useState<string>("");
+  const [messages, setMessages] = useState<string[]>([]);
+  const message = useComponentValue(MessageTable, singletonEntity);
+
+  useEffect(() => {
+    if (!message?.value) return;
+
+    setMessages((messages) => [...messages, `${new Date().toLocaleTimeString()}: ${message.value}`]);
+  }, [message]);
 
   return (
     <>
@@ -29,6 +39,22 @@ export const App = () => {
       >
         Increment
       </button>
+
+      <div>
+        <h1>Chat</h1>
+        <textarea value={messages.join("\n")} rows={10} cols={50} readOnly>
+          {messages.join("\n")}
+        </textarea>
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault();
+            await worldSend("sendMessage", [myMessage]);
+            setMyMessage("");
+          }}
+        >
+          <input value={myMessage} onChange={(event) => setMyMessage(event.target.value)} type="text" />
+        </form>
+      </div>
     </>
   );
 };
