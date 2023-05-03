22497a921560ea0e115a37ba20015a9523cf7c40
Fraser Scott <fraserdscott@gmail.com>
2023-05-03 15:05:00 +0200

    docs: document hasKey

--- packages/world/src/modules/keysintable/getKeysInTable.sol
+++ packages/world/src/modules/keysintable/getKeysInTable.sol
@@ -33,6 +33,9 @@ function getKeysInTable(IStore store, bytes32 tableId) view returns (bytes32[] m
   keysInTable = KeysInTable.getKeys(store, keysInTableTableId);
 }
 
+/**
+ * Get whether the key is in the given table.
+ */
 function hasKey(bytes32 tableId, bytes32[] memory key) view returns (bool) {
   bytes32 keysHash = keccak256(abi.encode(key));
 
