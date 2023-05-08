d60ca61772d9a935c2cc1b9667a05a5bb83b67b7
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-08 14:52:27 -0600

    store IErrors to IStoreErrors

--- packages/store/src/IStore.sol
+++ packages/store/src/IStore.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import { IErrors } from "./IErrors.sol";
+import { IStoreErrors } from "./IStoreErrors.sol";
 import { Schema } from "./Schema.sol";
 
 interface IStoreRead {
