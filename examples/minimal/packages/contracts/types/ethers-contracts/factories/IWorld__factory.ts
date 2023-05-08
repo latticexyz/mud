457f2331062eae3beb661f86b0e45eb56eada5ef
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-08 15:06:27 -0600

    build example

--- examples/minimal/packages/contracts/types/ethers-contracts/factories/IWorld__factory.ts
+++ examples/minimal/packages/contracts/types/ethers-contracts/factories/IWorld__factory.ts
@@ -92,6 +92,96 @@ const _abi = [
   {
     inputs: [
       {
+        internalType: "uint256",
+        name: "length",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "received",
+        type: "uint256",
+      },
+    ],
+    name: "StoreCore_DataIndexOverflow",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "expected",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "received",
+        type: "uint256",
+      },
+    ],
+    name: "StoreCore_InvalidDataLength",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "expected",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "received",
+        type: "uint256",
+      },
+    ],
+    name: "StoreCore_InvalidFieldNamesLength",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "StoreCore_NotDynamicField",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "StoreCore_NotImplemented",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "tableId",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "tableIdString",
+        type: "string",
+      },
+    ],
+    name: "StoreCore_TableAlreadyExists",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "tableId",
+        type: "bytes32",
+      },
+      {
+        internalType: "string",
+        name: "tableIdString",
+        type: "string",
+      },
+    ],
+    name: "StoreCore_TableNotFound",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
         internalType: "address",
         name: "system",
         type: "address",
