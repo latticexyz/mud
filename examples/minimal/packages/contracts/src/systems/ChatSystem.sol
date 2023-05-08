d802bdb39e51d51d1b683cf834c7d9bcb8a63fb0
alvarius <89248902+alvrs@users.noreply.github.com>
2023-05-08 23:26:33 +0200

    chore(examples/minimal): remove unused `SingletonKey` from ChatSystem

--- examples/minimal/packages/contracts/src/systems/ChatSystem.sol
+++ examples/minimal/packages/contracts/src/systems/ChatSystem.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.8.0;
 import { System } from "@latticexyz/world/src/System.sol";
 import { MessageTable } from "../codegen/Tables.sol";
 
-bytes32 constant SingletonKey = bytes32(uint256(0x060D));
-
 contract ChatSystem is System {
   function sendMessage(string memory message) public {
     MessageTable.emitEphemeral(message);
