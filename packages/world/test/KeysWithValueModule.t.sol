236f01f22c17a6b3a9dfcd2e4a5282776fc34e37
Fraser Scott <fraserdscott@gmail.com>
2023-05-09 15:31:34 +0200

    revert: remove accidental import

--- packages/world/test/KeysWithValueModule.t.sol
+++ packages/world/test/KeysWithValueModule.t.sol
@@ -17,7 +17,6 @@ import { MODULE_NAMESPACE } from "../src/modules/keyswithvalue/constants.sol";
 import { KeysWithValue } from "../src/modules/keyswithvalue/tables/KeysWithValue.sol";
 import { getKeysWithValue } from "../src/modules/keyswithvalue/getKeysWithValue.sol";
 import { getTargetTableSelector } from "../src/modules/utils/getTargetTableSelector.sol";
-import { getKeysInTable } from "../src/modules/keysintable/getKeysInTable.sol";
 
 contract KeysWithValueModuleTest is Test {
   using ResourceSelector for bytes32;
