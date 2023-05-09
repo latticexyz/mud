9448db3bb72d3c7671e77332b69d9b7f2a7c2cbd
Fraser Scott <fraserdscott@gmail.com>
2023-05-09 17:14:38 +0200

    feat: add table union query

--- packages/world/src/modules/keysintable/query.sol
+++ packages/world/src/modules/keysintable/query.sol
@@ -5,20 +5,43 @@ import { IStore } from "@latticexyz/store/src/IStore.sol";
 
 import { getKeysInTable } from "./getKeysInTable.sol";
 import { hasKey } from "./hasKey.sol";
-import { ArrayLib } from "../utils/ArrayLib.sol";
 
 function query(bytes32[] memory tableIds) view returns (bytes32[][] memory keys) {
-  keys = getKeysInTable(tableIds[0]);
+  // FIXME
 }
 
-function query(IStore store, bytes32[] memory tableIds) view returns (bytes32[][] memory keys) {
-  keys = getKeysInTable(store, tableIds[0]);
+function query(IStore store, bytes32[] memory tableIds) view returns (bytes32[][] memory keyTuples) {
+  keyTuples = new bytes32[][](0);
 
-  for (uint256 i = 1; i < tableIds.length; i++) {
-    for (uint256 j; j < keys.length; j++) {
-      if (!hasKey(store, tableIds[i], keys[j])) {
-        // do something
+  for (uint256 i; i < tableIds.length; i++) {
+    bytes32[][] memory tableKeyTuples = getKeysInTable(store, tableIds[i]);
+
+    bytes32[][] memory arr1 = keyTuples;
+    bytes32[][] memory arr2 = tableKeyTuples;
+
+    uint256 count;
+    for (uint256 j; j < arr1.length; j++) {
+      // if the current table has this key, ignore it
+      if (!hasKey(store, tableIds[i], arr1[j])) {
+        count++;
       }
     }
+
+    bytes32[][] memory result = new bytes32[][](count + arr2.length);
+
+    uint256 index;
+    for (uint256 j; j < arr1.length; j++) {
+      // if the current table has this key, ignore it
+      if (!hasKey(store, tableIds[i], arr1[j])) {
+        result[index] = arr1[j];
+        index++;
+      }
+    }
+
+    for (uint256 j; j < arr2.length; j++) {
+      result[j + count] = arr2[j];
+    }
+
+    keyTuples = result;
   }
 }
--- packages/world/test/query.t.sol
+++ packages/world/test/query.t.sol
@@ -15,155 +15,160 @@ import { CoreModule } from "../src/modules/core/CoreModule.sol";
 import { KeysInTableModule } from "../src/modules/keysintable/KeysInTableModule.sol";
 import { query } from "../src/modules/keysintable/query.sol";
 
-// Set up multiple tables
-// Install the module on both
 contract queryTest is Test {
   using ResourceSelector for bytes32;
   IBaseWorld world;
   KeysInTableModule keysInTableModule = new KeysInTableModule(); // Modules can be deployed once and installed multiple times
 
   bytes16 namespace = ROOT_NAMESPACE;
-  bytes16 sourceFileA = bytes16("sourceA");
-  bytes16 sourceFileB = bytes16("sourceB");
+  bytes16 nameA = bytes16("sourceA");
+  bytes16 nameB = bytes16("sourceB");
 
-  Schema sourceTableSchema;
-  Schema sourceTableKeySchema;
-  bytes32 sourceTableIdA = ResourceSelector.from(namespace, sourceFileA);
-  bytes32 sourceTableIdB = ResourceSelector.from(namespace, sourceFileB);
+  Schema tableSchema;
+  Schema tableKeySchema;
+  bytes32 tableA = ResourceSelector.from(namespace, nameA);
+  bytes32 tableB = ResourceSelector.from(namespace, nameB);
 
   function setUp() public {
-    sourceTableSchema = SchemaLib.encode(SchemaType.UINT256);
-    sourceTableKeySchema = SchemaLib.encode(SchemaType.BYTES32);
+    tableSchema = SchemaLib.encode(SchemaType.UINT256);
+    tableKeySchema = SchemaLib.encode(SchemaType.BYTES32);
     world = IBaseWorld(address(new World()));
     world.installRootModule(new CoreModule(), new bytes(0));
   }
 
   function _installKeysInTableModule() internal {
     // Register source table
-    sourceTableIdA = world.registerTable(namespace, sourceFileA, sourceTableSchema, sourceTableKeySchema);
-    sourceTableIdB = world.registerTable(namespace, sourceFileB, sourceTableSchema, sourceTableKeySchema);
+    tableA = world.registerTable(namespace, nameA, tableSchema, tableKeySchema);
+    tableB = world.registerTable(namespace, nameB, tableSchema, tableKeySchema);
 
     // Install the index module
     // TODO: add support for installing this via installModule
     // -> requires `callFrom` for the module to be able to register a hook in the name of the original caller
     // !gasreport install keys in table module
-    world.installRootModule(keysInTableModule, abi.encode(sourceTableIdA));
-    world.installRootModule(keysInTableModule, abi.encode(sourceTableIdB));
+    world.installRootModule(keysInTableModule, abi.encode(tableA));
+    world.installRootModule(keysInTableModule, abi.encode(tableB));
   }
 
-  function testQuery(bytes32 key1, bytes32 key2, uint256 value1, uint256 value2) public {
+  function testQuerySingleTable(bytes32[2] memory keys, uint256[2] memory values) public {
+    vm.assume(keys[0] != keys[1]);
+
     _installKeysInTableModule();
 
     bytes32[] memory keyTuple1 = new bytes32[](1);
-    keyTuple1[0] = key1;
+    keyTuple1[0] = keys[0];
     bytes32[] memory keyTuple2 = new bytes32[](1);
-    keyTuple2[0] = key2;
+    keyTuple2[0] = keys[1];
 
     // Set a value in the source table
-    world.setRecord(namespace, sourceFileA, keyTuple1, abi.encodePacked(value1));
+    world.setRecord(namespace, nameA, keyTuple1, abi.encodePacked(values[0]));
 
     bytes32[] memory tableIds = new bytes32[](1);
-    tableIds[0] = sourceTableIdA;
+    tableIds[0] = tableA;
 
     // !gasreport Get list of keys in a given table
-    bytes32[][] memory keysInTable = query(world, tableIds);
+    bytes32[][] memory keyTuples = query(world, tableIds);
 
-    // Assert that the list is correct
-    assertEq(keysInTable.length, 1);
-    assertEq(keysInTable[0][0], key1);
+    // Assert that the list includes all the keys in the table
+    assertEq(keyTuples.length, 1);
+    for (uint256 i; i < 1; i++) {
+      assertEq(keyTuples[i][0], keys[i]);
+    }
 
     // Set another key with a different value
-    world.setRecord(namespace, sourceFileA, keyTuple2, abi.encodePacked(value2));
+    world.setRecord(namespace, nameA, keyTuple2, abi.encodePacked(values[1]));
 
     // Get the list of keys in the target table
-    keysInTable = query(world, tableIds);
+    keyTuples = query(world, tableIds);
 
-    // Assert that the list is correct
-    assertEq(keysInTable.length, 2);
-    assertEq(keysInTable[0][0], key1);
-    assertEq(keysInTable[1][0], key2);
+    // Assert that the list includes all the keys in the table
+    assertEq(keyTuples.length, 2);
+    for (uint256 i; i < 2; i++) {
+      assertEq(keyTuples[i][0], keys[i]);
+    }
   }
 
-  // Put key1 and key2 into tableA. put only key1 in tableB
-  function testQueryMultiple(bytes32 key1, bytes32 key2, uint256 value1, uint256 value2) public {
+  // USING TWO TABLES****************************************
+
+  function testQueryTwoTables21(bytes32[2] memory keys, uint256[2] memory values) public {
+    vm.assume(keys[0] != keys[1]);
+
     _installKeysInTableModule();
 
     bytes32[] memory keyTuple1 = new bytes32[](1);
-    keyTuple1[0] = key1;
+    keyTuple1[0] = keys[0];
     bytes32[] memory keyTuple2 = new bytes32[](1);
-    keyTuple2[0] = key2;
+    keyTuple2[0] = keys[1];
 
     bytes32[] memory tableIds = new bytes32[](2);
-    tableIds[0] = sourceTableIdA;
-    tableIds[1] = sourceTableIdB;
+    tableIds[0] = tableA;
+    tableIds[1] = tableB;
 
-    // Set key1 in both tables
-    world.setRecord(namespace, sourceFileA, keyTuple1, abi.encodePacked(value1));
-    world.setRecord(namespace, sourceFileB, keyTuple1, abi.encodePacked(value1));
-
-    // but set key2 in one table
-    world.setRecord(namespace, sourceFileA, keyTuple2, abi.encodePacked(value2));
+    world.setRecord(namespace, nameA, keyTuple1, abi.encodePacked(values[0]));
+    world.setRecord(namespace, nameA, keyTuple2, abi.encodePacked(values[1]));
+    world.setRecord(namespace, nameB, keyTuple1, abi.encodePacked(values[0]));
 
     // !gasreport Get list of keys in a given table
-    bytes32[][] memory keysInTable = query(world, tableIds);
+    bytes32[][] memory keyTuples = query(world, tableIds);
 
-    // Assert that the list is the intersection of both table keys
-    assertEq(keysInTable.length, 1);
-    assertEq(keysInTable[0][0], key1);
+    // Assert that the list is the union of both tables keys
+    assertEq(keyTuples.length, 2);
+    assertEq(keyTuples[0][0], keys[1]);
+    assertEq(keyTuples[1][0], keys[0]);
   }
 
-  function testQueryMultiple2(bytes32 key1, bytes32 key2, uint256 value1, uint256 value2) public {
+  function testQueryTwoTables12(bytes32[2] memory keys, uint256[2] memory values) public {
+    vm.assume(keys[0] != keys[1]);
+
     _installKeysInTableModule();
 
     bytes32[] memory keyTuple1 = new bytes32[](1);
-    keyTuple1[0] = key1;
+    keyTuple1[0] = keys[0];
     bytes32[] memory keyTuple2 = new bytes32[](1);
-    keyTuple2[0] = key2;
+    keyTuple2[0] = keys[1];
 
     bytes32[] memory tableIds = new bytes32[](2);
-    tableIds[0] = sourceTableIdA;
-    tableIds[1] = sourceTableIdB;
+    tableIds[0] = tableA;
+    tableIds[1] = tableB;
 
-    // Set key1 in both tables
-    world.setRecord(namespace, sourceFileA, keyTuple1, abi.encodePacked(value1));
-
-    // but set key2 in one table
-    world.setRecord(namespace, sourceFileA, keyTuple1, abi.encodePacked(value2));
-    world.setRecord(namespace, sourceFileB, keyTuple2, abi.encodePacked(value1));
+    world.setRecord(namespace, nameA, keyTuple1, abi.encodePacked(values[0]));
+    world.setRecord(namespace, nameB, keyTuple1, abi.encodePacked(values[0]));
+    world.setRecord(namespace, nameB, keyTuple2, abi.encodePacked(values[1]));
 
     // !gasreport Get list of keys in a given table
-    bytes32[][] memory keysInTable = query(world, tableIds);
+    bytes32[][] memory keyTuples = query(world, tableIds);
 
-    // Assert that the list is the intersection of both table keys
-    assertEq(keysInTable.length, 1);
-    assertEq(keysInTable[0][0], key1);
+    // Assert that the list is the union of both tables keys
+    assertEq(keyTuples.length, 2);
+    assertEq(keyTuples[0][0], keys[0]);
+    assertEq(keyTuples[1][0], keys[1]);
   }
 
-  function testQueryMultiple3(bytes32 key1, bytes32 key2, uint256 value1, uint256 value2) public {
+  function testQueryTwoTables22(bytes32[2] memory keys, uint256[2] memory values) public {
+    vm.assume(keys[0] != keys[1]);
+
     _installKeysInTableModule();
 
     bytes32[] memory keyTuple1 = new bytes32[](1);
-    keyTuple1[0] = key1;
+    keyTuple1[0] = keys[0];
     bytes32[] memory keyTuple2 = new bytes32[](1);
-    keyTuple2[0] = key2;
+    keyTuple2[0] = keys[1];
 
     bytes32[] memory tableIds = new bytes32[](2);
-    tableIds[0] = sourceTableIdA;
-    tableIds[1] = sourceTableIdB;
+    tableIds[0] = tableA;
+    tableIds[1] = tableB;
 
-    // Set key1 in both tables
-    world.setRecord(namespace, sourceFileA, keyTuple1, abi.encodePacked(value1));
-
-    // but set key2 in one table
-    world.setRecord(namespace, sourceFileA, keyTuple1, abi.encodePacked(value2));
-    world.setRecord(namespace, sourceFileB, keyTuple2, abi.encodePacked(value1));
+    // Set both keys in both tables
+    world.setRecord(namespace, nameA, keyTuple1, abi.encodePacked(values[0]));
+    world.setRecord(namespace, nameA, keyTuple2, abi.encodePacked(values[1]));
+    world.setRecord(namespace, nameB, keyTuple1, abi.encodePacked(values[0]));
+    world.setRecord(namespace, nameB, keyTuple2, abi.encodePacked(values[1]));
 
     // !gasreport Get list of keys in a given table
-    bytes32[][] memory keysInTable = query(world, tableIds);
+    bytes32[][] memory keyTuples = query(world, tableIds);
 
-    // Assert that the list is the intersection of both table keys
-    assertEq(keysInTable.length, 2);
-    assertEq(keysInTable[0][0], key1);
-    assertEq(keysInTable[1][0], key1);
+    // Assert that the list is the union of both tables keys
+    assertEq(keyTuples.length, 2);
+    assertEq(keyTuples[0][0], keys[0]);
+    assertEq(keyTuples[1][0], keys[1]);
   }
 }
