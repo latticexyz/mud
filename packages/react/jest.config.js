274d5c9bb22c51732f28a9bd108d44a94dc694e6
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-10 16:01:05 -0600

    fix tests

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,9 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
-  moduleNameMapper: {
-    // fix TS build issues
-    "^(..?/.*).js$": "$1",
-  },
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^@latticexyz/recs$": "<rootDir>/../recs/src/index.ts",
+  },
 };
