8a7d7967bff81cc0d29afe753c0765e5fb656c3d
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-10 15:30:55 -0600

    fix test

--- packages/react/jest.config.cjs
+++ packages/react/jest.config.cjs
@@ -1,9 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
-  moduleNameMapper: {
-    // fix TS build issues
-    "^(..?/.*).js$": "$1",
-  },
   testEnvironment: "node",
 };
