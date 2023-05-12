9d2c1bcc90cee9476aa8e857d092aa3e391bde68
alvarius <89248902+alvrs@users.noreply.github.com>
2023-05-12 16:18:13 +0200

    refactor: move errors from cli to common, remove cli dependency in common (#754)
    * refactor: move errors from cli to common, remove cli dependency in common
    
    * chore: self-review, remove unrelated changes
    
    * fix: update MUDError import in world
    
    * build(common): add @types/node
    
    * sort package.json

--- packages/cli/src/utils/deploy-v2.ts
+++ packages/cli/src/utils/deploy-v2.ts
@@ -7,7 +7,7 @@ import { defaultAbiCoder as abi, Fragment, ParamType } from "ethers/lib/utils.js
 
 import { getOutDirectory, getScriptDirectory, cast, forge } from "@latticexyz/common/foundry";
 import { resolveWithContext } from "@latticexyz/config";
-import { MUDError } from "@latticexyz/config";
+import { MUDError } from "@latticexyz/common/errors";
 import { encodeSchema } from "@latticexyz/schema-type";
 import { StoreConfig, resolveAbiOrUserType } from "@latticexyz/store";
 import { WorldConfig, resolveWorldConfig } from "@latticexyz/world";
