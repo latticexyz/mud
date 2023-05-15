c5797ba4bd7c1cf96e677dc7dd741f7645e75e9b
alvrs <alvarius@lattice.xyz>
2023-05-15 20:45:18 +0200

    fix: remove default namespace

--- examples/minimal/packages/contracts/mud.config.ts
+++ examples/minimal/packages/contracts/mud.config.ts
@@ -2,7 +2,6 @@ import { mudConfig } from "@latticexyz/world/register";
 import { resolveTableId } from "@latticexyz/config";
 
 export default mudConfig({
-  namespace: "",
   overrideSystems: {
     IncrementSystem: {
       name: "increment",
