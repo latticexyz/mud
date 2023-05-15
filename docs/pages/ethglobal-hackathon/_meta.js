eccf1cffd68bc6865207ce1db48960a402e7d95e
biscaryn <33936909+biscaryn@users.noreply.github.com>
2023-05-15 22:50:54 +0200

    docs: add hackathon idea pages and splash plage (#828)
    * docs: add AW hackathon ideas as .mdx pages and add hacakthon splash pop-up page when first landing
    
    * docs: update index.mdx to include new components, update _meta.js
    
    * fix: change align attribute in div tag to flexbox to fix jsx error
    
    * fix: minor changes in response to comments
    
    * fix: link now opens in new tab in response to PR comment
    
    * use actual link
    
    ---------
    
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

---
+++ docs/pages/ethglobal-hackathon/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  chess: "Client agnostic chess",
+  snake: "Snake onchain",
+  "coin-war": "Coin war",
+  "not-monopoly": "Not Monopoly",
+  crypters: "Crypters",
+  "defi-in-a-box": "DeFi-in-a-Box",
+  rng: "RNG plug-in",
+};
