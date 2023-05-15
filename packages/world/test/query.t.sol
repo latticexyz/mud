5887d64d5574a311ed263f58e1c8ee934ad15bb1
Fraser Scott <fraserdscott@gmail.com>
2023-05-15 16:34:38 +0100

    refactor: copy original libQuery tests

--- packages/world/test/query.t.sol
+++ packages/world/test/query.t.sol
@@ -23,16 +23,21 @@ contract QueryTest is Test {
   KeysWithValueModule keysWithValueModule = new KeysWithValueModule();
 
   bytes16 namespace = ROOT_NAMESPACE;
-  bytes16 nameA = bytes16("sourceA");
-  bytes16 nameB = bytes16("sourceB");
+  bytes16 name1 = bytes16("source1");
+  bytes16 name2 = bytes16("source2");
+  bytes16 name3 = bytes16("source3");
 
   Schema tableSchema;
   Schema tableKeySchema;
-  bytes32 tableA = ResourceSelector.from(namespace, nameA);
-  bytes32 tableB = ResourceSelector.from(namespace, nameB);
+  bytes32 table1 = ResourceSelector.from(namespace, name1);
+  bytes32 table2 = ResourceSelector.from(namespace, name2);
+  bytes32 table3 = ResourceSelector.from(namespace, name3);
 
   uint256 value = 1;
-  bytes32[][] keys = new bytes32[][](2);
+  bytes32[] key1 = new bytes32[](1);
+  bytes32[] key2 = new bytes32[](1);
+  bytes32[] key3 = new bytes32[](1);
+  bytes32[] key4 = new bytes32[](1);
   QueryFragment[] fragmentsHasHas;
   QueryFragment[] fragmentsHasNot;
 
@@ -42,157 +47,225 @@ contract QueryTest is Test {
     world = IBaseWorld(address(new World()));
     world.installRootModule(new CoreModule(), new bytes(0));
 
-    keys[0] = new bytes32[](1);
-    keys[1] = new bytes32[](1);
-    keys[0][0] = "test1";
-    keys[1][0] = "test2";
+    key1[0] = "test1";
+    key2[0] = "test2";
+    key3[0] = "test3";
+    key4[0] = "test4";
 
-    fragmentsHasHas.push(QueryFragment(QueryType.Has, tableA, ""));
-    fragmentsHasHas.push(QueryFragment(QueryType.Has, tableB, ""));
-    fragmentsHasNot.push(QueryFragment(QueryType.Has, tableA, ""));
-    fragmentsHasNot.push(QueryFragment(QueryType.Not, tableB, ""));
+    fragmentsHasHas.push(QueryFragment(QueryType.Has, table1, ""));
+    fragmentsHasHas.push(QueryFragment(QueryType.Has, table2, ""));
+    fragmentsHasNot.push(QueryFragment(QueryType.Has, table1, ""));
+    fragmentsHasNot.push(QueryFragment(QueryType.Not, table2, ""));
   }
 
   function _installKeysInTableModule() internal {
     // Register source table
-    tableA = world.registerTable(namespace, nameA, tableSchema, tableKeySchema);
-    tableB = world.registerTable(namespace, nameB, tableSchema, tableKeySchema);
+    table1 = world.registerTable(namespace, name1, tableSchema, tableKeySchema);
+    table2 = world.registerTable(namespace, name2, tableSchema, tableKeySchema);
+    table3 = world.registerTable(namespace, name3, tableSchema, tableKeySchema);
 
     // Install the index module
     // TODO: add support for installing this via installModule
     // -> requires `callFrom` for the module to be able to register a hook in the name of the original caller
-    world.installRootModule(keysInTableModule, abi.encode(tableA));
-    world.installRootModule(keysInTableModule, abi.encode(tableB));
+    world.installRootModule(keysInTableModule, abi.encode(table1));
+    world.installRootModule(keysInTableModule, abi.encode(table2));
+    world.installRootModule(keysInTableModule, abi.encode(table3));
   }
 
   function _installKeysWithValueModule() internal {
     // Install the index module
     // TODO: add support for installing this via installModule
     // -> requires `callFrom` for the module to be able to register a hook in the name of the original caller
-    world.installRootModule(keysWithValueModule, abi.encode(tableA));
-    world.installRootModule(keysWithValueModule, abi.encode(tableB));
+    world.installRootModule(keysWithValueModule, abi.encode(table1));
+    world.installRootModule(keysWithValueModule, abi.encode(table2));
+    world.installRootModule(keysWithValueModule, abi.encode(table3));
   }
 
-  function testHas() public {
+  function testHasQuery() public {
     _installKeysInTableModule();
 
-    // Add the first key to table A
-    world.setRecord(namespace, nameA, keys[0], abi.encodePacked(value));
+    world.setRecord(namespace, name1, key1, abi.encode(1));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name2, key1, abi.encode(0));
 
+    // Query should return all keys in table1
     QueryFragment[] memory fragments = new QueryFragment[](1);
-    fragments[0] = QueryFragment(QueryType.Has, tableA, "");
-
-    // !gasreport running a Has query with one key
+    // The value argument is ignored in for Has query fragments
+    fragments[0] = QueryFragment(QueryType.Has, table1, new bytes(0));
     bytes32[][] memory keyTuples = query(world, fragments);
 
-    assertEq(keyTuples.length, 1);
-    for (uint256 i; i < 1; i++) {
-      assertEq(keyTuples[i][0], keys[i][0]);
-    }
-
-    // Add the second key to table A
-    world.setRecord(namespace, nameA, keys[1], abi.encodePacked(value));
-
-    // !gasreport running a Has query with two keys
-    keyTuples = query(world, fragments);
-
-    assertEq(keyTuples.length, 2);
-    for (uint256 i; i < 2; i++) {
-      assertEq(keyTuples[i][0], keys[i][0]);
-    }
-
-    // Add the first key to table B
-    world.setRecord(namespace, nameB, keys[0], abi.encodePacked(value));
-
-    bytes32[][] memory keyTuplesHasHas = query(world, fragmentsHasHas);
-    bytes32[][] memory keyTuplesHasNot = query(world, fragmentsHasNot);
-
-    assertEq(keyTuplesHasHas.length, 1);
-    assertEq(keyTuplesHasHas[0][0], keys[0][0]);
-    assertEq(keyTuplesHasNot.length, 1);
-    assertEq(keyTuplesHasNot[0][0], keys[1][0]);
-
-    // Add the second key to table B
-    world.setRecord(namespace, nameB, keys[1], abi.encodePacked(value));
-
-    // !gasreport running a Has, Has query with four keys
-    keyTuplesHasHas = query(world, fragmentsHasHas);
-    // !gasreport running a Has, HasNot query with four keys
-    keyTuplesHasNot = query(world, fragmentsHasNot);
-
-    assertEq(keyTuplesHasHas.length, 2);
-    for (uint256 i; i < 2; i++) {
-      assertEq(keyTuplesHasHas[i][0], keys[i][0]);
-    }
-    assertEq(keyTuplesHasNot.length, 0);
+    assertTrue(keyTuples.length == 2);
+    assertTrue(keyTuples[0][0] == key1[0]);
+    assertTrue(keyTuples[1][0] == key2[0]);
   }
 
-  function testHasReverseOrder() public {
-    _installKeysInTableModule();
-
-    world.setRecord(namespace, nameA, keys[0], abi.encodePacked(value));
-    world.setRecord(namespace, nameB, keys[0], abi.encodePacked(value));
-    world.setRecord(namespace, nameB, keys[1], abi.encodePacked(value));
-
-    bytes32[][] memory keyTuplesHasNot = query(world, fragmentsHasNot);
-    bytes32[][] memory keyTuplesHasHas = query(world, fragmentsHasHas);
-
-    assertEq(keyTuplesHasNot.length, 0);
-    assertEq(keyTuplesHasHas.length, 1);
-    assertEq(keyTuplesHasHas[0][0], keys[0][0]);
-
-    // Insert the second key into table A
-    world.setRecord(namespace, nameA, keys[1], abi.encodePacked(value));
-
-    keyTuplesHasNot = query(world, fragmentsHasNot);
-    keyTuplesHasHas = query(world, fragmentsHasHas);
-
-    assertEq(keyTuplesHasNot.length, 0);
-    assertEq(keyTuplesHasHas.length, 2);
-    for (uint256 i; i < 2; i++) {
-      assertEq(keyTuplesHasHas[i][0], keys[i][0]);
-    }
-  }
-
-  function testHasValue() public {
-    vm.assume(value != 0);
-
+  function testHasValueQuery() public {
     _installKeysInTableModule();
     _installKeysWithValueModule();
 
-    world.setRecord(namespace, nameA, keys[0], abi.encodePacked(value));
-    world.setRecord(namespace, nameB, keys[0], abi.encodePacked(value));
-    world.setRecord(namespace, nameB, keys[1], abi.encodePacked(value));
+    world.setRecord(namespace, name1, key1, abi.encode(2));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    // Query should return all keys in table1 with value 1
+    QueryFragment[] memory fragments = new QueryFragment[](1);
+    fragments[0] = QueryFragment(QueryType.HasValue, table1, abi.encode(1));
+    bytes32[][] memory keyTuples = query(world, fragments);
 
-    QueryFragment[] memory fragmentsHasHasvalue = new QueryFragment[](2);
-    QueryFragment[] memory fragmentsHasNotvalue = new QueryFragment[](2);
-    QueryFragment[] memory fragmentsHasvalueHas = new QueryFragment[](2);
-    QueryFragment[] memory fragmentsHasvalueHasValue = new QueryFragment[](2);
-    fragmentsHasHasvalue[0] = QueryFragment(QueryType.Has, tableA, "");
-    fragmentsHasHasvalue[1] = QueryFragment(QueryType.HasValue, tableB, abi.encodePacked(value));
-    fragmentsHasNotvalue[0] = QueryFragment(QueryType.Has, tableA, "");
-    fragmentsHasNotvalue[1] = QueryFragment(QueryType.NotValue, tableB, abi.encodePacked(value));
-    fragmentsHasvalueHas[0] = QueryFragment(QueryType.HasValue, tableA, abi.encodePacked(value));
-    fragmentsHasvalueHas[1] = QueryFragment(QueryType.Has, tableB, "");
-    fragmentsHasvalueHasValue[0] = QueryFragment(QueryType.HasValue, tableA, abi.encodePacked(value));
-    fragmentsHasvalueHasValue[1] = QueryFragment(QueryType.HasValue, tableB, abi.encodePacked(value));
+    assertTrue(keyTuples.length == 2);
+    assertTrue(keyTuples[0][0] == key2[0]);
+    assertTrue(keyTuples[1][0] == key3[0]);
+  }
 
-    // Assert that all the fragments return the correct keys
-    bytes32[][] memory keyTuplesHasHas = query(world, fragmentsHasHas);
-    assertEq(keyTuplesHasHas.length, 1);
-    assertEq(keyTuplesHasHas[0][0], keys[0][0]);
-    bytes32[][] memory keyTuplesHasNot = query(world, fragmentsHasNot);
-    assertEq(keyTuplesHasNot.length, 0);
-    // !gasreport running a Has, HasValue query with four keys
-    bytes32[][] memory keyTuplesHasHasValue = query(world, fragmentsHasHasvalue);
-    assertEq(keyTuplesHasHasValue.length, 1);
-    assertEq(keyTuplesHasHasValue[0][0], keys[0][0]);
-    bytes32[][] memory keyTuplesHasNotvalue = query(world, fragmentsHasNotvalue);
-    assertEq(keyTuplesHasNotvalue.length, 0);
-    bytes32[][] memory keyTuplesHasvalueHas = query(world, fragmentsHasvalueHas);
-    assertEq(keyTuplesHasvalueHas.length, 1);
-    // !gasreport running a HasValue, HasValue query with four keys
-    bytes32[][] memory keyTuplesHasvalueHasvalue = query(world, fragmentsHasvalueHasValue);
-    assertEq(keyTuplesHasvalueHasvalue.length, 1);
+  function testCombinedHasQuery() public {
+    _installKeysInTableModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(2));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key2, abi.encode(1));
+    world.setRecord(namespace, name2, key3, abi.encode(1));
+    world.setRecord(namespace, name3, key1, abi.encode(1));
+
+    // Query should return all entities that have table1 and table2
+    QueryFragment[] memory fragments = new QueryFragment[](2);
+    fragments[0] = QueryFragment(QueryType.Has, table1, new bytes(0));
+    fragments[1] = QueryFragment(QueryType.Has, table2, new bytes(0));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 2);
+    assertTrue(keyTuples[0][0] == key2[0]);
+    assertTrue(keyTuples[1][0] == key3[0]);
+  }
+
+  function testCombinedHasValueQuery() public {
+    _installKeysInTableModule();
+    _installKeysWithValueModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(2));
+    world.setRecord(namespace, name1, key2, abi.encode(2));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key2, abi.encode(1));
+    world.setRecord(namespace, name2, key3, abi.encode(1));
+    world.setRecord(namespace, name3, key1, abi.encode(1));
+
+    // Query should return all entities that have table1 and table2
+    QueryFragment[] memory fragments = new QueryFragment[](2);
+    fragments[0] = QueryFragment(QueryType.HasValue, table1, abi.encode(1));
+    fragments[1] = QueryFragment(QueryType.HasValue, table2, abi.encode(1));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 1);
+    assertTrue(keyTuples[0][0] == key3[0]);
+  }
+
+  function testCombinedHasHasValueQuery() public {
+    _installKeysInTableModule();
+    _installKeysWithValueModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(1));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key1, abi.encode(1));
+    world.setRecord(namespace, name2, key2, abi.encode(2));
+    world.setRecord(namespace, name2, key3, abi.encode(2));
+    world.setRecord(namespace, name2, key4, abi.encode(2));
+
+    // Query should return all entities that have table1 and table2
+    QueryFragment[] memory fragments = new QueryFragment[](2);
+    fragments[0] = QueryFragment(QueryType.Has, table1, new bytes(0));
+    fragments[1] = QueryFragment(QueryType.HasValue, table2, abi.encode(2));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 2);
+    assertTrue(keyTuples[0][0] == key2[0]);
+    assertTrue(keyTuples[1][0] == key3[0]);
+  }
+
+  function testCombinedHasNotQuery() public {
+    _installKeysInTableModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(1));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key1, abi.encode(1));
+    world.setRecord(namespace, name2, key2, abi.encode(2));
+    world.setRecord(namespace, name2, key3, abi.encode(2));
+    world.setRecord(namespace, name2, key4, abi.encode(2));
+
+    // Query should return all entities that have table1 and table2
+    QueryFragment[] memory fragments = new QueryFragment[](2);
+    fragments[0] = QueryFragment(QueryType.Has, table2, new bytes(0));
+    fragments[1] = QueryFragment(QueryType.Not, table1, new bytes(0));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 1);
+    assertTrue(keyTuples[0][0] == key4[0]);
+  }
+
+  function testCombinedHasValueNotQuery() public {
+    _installKeysInTableModule();
+    _installKeysWithValueModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(1));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key1, abi.encode(1));
+    world.setRecord(namespace, name2, key2, abi.encode(2));
+    world.setRecord(namespace, name2, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key4, abi.encode(1));
+
+    // Query should return all entities that have table1 and table2
+    QueryFragment[] memory fragments = new QueryFragment[](2);
+    fragments[0] = QueryFragment(QueryType.HasValue, table2, abi.encode(1));
+    fragments[1] = QueryFragment(QueryType.Not, table1, new bytes(0));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 1);
+    assertTrue(keyTuples[0][0] == key4[0]);
+  }
+
+  function testCombinedHasHasValueNotQuery() public {
+    _installKeysInTableModule();
+    _installKeysWithValueModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(1));
+    world.setRecord(namespace, name1, key2, abi.encode(1));
+    world.setRecord(namespace, name1, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key1, abi.encode(1));
+    world.setRecord(namespace, name2, key2, abi.encode(2));
+    world.setRecord(namespace, name2, key3, abi.encode(1));
+    world.setRecord(namespace, name2, key4, abi.encode(1));
+    world.setRecord(namespace, name3, key2, abi.encode(1));
+    world.setRecord(namespace, name3, key3, abi.encode(1));
+    world.setRecord(namespace, name3, key4, abi.encode(1));
+
+    // Query should return all entities that have table2 and not table1
+    QueryFragment[] memory fragments = new QueryFragment[](3);
+    fragments[0] = QueryFragment(QueryType.Has, table1, new bytes(0));
+    fragments[1] = QueryFragment(QueryType.HasValue, table2, abi.encode(1));
+    fragments[2] = QueryFragment(QueryType.Not, table3, new bytes(0));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 1);
+    assertTrue(keyTuples[0][0] == key1[0]);
+  }
+
+  function testNotValueQuery() public {
+    _installKeysInTableModule();
+    _installKeysWithValueModule();
+
+    world.setRecord(namespace, name1, key1, abi.encode(4));
+    world.setRecord(namespace, name1, key2, abi.encode(5));
+    world.setRecord(namespace, name1, key3, abi.encode(6));
+
+    // Query should return all entities with table1 except value 6
+    QueryFragment[] memory fragments = new QueryFragment[](2);
+    fragments[0] = QueryFragment(QueryType.Has, table1, new bytes(0));
+    fragments[1] = QueryFragment(QueryType.NotValue, table1, abi.encode(6));
+    bytes32[][] memory keyTuples = query(world, fragments);
+
+    assertTrue(keyTuples.length == 2);
+    assertTrue(keyTuples[0][0] == key1[0]);
+    assertTrue(keyTuples[1][0] == key2[0]);
   }
 }
