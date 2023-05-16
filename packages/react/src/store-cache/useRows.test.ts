bdb087d5cfe0d4fa7312769cfb0c21eceaf046b6
alvrs <alvarius@lattice.xyz>
2023-05-16 18:21:01 +0200

    add useRows hook

---
+++ packages/react/src/store-cache/useRows.test.ts
@@ -0,0 +1,122 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useRows } from "./useRows";
+import { mudConfig } from "@latticexyz/store/register";
+import { KeyValue, createDatabase, createDatabaseClient } from "@latticexyz/store-cache";
+import { describe, it, beforeEach, expect } from "vitest";
+
+const config = mudConfig({
+  tables: {
+    MultiKey: { primaryKeys: { first: "bytes32", second: "uint32" }, schema: "int32" },
+    Position: { schema: { x: "int32", y: "int32" } },
+  },
+});
+
+describe("useRows", () => {
+  let db: ReturnType<typeof createDatabase>;
+  let client: ReturnType<typeof createDatabaseClient<typeof config>>;
+
+  beforeEach(() => {
+    db = createDatabase();
+    client = createDatabaseClient(db, config);
+  });
+
+  it("should return all rows of the position table", () => {
+    const { result } = renderHook(() => useRows(client, { namespace: config["namespace"], table: "Position" }));
+
+    const positionUpdates: KeyValue<typeof config, "Position">[] = [
+      { key: { key: "0x00" }, value: { x: 1, y: 2 } },
+      { key: { key: "0x01" }, value: { x: 2, y: 3 } },
+      { key: { key: "0x02" }, value: { x: 3, y: 4 } },
+      { key: { key: "0x03" }, value: { x: 4, y: 5 } },
+    ];
+
+    const multiKeyUpdates: KeyValue<typeof config, "MultiKey">[] = [
+      { key: { first: "0x00", second: 4 }, value: { value: 1 } },
+      { key: { first: "0x01", second: 3 }, value: { value: 2 } },
+      { key: { first: "0x02", second: 2 }, value: { value: 3 } },
+      { key: { first: "0x03", second: 1 }, value: { value: 4 } },
+    ];
+
+    act(() => {
+      // Set values in the tables
+      for (const update of positionUpdates) client.tables.Position.set(update.key, update.value);
+      for (const update of multiKeyUpdates) client.tables.MultiKey.set(update.key, update.value);
+    });
+
+    expect(result.current.length).toBe(positionUpdates.length);
+    expect(result.current).toEqual([
+      ...positionUpdates.map((row) => ({ ...row, namespace: config["namespace"], table: "Position" })),
+    ]);
+
+    act(() => {
+      for (const update of positionUpdates.slice(0, 3)) client.tables.Position.remove(update.key);
+    });
+
+    expect(result.current.length).toBe(1);
+    expect(result.current).toEqual([
+      { key: { key: "0x03" }, value: { x: 4, y: 5 }, namespace: config["namespace"], table: "Position" },
+    ]);
+  });
+
+  // it("should return Position value for entity", () => {
+  //   const entity = createEntity(world, [withValue(Position, { x: 1, y: 1 })]);
+
+  //   const { result } = renderHook(() => useComponentValue(Position, entity));
+  //   expect(result.current).toEqual({ x: 1, y: 1 });
+
+  //   act(() => {
+  //     setComponent(Position, entity, { x: 0, y: 0 });
+  //   });
+  //   expect(result.current).toEqual({ x: 0, y: 0 });
+
+  //   act(() => {
+  //     removeComponent(Position, entity);
+  //   });
+  //   expect(result.current).toBe(undefined);
+  // });
+
+  // it("should re-render only when Position changes for entity", () => {
+  //   const entity = createEntity(world, [withValue(Position, { x: 1, y: 1 })]);
+  //   const otherEntity = createEntity(world, [withValue(Position, { x: 2, y: 2 })]);
+
+  //   const { result } = renderHook(() => useComponentValue(Position, entity));
+  //   expect(result.all.length).toBe(2);
+  //   expect(result.current).toEqual({ x: 1, y: 1 });
+
+  //   act(() => {
+  //     setComponent(Position, entity, { x: 0, y: 0 });
+  //   });
+  //   expect(result.all.length).toBe(3);
+  //   expect(result.current).toEqual({ x: 0, y: 0 });
+
+  //   act(() => {
+  //     setComponent(Position, otherEntity, { x: 0, y: 0 });
+  //     removeComponent(Position, otherEntity);
+  //   });
+  //   expect(result.all.length).toBe(3);
+  //   expect(result.current).toEqual({ x: 0, y: 0 });
+
+  //   act(() => {
+  //     removeComponent(Position, entity);
+  //   });
+  //   expect(result.all.length).toBe(4);
+  //   expect(result.current).toBe(undefined);
+  // });
+
+  // it("should return default value when Position is not set", () => {
+  //   const entity = createEntity(world);
+
+  //   const { result } = renderHook(() => useComponentValue(Position, entity, { x: -1, y: -1 }));
+  //   expect(result.current).toEqual({ x: -1, y: -1 });
+
+  //   act(() => {
+  //     setComponent(Position, entity, { x: 0, y: 0 });
+  //   });
+  //   expect(result.current).toEqual({ x: 0, y: 0 });
+
+  //   act(() => {
+  //     removeComponent(Position, entity);
+  //   });
+  //   expect(result.current).toEqual({ x: -1, y: -1 });
+  // });
+});
---
+++ packages/react/src/store-cache/useRows.ts
@@ -0,0 +1,24 @@
+import { DatabaseClient, FilterOptions, ScanResult } from "@latticexyz/store-cache";
+import { StoreConfig } from "@latticexyz/store";
+import { useEffect, useState } from "react";
+
+export function useRows<C extends StoreConfig, T extends keyof C["tables"] & string>(
+  storeCache: DatabaseClient<C>,
+  filter?: FilterOptions<C, T>
+) {
+  const [rows, setRows] = useState<ScanResult<C, T>>([]);
+
+  useEffect(() => {
+    setRows(storeCache.scan(filter));
+
+    const unsubscribe = storeCache.subscribe(() => {
+      // very naive implementation for now, but easier and probably more efficient than
+      // manually looping through the `rows` array for every update event
+      setRows(storeCache.scan(filter));
+    }, filter);
+
+    return unsubscribe;
+  }, []);
+
+  return rows;
+}
