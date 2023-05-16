82b08f3e8b1f39127cc877a3418a0bf1319d8a93
Andy Cernera <kooshaba@gmail.com>
2023-05-16 17:42:44 -0400

    fix: import mudConfig from world and import snapSync plugin separately

--- examples/minimal/packages/contracts/mud.config.ts
+++ examples/minimal/packages/contracts/mud.config.ts
@@ -1,4 +1,13 @@
-import { mudConfig } from "@latticexyz/world/snapSync";
+import { mudConfig } from "@latticexyz/world/register";
+
+/**
+ * Importing this enables "snap sync mode".
+ * It allows clients to sync the latest state of the world using view functions.
+ * This is a simple way to quickly sync without the use of an external indexer.
+ * This could lead to expensive queries on live RPCs if the world is large,
+ * so we suggest using MODE for production deployments.
+ */
+import "@latticexyz/world/snapSync";
 import { resolveTableId } from "@latticexyz/config";
 
 export default mudConfig({
