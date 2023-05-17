8a8cd9224ba26d9e09784eba95eb22be0c856728
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-17 10:03:28 -0700

    update vanilla template

---
+++ templates/vanilla/packages/client/src/mud/supportedChains.ts
@@ -0,0 +1,4 @@
+import { MUDChain, foundry, latticeTestnet } from "@latticexyz/common/chains";
+
+// If you are deploying to chains other than anvil or Lattice testnet, add them here
+export const supportedChains: MUDChain[] = [foundry, latticeTestnet];
