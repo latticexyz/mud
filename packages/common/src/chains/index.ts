f4ea0ef8b8c82fdc7d3cbb413cac078eec8f9b11
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-17 10:08:52 -0700

    don't re-export wagmi chains

--- packages/common/src/chains/index.ts
+++ packages/common/src/chains/index.ts
@@ -1,5 +1,2 @@
-// re-export wagmi chains to make them easier to reach
-export * from "@wagmi/chains";
-
 export type { MUDChain } from "./types";
 export { latticeTestnet } from "./latticeTestnet";
--- templates/vanilla/packages/client/src/mud/supportedChains.ts
+++ templates/vanilla/packages/client/src/mud/supportedChains.ts
@@ -1,4 +1,5 @@
-import { MUDChain, foundry, latticeTestnet } from "@latticexyz/common/chains";
+import { MUDChain, latticeTestnet } from "@latticexyz/common/chains";
+import { foundry } from "@wagmi/chains";
 
 // If you are deploying to chains other than anvil or Lattice testnet, add them here
 export const supportedChains: MUDChain[] = [foundry, latticeTestnet];
