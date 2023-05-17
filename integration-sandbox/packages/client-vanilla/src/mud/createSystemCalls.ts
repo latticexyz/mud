f7d1cb5adda0933472c9a84a071dff395f63f053
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-17 10:22:39 -0700

    comment out unused stuff in integration for now

--- integration-sandbox/packages/client-vanilla/src/mud/createSystemCalls.ts
+++ integration-sandbox/packages/client-vanilla/src/mud/createSystemCalls.ts
@@ -5,17 +5,14 @@ import { SetupNetworkResult } from "./setupNetwork";
 
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
-export function createSystemCalls(
-  { worldSend, txReduced$, singletonEntity }: SetupNetworkResult,
-  { Counter }: ClientComponents
-) {
-  const increment = async () => {
-    const tx = await worldSend("increment", []);
-    await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
-    return getComponentValue(Counter, singletonEntity);
-  };
+export function createSystemCalls(network: SetupNetworkResult, components: ClientComponents) {
+  // const increment = async () => {
+  //   const tx = await worldSend("increment", []);
+  //   await awaitStreamValue(txReduced$, (txHash) => txHash === tx.hash);
+  //   return getComponentValue(Counter, singletonEntity);
+  // };
 
   return {
-    increment,
+    // increment,
   };
 }
