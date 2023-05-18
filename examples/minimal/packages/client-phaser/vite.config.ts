c4cf9c5f8ed40a74f0be96738c6e18f46a504eb4
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-18 16:27:12 -0700

    feat(create-mud): enable sourcemaps

--- examples/minimal/packages/client-phaser/vite.config.ts
+++ examples/minimal/packages/client-phaser/vite.config.ts
@@ -9,20 +9,9 @@ export default defineConfig({
       strict: false,
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "es2022",
-    },
-  },
   build: {
-    rollupOptions: {
-      // TODO: revisit this config after splitting out mud config dependencies
-      // from the cli package so we don't need to bundle the cli package
-      external: ["chalk", "locate-path", "path-exists", "find-up"],
-    },
     target: "es2022",
-  },
-  define: {
-    "process.env": {},
+    minify: true,
+    sourcemap: true,
   },
 });
