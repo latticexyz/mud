4057ba54a55d9e07fe5417e6428655a5e5c86dd2
Fraser Scott <fraserdscott@gmail.com>
2023-05-19 14:05:24 +0100

    feat: add three.js to template, keyboard movement

--- templates/three/packages/client/src/App.tsx
+++ templates/three/packages/client/src/App.tsx
@@ -1,29 +1,79 @@
-import { useEntityQuery } from "@latticexyz/react";
+import React from "react";
+import { Canvas, Color, ThreeElements, useThree } from "@react-three/fiber";
+import { useComponentValue, useEntityQuery } from "@latticexyz/react";
+import { getComponentValueStrict, Has } from "@latticexyz/recs";
 import { useMUD } from "./MUDContext";
-import { Has } from "@latticexyz/recs";
+import { useKeyboardMovement } from "./useKeyboardMovement";
 
-export const App = () => {
+const Plane = (props: ThreeElements["mesh"]) => {
+  return (
+    <mesh {...props}>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <boxGeometry args={[10, 5, 10]} />
+      <meshStandardMaterial color="green" />
+    </mesh>
+  );
+};
+
+const Player = (props: ThreeElements["mesh"] & { color: Color }) => {
+  return (
+    <mesh {...props}>
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <boxGeometry args={[1, 2, 1]} />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  );
+};
+
+const Scene = () => {
   const {
     components: { Position },
-    systemCalls: { move },
+    network: { playerEntity },
   } = useMUD();
 
-  const positions = useEntityQuery([Has(Position)]);
+  useKeyboardMovement();
+
+  const playerPosition = useComponentValue(Position, playerEntity);
+  const players = useEntityQuery([Has(Position)]).map((entity) => {
+    const position = getComponentValueStrict(Position, entity);
+    return {
+      entity,
+      position,
+    };
+  });
+
+  useThree(({ camera }) => {
+    if (playerPosition) {
+      camera.position.set(playerPosition.x - 5, playerPosition.y + 5, playerPosition.z + 5);
+    } else {
+      camera.position.set(-5, 5, 5);
+    }
+    camera.rotation.order = "YXZ";
+    camera.rotation.y = -Math.PI / 4;
+    camera.rotation.x = Math.atan(-1 / Math.sqrt(2));
+  });
 
   return (
-    <>
-      <div>
-        Number of positions: <span>{positions?.length ?? "??"}</span>
-      </div>
-      <button
-        type="button"
-        onClick={async (event) => {
-          event.preventDefault();
-          await move(0, 0, 1);
-        }}
-      >
-        Move
-      </button>
-    </>
+    <group>
+      <ambientLight />
+      {/* eslint-disable-next-line react/no-unknown-property */}
+      <pointLight position={[10, 10, 10]} />
+      <Plane position={[0, -5, 0]} />
+      {players.map((p, i) => (
+        <Player
+          key={i}
+          color={Math.floor(parseInt(p.entity) * 123456) % 16777215}
+          position={[p.position.x, p.position.y, p.position.z]}
+        />
+      ))}
+    </group>
+  );
+};
+
+export const App = () => {
+  return (
+    <Canvas style={{ height: "100vh" }}>
+      <Scene />
+    </Canvas>
   );
 };
