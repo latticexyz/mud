3a8599753af98cab7fd958293d21d39e8c274a81
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-19 20:55:35 -0700

    collapse lines

--- docs/components/CollapseCode.tsx
+++ docs/components/CollapseCode.tsx
@@ -4,52 +4,35 @@ type Props = {
   children: ReactNode;
 };
 
-const css = `
-.CollapseCode .line:not(highlighted,.near-highlighted) {
-  opacity: 0;
-  position: absolute;
-}
-.CollapseCode .line:not(.highlighted,.near-highlighted) + .line.near-highlighted {
-  margin-top: 1em;
-}
-`;
-
 export function CollapseCode({ children }: Props) {
   const ref = useRef<HTMLDivElement>();
+
   useEffect(() => {
     if (!ref.current) return;
     const lines = Array.from(ref.current.querySelectorAll(".line"));
     const highlightedPositions = [];
-    const nearHighlightedPositions = [];
     lines.forEach((line, i) => {
       if (line.matches(".highlighted")) {
         highlightedPositions.push(i);
       }
     });
-    highlightedPositions.forEach((position) => {
-      const near = [position - 2, position - 1, position + 1, position + 2];
-      near.forEach((n) => {
-        if (n < 0) return;
-        if (n >= lines.length) return;
-        if (highlightedPositions.includes(n)) return;
-        if (nearHighlightedPositions.includes(n)) return;
-        nearHighlightedPositions.push(n);
-      });
-    });
-    console.log("highlightedPositions", highlightedPositions);
-    console.log("nearHighlightedPositions", nearHighlightedPositions);
+    if (!highlightedPositions.length) return;
     lines.forEach((line, i) => {
-      if (nearHighlightedPositions.includes(i)) {
-        line.classList.add("near-highlighted");
+      const distance = highlightedPositions.reduce((min, pos) => Math.min(min, Math.abs(pos - i)), Infinity);
+      if (distance >= 4) {
+        line.style.position = "absolute";
+        line.style.opacity = "0";
+        line.style.pointerEvents = "none";
+      } else if (distance === 3) {
+        line.style.opacity = "0.3";
+      } else if (distance === 2) {
+        line.style.opacity = "0.6";
       }
     });
   }, []);
   return (
-    <>
-      <div ref={ref} style={{ marginTop: "1.5rem" }} className="CollapseCode">
-        {children}
-      </div>
-      <style>{css}</style>
-    </>
+    <div ref={ref} style={{ marginTop: "1.5rem" }}>
+      {children}
+    </div>
   );
 }
