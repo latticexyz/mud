e1e4536f441eaeb266d2ca11c939ff9bf38dc20d
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-22 15:53:19 -0700

    use toHex instead

--- packages/network/src/v2/keyTupleToEntityID.spec.ts
+++ packages/network/src/v2/keyTupleToEntityID.spec.ts
@@ -4,9 +4,14 @@ describe("keyTupleToEntityID", () => {
   it("should use singleton key for empty key tuple", () => {
     expect(keyTupleToEntityID([])).toBe("0x000000000000000000000000000000000000000000000000000000000000060d");
   });
+
   it("should pad addresses", () => {
     expect(keyTupleToEntityID(["0xc0035952298729c7086058EaA13921626e22C070"])).toBe(
       "0x000000000000000000000000c0035952298729c7086058EaA13921626e22C070"
     );
+  });
+
+  it("convert number key to padded entity ID", () => {
+    expect(keyTupleToEntityID([1])).toBe("0x0000000000000000000000000000000000000000000000000000000000000001");
   });
 });
--- packages/network/src/v2/keyTupleToEntityID.ts
+++ packages/network/src/v2/keyTupleToEntityID.ts
@@ -1,13 +1,13 @@
 import { Entity } from "@latticexyz/recs";
 import { SingletonID } from "../workers";
-import { Hex, pad } from "viem";
+import { toHex } from "viem";
 
 // TODO: revisit key tuple format?
 export function keyTupleToEntityID(keyTuple: any[]): Entity {
   // v2 uses an empty key tuple as the singleton ID, so we'll return the corresponding v1 singleton entity ID to normalize this for now
   if (keyTuple.length === 0) {
-    return pad(SingletonID as Hex, { size: 32 }) as Entity;
+    return toHex(SingletonID, { size: 32 }) as Entity;
   }
   // TODO: this should probably be padded based on key schema (uint vs bytes32 will have different leading/trailing zeroes)
-  return keyTuple.map((key) => pad(key, { size: 32 })).join(":") as Entity;
+  return keyTuple.map((key) => toHex(key, { size: 32 })).join(":") as Entity;
 }
