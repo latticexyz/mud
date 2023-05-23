d5db2764f607d3842953ef53b0d1995be5556b43
Fraser Scott <fraserdscott@gmail.com>
2023-05-23 20:59:40 +0100

    chore: build templates

--- templates/react/packages/contracts/src/codegen/tables/Counter.sol
+++ templates/react/packages/contracts/src/codegen/tables/Counter.sol
@@ -36,10 +36,12 @@ library Counter {
   }
 
   /** Get the table's metadata */
-  function getMetadata() internal pure returns (string memory, string[] memory) {
+  function getMetadata() internal pure returns (string memory, string[] memory, string[] memory) {
+    string[] memory _keyNames = new string[](0);
+
     string[] memory _fieldNames = new string[](1);
     _fieldNames[0] = "value";
-    return ("Counter", _fieldNames);
+    return ("Counter", _keyNames, _fieldNames);
   }
 
   /** Register the table's schema */
@@ -54,14 +56,14 @@ library Counter {
 
   /** Set the table's metadata */
   function setMetadata() internal {
-    (string memory _tableName, string[] memory _fieldNames) = getMetadata();
-    StoreSwitch.setMetadata(_tableId, _tableName, _fieldNames);
+    (string memory _tableName, string[] memory _keyNames, string[] memory _fieldNames) = getMetadata();
+    StoreSwitch.setMetadata(_tableId, _tableName, _keyNames, _fieldNames);
   }
 
   /** Set the table's metadata (using the specified store) */
   function setMetadata(IStore _store) internal {
-    (string memory _tableName, string[] memory _fieldNames) = getMetadata();
-    _store.setMetadata(_tableId, _tableName, _fieldNames);
+    (string memory _tableName, string[] memory _keyNames, string[] memory _fieldNames) = getMetadata();
+    _store.setMetadata(_tableId, _tableName, _keyNames, _fieldNames);
   }
 
   /** Get value */
