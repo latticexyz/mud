9a2246e28373e0e8c3b572013132d9e7b2d3143d
dk1a <dk1a@protonmail.com>
2023-05-30 21:54:16 +0300

    refactor(world): use dynamic fieldIndex in KeysInTableHook

---
+++ packages/world/src/modules/keysintable/KeysInTableDynamicFieldIndex.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { Bytes } from "@latticexyz/store/src/Bytes.sol";
+
+import { KeysInTable, KeysInTableTableId } from "./tables/KeysInTable.sol";
+
+/**
+ * Based on `KeysInTable`, but allows dynamically passing fieldIndex.
+ * Dynamic fieldIndex relies on all fields having the same schema type.
+ */
+library KeysInTableDynamicFieldIndex {
+  function pushKeys(uint8 fieldIndex, bytes32 sourceTable, bytes32 _element) internal {
+    bytes32[] memory _keyTuple = new bytes32[](1);
+    _keyTuple[0] = bytes32((sourceTable));
+
+    StoreSwitch.pushToField(KeysInTableTableId, _keyTuple, fieldIndex, abi.encodePacked((_element)));
+  }
+
+  function updateKeys(uint8 fieldIndex, bytes32 sourceTable, uint256 _index, bytes32 _element) internal {
+    bytes32[] memory _keyTuple = new bytes32[](1);
+    _keyTuple[0] = bytes32((sourceTable));
+
+    StoreSwitch.updateInField(KeysInTableTableId, _keyTuple, fieldIndex, _index * 32, abi.encodePacked((_element)));
+  }
+
+  function popKeys(uint8 fieldIndex, bytes32 sourceTable) internal {
+    bytes32[] memory _keyTuple = new bytes32[](1);
+    _keyTuple[0] = bytes32((sourceTable));
+
+    StoreSwitch.popFromField(KeysInTableTableId, _keyTuple, fieldIndex, 32);
+  }
+
+  function getItemKeys(uint8 fieldIndex, bytes32 sourceTable, uint256 _index) internal view returns (bytes32) {
+    bytes32[] memory _keyTuple = new bytes32[](1);
+    _keyTuple[0] = bytes32((sourceTable));
+
+    bytes memory _blob = StoreSwitch.getFieldSlice(
+      KeysInTableTableId,
+      _keyTuple,
+      fieldIndex,
+      KeysInTable.getSchema(),
+      _index * 32,
+      (_index + 1) * 32
+    );
+    return (Bytes.slice32(_blob, 0));
+  }
+}
--- packages/world/src/modules/keysintable/KeysInTableHook.sol
+++ packages/world/src/modules/keysintable/KeysInTableHook.sol
@@ -5,6 +5,7 @@ import { IStoreHook } from "@latticexyz/store/src/IStore.sol";
 
 import { KeysInTable } from "./tables/KeysInTable.sol";
 import { UsedKeysIndex } from "./tables/UsedKeysIndex.sol";
+import { KeysInTableDynamicFieldIndex } from "./KeysInTableDynamicFieldIndex.sol";
 
 /**
  * Note: if a table with composite keys is used, only the first key is indexed
@@ -18,20 +19,8 @@ contract KeysInTableHook is IStoreHook {
       uint40 length = uint40(KeysInTable.lengthKeys0(tableId));
 
       // Push the key to the list of keys in this table
-      if (key.length > 0) {
-        KeysInTable.pushKeys0(tableId, key[0]);
-        if (key.length > 1) {
-          KeysInTable.pushKeys1(tableId, key[1]);
-          if (key.length > 2) {
-            KeysInTable.pushKeys2(tableId, key[2]);
-            if (key.length > 3) {
-              KeysInTable.pushKeys3(tableId, key[3]);
-              if (key.length > 4) {
-                KeysInTable.pushKeys4(tableId, key[4]);
-              }
-            }
-          }
-        }
+      for (uint8 fieldIndex = 0; fieldIndex < key.length; fieldIndex++) {
+        KeysInTableDynamicFieldIndex.pushKeys(fieldIndex, tableId, key[fieldIndex]);
       }
 
       // Update the index to avoid duplicating this key in the array
@@ -64,56 +53,20 @@ contract KeysInTableHook is IStoreHook {
         // Delete the list of keys in this table
         KeysInTable.deleteRecord(tableId);
       } else {
-        if (key.length > 0) {
-          bytes32[] memory lastKeyTuple = new bytes32[](key.length);
+        bytes32[] memory lastKeyTuple = new bytes32[](key.length);
 
-          bytes32 lastKey = KeysInTable.getItemKeys0(tableId, length - 1);
-          lastKeyTuple[0] = lastKey;
+        for (uint8 fieldIndex = 0; fieldIndex < key.length; fieldIndex++) {
+          bytes32 lastKey = KeysInTableDynamicFieldIndex.getItemKeys(fieldIndex, tableId, length - 1);
+          lastKeyTuple[fieldIndex] = lastKey;
 
           // Remove the key from the list of keys in this table
-          KeysInTable.updateKeys0(tableId, index, lastKey);
-          KeysInTable.popKeys0(tableId);
-
-          if (key.length > 1) {
-            lastKey = KeysInTable.getItemKeys1(tableId, length - 1);
-            lastKeyTuple[1] = lastKey;
-
-            // Remove the key from the list of keys in this table
-            KeysInTable.updateKeys1(tableId, index, lastKey);
-            KeysInTable.popKeys1(tableId);
-
-            if (key.length > 2) {
-              lastKey = KeysInTable.getItemKeys2(tableId, length - 1);
-              lastKeyTuple[2] = lastKey;
-
-              // Swap and pop the key from the list of keys in this table
-              KeysInTable.updateKeys2(tableId, index, lastKey);
-              KeysInTable.popKeys2(tableId);
-
-              if (key.length > 3) {
-                lastKey = KeysInTable.getItemKeys3(tableId, length - 1);
-                lastKeyTuple[3] = lastKey;
-
-                // Remove the key from the list of keys in this table
-                KeysInTable.updateKeys3(tableId, index, lastKey);
-                KeysInTable.popKeys3(tableId);
-
-                if (key.length > 4) {
-                  lastKey = KeysInTable.getItemKeys4(tableId, length - 1);
-                  lastKeyTuple[4] = lastKey;
-
-                  // Remove the key from the list of keys in this table
-                  KeysInTable.updateKeys4(tableId, index, lastKey);
-                  KeysInTable.popKeys4(tableId);
-                }
-              }
-            }
-          }
-
-          // Update the index of lastKey after swapping it with the deleted key
-          bytes32 lastKeyHash = keccak256(abi.encode(lastKeyTuple));
-          UsedKeysIndex.setIndex(tableId, lastKeyHash, index);
+          KeysInTableDynamicFieldIndex.updateKeys(fieldIndex, tableId, index, lastKey);
+          KeysInTableDynamicFieldIndex.popKeys(fieldIndex, tableId);
         }
+
+        // Update the index of lastKey after swapping it with the deleted key
+        bytes32 lastKeyHash = keccak256(abi.encode(lastKeyTuple));
+        UsedKeysIndex.setIndex(tableId, lastKeyHash, index);
       }
     }
   }
