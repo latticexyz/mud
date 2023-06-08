2cecdd35208044661a28fd6f9537abf33ec51533
dk1a <dk1a@protonmail.com>
2023-06-08 16:14:39 +0300

    Update packages/cli/src/commands/worldgen.ts
    Co-authored-by: alvarius <89248902+alvrs@users.noreply.github.com>

--- packages/cli/src/commands/worldgen.ts
+++ packages/cli/src/commands/worldgen.ts
@@ -6,7 +6,7 @@ import { worldgen } from "@latticexyz/world/node";
 import { getSrcDirectory } from "@latticexyz/common/foundry";
 import path from "path";
 import { rmSync } from "fs";
-import { getExistingContracts } from "../utils/getExistingContracts";
+import { getExistingContracts } from "../utils";
 
 type Options = {
   configPath?: string;
