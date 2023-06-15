926bf0a77ff82cce8d1e493004f4aab2c5c67c37
Kevin Ingersoll <kingersoll@gmail.com>
2023-06-14 21:48:51 -0700

    use events from store package

--- examples/minimal/packages/client-react/src/mud/setupViemNetwork.ts
+++ examples/minimal/packages/client-react/src/mud/setupViemNetwork.ts
@@ -1,17 +1,7 @@
 import { getNetworkConfig } from "./getNetworkConfig";
 import { createBlockEventsStream } from "@latticexyz/block-events-stream";
-// import { storeEventsAbi } from "@latticexyz/store";
+import { storeEventsAbi } from "@latticexyz/store";
 import { createPublicClient, http, fallback, webSocket } from "viem";
-import { parseAbi } from "viem/abi";
-
-// TODO: import from store once we move aside the solidity codegen stuff that breaks browser bundles
-export const storeEvents = [
-  "event StoreDeleteRecord(bytes32 table, bytes32[] key)",
-  "event StoreSetField(bytes32 table, bytes32[] key, uint8 schemaIndex, bytes data)",
-  "event StoreSetRecord(bytes32 table, bytes32[] key, bytes data)",
-  "event StoreEphemeralRecord(bytes32 table, bytes32[] key, bytes data)",
-] as const;
-export const storeEventsAbi = parseAbi(storeEvents);
 
 export async function setupViemNetwork() {
   const { chain } = await getNetworkConfig();
