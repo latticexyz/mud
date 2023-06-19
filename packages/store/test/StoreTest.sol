971afaec7627226ac34bacf9774f4324394e7dbf
Kevin Ingersoll <kingersoll@gmail.com>
2023-06-19 11:28:09 +0100

    flesh it out a bit more

--- packages/store/test/StoreTest.sol
+++ packages/store/test/StoreTest.sol
@@ -6,24 +6,48 @@ import { Test, console } from "forge-std/Test.sol";
 contract StoreTest is Test {
   string private __currentGasReportName;
   uint256 private __currentGasReportValue = gasleft();
+  mapping(string => uint256) private __gasReports;
+  string[] private __gasReportNames;
 
   function startGasReport(string memory name) internal {
+    require(
+      bytes(__currentGasReportName).length == 0,
+      string.concat(
+        'gas report "',
+        __currentGasReportName,
+        '" is already running and only one report can be run at a time'
+      )
+    );
+    require(__gasReports[name] == 0, string.concat('gas report name "', name, '" already used'));
     __currentGasReportName = name;
     vm.pauseGasMetering();
     __currentGasReportValue = gasleft();
     vm.resumeGasMetering();
   }
 
-  function endGasReport() internal view {
+  function endGasReport() internal {
     uint256 gas = gasleft();
-    uint256 gasUsed = __currentGasReportValue - gasleft();
-    require(gasUsed > 0, "gas-report: gas used can't have a negative value, did you forget to call startGasReport?");
-    console.log(string.concat("gas-report(", __currentGasReportName, "):", vm.toString(gasUsed)));
+    uint256 gasUsed = __currentGasReportValue - gas;
+    require(gasUsed > 0, "no or negative gas used, did you forget to call startGasReport?");
+    __gasReports[__currentGasReportName] = gasUsed;
+    __gasReportNames.push(__currentGasReportName);
+    printGasReport(__currentGasReportName);
+    __currentGasReportName = "";
   }
 
   modifier gasReport(string memory name) {
     startGasReport(name);
     _;
     endGasReport();
+  }
+
+  function printGasReport(string memory name) internal view {
+    console.log(string.concat("GAS REPORT: ", vm.toString(__gasReports[name]), " ", name));
+  }
+
+  function printGasReports() internal view {
+    for (uint256 i = 0; i < __gasReportNames.length; i++) {
+      printGasReport(__gasReportNames[i]);
+    }
   }
 }
