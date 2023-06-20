caffe5369b45a1c1e1f1c3535fd3140175eb1b89
dk1a <dk1a@protonmail.com>
2023-06-20 21:53:45 +0300

    add storeAddress to MudV2Test

--- packages/std-contracts/src/test/MudV2Test.t.sol
+++ packages/std-contracts/src/test/MudV2Test.t.sol
@@ -2,11 +2,16 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
+import { IStoreConsumer } from "@latticexyz/store/src/StoreConsumer.sol";
 
-contract MudV2Test is Test {
+contract MudV2Test is Test, IStoreConsumer {
   address worldAddress;
 
   function setUp() public virtual {
     worldAddress = vm.parseAddress(vm.readFile(".mudtest"));
+  }
+
+  function storeAddress(address) public view returns (address) {
+    return worldAddress;
   }
 }
