e1eb684c3f3eaf72fa53b40990f78a4cbb071a07
alvrs <alvarius@lattice.xyz>
2023-06-23 12:58:48 +0100

    add test cases

--- e2e/packages/sync-test/data/testData.ts
+++ e2e/packages/sync-test/data/testData.ts
@@ -14,5 +14,8 @@ export const testData2 = {
     { key: { key: 1 }, value: { value: 24 } },
     { key: { key: 2 }, value: { value: 1337 } },
   ],
-  Vector: [{ key: { key: 1 }, value: { x: 1, y: 42 } }],
+  Vector: [
+    { key: { key: 1 }, value: { x: 1, y: 42 } },
+    { key: { key: 32 }, value: { x: 1, y: -42 } },
+  ],
 } satisfies Data;
