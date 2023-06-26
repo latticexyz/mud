a8c6836e7f5d331a7014984f726bf457883e9108
Kevin Ingersoll <kingersoll@gmail.com>
2023-06-26 11:48:59 +0100

    revert unused files

--- packages/schema-type/src/typescript/index.ts
+++ packages/schema-type/src/typescript/index.ts
@@ -1,6 +1,5 @@
 export * from "./dynamicAbiTypes";
 export * from "./schemaAbiTypes";
 export * from "./schemaAbiTypeToDefaultValue";
-export * from "./schemaAbiTypeToPrimitiveType";
-export * from "./schemaPrimitiveTypes";
 export * from "./staticAbiTypes";
+export * from "./schemaAbiTypeToPrimitiveType";
--- packages/schema-type/src/typescript/schemaPrimitiveTypes.ts
+++
@@ -1,5 +0,0 @@
-import { schemaAbiTypeToDefaultValue } from "./schemaAbiTypeToDefaultValue";
-import { SchemaAbiType } from "./schemaAbiTypes";
-import { LiteralToBroad } from "./utils";
-
-export type SchemaPrimitiveType = LiteralToBroad<(typeof schemaAbiTypeToDefaultValue)[SchemaAbiType]>;
