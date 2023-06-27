f408fb9856835265c4f7ff7811c11a7ff2a8809e
dk1a <dk1a@protonmail.com>
2023-06-27 20:15:27 +0300

    no longer build mud.config.ts in store,world

--- packages/world/tsup.config.ts
+++ packages/world/tsup.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "mud.config.ts",
-    ".mud/expandedConfig.ts",
-    "ts/library/index.ts",
-    "ts/node/index.ts",
-    "ts/plugins/snapsync/index.ts",
-  ],
+  entry: [".mud/expandedConfig.ts", "ts/library/index.ts", "ts/node/index.ts", "ts/plugins/snapsync/index.ts"],
   target: "esnext",
   format: ["esm"],
   dts: false,
