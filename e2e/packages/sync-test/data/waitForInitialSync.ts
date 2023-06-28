accc062d1ebf9ec3b222e51210503c45d960c06e
alvrs <alvarius@lattice.xyz>
2023-06-28 22:20:14 +0100

    wait after initial load

--- e2e/packages/sync-test/data/waitForInitialSync.ts
+++ e2e/packages/sync-test/data/waitForInitialSync.ts
@@ -1,7 +1,9 @@
 import { SyncState } from "@latticexyz/network";
 import { Page, expect } from "@playwright/test";
+import { sleep } from "@latticexyz/utils";
 
 export async function waitForInitialSync(page: Page) {
   const syncState = page.locator("#sync-state");
   await expect(syncState).toHaveText(String(SyncState.LIVE));
+  await sleep(10000);
 }
