a425d2d56941b147af01482a9bbd79e5f654c7c0
alvrs <alvarius@lattice.xyz>
2023-07-03 18:34:07 +0100

    early return if no dynamic fields

--- packages/protocol-parser/src/encodeRecord.ts
+++ packages/protocol-parser/src/encodeRecord.ts
@@ -11,6 +11,8 @@ export function encodeRecord(schema: Schema, values: readonly (StaticPrimitiveTy
     .map((value, i) => encodeField(schema.staticFields[i], value).replace(/^0x/, ""))
     .join("");
 
+  if (schema.dynamicFields.length === 0) return `0x${staticData}`;
+
   const dynamicDataItems = dynamicValues.map((value, i) =>
     encodeField(schema.dynamicFields[i], value).replace(/^0x/, "")
   );
@@ -20,12 +22,9 @@ export function encodeRecord(schema: Schema, values: readonly (StaticPrimitiveTy
 
   const dynamicData = dynamicDataItems.join("");
 
-  const packedCounter =
-    schema.dynamicFields.length > 0
-      ? `${encodeField("uint56", dynamicTotalByteLength).replace(/^0x/, "")}${dynamicFieldByteLengths
-          .map((length) => encodeField("uint40", length).replace(/^0x/, ""))
-          .join("")}`.padEnd(64, "0")
-      : "";
+  const packedCounter = `${encodeField("uint56", dynamicTotalByteLength).replace(/^0x/, "")}${dynamicFieldByteLengths
+    .map((length) => encodeField("uint40", length).replace(/^0x/, ""))
+    .join("")}`.padEnd(64, "0");
 
   return `0x${staticData}${packedCounter}${dynamicData}`;
 }
