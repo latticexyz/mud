92e35d06c10e7d01641582023a6cd9056a2e39bd
alvrs <alvarius@lattice.xyz>
2023-07-03 17:05:27 +0100

    add test for encodeRecord with dynamic data

--- packages/protocol-parser/src/encodeRecord.test.ts
+++ packages/protocol-parser/src/encodeRecord.test.ts
@@ -9,4 +9,10 @@ describe("encodeRecord", () => {
       "0x0000000100000000000000000000000000000002000000000000130000000008000000000b0000000000000000000000000000000000000300000004736f6d6520737472696e67"
     );
   });
+
+  it("should not include the packed dynamic lengths if there are no dynamic fields", () => {
+    const schema = { staticFields: ["uint32", "uint128"], dynamicFields: [] } as const;
+    const hex = encodeRecord(schema, [1, 2n]);
+    expect(hex).toBe("0x0000000100000000000000000000000000000002");
+  });
 });
