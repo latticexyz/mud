339abfc7c905773b5d19a56692a605c938843391
alvrs <alvarius@lattice.xyz>
2023-07-03 17:01:18 +0100

    remove sleep

--- e2e/packages/sync-test/data/waitForInitialSync.ts
+++ e2e/packages/sync-test/data/waitForInitialSync.ts
@@ -1,9 +1,7 @@
 import { SyncState } from "@latticexyz/network";
 import { Page, expect } from "@playwright/test";
-import { sleep } from "@latticexyz/utils";
 
 export async function waitForInitialSync(page: Page) {
   const syncState = page.locator("#sync-state");
   await expect(syncState).toHaveText(String(SyncState.LIVE));
-  await sleep(10000);
 }
