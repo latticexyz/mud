d1e90c251c87f0645a353acee080367f5a4b4713
Kevin Ingersoll <kingersoll@gmail.com>
2023-07-10 16:37:50 +0100

    clean up

--- packages/store-indexer/src/kysely.test.ts
+++ packages/store-indexer/src/kysely.test.ts
@@ -1,11 +1,6 @@
 import { beforeEach, describe, it, expect } from "vitest";
 import initSqlJs from "sql.js";
-import { drizzle, SQLJsDatabase } from "drizzle-orm/sql-js";
-import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core";
-import { DefaultLogger, sql } from "drizzle-orm";
 import { Kysely, SqliteDialect, SqliteDatabase, SqliteStatement } from "kysely";
-import sqlite from "better-sqlite3";
-import { fromBytes } from "viem";
 
 type SchemasTable = {
   tableNamespace: string;
@@ -26,50 +21,52 @@ describe("sqlite", () => {
   let db: Kysely<Database>;
 
   beforeEach(async () => {
-    const sql = await initSqlJs();
-    const sqlDb = new sql.Database();
-
     db = new Kysely<Database>({
       dialect: new SqliteDialect({
-        database: {
-          close: (): void => sqlDb.close(),
-          prepare: (sql): SqliteStatement => {
-            if (/;/.test(sql)) {
-              // Not sure what to do with `reader` for multiple queries, so reject
-              throw new Error("Only one query supported");
-            }
+        database: async (): Promise<SqliteDatabase> => {
+          const sql = await initSqlJs();
+          const sqlDb = new sql.Database();
 
-            return {
-              // TODO: better way to detect if this is a select statement
-              reader: /select\s/i.test(sql),
-              all: (params) => {
-                const results = sqlDb.exec(sql, params as any[]);
-                if (results.length > 1) {
-                  // Not sure what to do with `reader` for multiple queries, so reject
-                  throw new Error("Only one query supported");
-                }
+          return {
+            close: (): void => sqlDb.close(),
+            prepare: (sql): SqliteStatement => {
+              if (/;/.test(sql)) {
+                // Not sure what to do with `reader` for multiple queries, so reject
+                throw new Error("Only one query supported");
+              }
 
-                const { columns, values: rows } = results[0];
-                return rows.map((values) => Object.fromEntries(values.map((value, i) => [columns[i], value])));
-              },
-              run: (params) => {
-                // console.log(fromBytes(sqlDb.export(), "string"));
-                const results = sqlDb.exec(sql, params as any[]);
-                if (results.length > 1) {
-                  // Not sure what to do with `reader` for multiple queries, so reject
-                  throw new Error("Only one query supported");
-                }
+              return {
+                // TODO: better way to detect if this is a select statement
+                reader: /select\s/i.test(sql),
+                all: (params) => {
+                  const results = sqlDb.exec(sql, params as any[]);
+                  if (results.length > 1) {
+                    // Not sure what to do with `reader` for multiple queries, so reject
+                    throw new Error("Only one query supported");
+                  }
 
-                // console.log("run results", JSON.stringify(results));
-                // console.log(fromBytes(sqlDb.export(), "string"));
-                return {
-                  changes: sqlDb.getRowsModified(),
-                  // TODO: figure out how to return the last insert id
-                  lastInsertRowid: 0,
-                };
-              },
-            } as SqliteStatement;
-          },
+                  const { columns, values: rows } = results[0];
+                  return rows.map((values) => Object.fromEntries(values.map((value, i) => [columns[i], value])));
+                },
+                run: (params) => {
+                  // console.log(fromBytes(sqlDb.export(), "string"));
+                  const results = sqlDb.exec(sql, params as any[]);
+                  if (results.length > 1) {
+                    // Not sure what to do with `reader` for multiple queries, so reject
+                    throw new Error("Only one query supported");
+                  }
+
+                  // console.log("run results", JSON.stringify(results));
+                  // console.log(fromBytes(sqlDb.export(), "string"));
+                  return {
+                    changes: sqlDb.getRowsModified(),
+                    // TODO: figure out how to return the last insert id
+                    lastInsertRowid: 0,
+                  };
+                },
+              } as SqliteStatement;
+            },
+          };
         },
       }),
     });
