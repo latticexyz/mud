8513609afef3ed8f1f1355dd34de94bd56234725
dk1a <dk1a@protonmail.com>
2023-07-13 17:28:07 +0300

    build(schema-type): add gas report

--- packages/schema-type/test/solidity/SchemaType.t.sol
+++ packages/schema-type/test/solidity/SchemaType.t.sol
@@ -3,9 +3,10 @@ pragma solidity >=0.8.0;
 
 import { Test } from "forge-std/Test.sol";
 import { stdError } from "forge-std/StdError.sol";
+import { GasReporter } from "@latticexyz/gas-report/src/GasReporter.sol";
 import { SchemaType } from "../../src/solidity/SchemaType.sol";
 
-contract SchemaTypeTest is Test {
+contract SchemaTypeTest is Test, GasReporter {
   uint256 internal constant SCHEMA_TYPE_LENGTH = 198;
 
   function testGetStaticByteLength() public {
@@ -52,5 +53,67 @@ contract SchemaTypeTest is Test {
   function testSchemaTypeOverMaxLength() public {
     vm.expectRevert(stdError.enumConversionError);
     SchemaType(SCHEMA_TYPE_LENGTH).getStaticByteLength();
+  }
+
+  function testGas() public {
+    startGasReport("getStaticByteLength: uint8");
+    SchemaType.UINT8.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: uint32");
+    SchemaType.UINT32.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: uint256");
+    SchemaType.UINT256.getStaticByteLength();
+    endGasReport();
+
+    startGasReport("getStaticByteLength: int8");
+    SchemaType.INT8.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: int32");
+    SchemaType.INT32.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: int256");
+    SchemaType.INT256.getStaticByteLength();
+    endGasReport();
+
+    startGasReport("getStaticByteLength: bytes1");
+    SchemaType.BYTES1.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: bytes4");
+    SchemaType.BYTES4.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: bytes32");
+    SchemaType.BYTES32.getStaticByteLength();
+    endGasReport();
+
+    startGasReport("getStaticByteLength: bool");
+    SchemaType.BOOL.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: address");
+    SchemaType.ADDRESS.getStaticByteLength();
+    endGasReport();
+
+    startGasReport("getStaticByteLength: uint8[]");
+    SchemaType.UINT8_ARRAY.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: uint32[]");
+    SchemaType.UINT32_ARRAY.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: uint256[]");
+    SchemaType.UINT256_ARRAY.getStaticByteLength();
+    endGasReport();
+
+    startGasReport("getStaticByteLength: int256[]");
+    SchemaType.INT256_ARRAY.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: bytes32[]");
+    SchemaType.BYTES32_ARRAY.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: bytes");
+    SchemaType.BYTES.getStaticByteLength();
+    endGasReport();
+    startGasReport("getStaticByteLength: string");
+    SchemaType.STRING.getStaticByteLength();
+    endGasReport();
   }
 }
