ed9cdbe66328799cc6d41b9fe68074671c44e458
Kevin Ingersoll <kingersoll@gmail.com>
2023-07-17 13:58:51 +0100

    snapshot for calls

--- packages/store-sync/src/blockLogsToStorage.test.ts
+++ packages/store-sync/src/blockLogsToStorage.test.ts
@@ -77,8 +77,61 @@ describe("blockLogsToStorage", () => {
       ],
     });
 
-    expect(mockedCallbacks.storeOperations).toHaveBeenCalledTimes(1);
-    // TODO: figure out if we can inline snapshot what storeOperations was called with?
+    expect(mockedCallbacks.storeOperations).toMatchInlineSnapshot(`
+      [MockFunction spy] {
+        "calls": [
+          [
+            {
+              "blockNumber": 5448n,
+              "operations": [
+                {
+                  "keyTuple": {
+                    "item": 1,
+                    "itemVariant": 1,
+                    "owner": "0x796eb990A3F9C431C69149c7a168b91596D87F60",
+                  },
+                  "log": {
+                    "address": "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+                    "args": {
+                      "data": "0x00000008",
+                      "key": [
+                        "0x000000000000000000000000796eb990a3f9c431c69149c7a168b91596d87f60",
+                        "0x0000000000000000000000000000000000000000000000000000000000000001",
+                        "0x0000000000000000000000000000000000000000000000000000000000000001",
+                      ],
+                      "schemaIndex": 0,
+                      "table": "0x00000000000000000000000000000000496e76656e746f727900000000000000",
+                    },
+                    "blockHash": "0x03e962e7402b2ab295b92feac342a132111dd14b0d1fd4d4a0456fdc77981577",
+                    "blockNumber": 5448n,
+                    "data": "0x00000000000000000000000000000000496e76656e746f7279000000000000000000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000003000000000000000000000000796eb990a3f9c431c69149c7a168b91596d87f600000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000040000000800000000000000000000000000000000000000000000000000000000",
+                    "eventName": "StoreSetField",
+                    "logIndex": 0,
+                    "removed": false,
+                    "topics": [
+                      "0xd01f9f1368f831528fc9fe6442366b2b7d957fbfff3bcf7c24d9ab5fe51f8c46",
+                    ],
+                    "transactionHash": "0xa6986924609542dc4c2d81c53799d8eab47109ef34ee1e422de595e19ee9bfa4",
+                    "transactionIndex": 0,
+                  },
+                  "name": "Inventory",
+                  "namespace": "",
+                  "type": "SetField",
+                  "value": 8,
+                  "valueName": "amount",
+                },
+              ],
+            },
+          ],
+        ],
+        "results": [
+          {
+            "type": "return",
+            "value": undefined,
+          },
+        ],
+      }
+    `);
 
     expect(operations).toMatchInlineSnapshot(`
       {
