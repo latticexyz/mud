c1551b37b046e0ef793e18c7e8b7e48b3a2c09f5
Kevin Ingersoll <kingersoll@gmail.com>
2023-07-20 14:49:42 +0100

    add failing test

--- packages/common/src/TableId.test.ts
+++ packages/common/src/TableId.test.ts
@@ -9,7 +9,14 @@ describe("TableId", () => {
     );
   });
 
+  it("can convert from hex string", () => {
+    const tableId = TableId.fromHex("0x6e616d657370616365000000000000006e616d65000000000000000000000000");
+    expect(tableId.namespace).toMatchInlineSnapshot('"namespace"');
+    expect(tableId.name).toMatchInlineSnapshot('"name"');
+  });
+
   it("truncates namespaces >16 bytes", () => {
+    expect(TableId.toHex("AVeryLongNamespace", "name")).toMatchInlineSnapshot();
     const tableId = new TableId("AVeryLongNamespace", "name");
     expect(tableId.toHex()).toMatchInlineSnapshot(
       '"0x41566572794c6f6e674e616d657370616e616d65000000000000000000000000"'
@@ -18,16 +25,11 @@ describe("TableId", () => {
   });
 
   it("truncates names >16 bytes", () => {
+    expect(TableId.toHex("namespace", "AnUnnecessarilyLongName")).toMatchInlineSnapshot();
     const tableId = new TableId("namespace", "AnUnnecessarilyLongName");
     expect(tableId.toHex()).toMatchInlineSnapshot(
       '"0x6e616d65737061636500000000000000416e556e6e65636573736172696c794c"'
     );
     expect(TableId.fromHex(tableId.toHex()).name).toMatchInlineSnapshot('"AnUnnecessarilyL"');
-  });
-
-  it("can convert from hex string", () => {
-    const tableId = TableId.fromHex("0x6e616d657370616365000000000000006e616d65000000000000000000000000");
-    expect(tableId.namespace).toMatchInlineSnapshot('"namespace"');
-    expect(tableId.name).toMatchInlineSnapshot('"name"');
   });
 });
