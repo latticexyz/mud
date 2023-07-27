4223bfd28793b534ba92fd1d09ac04ddc3a94f9c
dk1a <dk1a@protonmail.com>
2023-07-27 14:50:04 +0300

    50 less gas for 0 offset storage, 19 more for >0 offset

--- packages/store/src/Storage.sol
+++ packages/store/src/Storage.sol
@@ -27,48 +27,50 @@ library Storage {
    * @notice Stores raw bytes to storage at the given storagePointer and offset (keeping the rest of the word intact)
    */
   function store(uint256 storagePointer, uint256 offset, uint256 memoryPointer, uint256 length) internal {
-    // Support offsets that are greater than 32 bytes by incrementing the storagePointer and decrementing the offset
-    unchecked {
-      storagePointer += offset / 32;
-      offset %= 32;
-    }
-
-    // For the first word, if there is an offset, apply a mask to beginning
     if (offset > 0) {
-      // Get the word's remaining length after the offset
-      uint256 wordRemainder;
-      // (safe because of `offset %= 32` at the start)
+      // Support offsets that are greater than 32 bytes by incrementing the storagePointer and decrementing the offset
       unchecked {
-        wordRemainder = 32 - offset;
+        storagePointer += offset / 32;
+        offset %= 32;
       }
 
-      uint256 mask = leftMask(length);
-      /// @solidity memory-safe-assembly
-      assembly {
-        // Load data from memory and offset it to match storage
-        let bitOffset := mul(offset, 8)
-        mask := shr(bitOffset, mask)
-        let offsetData := shr(bitOffset, mload(memoryPointer))
+      // For the first word, if there is an offset, apply a mask to beginning
+      if (offset > 0) {
+        // Get the word's remaining length after the offset
+        uint256 wordRemainder;
+        // (safe because of `offset %= 32` at the start)
+        unchecked {
+          wordRemainder = 32 - offset;
+        }
 
-        sstore(
-          storagePointer,
-          or(
-            // Store the middle part
-            and(offsetData, mask),
-            // Preserve the surrounding parts
-            and(sload(storagePointer), not(mask))
+        uint256 mask = leftMask(length);
+        /// @solidity memory-safe-assembly
+        assembly {
+          // Load data from memory and offset it to match storage
+          let bitOffset := mul(offset, 8)
+          mask := shr(bitOffset, mask)
+          let offsetData := shr(bitOffset, mload(memoryPointer))
+
+          sstore(
+            storagePointer,
+            or(
+              // Store the middle part
+              and(offsetData, mask),
+              // Preserve the surrounding parts
+              and(sload(storagePointer), not(mask))
+            )
           )
-        )
-      }
-      // Return if done
-      if (length <= wordRemainder) return;
+        }
+        // Return if done
+        if (length <= wordRemainder) return;
 
-      // Advance pointers
-      storagePointer += 1;
-      // (safe because of `length <= prefixLength` earlier)
-      unchecked {
-        memoryPointer += wordRemainder;
-        length -= wordRemainder;
+        // Advance pointers
+        storagePointer += 1;
+        // (safe because of `length <= prefixLength` earlier)
+        unchecked {
+          memoryPointer += wordRemainder;
+          length -= wordRemainder;
+        }
       }
     }
 
@@ -129,46 +131,48 @@ library Storage {
    * @notice Append raw bytes from storage at the given storagePointer, offset, and length to the given memoryPointer
    */
   function load(uint256 storagePointer, uint256 length, uint256 offset, uint256 memoryPointer) internal view {
-    // Support offsets that are greater than 32 bytes by incrementing the storagePointer and decrementing the offset
-    unchecked {
-      storagePointer += offset / 32;
-      offset %= 32;
-    }
-
-    // For the first word, if there is an offset, apply a mask to beginning
     if (offset > 0) {
-      // Get the word's remaining length after the offset
-      uint256 wordRemainder;
-      // (safe because of `offset %= 32` at the start)
+      // Support offsets that are greater than 32 bytes by incrementing the storagePointer and decrementing the offset
       unchecked {
-        wordRemainder = 32 - offset;
+        storagePointer += offset / 32;
+        offset %= 32;
       }
 
-      uint256 mask = leftMask(wordRemainder);
-      /// @solidity memory-safe-assembly
-      assembly {
-        // Load data from storage and offset it to match memory
-        let offsetData := shl(mul(offset, 8), sload(storagePointer))
+      // For the first word, if there is an offset, apply a mask to beginning
+      if (offset > 0) {
+        // Get the word's remaining length after the offset
+        uint256 wordRemainder;
+        // (safe because of `offset %= 32` at the start)
+        unchecked {
+          wordRemainder = 32 - offset;
+        }
 
-        mstore(
-          memoryPointer,
-          or(
-            // store the middle part
-            and(offsetData, mask),
-            // preserve the surrounding parts
-            and(mload(memoryPointer), not(mask))
+        uint256 mask = leftMask(wordRemainder);
+        /// @solidity memory-safe-assembly
+        assembly {
+          // Load data from storage and offset it to match memory
+          let offsetData := shl(mul(offset, 8), sload(storagePointer))
+
+          mstore(
+            memoryPointer,
+            or(
+              // store the middle part
+              and(offsetData, mask),
+              // preserve the surrounding parts
+              and(mload(memoryPointer), not(mask))
+            )
           )
-        )
-      }
-      // Return if done
-      if (length <= wordRemainder) return;
+        }
+        // Return if done
+        if (length <= wordRemainder) return;
 
-      // Advance pointers
-      storagePointer += 1;
-      // (safe because of `length <= prefixLength` earlier)
-      unchecked {
-        memoryPointer += wordRemainder;
-        length -= wordRemainder;
+        // Advance pointers
+        storagePointer += 1;
+        // (safe because of `length <= prefixLength` earlier)
+        unchecked {
+          memoryPointer += wordRemainder;
+          length -= wordRemainder;
+        }
       }
     }
 
