d8a0a214731aca7ad880973c7db436a34c10361d
dk1a <dk1a@protonmail.com>
2023-08-03 22:00:05 +0300

    testGas

--- packages/store/test/PackedCounter.t.sol
+++ packages/store/test/PackedCounter.t.sol
@@ -51,30 +51,6 @@ contract PackedCounterTest is Test, GasReporter {
     assertEq(packedCounter.total(), 124);
   }
 
-  function testTotal() public returns (PackedCounter packedCounter, uint256 total) {
-    bytes32[] memory a1 = new bytes32[](1);
-    address[] memory a2 = new address[](2);
-    uint8[] memory a3 = new uint8[](3);
-    int128[] memory a4 = new int128[](4);
-
-    startGasReport("pack 4 lengths into PackedCounter");
-    unchecked {
-      packedCounter = PackedCounterLib.pack(
-        uint40(a1.length * 32),
-        uint40(a2.length * 20),
-        uint40(a3.length * 1),
-        uint40(a4.length * 16)
-      );
-    }
-    endGasReport();
-
-    startGasReport("get total of PackedCounter");
-    total = packedCounter.total();
-    endGasReport();
-
-    assertEq(packedCounter.total(), 1 * 32 + 2 * 20 + 3 * 1 + 4 * 16);
-  }
-
   function testAtIndex() public returns (uint256 value) {
     PackedCounter packedCounter = PackedCounterLib.pack(1, 2, 3, 4);
 
@@ -100,5 +76,35 @@ contract PackedCounterTest is Test, GasReporter {
     assertEq(packedCounter.atIndex(2), 5);
     assertEq(packedCounter.atIndex(3), 4);
     assertEq(packedCounter.total(), 12);
+  }
+
+  function testGas() public returns (PackedCounter packedCounter, uint256 total) {
+    bytes32[] memory a1 = new bytes32[](1);
+    address[] memory a2 = new address[](2);
+    uint8[] memory a3 = new uint8[](3);
+    int128[] memory a4 = new int128[](4);
+
+    startGasReport("pack 1 length into PackedCounter");
+    unchecked {
+      packedCounter = PackedCounterLib.pack(uint40(a1.length * 32));
+    }
+    endGasReport();
+    assertEq(packedCounter.total(), 1 * 32);
+
+    startGasReport("pack 4 lengths into PackedCounter");
+    unchecked {
+      packedCounter = PackedCounterLib.pack(
+        uint40(a1.length * 32),
+        uint40(a2.length * 20),
+        uint40(a3.length * 1),
+        uint40(a4.length * 16)
+      );
+    }
+    endGasReport();
+
+    startGasReport("get total of PackedCounter");
+    total = packedCounter.total();
+    endGasReport();
+    assertEq(packedCounter.total(), 1 * 32 + 2 * 20 + 3 * 1 + 4 * 16);
   }
 }
