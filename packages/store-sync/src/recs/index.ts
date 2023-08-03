6d9f166df373acfcc84278de5c24e6796ce063ce
Kevin Ingersoll <kingersoll@gmail.com>
2023-08-03 18:02:50 +0100

    move up singletonEntity

--- packages/store-sync/src/recs/index.ts
+++ packages/store-sync/src/recs/index.ts
@@ -4,4 +4,5 @@ export * from "./encodeEntity";
 export * from "./entityToHexKeyTuple";
 export * from "./hexKeyTupleToEntity";
 export * from "./recsStorage";
+export * from "./singletonEntity";
 export * from "./syncToRecs";
