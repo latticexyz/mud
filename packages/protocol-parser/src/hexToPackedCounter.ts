a03ee0bd737209be14c497af4c11c7b7fa966524
dk1a <dk1a@protonmail.com>
2023-08-08 12:18:02 +0300

    update comment in hexToPackedCounter

--- packages/protocol-parser/src/hexToPackedCounter.ts
+++ packages/protocol-parser/src/hexToPackedCounter.ts
@@ -5,8 +5,8 @@ import { InvalidHexLengthForPackedCounterError, PackedCounterLengthMismatchError
 
 // Keep this logic in sync with PackedCounter.sol
 
-// - First 7 bytes (uint56) are used for the total byte length of the dynamic data
-// - The next 5 byte (uint40) sections are used for the byte length of each field, in the same order as the schema's dynamic fields
+// - Last 7 bytes (uint56) are used for the total byte length of the dynamic data
+// - The next 5 byte (uint40) sections are used for the byte length of each field, indexed from right to left
 
 // We use byte lengths rather than item counts so that, on chain, we can slice without having to get the schema first (and thus the field lengths of each dynamic type)
 
