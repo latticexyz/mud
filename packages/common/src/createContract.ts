35c9f33dfb84b0bb94e0f7a8b0c9830761795bdb
Kevin Ingersoll <kingersoll@gmail.com>
2023-08-09 11:08:02 -0700

    feat(common): remove need for tx queue in `createContract` (#1271)

--- packages/common/src/createContract.ts
+++ packages/common/src/createContract.ts
@@ -5,7 +5,6 @@ import {
   Chain,
   GetContractParameters,
   GetContractReturnType,
-  Hex,
   PublicClient,
   SimulateContractParameters,
   Transport,
@@ -13,7 +12,6 @@ import {
   WriteContractParameters,
   getContract,
 } from "viem";
-import pQueue from "p-queue";
 import pRetry from "p-retry";
 import { createNonceManager } from "./createNonceManager";
 import { debug as parentDebug } from "./debug";
@@ -62,11 +60,6 @@ export function createContract<
       address: walletClient.account.address,
     });
 
-    // Concurrency of one means transactions will be queued and inserted into the mem pool synchronously and in order.
-    // Although increasing this will allow for more parallel requests/transactions and nonce errors will get automatically retried,
-    // we can't guarantee local nonce accurancy due to needing async operations (simulate) before incrementing the nonce.
-    const queue = new pQueue({ concurrency: 1 });
-
     // Replace write calls with our own proxy. Implemented ~the same as viem, but adds better handling of nonces (via queue + retries).
     contract.write = new Proxy(
       {},
@@ -80,34 +73,31 @@ export function createContract<
               }
             >getFunctionParameters(parameters as any);
 
-            async function write(): Promise<Hex> {
-              if (!nonceManager.hasNonce()) {
-                await nonceManager.resetNonce();
-              }
+            // Temporarily override base fee for our default anvil config
+            // TODO: replace with https://github.com/wagmi-dev/viem/pull/1006 once merged
+            // TODO: more specific mud foundry check? or can we safely assume anvil+mud will be block fee zero for now?
+            if (
+              walletClient.chain.id === 31337 &&
+              options.maxFeePerGas == null &&
+              options.maxPriorityFeePerGas == null
+            ) {
+              debug("assuming zero base fee for anvil chain");
+              options.maxFeePerGas = 0n;
+              options.maxPriorityFeePerGas = 0n;
+            }
 
-              // Temporarily override base fee for our default anvil config
-              // TODO: remove once https://github.com/wagmi-dev/viem/pull/963 is fixed
-              // TODO: more specific mud foundry check? or can we safely assume anvil+mud will be block fee zero for now?
-              if (
-                walletClient.chain.id === 31337 &&
-                options.maxFeePerGas == null &&
-                options.maxPriorityFeePerGas == null
-              ) {
-                options.maxFeePerGas = 0n;
-                options.maxPriorityFeePerGas = 0n;
-              }
-
+            async function prepareWrite(): Promise<
+              WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>
+            > {
               if (options.gas) {
-                const nonce = nonceManager.nextNonce();
-                debug("gas provided, skipping simulate and calling write function with nonce", nonce, options);
-                return await walletClient.writeContract({
+                debug("gas provided, skipping simulate", functionName, args, options);
+                return {
                   address,
                   abi,
                   functionName,
                   args,
-                  nonce,
                   ...options,
-                } as unknown as WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>);
+                } as unknown as WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>;
               }
 
               debug("simulating write", functionName, args, options);
@@ -120,29 +110,37 @@ export function createContract<
                 account: options.account ?? walletClient.account,
               } as unknown as SimulateContractParameters<TAbi, typeof functionName, TChain>);
 
-              const nonce = nonceManager.nextNonce();
-              debug("calling write function with nonce", nonce, request);
-              return await walletClient.writeContract({
-                nonce,
-                ...request,
-              } as unknown as WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>);
+              return request as unknown as WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>;
             }
 
-            return await queue.add(
-              () =>
-                pRetry(write, {
-                  retries: 3,
-                  onFailedAttempt: async (error) => {
-                    // On nonce errors, reset the nonce and retry
-                    if (nonceManager.shouldResetNonce(error)) {
-                      debug("got nonce error, retrying", error);
-                      await nonceManager.resetNonce();
-                      return;
-                    }
-                    throw error;
-                  },
-                }),
-              { throwOnTimeout: true }
+            const preparedWrite = await prepareWrite();
+
+            return await pRetry(
+              async () => {
+                if (!nonceManager.hasNonce()) {
+                  await nonceManager.resetNonce();
+                }
+
+                const nonce = nonceManager.nextNonce();
+                debug("calling write function with nonce", nonce, preparedWrite);
+                return await walletClient.writeContract({
+                  nonce,
+                  ...preparedWrite,
+                });
+              },
+              {
+                retries: 3,
+                onFailedAttempt: async (error) => {
+                  // On nonce errors, reset the nonce and retry
+                  if (nonceManager.shouldResetNonce(error)) {
+                    debug("got nonce error, retrying", error);
+                    await nonceManager.resetNonce();
+                    return;
+                  }
+                  // TODO: prepareWrite again if there are gas errors?
+                  throw error;
+                },
+              }
             );
           };
         },
