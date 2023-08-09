6071163f70599384c5034dd772ef6fc7cdae9983
Kevin Ingersoll <kingersoll@gmail.com>
2023-08-09 07:24:10 -0700

    feat(common): move zero gas fee override to `createContract` (#1266)

--- packages/common/src/createContract.ts
+++ packages/common/src/createContract.ts
@@ -17,10 +17,11 @@ import pQueue from "p-queue";
 import pRetry from "p-retry";
 import { createNonceManager } from "./createNonceManager";
 import { debug as parentDebug } from "./debug";
+import { UnionOmit } from "./type-utils/common";
 
 const debug = parentDebug.extend("createContract");
 
-// copied from viem because it isn't exported
+// copied from viem because this isn't exported
 // TODO: import from viem?
 function getFunctionParameters(values: [args?: readonly unknown[], options?: object]): {
   args: readonly unknown[];
@@ -72,31 +73,59 @@ export function createContract<
       {
         get(_, functionName: string): GetContractReturnType<Abi, PublicClient, WalletClient>["write"][string] {
           return async (...parameters) => {
-            const { args, options } = getFunctionParameters(parameters as any);
+            const { args, options } = <
+              {
+                args: unknown[];
+                options: UnionOmit<WriteContractParameters, "abi" | "address" | "functionName" | "args">;
+              }
+            >getFunctionParameters(parameters as any);
 
             async function write(): Promise<Hex> {
               if (!nonceManager.hasNonce()) {
                 await nonceManager.resetNonce();
               }
 
+              // Temporarily override base fee for our default anvil config
+              // TODO: remove once https://github.com/wagmi-dev/viem/pull/963 is fixed
+              // TODO: more specific mud foundry check? or can we safely assume anvil+mud will be block fee zero for now?
+              if (
+                walletClient.chain.id === 31337 &&
+                options.maxFeePerGas == null &&
+                options.maxPriorityFeePerGas == null
+              ) {
+                options.maxFeePerGas = 0n;
+                options.maxPriorityFeePerGas = 0n;
+              }
+
+              if (options.gas) {
+                const nonce = nonceManager.nextNonce();
+                debug("gas provided, skipping simulate and calling write function with nonce", nonce, options);
+                return await walletClient.writeContract({
+                  address,
+                  abi,
+                  functionName,
+                  args,
+                  nonce,
+                  ...options,
+                } as unknown as WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>);
+              }
+
               debug("simulating write", functionName, args, options);
               const { request } = await publicClient.simulateContract({
-                account: walletClient.account,
                 address,
                 abi,
                 functionName,
                 args,
                 ...options,
+                account: options.account ?? walletClient.account,
               } as unknown as SimulateContractParameters<TAbi, typeof functionName, TChain>);
 
               const nonce = nonceManager.nextNonce();
               debug("calling write function with nonce", nonce, request);
-              const result = await walletClient.writeContract({
+              return await walletClient.writeContract({
                 nonce,
                 ...request,
               } as unknown as WriteContractParameters<TAbi, typeof functionName, TChain, TAccount>);
-
-              return result;
             }
 
             return await queue.add(
