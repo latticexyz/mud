1550099b25632a60ab8bcb4e007fb85a87cf9c08
0xhank <zeroxhank@gmail.com>
2023-08-11 13:14:18 -0400

    chore: remove unused import

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,5 @@
-export * from "./defineReactComponent";
-export * from "./store-cache";
 export * from "./useComponentValue";
 export * from "./useDeprecatedComputedValue";
 export * from "./useEntityQuery";
 export * from "./useObservableValue";
+export * from "./store-cache";
