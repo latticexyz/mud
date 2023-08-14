83d9589f49eb32848d02d9f1383638ce1a6ad9a0
dk1a <dk1a@protonmail.com>
2023-08-14 19:54:02 +0300

    test(store): add more gas tests for table methods (#1300)

--- packages/store/test/tables/Hooks.t.sol
+++ packages/store/test/tables/Hooks.t.sol
@@ -7,25 +7,25 @@ import { StoreReadWithStubs } from "../../src/StoreReadWithStubs.sol";
 import { Hooks } from "../../src/codegen/Tables.sol";
 
 contract HooksTest is Test, GasReporter, StoreReadWithStubs {
-  function testSetAndGet() public {
+  function testTable() public {
     // Hooks schema is already registered by StoreCore
     bytes32 key = keccak256("somekey");
 
     address[] memory addresses = new address[](1);
     addresses[0] = address(this);
 
-    startGasReport("set field in Hooks");
+    startGasReport("Hooks: set field (cold)");
     Hooks.set(key, addresses);
     endGasReport();
 
-    startGasReport("get field from Hooks (warm)");
+    startGasReport("Hooks: get field (warm)");
     address[] memory returnedAddresses = Hooks.get(key);
     endGasReport();
 
     assertEq(returnedAddresses.length, addresses.length);
     assertEq(returnedAddresses[0], addresses[0]);
 
-    startGasReport("push field to Hooks");
+    startGasReport("Hooks: push 1 element (cold)");
     Hooks.push(key, addresses[0]);
     endGasReport();
 
@@ -33,5 +33,44 @@ contract HooksTest is Test, GasReporter, StoreReadWithStubs {
 
     assertEq(returnedAddresses.length, 2);
     assertEq(returnedAddresses[1], addresses[0]);
+
+    startGasReport("Hooks: pop 1 element (warm)");
+    Hooks.pop(key);
+    endGasReport();
+
+    returnedAddresses = Hooks.get(key);
+
+    assertEq(returnedAddresses.length, 1);
+    assertEq(returnedAddresses[0], addresses[0]);
+
+    startGasReport("Hooks: push 1 element (warm)");
+    Hooks.push(key, addresses[0]);
+    endGasReport();
+
+    returnedAddresses = Hooks.get(key);
+
+    assertEq(returnedAddresses.length, 2);
+    assertEq(returnedAddresses[1], addresses[0]);
+
+    address newAddress = address(bytes20(keccak256("alice")));
+    startGasReport("Hooks: update 1 element (warm)");
+    Hooks.update(key, 1, newAddress);
+    endGasReport();
+
+    returnedAddresses = Hooks.get(key);
+    assertEq(returnedAddresses.length, 2);
+    assertEq(returnedAddresses[0], addresses[0]);
+    assertEq(returnedAddresses[1], newAddress);
+
+    startGasReport("Hooks: delete record (warm)");
+    Hooks.deleteRecord(key);
+    endGasReport();
+
+    returnedAddresses = Hooks.get(key);
+    assertEq(returnedAddresses.length, 0);
+
+    startGasReport("Hooks: set field (warm)");
+    Hooks.set(key, addresses);
+    endGasReport();
   }
 }
