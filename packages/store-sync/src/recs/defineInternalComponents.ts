20b7d1ec36ae2e36def3ff6dd31c368d235f2138
Kevin Ingersoll <kingersoll@gmail.com>
2023-08-14 16:23:18 +0100

    internal components no longer need to satisfy metadata

--- packages/store-sync/src/recs/defineInternalComponents.ts
+++ packages/store-sync/src/recs/defineInternalComponents.ts
@@ -1,39 +1,16 @@
-import { World, defineComponent, Type, Component, Schema } from "@latticexyz/recs";
+import { World, defineComponent, Type, Component, Schema, Metadata } from "@latticexyz/recs";
 import { Table } from "../common";
-import { StoreComponentMetadata } from "./common";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function defineInternalComponents(world: World) {
   return {
-    TableMetadata: defineComponent<{ table: Type.T }, StoreComponentMetadata, Table>(
-      world,
-      { table: Type.T },
-      {
-        metadata: {
-          componentName: "TableMetadata",
-          tableName: "recs:TableMetadata",
-          keySchema: {},
-          valueSchema: {},
-        },
-      }
-    ),
-    SyncProgress: defineComponent(
-      world,
-      {
-        step: Type.String,
-        message: Type.String,
-        percentage: Type.Number,
-        latestBlockNumber: Type.BigInt,
-        lastBlockNumberProcessed: Type.BigInt,
-      },
-      {
-        metadata: {
-          componentName: "SyncProgress",
-          tableName: "recs:SyncProgress",
-          keySchema: {},
-          valueSchema: {},
-        },
-      }
-    ),
-  } as const satisfies Record<string, Component<Schema, StoreComponentMetadata>>;
+    TableMetadata: defineComponent<{ table: Type.T }, Metadata, Table>(world, { table: Type.T }),
+    SyncProgress: defineComponent(world, {
+      step: Type.String,
+      message: Type.String,
+      percentage: Type.Number,
+      latestBlockNumber: Type.BigInt,
+      lastBlockNumberProcessed: Type.BigInt,
+    }),
+  } as const satisfies Record<string, Component<Schema, Metadata>>;
 }
