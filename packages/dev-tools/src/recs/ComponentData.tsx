bf18ab0fe5931b2557834b0f22634329ba5ba0dc
Kevin Ingersoll <kingersoll@gmail.com>
2023-08-14 16:39:43 +0100

    improve support for non-store components

--- packages/dev-tools/src/recs/ComponentData.tsx
+++ packages/dev-tools/src/recs/ComponentData.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useDevToolsContext } from "../DevToolsContext";
 import { ComponentDataTable } from "./ComponentDataTable";
 import { isStoreComponent } from "@latticexyz/store-sync/recs";
+import { StoreComponentDataTable } from "./StoreComponentDataTable";
 
 // TODO: use react-table or similar for better perf with lots of logs
 
@@ -13,9 +14,13 @@ export function ComponentData() {
   const component = world.components.find((component) => component.id === idParam);
 
   // TODO: error message or redirect?
-  if (!component || !isStoreComponent(component)) return null;
+  if (!component) return null;
 
   // key here is useful to force a re-render on component changes,
   // otherwise state hangs around from previous render during navigation (entities)
-  return <ComponentDataTable key={component.id} component={component} />;
+  return isStoreComponent(component) ? (
+    <StoreComponentDataTable key={component.id} component={component} />
+  ) : (
+    <ComponentDataTable key={component.id} component={component} />
+  );
 }
--- packages/store-sync/src/recs/defineInternalComponents.ts
+++ packages/store-sync/src/recs/defineInternalComponents.ts
@@ -4,13 +4,21 @@ import { Table } from "../common";
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function defineInternalComponents(world: World) {
   return {
-    TableMetadata: defineComponent<{ table: Type.T }, Metadata, Table>(world, { table: Type.T }),
-    SyncProgress: defineComponent(world, {
-      step: Type.String,
-      message: Type.String,
-      percentage: Type.Number,
-      latestBlockNumber: Type.BigInt,
-      lastBlockNumberProcessed: Type.BigInt,
-    }),
+    TableMetadata: defineComponent<{ table: Type.T }, Metadata, Table>(
+      world,
+      { table: Type.T },
+      { metadata: { componentName: "TableMetadata" } }
+    ),
+    SyncProgress: defineComponent(
+      world,
+      {
+        step: Type.String,
+        message: Type.String,
+        percentage: Type.Number,
+        latestBlockNumber: Type.BigInt,
+        lastBlockNumberProcessed: Type.BigInt,
+      },
+      { metadata: { componentName: "SyncProgress" } }
+    ),
   } as const satisfies Record<string, Component<Schema, Metadata>>;
 }
