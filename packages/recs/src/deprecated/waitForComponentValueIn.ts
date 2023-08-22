a2db9954695c9900a85b495a22d1f37c330d9e11
alvrs <alvarius@lattice.xyz>
2023-08-22 15:21:26 +0200

    fix waitForComponentValueIn

--- packages/recs/src/deprecated/waitForComponentValueIn.ts
+++ packages/recs/src/deprecated/waitForComponentValueIn.ts
@@ -1,6 +1,6 @@
 import { deferred } from "@latticexyz/utils";
-import { filter } from "rxjs";
-import { componentValueEquals } from "../Component";
+import { filter, map, startWith } from "rxjs";
+import { componentValueEquals, getComponentValue } from "../Component";
 import { Component, Metadata, Entity, ComponentValue, Schema } from "../types";
 
 export function waitForComponentValueIn<S extends Schema, T>(
@@ -10,10 +10,22 @@ export function waitForComponentValueIn<S extends Schema, T>(
 ): Promise<void> {
   const [resolve, , promise] = deferred<void>();
 
+  const currentValue = getComponentValue(component, entity);
+  if (values.find((value) => componentValueEquals(value, currentValue))) {
+    resolve();
+  }
+
   let dispose = resolve;
-  const subscription = component.update$
+
+  const value$ = component.update$.pipe(
+    filter((update) => update.entity === entity), // Ignore updates of other entities
+    map((update) => update.value[0]) // Map the update to the current value
+  );
+
+  const subscription = value$
     .pipe(
-      filter((e) => e.entity === entity && Boolean(values.find((value) => componentValueEquals(value, e.value[0]))))
+      startWith(getComponentValue(component, entity)),
+      filter((currentValue) => Boolean(values.find((searchValue) => componentValueEquals(searchValue, currentValue))))
     )
     .subscribe(() => {
       resolve();
