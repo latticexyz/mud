c03aff39e9882c8a827a3ed1ee81816231973816
Kevin Ingersoll <kingersoll@gmail.com>
2023-08-23 02:37:43 -0700

    chore: delete std-client package (#1342)

--- packages/std-client/src/deprecated/components/BoolComponent.ts
+++
@@ -1,8 +0,0 @@
-import { defineComponent, Metadata, Type, World } from "@latticexyz/recs";
-
-export function defineBoolComponent<M extends Metadata>(
-  world: World,
-  options?: { id?: string; metadata?: M; indexed?: boolean }
-) {
-  return defineComponent<{ value: Type.Boolean }, M>(world, { value: Type.Boolean }, options);
-}
--- packages/std-client/src/deprecated/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { defineDevHighlightComponent } from "./DevHighlight";
-export { defineNumberComponent } from "./NumberComponent";
-export { defineBoolComponent } from "./BoolComponent";
-export { defineCoordComponent } from "./CoordComponent";
-export { defineStringComponent } from "./StringComponent";
-export { defineVoxelCoordComponent } from "./VoxelCoordComponent";
