d1906c7a607af4df25c8fe1a76966a2a46ae2b82
Ori Pomeerantz <qbzzt1@gmail.com>
2023-08-28 08:58:24 -0500

    Example that uses a forge script

---
+++ examples/onchain-client/script/ScriptClient.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+
+import { Counter } from "codegen/Tables.sol";
+import { IWorld } from "codegen/world/IWorld.sol";
+
+contract ScriptClient is Script {
+  IWorld world;
+
+  function run() external {
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 myPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    // Start broadcasting transactions from that account
+    vm.startBroadcast(myPrivateKey);
+
+    // Connect to the world using `WORLD_ADDR` from .env
+    console.log("Connecting to world at ", vm.envAddress("WORLD_ADDR"));
+    world = IWorld(vm.envAddress("WORLD_ADDR"));
+
+    console.log("The counter values is now ", Counter.get(world));
+
+    console.log("Incrementing the counter");
+    world.increment();
+
+    console.log("The counter values is now ", Counter.get(world));
+
+    // Stop broadcasting transactions from te account
+    vm.stopBroadcast();
+  }
+}
