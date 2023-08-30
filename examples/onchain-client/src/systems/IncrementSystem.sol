fece8be39d1ef3103c15cb4b8ae4b31bda438190
Ori Pomeerantz <qbzzt1@gmail.com>
2023-08-30 09:45:10 -0500

    Added @alvrs comments

--- examples/onchain-client/src/systems/IncrementSystem.sol
+++ examples/onchain-client/src/systems/IncrementSystem.sol
@@ -5,10 +5,5 @@ import { System } from "@latticexyz/world/src/System.sol";
 import { Counter } from "../codegen/Tables.sol";
 
 contract IncrementSystem is System {
-  function increment() public returns (uint32) {
-    uint32 counter = Counter.get();
-    uint32 newValue = counter + 1;
-    Counter.set(newValue);
-    return newValue;
-  }
+  function increment() public returns (uint32);
 }
