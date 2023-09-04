c3366e883846e7a3d445b5466fb74e2c8e7add08
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-04 11:39:59 +0100

    update dev tools

--- packages/dev-tools/src/events/EventIcon.tsx
+++ packages/dev-tools/src/events/EventIcon.tsx
@@ -1,22 +1,21 @@
 import { assertExhaustive } from "@latticexyz/common/utils";
-import { StoreConfig } from "@latticexyz/store";
-import { StorageOperation } from "@latticexyz/store-sync";
+import { StorageAdapterLog } from "@latticexyz/store-sync";
 
 type Props = {
-  type: StorageOperation<StoreConfig>["type"];
+  type: StorageAdapterLog["eventName"];
 };
 
 export function EventIcon({ type }: Props) {
   switch (type) {
-    case "SetRecord":
+    case "StoreSetRecord":
       return <span className="text-green-500 font-bold">=</span>;
-    case "SetField":
+    case "StoreSpliceRecord":
       return <span className="text-cyan-500 font-bold">+</span>;
-    case "DeleteRecord":
+    case "StoreDeleteRecord":
       return <span className="text-red-500 font-bold">-</span>;
-    // case "EphemeralRecord":
-    //   return <span className="text-violet-400 font-bold">~</span>;
+    case "StoreEphemeralRecord":
+      return <span className="text-violet-400 font-bold">~</span>;
     default:
-      return assertExhaustive(type, `Unexpected storage operation type: ${type}`);
+      return assertExhaustive(type, `Unexpected event type: ${type}`);
   }
 }
---
+++ packages/dev-tools/src/events/LogsTable.tsx
@@ -0,0 +1,57 @@
+import { StorageAdapterLog } from "@latticexyz/store-sync";
+import { serialize } from "../serialize";
+import { EventIcon } from "./EventIcon";
+import { hexToTableId } from "@latticexyz/common";
+
+// TODO: use react-table or similar for better perf with lots of logs
+
+type Props = {
+  logs: StorageAdapterLog[];
+};
+
+export function LogsTable({ logs }: Props) {
+  return (
+    <table className="w-full table-fixed -mx-1">
+      <thead className="sticky top-0 z-10 bg-slate-800 text-amber-200/80 text-left">
+        <tr>
+          <th className="px-1 pt-1.5 pb-0.5 font-semibold uppercase text-xs w-2/12">block</th>
+          <th className="px-1 pt-1.5 pb-0.5 font-semibold uppercase text-xs w-2/12">table</th>
+          <th className="px-1 pt-1.5 pb-0.5 font-semibold uppercase text-xs w-2/12">key</th>
+          <th className="px-1 pt-1.5 pb-0.5 font-semibold uppercase text-xs w-[1em]"></th>
+          <th className="px-1 pt-1.5 pb-0.5 font-semibold uppercase text-xs">value</th>
+        </tr>
+      </thead>
+      <tbody className="font-mono text-xs">
+        {logs.map((log) => {
+          const { namespace, name } = hexToTableId(log.args.table);
+          return (
+            <tr
+              key={
+                log.blockHash != null && log.logIndex != null
+                  ? `${log.blockHash}:${log.logIndex}`
+                  : `${namespace}:${name}:${log.args.key.join(",")}`
+              }
+              className="hover:bg-blue-800"
+            >
+              <td className="px-1 whitespace-nowrap overflow-hidden text-ellipsis text-white/40">
+                {log.blockNumber?.toString()}
+              </td>
+              <td className="px-1 whitespace-nowrap overflow-hidden text-ellipsis">
+                {namespace}:{name}
+              </td>
+              <td className="px-1 whitespace-nowrap overflow-hidden text-ellipsis">{log.args.key.join(",")}</td>
+              <td className="px-1 whitespace-nowrap">
+                <EventIcon type={log.eventName} />
+              </td>
+              <td className="px-1 whitespace-nowrap overflow-hidden text-ellipsis">
+                {/* TODO: decode these values if we can */}
+                {log.eventName === "StoreSetRecord" || log.eventName === "StoreEphemeralRecord" ? log.args.data : null}
+                {log.eventName === "StoreSpliceRecord" ? log.args.data : null}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
