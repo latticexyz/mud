3f31248b223e9972aed6fd967b6ee5d52a6c09cf
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-06 14:02:57 -0500

    feat: unify the client boilerplate across templates
    Using the "magic" of symbolic links

--- templates/phaser/packages/client/src/mud/supportedChains.ts
+++
@@ -1,4 +0,0 @@
-import { MUDChain, latticeTestnet, mudFoundry } from "@latticexyz/common/chains";
-
-// If you are deploying to chains other than anvil or Lattice testnet, add them here
-export const supportedChains: MUDChain[] = [mudFoundry, latticeTestnet];
