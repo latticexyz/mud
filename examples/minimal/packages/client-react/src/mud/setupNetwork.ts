78bea9dd965fd26a42229a525b141c67eb4d6302
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-06 11:54:42 +0100

    migrate example

--- examples/minimal/packages/client-react/src/mud/setupNetwork.ts
+++ examples/minimal/packages/client-react/src/mud/setupNetwork.ts
@@ -3,7 +3,7 @@ import { createFaucetService } from "@latticexyz/services/faucet";
 import { encodeEntity, syncToRecs } from "@latticexyz/store-sync/recs";
 import { getNetworkConfig } from "./getNetworkConfig";
 import { world } from "./world";
-import { IWorld__factory } from "contracts/types/ethers-contracts/factories/IWorld__factory";
+import IWorldAbi from "contracts/abi-ts/IWorld.sol/IWorld";
 import { ContractWrite, createBurnerAccount, createContract, transportObserver } from "@latticexyz/common";
 import { Subject, share } from "rxjs";
 import mudConfig from "contracts/mud.config";
@@ -30,7 +30,7 @@ export async function setupNetwork() {
   const write$ = new Subject<ContractWrite>();
   const worldContract = createContract({
     address: networkConfig.worldAddress as Hex,
-    abi: IWorld__factory.abi,
+    abi: IWorldAbi,
     publicClient,
     walletClient: burnerWalletClient,
     onWrite: (write) => write$.next(write),
