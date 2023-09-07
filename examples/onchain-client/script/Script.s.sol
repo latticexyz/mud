9ee4f067178172870d6e5b6deba35ebdfe60eb4f
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-06 21:07:01 -0500

    Script still not working :-(

--- examples/onchain-client/script/Script.s.sol
+++ examples/onchain-client/script/Script.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import { Script } from "forge-std/Script.sol";
-import { console } from "forge-std/console.sol";
+import { Script } from "forge-std/src/Script.sol";
+import { console } from "forge-std/src/console.sol";
 
 import { Counter } from "codegen/Tables.sol";
 import { IWorld } from "codegen/world/IWorld.sol";
