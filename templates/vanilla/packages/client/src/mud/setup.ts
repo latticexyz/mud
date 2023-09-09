1b78e03e93e779b84838b78468338fac2e09a500
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-09 10:11:56 -0500

    Update setup.ts

--- templates/vanilla/packages/client/src/mud/setup.ts
+++ templates/vanilla/packages/client/src/mud/setup.ts
@@ -6,39 +6,12 @@ import { createClientComponents } from "./createClientComponents";
 import { createSystemCalls } from "./createSystemCalls";
 import { setupNetwork } from "./setupNetwork";
 
-/*
- * The type definition for the return type of setup.
- * The result is an Awaited (https://www.typescriptlang.org/docs/handbook/utility-types.html#awaitedtype),
- * which means a result that may not be immediately available.
- *
- * The Awaited result is of type ReturnType<typeof setup>, which
- * means that TypeScript will see the type that the setup function
- * returns and use that.
- */
 export type SetupResult = Awaited<ReturnType<typeof setup>>;
 
-/*
- * This is the setup function that is called from the client code
- * (index.ts or index.tsx, depending on the client framework).
- */
 export async function setup() {
-  /*
-   * Get the network information, the components, and the system calls from the imported code.
-   */
   const network = await setupNetwork();
   const components = createClientComponents(network);
   const systemCalls = createSystemCalls(network, components);
-
-  /*
-   * Return all of this information.
-   * This structure's syntax is shorthand for:
-   *
-   *   {
-   *     "network": network,
-   *     "components": components,
-   *     "systemCalls": systemCalls
-   *   }
-   */
 
   return {
     network,
