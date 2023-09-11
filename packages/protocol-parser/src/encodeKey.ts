6d3a4b35877e596ec209015436b5dcba1138a09f
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-11 03:57:16 -0700

    Update packages/protocol-parser/src/encodeKey.ts
    Co-authored-by: alvarius <alvarius@lattice.xyz>

--- packages/protocol-parser/src/encodeKey.ts
+++ packages/protocol-parser/src/encodeKey.ts
@@ -5,6 +5,6 @@ import { encodeKeyTuple } from "./encodeKeyTuple";
 
 export function encodeKey<TSchema extends KeySchema>(keySchema: TSchema, key: SchemaToPrimitives<TSchema>): Hex[] {
   const staticFields = Object.values(keySchema).filter(isStaticAbiType);
-  // TODO: refactor and move all encodeKey logic into this method so we can delete encodeKey
+  // TODO: refactor and move all encodeKeyTuple logic into this method so we can delete encodeKeyTuple
   return encodeKeyTuple({ staticFields, dynamicFields: [] }, Object.values(key));
 }
