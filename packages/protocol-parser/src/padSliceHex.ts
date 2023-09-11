075a8ecbef6becec3700f876abec42f83f637ee0
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-11 14:57:59 +0100

    add typedoc and rename for clarity

--- packages/protocol-parser/src/padSliceHex.ts
+++
@@ -1,8 +0,0 @@
-import { Hex } from "viem";
-
-export function padSliceHex(data: Hex, start: number, end?: number): Hex {
-  return `0x${data
-    .replace(/^0x/, "")
-    .slice(start * 2, end != null ? end * 2 : undefined)
-    .padEnd(((end ?? start) - start) * 2, "0")}`;
-}
