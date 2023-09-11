4098d312f4a7c72da90fc3ad1247deb89d79d1e0
alvrs <alvarius@lattice.xyz>
2023-09-11 14:16:34 +0100

    add test for WorldContext

--- packages/world/src/WorldContext.sol
+++ packages/world/src/WorldContext.sol
@@ -4,24 +4,28 @@ pragma solidity >=0.8.0;
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { revertWithBytes } from "./revertWithBytes.sol";
 
+// The context size is 20 bytes for msg.sender, and 32 bytes for msg.value
+uint256 constant CONTEXT_BYTES = 20 + 32;
+
 // Similar to https://eips.ethereum.org/EIPS/eip-2771, but any contract can be the trusted forwarder.
 // This should only be used for contracts without own storage, like Systems.
 abstract contract WorldContextConsumer {
   // Extract the trusted msg.sender value appended to the calldata
   function _msgSender() internal view returns (address sender) {
     assembly {
+      // Load 32 bytes from calldata at position calldatasize() - context size,
+      // then shift left 96 bits (to right-align the address)
       // 96 = 256 - 20 * 8
-      sender := shr(96, calldataload(sub(calldatasize(), 20)))
+      sender := shr(96, calldataload(sub(calldatasize(), CONTEXT_BYTES)))
     }
     if (sender == address(0)) sender = msg.sender;
   }
 
   // Extract the trusted msg.value value appended to the calldata
   function _msgValue() internal pure returns (uint256 value) {
-    // TODO: make math work
     assembly {
-      // 96 = 256 - 20 * 8
-      value := shr(96, calldataload(sub(calldatasize(), 20)))
+      // Load 32 bytes from calldata at position calldatasize() - 32 bytes,
+      value := calldataload(sub(calldatasize(), 32))
     }
   }
 
---
+++ packages/world/test/WorldContext.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "forge-std/Test.sol";
+import { GasReporter } from "@latticexyz/gas-report/src/GasReporter.sol";
+import { WorldContextProvider, WorldContextConsumer } from "../src/WorldContext.sol";
+
+contract TestContextConsumer is WorldContextConsumer {
+  event Context(bytes args, address msgSender, uint256 msgValue);
+
+  function emitContext(bytes memory someArgument) public {
+    emit Context(someArgument, _msgSender(), _msgValue());
+  }
+}
+
+contract WorldContextTest is Test, GasReporter {
+  event Context(bytes args, address msgSender, uint256 msgValue);
+
+  TestContextConsumer public consumer = new TestContextConsumer();
+
+  function testFuzzAppendContext(bytes memory funcSelectorAndArgs, address msgSender, uint256 msgValue) public {
+    assertEq(
+      keccak256(abi.encodePacked(funcSelectorAndArgs, msgSender, msgValue)),
+      keccak256(WorldContextProvider.appendContext(funcSelectorAndArgs, msgSender, msgValue))
+    );
+  }
+
+  function testFuzzCallExtractContext(bytes memory args, address msgSender, uint256 msgValue) public {
+    vm.assume(msgSender != address(0));
+
+    vm.expectEmit(true, true, true, true);
+    emit Context(args, msgSender, msgValue);
+    WorldContextProvider.callWithContextOrRevert({
+      msgSender: msgSender,
+      msgValue: msgValue,
+      target: address(consumer),
+      funcSelectorAndArgs: abi.encodeWithSelector(TestContextConsumer.emitContext.selector, args)
+    });
+  }
+}
