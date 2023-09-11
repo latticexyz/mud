ac4063b3eebba9dbeb743961d5783f1b65c7fe9a
Ori Pomeerantz <qbzzt1@gmail.com>
2023-09-11 12:09:02 -0500

    update the phaser template with comments

--- templates/phaser/packages/client/src/mud/createSystemCalls.ts
+++ templates/phaser/packages/client/src/mud/createSystemCalls.ts
@@ -1,3 +1,14 @@
+/*
+ * Creates components for use by the client.
+ *
+ * By default it returns the components from setupNetwork.ts, those which are
+ * automatically inferred from the mud.config.ts table definitions.
+ *
+ * However, you can add or override components here as needed. This
+ * lets you add user defined components, which may or may not have
+ * an onchain component.
+ */
+
 import { getComponentValue } from "@latticexyz/recs";
 import { ClientComponents } from "./createClientComponents";
 import { SetupNetworkResult } from "./setupNetwork";
@@ -6,10 +17,33 @@ import { singletonEntity } from "@latticexyz/store-sync/recs";
 export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
+  /*
+   * The parameter list informs TypeScript that:
+   *
+   * - The first parameter is expected to be a
+   *   SetupNetworkResult, as defined in setupNetwork.ts
+   *
+   * - Out of this parameter, we only care about two fields:
+   *   - worldContract (which comes from createContract, see
+   *     https://github.com/latticexyz/mud/blob/main/templates/phaser/packages/client/src/mud/setupNetwork.ts#L31).
+   *   - waitForTransaction (which comes from syncToRecs, see
+   *     https://github.com/latticexyz/mud/blob/main/templates/phaser/packages/client/src/mud/setupNetwork.ts#L39).
+   *
+   * - From the second parameter, which is a ClientComponent,
+   *   we only care about Counter. This parameter comes to use
+   *   through createClientComponents.ts, but it originates in
+   *   syncToRecs (https://github.com/latticexyz/mud/blob/main/templates/phaser/packages/client/src/mud/setupNetwork.ts#L39).
+   */
   { worldContract, waitForTransaction }: SetupNetworkResult,
   { Counter }: ClientComponents
 ) {
   const increment = async () => {
+    /*
+     * Because IncrementSystem
+     * (https://mud.dev/tutorials/walkthrough/minimal-onchain#incrementsystemsol)
+     * is in the root namespace, `.increment` can be called directly
+     * on the World contract.
+     */
     const tx = await worldContract.write.increment();
     await waitForTransaction(tx);
     return getComponentValue(Counter, singletonEntity);
