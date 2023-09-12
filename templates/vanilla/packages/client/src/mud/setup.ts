d2c8488afb4bca3fec9c1de44d24ba2d6fac6c25
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-12 12:12:21 -0500

    docs(create-mud): explain boilerplate in templates (#1427)
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- templates/vanilla/packages/client/src/mud/setup.ts
+++ templates/vanilla/packages/client/src/mud/setup.ts
@@ -1,3 +1,7 @@
+/*
+ * This file sets up all the definitions required for a MUD client.
+ */
+
 import { createClientComponents } from "./createClientComponents";
 import { createSystemCalls } from "./createSystemCalls";
 import { setupNetwork } from "./setupNetwork";
@@ -8,6 +12,7 @@ export async function setup() {
   const network = await setupNetwork();
   const components = createClientComponents(network);
   const systemCalls = createSystemCalls(network, components);
+
   return {
     network,
     components,
