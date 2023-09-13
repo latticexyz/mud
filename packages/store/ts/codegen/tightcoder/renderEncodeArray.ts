c0489836300b2fc724724c63cbf773f48077e825
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-14 00:12:07 +0100

    revert new import, improve diff suppression

--- packages/store/ts/codegen/tightcoder/renderEncodeArray.ts
+++ packages/store/ts/codegen/tightcoder/renderEncodeArray.ts
@@ -6,7 +6,6 @@ export function renderEncodeArray() {
   return `
     ${renderedSolidityHeader}
     import { TightCoder } from "./TightCoder.sol";
-    import { Slice } from "../Slice.sol";
     library EncodeArray {
       ${staticAbiTypes
         .map((staticAbiType) =>
