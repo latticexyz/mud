7f1630638ee34cd235017641457905d6db721aec
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-13 16:03:36 +0100

    use rpc logs instead

--- packages/store-sync/src/recs/recsStorage.test.ts
+++ packages/store-sync/src/recs/recsStorage.test.ts
@@ -2,16 +2,25 @@ import { describe, expect, it } from "vitest";
 import { blockLogsToStorage } from "../blockLogsToStorage";
 import { recsStorage } from "./recsStorage";
 import { createWorld, getComponentEntities, getComponentValue } from "@latticexyz/recs";
-import superjson from "superjson";
 // TODO: figure out if this needs to be imported from a package rather than relative path
 // TODO: if relative imports are fine, probably should put the e2e test data with its mud config?
 import mudConfig from "../../../../e2e/packages/contracts/mud.config";
-import testLogsJson from "../../../../test-data/world-logs.json";
+import worldRpcLogs from "../../../../test-data/world-logs.json";
 import { groupLogsByBlockNumber } from "@latticexyz/block-logs-stream";
 import { StoreEventsLog } from "../common";
 import { singletonEntity } from "./singletonEntity";
+import { RpcLog, formatLog, decodeEventLog, Hex } from "viem";
+import { storeEventsAbi } from "@latticexyz/store";
 
-const testLogs = superjson.deserialize<readonly StoreEventsLog[]>(testLogsJson as any);
+const worldLogs = worldRpcLogs.map((log) => {
+  const { eventName, args } = decodeEventLog({
+    abi: storeEventsAbi,
+    data: log.data as Hex,
+    topics: log.topics as [Hex, ...Hex[]],
+    strict: true,
+  });
+  return formatLog(log as any as RpcLog, { args, eventName: eventName as string }) as StoreEventsLog;
+});
 
 describe("recsStorage", () => {
   it("creates components", async () => {
@@ -26,7 +35,7 @@ describe("recsStorage", () => {
     const world = createWorld();
     const { storageAdapter, components } = recsStorage({ world, config: mudConfig });
 
-    const blocks = groupLogsByBlockNumber(testLogs);
+    const blocks = groupLogsByBlockNumber(worldLogs);
     await Promise.all(blocks.map(async (block) => await blockLogsToStorage(storageAdapter)(block)));
 
     expect(Array.from(getComponentEntities(components.NumberList))).toMatchInlineSnapshot(`
