b774925fdd91abfd1d762ec1f10c13096311809a
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-14 17:52:10 +0100

    store/MudTest -> world/MudTest

--- examples/minimal/packages/contracts/test/ChatNamespaced.t.sol
+++ examples/minimal/packages/contracts/test/ChatNamespaced.t.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
-import { MudTest } from "@latticexyz/store/test/MudTest.t.sol";
+import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
 import { StoreCore } from "@latticexyz/store/src/StoreCore.sol";
 
--- packages/world/test/MudTest.t.sol
+++ packages/world/test/MudTest.t.sol
@@ -2,10 +2,10 @@
 pragma solidity >=0.8.0;
 
 import { Test } from "forge-std/Test.sol";
-import { StoreSwitch } from "../src/StoreSwitch.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
 abstract contract MudTest is Test {
-  address worldAddress;
+  address public worldAddress;
 
   function setUp() public virtual {
     worldAddress = vm.parseAddress(vm.readFile(".mudtest"));
