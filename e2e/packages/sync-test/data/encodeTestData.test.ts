a9b38647b91bb93a1ff0cf572da807279acc4aca
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-14 13:11:39 +0100

    update test data

--- e2e/packages/sync-test/data/encodeTestData.test.ts
+++ e2e/packages/sync-test/data/encodeTestData.test.ts
@@ -3,24 +3,36 @@ import { encodeTestData } from "./encodeTestData";
 
 describe("encodeTestData", () => {
   it("should encode numbers", () => {
-    expect(encodeTestData({ Number: [{ key: { key: 42 }, value: { value: 1337 } }] })).toStrictEqual({
-      Number: [
-        {
-          key: ["0x000000000000000000000000000000000000000000000000000000000000002a"],
-          value: "0x000005390000000000000000000000000000000000000000000000000000000000000000",
-          valueSchema: "0x0004010003000000000000000000000000000000000000000000000000000000",
-        },
-      ],
-    });
+    expect(encodeTestData({ Number: [{ key: { key: 42 }, value: { value: 1337 } }] })).toMatchInlineSnapshot(`
+      {
+        "Number": [
+          {
+            "dynamicData": "0x",
+            "encodedLengths": "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "fieldLayout": "0x0004010004000000000000000000000000000000000000000000000000000000",
+            "key": [
+              "0x000000000000000000000000000000000000000000000000000000000000002a",
+            ],
+            "staticData": "0x00000539",
+          },
+        ],
+      }
+    `);
 
-    expect(encodeTestData({ Vector: [{ key: { key: 1337 }, value: { x: 42, y: -69 } }] })).toStrictEqual({
-      Vector: [
-        {
-          key: ["0x0000000000000000000000000000000000000000000000000000000000000539"],
-          value: "0x0000002affffffbb0000000000000000000000000000000000000000000000000000000000000000",
-          valueSchema: "0x0008020023230000000000000000000000000000000000000000000000000000",
-        },
-      ],
-    });
+    expect(encodeTestData({ Vector: [{ key: { key: 1337 }, value: { x: 42, y: -69 } }] })).toMatchInlineSnapshot(`
+      {
+        "Vector": [
+          {
+            "dynamicData": "0x",
+            "encodedLengths": "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "fieldLayout": "0x0008020004040000000000000000000000000000000000000000000000000000",
+            "key": [
+              "0x0000000000000000000000000000000000000000000000000000000000000539",
+            ],
+            "staticData": "0x0000002affffffbb",
+          },
+        ],
+      }
+    `);
   });
 });
