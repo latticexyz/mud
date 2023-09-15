76055f45c60b47b7579b3f977e6152c8aecd31d1
John Grant <johngrantuk@googlemail.com>
2023-09-15 15:35:53 +0100

    refactor(cli): split up deploy into modules (#1384)
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>
    Co-authored-by: alvrs <alvarius@lattice.xyz>

---
+++ packages/cli/src/utils/systems/getRegisterFunctionSelectorsCallData.ts
@@ -0,0 +1,68 @@
+import { tableIdToHex } from "@latticexyz/common";
+import { System } from "./types";
+import { loadFunctionSignatures, toFunctionSelector } from "./utils";
+import { CallData } from "../utils/types";
+
+export function getRegisterFunctionSelectorsCallData(input: {
+  systemName: string;
+  system: System;
+  namespace: string;
+  forgeOutDirectory: string;
+}): CallData[] {
+  // Register system at route
+  const callData: CallData[] = [];
+  const { systemName, namespace, forgeOutDirectory, system } = input;
+
+  if (system.registerFunctionSelectors) {
+    const baseSystemFunctionNames = loadFunctionSignatures("System", forgeOutDirectory).map((sig) => sig.functionName);
+    const functionSignatures = loadFunctionSignatures(systemName, forgeOutDirectory).filter(
+      (sig) => systemName === "System" || !baseSystemFunctionNames.includes(sig.functionName)
+    );
+    const isRoot = namespace === "";
+    for (const { functionName, functionArgs } of functionSignatures) {
+      callData.push(
+        getRegisterFunctionSelectorCallData({
+          namespace,
+          name: system.name,
+          systemName,
+          functionName,
+          functionArgs,
+          isRoot,
+        })
+      );
+    }
+  }
+  return callData;
+}
+
+function getRegisterFunctionSelectorCallData(input: {
+  namespace: string;
+  name: string;
+  systemName: string;
+  functionName: string;
+  functionArgs: string;
+  isRoot: boolean;
+}): CallData {
+  const { namespace, name, systemName, functionName, functionArgs, isRoot } = input;
+  const functionSignature = isRoot
+    ? functionName + functionArgs
+    : `${namespace}_${name}_${functionName}${functionArgs}`;
+
+  if (isRoot) {
+    const worldFunctionSelector = toFunctionSelector(
+      functionSignature === ""
+        ? { functionName: systemName, functionArgs } // Register the system's fallback function as `<systemName>(<args>)`
+        : { functionName, functionArgs }
+    );
+    const systemFunctionSelector = toFunctionSelector({ functionName, functionArgs });
+    return {
+      func: "registerRootFunctionSelector",
+      args: [tableIdToHex(namespace, name), worldFunctionSelector, systemFunctionSelector],
+    };
+  } else {
+    return {
+      func: "registerRootFunctionSelector",
+      args: [tableIdToHex(namespace, name), functionName, functionArgs],
+    };
+  }
+}
---
+++ packages/cli/src/utils/utils/getContractData.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+import path from "path";
+import { Fragment } from "ethers/lib/utils.js";
+import { MUDError } from "@latticexyz/common/errors";
+
+/**
+ * Load the contract's abi and bytecode from the file system
+ * @param contractName: Name of the contract to load
+ */
+export function getContractData(
+  contractName: string,
+  forgeOutDirectory: string
+): { bytecode: string; abi: Fragment[] } {
+  let data: any;
+  const contractDataPath = path.join(forgeOutDirectory, contractName + ".sol", contractName + ".json");
+  try {
+    data = JSON.parse(readFileSync(contractDataPath, "utf8"));
+  } catch (error: any) {
+    throw new MUDError(`Error reading file at ${contractDataPath}`);
+  }
+
+  const bytecode = data?.bytecode?.object;
+  if (!bytecode) throw new MUDError(`No bytecode found in ${contractDataPath}`);
+
+  const abi = data?.abi;
+  if (!abi) throw new MUDError(`No ABI found in ${contractDataPath}`);
+
+  return { abi, bytecode };
+}
