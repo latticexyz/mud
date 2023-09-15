362b3489b3f781c4b2fece33b7d23b40edf9052a
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-15 09:58:12 +0100

    prototype resource type

--- packages/world/src/ResourceSelector.sol
+++ packages/world/src/ResourceSelector.sol
@@ -3,53 +3,65 @@ pragma solidity >=0.8.0;
 import { ROOT_NAMESPACE, ROOT_NAME } from "./constants.sol";
 import { Bytes } from "@latticexyz/store/src/Bytes.sol";
 
-bytes16 constant ROOT_NAMESPACE_STRING = bytes16("ROOT_NAMESPACE");
-bytes16 constant ROOT_NAME_STRING = bytes16("ROOT_NAME");
-
+/**
+ * A ResourceSelector is a 32-byte value that uniquely identifies a resource
+ * - 2 bytes for the resource type
+ * - 14 bytes for the namespace
+ * - 16 bytes for the name
+ */
 library ResourceSelector {
   /**
-   * Create a 32-byte resource selector from a namespace and a name.
-   *
-   * A ResourceSelector is a 32-byte value that uniquely identifies a resource.
-   * The first 16 bytes represent the namespace, the last 16 bytes represent the name.
+   * Create a 32-byte resource selector from the resource type, namespace, and name.
    */
-  function from(bytes16 namespace, bytes16 name) internal pure returns (bytes32) {
-    return bytes32(namespace) | (bytes32(name) >> 128);
+  function from(bytes2 resourceType, bytes14 namespace, bytes16 name) internal pure returns (bytes32) {
+    return bytes32(resourceType) | (bytes32(namespace) >> (8 * 2)) | (bytes32(name) >> (8 * 16));
   }
 
   /**
-   * Create a 32-byte resource selector from a namespace. The selector points to the namespace's root name.
+   * Create a 32-byte resource selector from a resource type and namespace. The selector points to the namespace's root name.
    */
-  function from(bytes16 namespace) internal pure returns (bytes32) {
-    return bytes32(namespace);
+  function from(bytes2 resourceType, bytes14 namespace) internal pure returns (bytes32) {
+    return bytes32(resourceType) | (bytes32(namespace) >> (8 * 2));
   }
 
   /**
    * Get the namespace of a ResourceSelector.
    */
-  function getNamespace(bytes32 resourceSelector) internal pure returns (bytes16) {
-    return bytes16(resourceSelector);
+  function getType(bytes32 resourceSelector) internal pure returns (bytes2) {
+    return bytes2(resourceSelector);
+  }
+
+  /**
+   * Get the namespace of a ResourceSelector.
+   */
+  function getNamespace(bytes32 resourceSelector) internal pure returns (bytes14) {
+    return bytes14(resourceSelector << (8 * 2));
   }
 
   /**
    * Get the name of a ResourceSelector.
    */
   function getName(bytes32 resourceSelector) internal pure returns (bytes16) {
-    return bytes16(resourceSelector << 128);
+    return bytes16(resourceSelector << (8 * 16));
   }
 
   /**
    * Convert a selector to a string for more readable logs
    */
   function toString(bytes32 resourceSelector) internal pure returns (string memory) {
-    bytes16 namespace = getNamespace(resourceSelector);
+    bytes2 resourceType = getType(resourceSelector);
+    bytes14 namespace = getNamespace(resourceSelector);
     bytes16 name = getName(resourceSelector);
     return
       string(
         abi.encodePacked(
-          namespace == ROOT_NAMESPACE ? ROOT_NAMESPACE_STRING : namespace,
-          "/",
-          name == ROOT_NAME ? ROOT_NAME_STRING : name
+          resourceType,
+          ":",
+          // TODO: should we trim these?
+          namespace == ROOT_NAMESPACE ? bytes14("ROOT_NAMESPACE") : namespace,
+          ":",
+          // TODO: should we trim these?
+          name == ROOT_NAME ? bytes16("ROOT_NAME") : name
         )
       );
   }
---
+++ packages/world/src/resourceTypes.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+bytes2 constant RESOURCE_NAMESPACE = "ns";
+bytes2 constant RESOURCE_TABLE = "tb";
+bytes2 constant RESOURCE_TABLE_OFFCHAIN = "tf";
+bytes2 constant RESOURCE_SYSTEM = "sy";
+bytes2 constant RESOURCE_MODULE = "md";
