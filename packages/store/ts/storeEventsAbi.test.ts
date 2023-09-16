b5adeb13556936383c7015cf0f824b43cc50c0c6
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-16 21:07:15 +0100

    add indexed to storeEvents and fix test

--- packages/store/ts/storeEventsAbi.test.ts
+++ packages/store/ts/storeEventsAbi.test.ts
@@ -1,22 +1,23 @@
 import { describe, expect, it } from "vitest";
 import { storeEventsAbi } from "./storeEventsAbi";
 import IStoreAbi from "../out/IStore.sol/IStore.abi.json";
+import { AbiEvent } from "abitype";
 
 // Make sure `storeEvents` stays in sync with Solidity definition/events
 
 describe("storeEventsAbi", () => {
   it("should match the store ABI", () => {
-    const expectedAbi = IStoreAbi.filter((item) => item.type === "event")
+    const expectedEvents = IStoreAbi.filter((item) => item.type === "event") as readonly AbiEvent[];
+    const expectedAbi = expectedEvents
       .map((item) => ({
-        // just return data that abitype cares about
+        // return data in a shape that matches abitype's parseAbi
         type: item.type,
         name: item.name,
-        inputs: [
-          ...item.inputs.map((input) => ({
-            name: input.name,
-            type: input.type,
-          })),
-        ],
+        inputs: item.inputs.map((input) => ({
+          name: input.name,
+          type: input.type,
+          ...(input.indexed ? { indexed: true } : null),
+        })),
       }))
       .sort((a, b) => a.name.localeCompare(b.name));
 
