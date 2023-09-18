b3c22a183c0b288b9eb1487e4fef125bf7dae915
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-18 21:25:08 +0100

    refactor(store-indexer): add readme, refactor common env (#1533)

--- packages/store-indexer/bin/sqlite-indexer.ts
+++ packages/store-indexer/bin/sqlite-indexer.ts
@@ -13,30 +13,13 @@ import { chainState, schemaVersion, syncToSqlite } from "@latticexyz/store-sync/
 import { createQueryAdapter } from "../src/sqlite/createQueryAdapter";
 import { isDefined } from "@latticexyz/common/utils";
 import { combineLatest, filter, first } from "rxjs";
+import { parseEnv } from "./parseEnv";
 
-const env = z
-  .intersection(
-    z.object({
-      HOST: z.string().default("0.0.0.0"),
-      PORT: z.coerce.number().positive().default(3001),
-      SQLITE_FILENAME: z.string().default("indexer.db"),
-      START_BLOCK: z.coerce.bigint().nonnegative().default(0n),
-      MAX_BLOCK_RANGE: z.coerce.bigint().positive().default(1000n),
-      POLLING_INTERVAL: z.coerce.number().positive().default(1000),
-    }),
-    z
-      .object({
-        RPC_HTTP_URL: z.string(),
-        RPC_WS_URL: z.string(),
-      })
-      .partial()
-      .refine((values) => Object.values(values).some(isDefined))
-  )
-  .parse(process.env, {
-    errorMap: (issue) => ({
-      message: `Missing or invalid environment variable: ${issue.path.join(".")}`,
-    }),
-  });
+const env = parseEnv(
+  z.object({
+    SQLITE_FILENAME: z.string().default("indexer.db"),
+  })
+);
 
 const transports: Transport[] = [
   // prefer WS when specified
