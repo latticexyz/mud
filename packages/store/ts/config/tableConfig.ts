c4a1a5c90a4e228ee26c24d146442ed958216f57
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-19 19:55:46 +0100

    hitting limits of zod

--- packages/store/ts/config/tableConfig.ts
+++ packages/store/ts/config/tableConfig.ts
@@ -1,5 +1,6 @@
+import { tableIdToHex } from "@latticexyz/common";
 import { SchemaAbiType, StaticAbiType, schemaAbiTypes, staticAbiTypes } from "@latticexyz/schema-type";
-import { z } from "zod";
+import { ZodAny, ZodTypeAny, z } from "zod";
 
 // TODO: combine with protocol-parser def and move up to schema-type?
 export type KeySchema = Readonly<Record<string, StaticAbiType>>;
@@ -22,41 +23,93 @@ export type TableConfig = Readonly<{
   };
 }>;
 
-const tableSchema = z.object({
-  type: z.enum(["table", "offchainTable"]).default("table").optional(),
-  namespace: z.string().optional(),
-  name: z.string().optional(),
-  // TODO: refine/validate key/field names
-  keySchema: z.record(z.enum(staticAbiTypes)).default({ key: "bytes32" }).optional(),
-  valueSchema: z.record(z.enum(schemaAbiTypes)),
+const configSchema = z.object({
+  namespace: z.string().default(""),
+  // TODO: refine/validate table names
+  tables: z
+    .record(
+      z.string(),
+      z.object({
+        type: z.enum(["table", "offchainTable"]).default("table"),
+        namespace: z.string().optional(),
+        // TODO: refine/validate key/field names
+        keySchema: z.record(z.enum(staticAbiTypes)).default({ key: "bytes32" } as const),
+        valueSchema: z.record(z.enum(schemaAbiTypes)),
+        // TODO: codegen
+      })
+    )
+    .default({}),
   // TODO: codegen
 });
 
-// TODO: refine/validate table names
-const tablesSchema = z.record(tableSchema);
+type ConfigInput = z.input<typeof configSchema>;
+type ConfigInputParsed = z.output<typeof configSchema>;
 
-const namespaceSchema = z.object({
-  tables: tablesSchema.optional(),
-});
+type TableInput = ConfigInputParsed["tables"][string];
+type TableInputParsed = ConfigInputParsed["tables"][string];
+type TableOutput<defaultNamespace extends string, tableName extends string, table extends TableInput> = table & {
+  namespace: table["namespace"] extends string ? table["namespace"] : defaultNamespace;
+  name: tableName;
+  tableId: `0x${string}`;
+};
 
-// TODO: overall codegen options
-const configSchema = namespaceSchema.merge(
-  z.object({
-    // TODO: refine/validate namespace keys
-    namespaces: z.record(namespaceSchema).optional(),
-  })
-);
+type ConfigOutput<input extends ConfigInput, parsed extends ConfigInputParsed> = {
+  tables: {
+    [tableName in keyof NonNullable<input["tables"]> & string]: TableOutput<
+      input["namespace"] extends string ? input["namespace"] : "",
+      tableName,
+      NonNullable<input["tables"]>[tableName]
+    >;
+  };
+};
 
-function storeConfig<TConfig extends z.infer<typeof configSchema>>(
-  config: TConfig
-): Readonly<{ tables: TableConfig[] }> {
-  const parsed = configSchema.parse(config);
-  return { tables: [] };
+type ExpandedConfig<
+  schema extends ZodTypeAny,
+  input extends z.input<schema> = z.input<schema>,
+  output extends z.output<schema> = z.output<schema>
+> = {
+  tables: {
+    [tableName in keyof output["tables"] & string]: output["tables"][tableName] & {
+      namespace: output["tables"][tableName]["namespace"] extends string
+        ? output["tables"][tableName]["namespace"]
+        : output["namespace"];
+    };
+  };
+};
+
+function transformTable<namespace extends string, tableName extends string, table extends TableInputParsed>(
+  namespace: namespace,
+  tableName: tableName,
+  table: table
+): TableOutput<namespace, tableName, table> {
+  return {
+    ...table,
+    namespace,
+    name: tableName,
+    tableId: tableIdToHex(namespace, tableName),
+  };
+}
+
+function storeConfig<TConfig extends ConfigInput>(config: TConfig): ConfigOutput<TConfig> {
+  const parsedConfig = configSchema.parse(config);
+
+  const tables = Object.fromEntries(
+    Object.entries(parsedConfig.tables).map(([tableName, table]) => [
+      tableName,
+      transformTable(table.namespace ?? parsedConfig.namespace, tableName, table),
+    ])
+  ) as ConfigOutput<TConfig>["tables"];
+
+  return {
+    tables,
+  };
 }
 
 const config = storeConfig({
+  namespace: "hello",
   tables: {
     Position: {
+      namespace: "overridden",
       keySchema: {
         x: "uint32",
         y: "uint32",
@@ -65,5 +118,19 @@ const config = storeConfig({
         entity: "bytes32",
       },
     },
+    Entity: {
+      valueSchema: {
+        dead: "bool",
+      },
+    },
   },
-});
+} as const);
+
+config.tables.Position.namespace;
+//                      ^?
+config.tables.Position.name;
+//                      ^?
+config.tables.Position.tableId;
+//                      ^?
+config.tables.Entity.keySchema;
+//                      ^?
