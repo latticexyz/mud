43ad7223ca020873084ab83553ef98f8932676f7
alvarius <alvarius@lattice.xyz>
2023-09-20 20:16:20 +0100

    Update packages/world/src/modules/keyswithvalue/constants.sol
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/world/src/modules/keyswithvalue/constants.sol
+++ packages/world/src/modules/keyswithvalue/constants.sol
@@ -4,4 +4,4 @@ pragma solidity >=0.8.0;
 // Limiting the module namespace to 7 bytes so the remaining 7 bytes
 // can be used for an identifier of the source table namespace to avoid
 // collisions between tables with the same name in different namespaces
-bytes7 constant MODULE_NAMESPACE = "kyswval";
+bytes7 constant MODULE_NAMESPACE = "keywval";
