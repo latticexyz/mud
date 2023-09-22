8ef49ae693282edcfd3d37238181c4e5cce9d05e
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-22 11:18:39 +0100

    feat(store-indexer,store-sync): support filtering indexer query by tableIds

--- packages/store-sync/src/trpc-indexer/common.ts
+++ packages/store-sync/src/trpc-indexer/common.ts
@@ -2,10 +2,7 @@ import { Hex } from "viem";
 import { TableWithRecords } from "../common";
 
 export type QueryAdapter = {
-  findAll: (
-    chainId: number,
-    address?: Hex
-  ) => Promise<{
+  findAll: (opts: { chainId: number; address?: Hex; tableIds?: Hex[] }) => Promise<{
     blockNumber: bigint | null;
     tables: TableWithRecords[];
   }>;
