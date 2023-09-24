a97f5173ee683c48314f2bafffab6ffe5c3d8b20
alvrs <alvarius@lattice.xyz>
2023-09-24 18:03:04 +0100

    fix examples

--- examples/minimal/packages/contracts/test/ChatNamespaced.t.sol
+++ examples/minimal/packages/contracts/test/ChatNamespaced.t.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.21;
 
 import "forge-std/Test.sol";
 import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
-import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
+import { getKeysWithValue } from "@latticexyz/world-modules/src/modules/keyswithvalue/getKeysWithValue.sol";
 import { StoreCore } from "@latticexyz/store/src/StoreCore.sol";
 
 import { IWorld } from "../src/codegen/world/IWorld.sol";
