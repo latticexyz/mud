9517cfd9091deeba583856d033675f70d0923ca4
alvrs <alvarius@lattice.xyz>
2023-09-24 17:44:17 +0100

    remove optional modules from world

--- packages/world/src/modules/keysintable/constants.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.21;
-
-// Limiting the module namespace to 8 bytes so the last 8 bytes
-// can be used for an identifier of the source table namespace to avoid
-// collisions between tables with the same name in different namespaces
-bytes8 constant MODULE_NAMESPACE = "keystab";
--- packages/world/src/modules/keyswithvalue/constants.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.21;
-
-// Limiting the module namespace to 7 bytes so the remaining 7 bytes
-// can be used for an identifier of the source table namespace to avoid
-// collisions between tables with the same name in different namespaces
-bytes7 constant MODULE_NAMESPACE = "keywval";
