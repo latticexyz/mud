a2bca5748f3cee08c65c2d9a769cf8d1b15900ca
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-25 14:50:55 +0000

    remove unused imports

--- packages/store/src/StoreData.sol
+++ packages/store/src/StoreData.sol
@@ -2,13 +2,8 @@
 pragma solidity >=0.8.21;
 
 import { STORE_VERSION } from "./version.sol";
-import { IStore, IStoreData } from "./IStore.sol";
+import { IStoreData } from "./IStore.sol";
 import { StoreRead } from "./StoreRead.sol";
-import { StoreCore } from "./StoreCore.sol";
-import { FieldLayout } from "./FieldLayout.sol";
-import { Schema } from "./Schema.sol";
-import { PackedCounter } from "./PackedCounter.sol";
-import { ResourceId } from "./ResourceId.sol";
 
 abstract contract StoreData is IStoreData, StoreRead {
   constructor() {
