c4f49240d7767c3fa7a25926f74b4b62ad67ca04
dk1a <dk1a@protonmail.com>
2023-09-25 03:53:40 +0300

    fix(common): fix memory corruption for dynamic to static array conversion (#1598)
    Co-authored-by: alvarius <alvarius@lattice.xyz>

--- packages/cli/contracts/src/codegen/tables/Dynamics1.sol
+++ packages/cli/contracts/src/codegen/tables/Dynamics1.sol
@@ -1495,37 +1495,67 @@ library Dynamics1 {
 }
 
 function toStaticArray_bytes32_1(bytes32[] memory _value) pure returns (bytes32[1] memory _result) {
-  // in memory static arrays are just dynamic arrays without the length byte
-  assembly {
-    _result := add(_value, 0x20)
+  if (_value.length < 1) {
+    // return an uninitialized array if the length is smaller than the fixed length to avoid memory corruption
+    return _result;
+  } else {
+    // in memory static arrays are just dynamic arrays without the length byte
+    // (without the length check this could lead to memory corruption)
+    assembly {
+      _result := add(_value, 0x20)
+    }
   }
 }
 
 function toStaticArray_int32_2(int32[] memory _value) pure returns (int32[2] memory _result) {
-  // in memory static arrays are just dynamic arrays without the length byte
-  assembly {
-    _result := add(_value, 0x20)
+  if (_value.length < 2) {
+    // return an uninitialized array if the length is smaller than the fixed length to avoid memory corruption
+    return _result;
+  } else {
+    // in memory static arrays are just dynamic arrays without the length byte
+    // (without the length check this could lead to memory corruption)
+    assembly {
+      _result := add(_value, 0x20)
+    }
   }
 }
 
 function toStaticArray_uint128_3(uint128[] memory _value) pure returns (uint128[3] memory _result) {
-  // in memory static arrays are just dynamic arrays without the length byte
-  assembly {
-    _result := add(_value, 0x20)
+  if (_value.length < 3) {
+    // return an uninitialized array if the length is smaller than the fixed length to avoid memory corruption
+    return _result;
+  } else {
+    // in memory static arrays are just dynamic arrays without the length byte
+    // (without the length check this could lead to memory corruption)
+    assembly {
+      _result := add(_value, 0x20)
+    }
   }
 }
 
 function toStaticArray_address_4(address[] memory _value) pure returns (address[4] memory _result) {
-  // in memory static arrays are just dynamic arrays without the length byte
-  assembly {
-    _result := add(_value, 0x20)
+  if (_value.length < 4) {
+    // return an uninitialized array if the length is smaller than the fixed length to avoid memory corruption
+    return _result;
+  } else {
+    // in memory static arrays are just dynamic arrays without the length byte
+    // (without the length check this could lead to memory corruption)
+    assembly {
+      _result := add(_value, 0x20)
+    }
   }
 }
 
 function toStaticArray_bool_5(bool[] memory _value) pure returns (bool[5] memory _result) {
-  // in memory static arrays are just dynamic arrays without the length byte
-  assembly {
-    _result := add(_value, 0x20)
+  if (_value.length < 5) {
+    // return an uninitialized array if the length is smaller than the fixed length to avoid memory corruption
+    return _result;
+  } else {
+    // in memory static arrays are just dynamic arrays without the length byte
+    // (without the length check this could lead to memory corruption)
+    assembly {
+      _result := add(_value, 0x20)
+    }
   }
 }
 
