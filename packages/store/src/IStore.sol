7742e86d056d80cdd8b3f0c90bc28ff8a69020f1
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-25 15:04:35 +0000

    move events to its own file for visibility

--- packages/store/src/IStore.sol
+++ packages/store/src/IStore.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.21;
 
 import { IStoreErrors } from "./IStoreErrors.sol";
+import { IStoreEvents } from "./IStoreEvents.sol";
 import { PackedCounter } from "./PackedCounter.sol";
 import { FieldLayout } from "./FieldLayout.sol";
 import { Schema } from "./Schema.sol";
@@ -112,26 +113,6 @@ interface IStoreRead {
     uint256 start,
     uint256 end
   ) external view returns (bytes memory data);
-}
-
-interface IStoreEvents {
-  event Store_SetRecord(
-    ResourceId indexed tableId,
-    bytes32[] keyTuple,
-    bytes staticData,
-    PackedCounter encodedLengths,
-    bytes dynamicData
-  );
-  event Store_SpliceStaticData(ResourceId indexed tableId, bytes32[] keyTuple, uint48 start, bytes data);
-  event Store_SpliceDynamicData(
-    ResourceId indexed tableId,
-    bytes32[] keyTuple,
-    uint48 start,
-    uint40 deleteCount,
-    PackedCounter encodedLengths,
-    bytes data
-  );
-  event Store_DeleteRecord(ResourceId indexed tableId, bytes32[] keyTuple);
 }
 
 interface IStoreWrite is IStoreEvents {
