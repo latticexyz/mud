f602dc90a171ebb39fdb2c5e3739425afc951009
alvrs <alvarius@lattice.xyz>
2023-09-25 02:05:56 +0100

    move store test tables to test folder

--- packages/store/test/Gas.t.sol
+++ packages/store/test/Gas.t.sol
@@ -7,7 +7,8 @@ import { Bytes } from "../src/Bytes.sol";
 import { SliceLib } from "../src/Slice.sol";
 import { Storage } from "../src/Storage.sol";
 import { PackedCounter } from "../src/PackedCounter.sol";
-import { Mixed, MixedData } from "../src/codegen/index.sol";
+
+import { Mixed, MixedData } from "./codegen/index.sol";
 
 contract SomeContract {
   function doSomethingWithBytes(bytes memory data) public {}
