279b15014c42abda622537cd2abddc5ad8e1a2da
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-27 18:28:57 -0500

    docs(natspec): world/*[Ss]ys*

--- packages/world/src/SystemHook.sol
+++ packages/world/src/SystemHook.sol
@@ -4,8 +4,19 @@ pragma solidity >=0.8.21;
 import { ISystemHook, SYSTEM_HOOK_INTERFACE_ID } from "./ISystemHook.sol";
 import { ERC165_INTERFACE_ID } from "./IERC165.sol";
 
+/**
+ * @title SystemHook
+ * @dev The SystemHook contract provides a base implementation for system hooks.
+ * It includes the functionality to support ERC-165 interface identification.
+ * System hooks are used for executing additional logic before or after certain system actions.
+ */
 abstract contract SystemHook is ISystemHook {
-  // ERC-165 supportsInterface (see https://eips.ethereum.org/EIPS/eip-165)
+  /**
+   * @notice Checks if the contract implements a given interface.
+   * @dev Overridden from IERC165 to include the system hook interface.
+   * @param interfaceId The bytes4 interface identifier, as specified in ERC-165.
+   * @return true if the contract implements `interfaceId` and the identifier is not 0xffffffff; false otherwise.
+   */
   function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
     return interfaceId == SYSTEM_HOOK_INTERFACE_ID || interfaceId == ERC165_INTERFACE_ID;
   }
--- packages/world/src/systemHookTypes.sol
+++ packages/world/src/systemHookTypes.sol
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
+/**
+ * @title System Hook Types
+ * @dev This module provides constants for defining the different types of system hooks.
+ * System hooks can be used to execute additional logic before or after system actions.
+ */
+
+/**
+ * @notice Constant representing a hook that is triggered before a system call.
+ * @dev This constant uses a bit-wise left shift for unique identification.
+ */
 uint8 constant BEFORE_CALL_SYSTEM = 1 << 0;
+
+/**
+ * @notice Constant representing a hook that is triggered after a system call.
+ * @dev This constant uses a bit-wise left shift for unique identification.
+ */
 uint8 constant AFTER_CALL_SYSTEM = 1 << 1;
 
+/**
+ * @notice Constant representing all types of system hooks.
+ * @dev It's a combination of all the system hook types.
+ */
 uint8 constant ALL = BEFORE_CALL_SYSTEM | AFTER_CALL_SYSTEM;
