0ebd5e70241ed0db49e9502f792124d83edbb3f2
alvrs <alvarius@lattice.xyz>
2023-09-27 15:05:04 +0100

    document IStoreData

--- packages/store/src/IStoreData.sol
+++ packages/store/src/IStoreData.sol
@@ -5,12 +5,20 @@ import { IStoreRead } from "./IStoreRead.sol";
 import { IStoreWrite } from "./IStoreWrite.sol";
 
 /**
- * The IStoreData interface includes methods for reading and writing table values.
- * These methods are frequently invoked during runtime, so it is essential to prioritize
- * optimizing their gas cost
+ * @title IStoreData
+ * @notice The IStoreData interface includes methods for reading and writing table values.
+ * @dev These methods are frequently invoked during runtime, so it is essential to prioritize optimizing their gas cost.
  */
 interface IStoreData is IStoreRead, IStoreWrite {
+  /**
+   * @notice Emitted when the store is initialized.
+   * @param storeVersion The version of the Store contract.
+   */
   event HelloStore(bytes32 indexed storeVersion);
 
-  function storeVersion() external view returns (bytes32);
+  /**
+   * @notice Returns the version of the Store contract.
+   * @return version The version of the Store contract.
+   */
+  function storeVersion() external view returns (bytes32 version);
 }
