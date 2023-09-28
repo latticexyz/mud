ad8edca97efc1b8a6595922286fe29673f59743c
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-27 22:07:18 -0500

    Still trying

--- packages/world/src/version.sol
+++ packages/world/src/version.sol
@@ -2,9 +2,9 @@
 pragma solidity >=0.8.21;
 
 /**
- * @notice World Version Constant
- * @notice Defines the version identifier for the World contract or module.
- * @dev This version identifier can be used for version checks, logging, and more.
+ * @dev World Version Constant
+ * Defines the version identifier for the World contract or module.
+ * This version identifier can be used for version checks, logging, and more.
  */
 
 bytes32 constant WORLD_VERSION = "1.0.0-unaudited";
