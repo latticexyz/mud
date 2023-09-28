345cb62f8fd233fe8d6816c6ea689f68a7115321
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-27 21:34:41 -0500

    Fix test error:
    ```
    @latticexyz/world:build: Error (6546): Documentation tag @notice not valid for file-level variables.
    @latticexyz/world:build:   --> src/systemHookTypes.sol:10:1:
    @latticexyz/world:build:    |
    @latticexyz/world:build: 10 | /**
    @latticexyz/world:build:    | ^ (Relevant source part starts here and spans across multiple lines).
    @latticexyz/world:build:
    @latticexyz/world:build: Error (6546): Documentation tag @notice not valid for file-level variables.
    @latticexyz/world:build:   --> src/systemHookTypes.sol:16:1:
    @latticexyz/world:build:    |
    @latticexyz/world:build: 16 | /**
    @latticexyz/world:build:    | ^ (Relevant source part starts here and spans across multiple lines).
    @latticexyz/world:build:
    @latticexyz/world:build: Error (6546): Documentation tag @notice not valid for file-level variables.
    @latticexyz/world:build:   --> src/systemHookTypes.sol:22:1:
    @latticexyz/world:build:    |
    @latticexyz/world:build: 22 | /**
    @latticexyz/world:build:    | ^ (Relevant source part starts here and spans across multiple lines).
    @latticexyz/world:build:
    ```

--- packages/world/src/systemHookTypes.sol
+++ packages/world/src/systemHookTypes.sol
@@ -8,19 +8,19 @@ pragma solidity >=0.8.21;
  */
 
 /**
- * @notice Constant representing a hook that is triggered before a system call.
+ * @title Constant representing a hook that is triggered before a system call.
  * @dev This constant uses a bit-wise left shift for unique identification.
  */
 uint8 constant BEFORE_CALL_SYSTEM = 1 << 0;
 
 /**
- * @notice Constant representing a hook that is triggered after a system call.
+ * @title Constant representing a hook that is triggered after a system call.
  * @dev This constant uses a bit-wise left shift for unique identification.
  */
 uint8 constant AFTER_CALL_SYSTEM = 1 << 1;
 
 /**
- * @notice Constant representing all types of system hooks.
+ * @title Constant representing all types of system hooks.
  * @dev It's a combination of all the system hook types.
  */
 uint8 constant ALL = BEFORE_CALL_SYSTEM | AFTER_CALL_SYSTEM;
