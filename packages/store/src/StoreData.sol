45ad5849d283ab6475148b89e9eea9e085570888
Ori Pomeerantz <qbzzt1@gmail.com>
2023-09-28 14:43:49 -0500

    docs(natspec): store/Store* (except for StoreCore)

--- packages/store/src/StoreData.sol
+++ packages/store/src/StoreData.sol
@@ -6,12 +6,25 @@ import { IStoreData } from "./IStoreData.sol";
 import { StoreRead } from "./StoreRead.sol";
 import { StoreCore } from "./StoreCore.sol";
 
+/**
+ * @title Store Data Contract
+ * @notice This contract integrates the core storage functionalities and provides an interface for data storage.
+ * @dev This is an abstract contract and should be inherited by other contracts that need storage functionalities.
+ */
 abstract contract StoreData is IStoreData, StoreRead {
+  /**
+   * @notice Constructs the StoreData contract and initializes the StoreCore.
+   * @dev Emits a HelloStore event upon creation.
+   */
   constructor() {
     StoreCore.initialize();
     emit HelloStore(STORE_VERSION);
   }
 
+  /**
+   * @notice Retrieves the version of the store.
+   * @return The current version of the store as a bytes32.
+   */
   function storeVersion() public pure returns (bytes32) {
     return STORE_VERSION;
   }
--- packages/store/src/StoreRead.sol
+++ packages/store/src/StoreRead.sol
@@ -9,19 +9,46 @@ import { Schema } from "./Schema.sol";
 import { PackedCounter } from "./PackedCounter.sol";
 import { ResourceId } from "./ResourceId.sol";
 
+/**
+ * @title StoreRead
+ * @dev A contract that provides read operations for storage tables using `StoreCore`.
+ */
 contract StoreRead is IStoreRead {
+  /**
+   * @notice Fetches the field layout for a given table.
+   * @param tableId The ID of the table for which to retrieve the field layout.
+   * @return fieldLayout The layout of fields in the specified table.
+   */
   function getFieldLayout(ResourceId tableId) public view virtual returns (FieldLayout fieldLayout) {
     fieldLayout = StoreCore.getFieldLayout(tableId);
   }
 
+  /**
+   * @notice Retrieves the value schema for a given table.
+   * @param tableId The ID of the table.
+   * @return valueSchema The schema for values in the specified table.
+   */
   function getValueSchema(ResourceId tableId) public view virtual returns (Schema valueSchema) {
     valueSchema = StoreCore.getValueSchema(tableId);
   }
 
+  /**
+   * @notice Retrieves the key schema for a given table.
+   * @param tableId The ID of the table.
+   * @return keySchema The schema for keys in the specified table.
+   */
   function getKeySchema(ResourceId tableId) public view virtual returns (Schema keySchema) {
     keySchema = StoreCore.getKeySchema(tableId);
   }
 
+  /**
+   * @notice Fetches a record from a specified table using a provided key tuple.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the record.
+   * @return staticData The static data of the record.
+   * @return encodedLengths Encoded lengths of dynamic data.
+   * @return dynamicData The dynamic data of the record.
+   */
   function getRecord(
     ResourceId tableId,
     bytes32[] calldata keyTuple
@@ -29,6 +56,15 @@ contract StoreRead is IStoreRead {
     return StoreCore.getRecord(tableId, keyTuple);
   }
 
+  /**
+   * @notice Fetches a record from a specified table using a provided key tuple and field layout.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the record.
+   * @param fieldLayout The layout of fields to retrieve.
+   * @return staticData The static data of the record.
+   * @return encodedLengths Encoded lengths of dynamic data.
+   * @return dynamicData The dynamic data of the record.
+   */
   function getRecord(
     ResourceId tableId,
     bytes32[] calldata keyTuple,
@@ -37,6 +73,13 @@ contract StoreRead is IStoreRead {
     return StoreCore.getRecord(tableId, keyTuple, fieldLayout);
   }
 
+  /**
+   * @notice Retrieves data for a specified field in a record.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the field.
+   * @param fieldIndex Index of the field to retrieve.
+   * @return data The data of the specified field.
+   */
   function getField(
     ResourceId tableId,
     bytes32[] calldata keyTuple,
@@ -45,6 +88,14 @@ contract StoreRead is IStoreRead {
     data = StoreCore.getField(tableId, keyTuple, fieldIndex);
   }
 
+  /**
+   * @notice Retrieves data for a specified field in a record, considering a field layout.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the field.
+   * @param fieldIndex Index of the field to retrieve.
+   * @param fieldLayout The layout of fields for the retrieval.
+   * @return data The data of the specified field.
+   */
   function getField(
     ResourceId tableId,
     bytes32[] calldata keyTuple,
@@ -54,6 +105,14 @@ contract StoreRead is IStoreRead {
     data = StoreCore.getField(tableId, keyTuple, fieldIndex, fieldLayout);
   }
 
+  /**
+   * @notice Retrieves data for a specific static (fixed length) field in a record.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the static field.
+   * @param fieldIndex Index of the static field to retrieve.
+   * @param fieldLayout The layout of fields for the retrieval.
+   * @return data The static data of the specified field.
+   */
   function getStaticField(
     ResourceId tableId,
     bytes32[] calldata keyTuple,
@@ -63,6 +122,13 @@ contract StoreRead is IStoreRead {
     data = StoreCore.getStaticField(tableId, keyTuple, fieldIndex, fieldLayout);
   }
 
+  /**
+   * @notice Retrieves data for a specific dynamic (variable length) field in a record.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the dynamic field.
+   * @param dynamicFieldIndex Index of the dynamic field to retrieve.
+   * @return data The dynamic data of the specified field.
+   */
   function getDynamicField(
     ResourceId tableId,
     bytes32[] calldata keyTuple,
@@ -71,6 +137,13 @@ contract StoreRead is IStoreRead {
     data = StoreCore.getDynamicField(tableId, keyTuple, dynamicFieldIndex);
   }
 
+  /**
+   * @notice Calculates the length of a specified field in a record.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key.
+   * @param fieldIndex Index of the field to measure.
+   * @return The length of the specified field.
+   */
   function getFieldLength(
     ResourceId tableId,
     bytes32[] memory keyTuple,
@@ -79,6 +152,14 @@ contract StoreRead is IStoreRead {
     return StoreCore.getFieldLength(tableId, keyTuple, fieldIndex);
   }
 
+  /**
+   * @notice Calculates the length of a specified field in a record, considering a field layout.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key.
+   * @param fieldIndex Index of the field to measure.
+   * @param fieldLayout The layout of fields for measurement.
+   * @return The length of the specified field.
+   */
   function getFieldLength(
     ResourceId tableId,
     bytes32[] memory keyTuple,
@@ -88,6 +169,13 @@ contract StoreRead is IStoreRead {
     return StoreCore.getFieldLength(tableId, keyTuple, fieldIndex, fieldLayout);
   }
 
+  /**
+   * @notice Calculates the length of a specified dynamic (variable length) field in a record.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key.
+   * @param dynamicFieldIndex Index of the dynamic field to measure.
+   * @return The length of the specified dynamic field.
+   */
   function getDynamicFieldLength(
     ResourceId tableId,
     bytes32[] memory keyTuple,
@@ -95,6 +183,16 @@ contract StoreRead is IStoreRead {
   ) public view virtual returns (uint256) {
     return StoreCore.getFieldLength(tableId, keyTuple, dynamicFieldIndex);
   }
+
+  /**
+   * @notice Retrieves a slice of a dynamic (variable length) field.
+   * @param tableId The ID of the table.
+   * @param keyTuple The tuple used as a key to fetch the dynamic field slice.
+   * @param dynamicFieldIndex Index of the dynamic field to slice.
+   * @param start The starting position of the slice.
+   * @param end The ending position of the slice.
+   * @return The sliced data from the specified dynamic field.
+   */
 
   function getDynamicFieldSlice(
     ResourceId tableId,
