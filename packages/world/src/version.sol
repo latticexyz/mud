d1b870f64b811e6d8bed45529adbf642cdeb9ab5
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-27 22:02:23 -0500

    still figuring out how to get the NatSpec past the tests

--- packages/world/src/version.sol
+++ packages/world/src/version.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.21;
 
 /**
- * @title World Version Constant
+ * @notice World Version Constant
  * @notice Defines the version identifier for the World contract or module.
  * @dev This version identifier can be used for version checks, logging, and more.
  */
