da6e0de8c6c2e6888f6d861a0de574bf11fac2c3
Kevin Ingersoll <kingersoll@gmail.com>
2023-09-28 15:05:46 +0000

    feat(services): remove all but faucet client (#1635)

--- packages/services/pkg/mode/errors.go
+++
@@ -1,6 +0,0 @@
-package mode
-
-import "errors"
-
-var ErrTableDoesNotExist = errors.New("table does not exist")
-var ErrTableIsEmpty = errors.New("table is empty")
--- packages/services/pkg/utils/constants.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "time"
-
-// RetryAttempts is how many attempts are made to reconnect to an Ethereum client before failing.
-const RetryAttempts = 60 * 60
-
-// RetryDelay is the delay between reconnects to an Ethereum client.
-const RetryDelay = 1 * time.Second
