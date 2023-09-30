cf2104eb152e67540a22fff99e379b8ce60ec005
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-29 19:35:58 -0500

    docs(world): add NatSpec to CoreModule (#1664)
    Co-authored-by: alvarius <alvarius@lattice.xyz>

--- packages/world/src/modules/core/implementations/ModuleInstallationSystem.sol
+++ packages/world/src/modules/core/implementations/ModuleInstallationSystem.sol
@@ -10,11 +10,16 @@ import { InstalledModules } from "../../../codegen/tables/InstalledModules.sol";
 import { requireInterface } from "../../../requireInterface.sol";
 
 /**
- * Installation of (non-root) modules in the World.
+ * @title Module Installation System
+ * @dev A system contract to handle the installation of (non-root) modules in the World.
  */
 contract ModuleInstallationSystem is System {
   /**
-   * Install the given module at the given namespace in the World.
+   * @notice Installs a module into the World under a specified namespace.
+   * @dev Validates the given module against the IModule interface and delegates the installation process.
+   * The module is then registered in the InstalledModules table.
+   * @param module The module to be installed.
+   * @param args Arguments for the module installation.
    */
   function installModule(IModule module, bytes memory args) public {
     // Require the provided address to implement the IModule interface
