3086581a2c2acd8b8f0d85fd5a4c7571e7982660
Ori Pomerantz <qbzzt1@gmail.com>
2023-09-29 19:12:48 -0500

    docs(store): add NatSpec to Store utils (#1648)
    Co-authored-by: alvarius <alvarius@lattice.xyz>

--- packages/store/src/leftMask.sol
+++ packages/store/src/leftMask.sol
@@ -2,19 +2,27 @@
 pragma solidity >=0.8.21;
 
 /**
- * Adapted from https://github.com/dk1a/solidity-stringutils/blob/main/src/utils/mem.sol#L149-L167
- * @dev Left-aligned byte mask (e.g. for partial mload/mstore).
- * For byteLength >= 32 returns type(uint256).max
- *
- * length 0:   0x000000...000000
- * length 1:   0xff0000...000000
- * length 2:   0xffff00...000000
- * ...
- * length 30:  0xffffff...ff0000
- * length 31:  0xffffff...ffff00
- * length 32+: 0xffffff...ffffff
+ * @title Byte Mask Utility
+ * @notice Utility functions to manage bytes in memory.
+ * @dev Adapted from https://github.com/dk1a/solidity-stringutils/blob/main/src/utils/mem.sol#L149-L167
  */
-function leftMask(uint256 byteLength) pure returns (uint256) {
+
+/**
+ * @notice Computes a left-aligned byte mask based on the provided byte length.
+ * @dev The mask is used to extract a specified number of leftmost bytes.
+ *      For byte lengths greater than or equal to 32, it returns the max value of type(uint256).
+ *      Examples:
+ *          length 0:   0x000000...000000
+ *          length 1:   0xff0000...000000
+ *          length 2:   0xffff00...000000
+ *          ...
+ *          length 30:  0xffffff...ff0000
+ *          length 31:  0xffffff...ffff00
+ *          length 32+: 0xffffff...ffffff
+ * @param byteLength The number of leftmost bytes to be masked.
+ * @return mask A left-aligned byte mask corresponding to the specified byte length.
+ */
+function leftMask(uint256 byteLength) pure returns (uint256 mask) {
   unchecked {
     return ~(type(uint256).max >> (byteLength * 8));
   }
--- packages/store/src/version.sol
+++ packages/store/src/version.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
+/**
+ * @title Store Versioning
+ * @notice Contains a constant representing the version of the store.
+ */
+
+/// @dev Identifier for the current store version.
 bytes32 constant STORE_VERSION = "1.0.0-unaudited";
