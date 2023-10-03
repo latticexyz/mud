52daba2f7987abd5b155bab1640622aff28f047f
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-03 16:10:37 +0100

    revert example change

--- examples/minimal/packages/client-react/src/mud/setupNetwork.ts
+++ examples/minimal/packages/client-react/src/mud/setupNetwork.ts
@@ -16,7 +16,7 @@ export async function setupNetwork() {
 
   const clientOptions = {
     chain: networkConfig.chain,
-    transport: transportObserver(http()),
+    transport: transportObserver(fallback([webSocket(), http()])),
     pollingInterval: 1000,
   } as const satisfies ClientConfig;
 
