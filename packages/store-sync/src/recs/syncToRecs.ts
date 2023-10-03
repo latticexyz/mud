c714e5d17ebed86fe93260f7381a0844d9697958
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-03 09:45:19 +0100

    feat(recs,store-sync): skip update stream when hydrating (#1661)

--- packages/store-sync/src/recs/syncToRecs.ts
+++ packages/store-sync/src/recs/syncToRecs.ts
@@ -1,5 +1,5 @@
 import { StoreConfig } from "@latticexyz/store";
-import { World as RecsWorld, getComponentValue, setComponent } from "@latticexyz/recs";
+import { Component as RecsComponent, World as RecsWorld, getComponentValue, setComponent } from "@latticexyz/recs";
 import { SyncOptions, SyncResult } from "../common";
 import { RecsStorageAdapter, recsStorage } from "./recsStorage";
 import { createStoreSync } from "../createStoreSync";
@@ -28,7 +28,12 @@ export async function syncToRecs<TConfig extends StoreConfig = StoreConfig>({
   indexerUrl,
   startSync = true,
 }: SyncToRecsOptions<TConfig>): Promise<SyncToRecsResult<TConfig>> {
-  const { storageAdapter, components } = recsStorage({ world, config });
+  const { storageAdapter, components } = recsStorage({
+    world,
+    config,
+    shouldSkipUpdateStream: (): boolean =>
+      getComponentValue(components.SyncProgress, singletonEntity)?.step !== SyncStep.LIVE,
+  });
 
   const storeSync = await createStoreSync({
     storageAdapter,
@@ -40,14 +45,27 @@ export async function syncToRecs<TConfig extends StoreConfig = StoreConfig>({
     indexerUrl,
     initialState,
     onProgress: ({ step, percentage, latestBlockNumber, lastBlockNumberProcessed, message }) => {
-      if (getComponentValue(components.SyncProgress, singletonEntity)?.step !== SyncStep.LIVE) {
-        setComponent(components.SyncProgress, singletonEntity, {
-          step,
-          percentage,
-          latestBlockNumber,
-          lastBlockNumberProcessed,
-          message,
-        });
+      // already live, no need for more progress updates
+      if (getComponentValue(components.SyncProgress, singletonEntity)?.step === SyncStep.LIVE) return;
+
+      setComponent(components.SyncProgress, singletonEntity, {
+        step,
+        percentage,
+        latestBlockNumber,
+        lastBlockNumberProcessed,
+        message,
+      });
+
+      // when we switch to live, trigger update for all entities in all components
+      if (step === SyncStep.LIVE) {
+        for (const _component of Object.values(components)) {
+          // downcast component for easier calling of generic methods on all components
+          const component = _component as RecsComponent;
+          for (const entity of component.entities()) {
+            const value = getComponentValue(component, entity);
+            component.update$.next({ component, entity, value: [value, value] });
+          }
+        }
       }
     },
   });
