d0228876ad5a374774484be69040db86a8e36d7e
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-03 18:56:50 +0100

    import typed ABIs

--- packages/cli/src/utils/modules/constants.ts
+++ packages/cli/src/utils/modules/constants.ts
@@ -1,5 +1,8 @@
+import KeysWithValueModuleAbi from "@latticexyz/world-modules/out/KeysWithValueModule.sol/KeysWithValueModule.abi.json" assert { type: "json" };
 import KeysWithValueModuleData from "@latticexyz/world-modules/out/KeysWithValueModule.sol/KeysWithValueModule.json" assert { type: "json" };
+import KeysInTableModuleAbi from "@latticexyz/world-modules/out/KeysInTableModule.sol/KeysInTableModule.abi.json" assert { type: "json" };
 import KeysInTableModuleData from "@latticexyz/world-modules/out/KeysInTableModule.sol/KeysInTableModule.json" assert { type: "json" };
+import UniqueEntityModuleAbi from "@latticexyz/world-modules/out/UniqueEntityModule.sol/UniqueEntityModule.abi.json" assert { type: "json" };
 import UniqueEntityModuleData from "@latticexyz/world-modules/out/UniqueEntityModule.sol/UniqueEntityModule.json" assert { type: "json" };
 import { ContractCode } from "../utils/types";
 
@@ -7,17 +10,17 @@ import { ContractCode } from "../utils/types";
 export const defaultModuleContracts: ContractCode[] = [
   {
     name: "KeysWithValueModule",
-    abi: KeysWithValueModuleData.abi,
+    abi: KeysWithValueModuleAbi,
     bytecode: KeysWithValueModuleData.bytecode,
   },
   {
     name: "KeysInTableModule",
-    abi: KeysInTableModuleData.abi,
+    abi: KeysInTableModuleAbi,
     bytecode: KeysInTableModuleData.bytecode,
   },
   {
     name: "UniqueEntityModule",
-    abi: UniqueEntityModuleData.abi,
+    abi: UniqueEntityModuleAbi,
     bytecode: UniqueEntityModuleData.bytecode,
   },
 ];
