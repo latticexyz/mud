3d0b3edb46b266fccb40e26e8243d7628bea8baf
Kevin Ingersoll <kingersoll@gmail.com>
2023-10-03 18:50:16 +0100

    feat(cli): remove backup/restore/force options from set-version (#1687)

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -1,4 +1,21 @@
 import { defineConfig } from "tsup";
+import glob from "glob";
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import { MudPackages } from "./src/common";
+
+const mudWorkspace = path.normalize(`${__dirname}/../..`);
+
+const mudPackages: MudPackages = Object.fromEntries(
+  glob
+    .sync(path.join(mudWorkspace, `packages/*/package.json`))
+    .map((filename) => [
+      path.relative(mudWorkspace, path.dirname(filename)),
+      JSON.parse(readFileSync(filename, "utf8")),
+    ])
+    .filter(([, packageJson]) => !packageJson.private)
+    .map(([localPath, packageJson]) => [packageJson.name, { localPath }])
+);
 
 export default defineConfig({
   entry: ["src/index.ts", "src/mud.ts"],
@@ -8,4 +25,7 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   minify: true,
+  env: {
+    MUD_PACKAGES: JSON.stringify(mudPackages),
+  },
 });
