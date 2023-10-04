b7d9d89ffef899f1f20f1bbadf14d168c6cb5176
Ori Pomerantz <qbzzt1@gmail.com>
2023-10-04 10:05:19 -0500

    update players-and-movement.mdx

--- docs/pages/tutorials/emojimon/_meta.js
+++ docs/pages/tutorials/emojimon/_meta.js
@@ -1,9 +1,9 @@
 export default {
   "preface-the-ecs-model": "Preface: the ECS model",
   "getting-started": "Getting started",
-  "players-and-movement": "1. Players and movement",
-  "map-and-terrain": "2. Map and terrain",
-  "a-wild-emojimon-appears": "3. A wild Emojimon appears",
-  "make-it-multiplayer": "4. Make it multiplayer",
-  "deploy-to-testnet": "5. Deploy to testnet",
+  "players-and-movement": "Players and movement",
+  "map-and-terrain": "Map and terrain",
+  "a-wild-emojimon-appears": "A wild Emojimon appears",
+  "make-it-multiplayer": "Make it multiplayer",
+  "deploy-to-testnet": "Deploy to testnet",
 };
