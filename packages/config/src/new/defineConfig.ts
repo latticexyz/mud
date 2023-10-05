6f8401edf2be2a9853d84c1ef45e8cef9248961f
alvrs <alvarius@lattice.xyz>
2023-10-05 18:31:57 +0100

    experiment 5

--- packages/config/src/new/defineConfig.ts
+++ packages/config/src/new/defineConfig.ts
@@ -1,209 +1,68 @@
-// --- Types shared between the input and output of the config
-
-import { SchemaAbiType, StaticAbiType } from "@latticexyz/schema-type";
-
-interface PluginConfig {
-  resolveConfig: <TConfigInput extends ConfigInput>(config: TConfigInput) => TConfigInput & ConfigOutput<TConfigInput>;
-}
-
-interface PluginsConfigInput {
-  [key: string]: PluginConfig;
-}
-
-interface PluginsConfigOutput {
-  [key: string]: PluginConfig;
-}
-
 interface ConfigInput {
-  plugins: PluginsConfigInput;
-}
-
-interface ConfigOutput<TConfigInput extends ConfigInput> {
-  plugins: TConfigInput["plugins"] & PluginsConfigOutput;
-}
-
-// --- Implementation
-
-export function defineConfig<TConfigInput extends ConfigInput>(
-  configInput: TConfigInput
-): TConfigInput & ConfigOutput<TConfigInput> {
-  return Object.values(configInput.plugins).reduce(
-    (config, plugin) => plugin.resolveConfig(config),
-    configInput
-  ) as TConfigInput & ConfigOutput<TConfigInput>;
-}
-
-// --- Store config (to be moved to packages/store)
-
-interface KeySchema {
-  [key: string]: StaticAbiType;
-}
-
-interface ValueSchema {
-  [key: string]: SchemaAbiType;
-}
-
-interface TableConfigInput {
-  directory?: string;
-  name?: string;
-  tableIdArgument?: boolean;
-  storeArgument?: boolean;
-  dataStruct?: boolean;
-  offchainOnly?: boolean;
-  keySchema?: KeySchema;
-  valueSchema?: ValueSchema;
-}
-
-interface TableConfigOutput<TConfigInput extends ConfigInput> {
-  directory: string;
   name: string;
-  tableIdArgument: boolean;
-  storeArgument: boolean;
-  dataStruct: boolean;
-  offchainOnly: boolean;
-  keySchema: KeySchema;
-  valueSchema: ValueSchema;
 }
 
-interface TablesConfigInput {
-  [key: string]: TableConfigInput;
+interface MudPlugin<TConfigInput extends ConfigInput> {
+  input: unknown;
+  output: unknown;
 }
 
-interface TablesConfigOutput<TConfigInput extends ConfigInput> {
-  [key: string]: TableConfigOutput<TConfigInput>;
+interface IPluginA<TConfigInput extends ConfigInput> extends MudPlugin<TConfigInput> {
+  input: TConfigInput;
+  output: TConfigInput & { resolvedA: `${TConfigInput["name"]}-A` };
 }
 
-interface EnumsConfigInput {
-  [key: string]: string[];
+interface IPluginB<TConfigInput extends ConfigInput> extends MudPlugin<TConfigInput> {
+  input: TConfigInput;
+  output: TConfigInput & { resolvedB: `${TConfigInput["name"]}-B` };
 }
 
-interface EnumsConfigOutput {
-  [key: string]: string[];
+type ExtractOutput<TMudPlugin extends MudPlugin<ConfigInput>> = TMudPlugin["output"];
+type First<TArr extends unknown[]> = TArr extends [infer First, ...infer Remaining] ? First : never;
+
+type CombinedOutput<TConfigInput extends ConfigInput, TPlugins extends unknown[]> = TPlugins extends never[]
+  ? TConfigInput
+  : TPlugins extends [infer First, ...infer Remaining]
+  ? 1
+  : // ? FirstPlugin extends MudPlugin<TConfigInput>
+    // ? CombinedOutput<TConfigInput & ExtractOutput<FirstPlugin>, RemainingPlugins>
+    // : 1
+    TPlugins;
+
+class PluginA<TConfigInput extends ConfigInput> implements IPluginA<TConfigInput> {
+  input: TConfigInput;
+  output: IPluginA<TConfigInput>["output"];
+
+  constructor(config: TConfigInput) {
+    this.input = config;
+    this.output = { ...config, resolvedA: `${config.name}-A` };
+  }
 }
 
-interface UserTypeConfigInput {
-  filePath: string;
-  internalType: string;
+class PluginB<TConfigInput extends ConfigInput> implements IPluginB<TConfigInput> {
+  input: TConfigInput;
+  output: IPluginB<TConfigInput>["output"];
+
+  constructor(config: TConfigInput) {
+    this.input = config;
+    this.output = { ...config, resolvedB: `${config.name}-B` };
+  }
 }
 
-interface UserTypeConfigOutput {
-  filePath: string;
-  internalType: string;
+const config = { name: "hello" } as const satisfies ConfigInput;
+
+function defineConfig<TConfigInput extends ConfigInput, TPlugins extends MudPlugin<TConfigInput>[]>(
+  configInput: TConfigInput,
+  plugins: TPlugins
+): CombinedOutput<TConfigInput, TPlugins> {
+  return {} as CombinedOutput<TConfigInput, TPlugins>;
 }
 
-interface UserTypesConfigInput {
-  [key: string]: UserTypeConfigInput;
-}
+const plugins = [new PluginA(config), new PluginB(config)];
 
-interface UserTypesConfigOutput {
-  [key: string]: UserTypeConfigOutput;
-}
+const resolved = defineConfig(config, plugins);
 
-interface ConfigInput {
-  tables?: TablesConfigInput;
-  enums?: EnumsConfigInput;
-  userTypes?: UserTypesConfigInput;
-}
+type TFirst = First<typeof plugins>;
 
-interface ConfigOutput<TConfigInput extends ConfigInput> {
-  tables: TConfigInput["tables"] & TablesConfigOutput<TConfigInput>;
-  enums: TConfigInput["enums"] & EnumsConfigOutput;
-  userTypes: TConfigInput["userTypes"] & UserTypesConfigOutput;
-}
-
-// -- World config (to be moved to packages/world)
-interface NamespaceConfigInput {
-  tables?: TablesConfigInput;
-}
-
-interface NamespaceConfigOutput {
-  tables?: TablesConfigInput;
-}
-
-interface NamespacesConfigInput {
-  [key: string]: NamespaceConfigInput;
-}
-
-interface NamespacesConfigOutput {
-  [key: string]: NamespaceConfigOutput;
-}
-
-interface ConfigInput {
-  namespaces?: NamespacesConfigInput;
-}
-
-// Helper type to go from table nested in namespace to namespace_table
-type PrefixKeys<TRecord extends Record<string, unknown> | undefined, Prefix extends string> = TRecord extends undefined
-  ? undefined
-  : {
-      [key in keyof TRecord & string as `${Prefix}_${key}`]: TRecord[key];
-    };
-
-// From https://github.com/sindresorhus/type-fest/blob/113400b53a3b20eac75466b8e0ecc1bbb983e56f/source/union-to-intersection.d.ts#L46
-// TODO: import type-fest
-export type UnionToIntersection<Union> = // `extends unknown` is always going to be the case and is used to convert the
-  // `Union` into a [distributive conditional
-  // type](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#distributive-conditional-types).
-  (
-    Union extends unknown
-      ? // The union type is used as the only argument to a function since the union
-        // of function arguments is an intersection.
-        (distributedUnion: Union) => void
-      : // This won't happen.
-        never
-  ) extends // Infer the `Intersection` type since TypeScript represents the positional
-  // arguments of unions of functions as an intersection of the union.
-  (mergedIntersection: infer Intersection) => void
-    ? Intersection
-    : never;
-
-type FlattenNamespacesConfig<TNamespacesConfig extends NamespacesConfigInput | undefined> =
-  TNamespacesConfig extends NamespacesConfigInput
-    ? UnionToIntersection<
-        {
-          [TNamespace in keyof TNamespacesConfig & string]: PrefixKeys<
-            TNamespacesConfig[TNamespace]["tables"],
-            TNamespace
-          >;
-        }[keyof TNamespacesConfig & string]
-      >
-    : never;
-
-interface ConfigOutput<TConfigInput extends ConfigInput> {
-  namespaces: TConfigInput["namespaces"] & NamespacesConfigOutput;
-  tables: TConfigInput["tables"] &
-    TablesConfigOutput<TConfigInput> &
-    FlattenNamespacesConfig<TConfigInput["namespaces"]>;
-}
-
-//--------- Testing
-
-const config = defineConfig({
-  tables: {
-    foo: {
-      name: "foo",
-    },
-    bar: {
-      name: "bar",
-    },
-  },
-  namespaces: {
-    namespace1: {
-      tables: {
-        foo: {
-          name: "namespaced_foo",
-        },
-      },
-    },
-  },
-  plugins: {},
-} as const);
-
-config.tables.foo.name;
-//                 ^?
-
-config.tables.bar.name;
-//                 ^?
-
-config.tables;
-//         ^?
+resolved;
+// ^?
