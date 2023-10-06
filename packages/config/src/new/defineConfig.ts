68f2c587dbabb0b2ba3898837682ac62dd26db48
alvrs <alvarius@lattice.xyz>
2023-10-06 11:33:41 +0200

    plugins can extend the output and other plugins transform the output of other plugins

--- packages/config/src/new/defineConfig.ts
+++ packages/config/src/new/defineConfig.ts
@@ -18,31 +18,63 @@ type ConfigResolver = <TConfigInput extends ConfigInput>(configInput: TConfigInp
 // ----
 
 type PluginAOutput<TConfigInput extends ConfigInput> = {
+  tables: {
+    pluginATable: "pluginA";
+  };
   resolvedA: `${TConfigInput["name"]}-A`;
+  resolvedTables: {
+    [key in keyof TConfigInput["tables"] &
+      string as `resolved${Capitalize<key>}`]: `${TConfigInput["tables"][key]}-resolved`;
+  };
 };
 
 const PluginA = (<TConfigInput extends ConfigInput>(
   configInput: TConfigInput
 ): TConfigInput & PluginAOutput<TConfigInput> => {
-  return { ...configInput, resolvedA: `${configInput.name}-A` };
+  return {
+    ...configInput,
+    tables: { ...configInput.tables, pluginATable: "pluginA" },
+    resolvedA: `${configInput.name}-A`,
+    resolvedTables: {} as any, // TODO: actually resolve the values as specified in the type
+  };
 }) satisfies ConfigResolver;
 
 type PluginBOutput<TConfigInput extends ConfigInput> = {
+  tables: {
+    pluginBTable: "pluginB";
+  };
   resolvedB: `${TConfigInput["name"]}-B`;
 };
 
 const PluginB = (<TConfigInput extends ConfigInput>(
   configInput: TConfigInput
 ): PluginBOutput<TConfigInput> & TConfigInput => {
-  return { ...configInput, resolvedB: `${configInput.name}-B` };
+  return {
+    ...configInput,
+    tables: { ...configInput.tables, pluginBTable: "pluginB" },
+    resolvedB: `${configInput.name}-B`,
+  };
 }) satisfies ConfigResolver;
 
-const config = { name: "name", tables: {} } as const satisfies ConfigInput;
-
-const resolved = PluginA(PluginB(config));
+const resolved = PluginA(PluginB({ name: "name", tables: { table1: "user-table" } } as const));
 
 resolved.resolvedA;
 //        ^?
 
 resolved.resolvedB;
 //       ^?
+
+resolved.tables.table1;
+//              ^?
+
+resolved.tables.pluginATable;
+//              ^?
+
+resolved.tables.pluginBTable;
+//              ^?
+
+resolved.resolvedTables.resolvedPluginBTable;
+//                      ^?
+
+resolved.resolvedTables.resolvedTable1;
+//                      ^?
