8402b3d05030ac7dcb6994f9d4292ff7c805a004
Ori Pomerantz <qbzzt1@gmail.com>
2023-10-14 12:44:19 -0500

    docs(template): update links in comments

--- templates/threejs/packages/client/src/mud/createSystemCalls.ts
+++ templates/threejs/packages/client/src/mud/createSystemCalls.ts
@@ -16,16 +16,18 @@ export function createSystemCalls(
    * - The first parameter is expected to be a
    *   SetupNetworkResult, as defined in setupNetwork.ts
    *
-   * - Out of this parameter, we only care about two fields:
+   *   Out of this parameter, we only care about two fields:
    *   - worldContract (which comes from getContract, see
-   *     https://github.com/latticexyz/mud/blob/26dabb34321eedff7a43f3fcb46da4f3f5ba3708/templates/threejs/packages/client/src/mud/setupNetwork.ts#L31).
+   *     https://github.com/latticexyz/mud/blob/main/templates/three/packages/client/src/mud/setupNetwork.ts#L63-L68).
+   *
    *   - waitForTransaction (which comes from syncToRecs, see
-   *     https://github.com/latticexyz/mud/blob/26dabb34321eedff7a43f3fcb46da4f3f5ba3708/templates/threejs/packages/client/src/mud/setupNetwork.ts#L39).
+   *     https://github.com/latticexyz/mud/blob/main/templates/three/packages/client/src/mud/setupNetwork.ts#L77-L83).
    *
    * - From the second parameter, which is a ClientComponent,
-   *   we only care about Position. This parameter comes to use
+   *   we only care about Counter. This parameter comes to use
    *   through createClientComponents.ts, but it originates in
-   *   syncToRecs (https://github.com/latticexyz/mud/blob/26dabb34321eedff7a43f3fcb46da4f3f5ba3708/templates/threejs/packages/client/src/mud/setupNetwork.ts#L39).
+   *   syncToRecs
+   *   (https://github.com/latticexyz/mud/blob/main/templates/vanilla/packages/client/src/mud/setupNetwork.ts#L77-L83).
    */
   { worldContract, waitForTransaction, playerEntity }: SetupNetworkResult,
   { Position }: ClientComponents
