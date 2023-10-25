0bc6229cb5905121a808bab49d82e21b4892c667
alvrs <alvarius@lattice.xyz>
2023-10-25 22:16:26 +0100

    add actual resolver

--- packages/store/ts/config/experimental/resolveConfig.ts
+++ packages/store/ts/config/experimental/resolveConfig.ts
@@ -3,7 +3,8 @@ import { StoreConfig, TableConfig, UserTypesConfig } from "../storeConfig";
 import { UserType } from "@latticexyz/common/codegen";
 
 export type ResolvedStoreConfig<TStoreConfig extends StoreConfig> = TStoreConfig & {
-  resolved: {
+  /** @deprecated Note: this property is experimental and expected to change */
+  _resolved: {
     tables: {
       [key in keyof TStoreConfig["tables"]]: ResolvedTableConfig<
         TStoreConfig["tables"][key],
@@ -19,9 +20,21 @@ export type ResolvedTableConfig<
   TUserTypes extends UserTypesConfig,
   TEnumNames extends StringForUnion
 > = Omit<TTableConfig, "keySchema" | "valueSchema"> & {
-  keySchema: ResolvedSchema<TTableConfig["keySchema"], TUserTypes, TEnumNames>;
-  valueSchema: ResolvedSchema<TTableConfig["valueSchema"], TUserTypes, TEnumNames>;
+  keySchema: ResolvedKeySchema<TTableConfig["keySchema"], TUserTypes, TEnumNames>;
+  valueSchema: ResolvedValueSchema<TTableConfig["valueSchema"], TUserTypes, TEnumNames>;
 };
+
+export type ResolvedKeySchema<
+  TKeySchema extends TableConfig["keySchema"],
+  TUserTypes extends UserTypesConfig,
+  TEnumNames extends StringForUnion
+> = ResolvedSchema<TKeySchema, TUserTypes, TEnumNames>;
+
+export type ResolvedValueSchema<
+  TValueSchema extends TableConfig["valueSchema"],
+  TUserTypes extends UserTypesConfig,
+  TEnumNames extends StringForUnion
+> = ResolvedSchema<TValueSchema, TUserTypes, TEnumNames>;
 
 export type ResolvedSchema<
   TSchema extends TableConfig["keySchema"] | TableConfig["valueSchema"],
@@ -47,5 +60,80 @@ export type ResolvedSchema<
 export function resolveConfig<TStoreConfig extends StoreConfig>(
   config: TStoreConfig
 ): ResolvedStoreConfig<TStoreConfig> {
-  return config as ResolvedStoreConfig<TStoreConfig>;
+  const resolvedTables: Record<string, ReturnType<typeof resolveTable>> = {};
+
+  for (const key of Object.keys(config.tables)) {
+    resolvedTables[key] = resolveTable(config.tables[key], config.userTypes, Object.keys(config.enums)) as ReturnType<
+      typeof resolveTable
+    >;
+  }
+
+  return {
+    ...config,
+    _resolved: {
+      tables: resolvedTables as ResolvedStoreConfig<TStoreConfig>["_resolved"]["tables"],
+    },
+  };
+}
+
+function resolveTable<
+  TTableConfig extends TableConfig,
+  TUserTypes extends UserTypesConfig,
+  TEnums extends StringForUnion[]
+>(
+  tableConfig: TTableConfig,
+  userTypes: TUserTypes,
+  enums: TEnums
+): ResolvedTableConfig<TTableConfig, TUserTypes, TEnums[number]> {
+  return {
+    ...tableConfig,
+    keySchema: resolveKeySchema(tableConfig.keySchema ?? { key: "bytes32" }, userTypes, enums),
+    valueSchema: resolveValueSchema(tableConfig.valueSchema, userTypes, enums),
+  } as ResolvedTableConfig<TTableConfig, TUserTypes, TEnums[number]>;
+}
+
+function resolveKeySchema<
+  TKeySchema extends TableConfig["keySchema"],
+  TUserTypes extends UserTypesConfig,
+  TEnums extends StringForUnion[]
+>(
+  keySchema: TKeySchema,
+  userTypes: TUserTypes,
+  enums: TEnums
+): ResolvedKeySchema<TKeySchema extends undefined ? { key: "bytes32" } : TKeySchema, TUserTypes, TEnums[number]> {
+  const schema = (keySchema ?? { key: "bytes32" }) as TKeySchema extends undefined ? { key: "bytes32" } : TKeySchema;
+  return resolveSchema(schema, userTypes, enums);
+}
+
+function resolveValueSchema<
+  TValueSchema extends TableConfig["valueSchema"],
+  TUserTypes extends UserTypesConfig,
+  TEnums extends StringForUnion[]
+>(
+  schema: TValueSchema,
+  userTypes: TUserTypes,
+  enums: TEnums
+): ResolvedValueSchema<TValueSchema, TUserTypes, TEnums[number]> {
+  return resolveSchema(schema, userTypes, enums);
+}
+
+function resolveSchema<
+  TSchema extends NonNullable<TableConfig["keySchema"]> | TableConfig["valueSchema"],
+  TUserTypes extends UserTypesConfig,
+  TEnums extends StringForUnion[]
+>(schema: TSchema, { userTypes }: TUserTypes, enums: TEnums): ResolvedSchema<TSchema, TUserTypes, TEnums[number]> {
+  const resolvedSchema: Record<string, { internalType: string; type: string }> = {};
+  for (const [key, value] of Object.entries(schema)) {
+    resolvedSchema[key] = {
+      // This mirrors the logic in the `ResolvedSchema` type
+      type:
+        userTypes && key in userTypes
+          ? userTypes[key].internalType
+          : enums.includes(value)
+          ? ("uint8" as const)
+          : value,
+      internalType: value,
+    };
+  }
+  return resolvedSchema as ResolvedValueSchema<TSchema, TUserTypes, TEnums[number]>;
 }
