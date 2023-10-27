839f31377cefd1f09b8c31f311e4f713f5aa71c5
alvrs <alvarius@lattice.xyz>
2023-10-27 12:11:27 +0200

    update mapObject

--- packages/common/src/utils/mapObject.ts
+++ packages/common/src/utils/mapObject.ts
@@ -5,9 +5,7 @@ export function mapObject<
   Source extends Record<string | number | symbol, unknown>,
   Target extends { [key in keyof Source]: unknown }
 >(source: Source, valueMap: (key: keyof Source, value: Source[typeof key]) => Target[typeof key]): Target {
-  const target: Record<string, unknown> = {};
-  for (const [key, value] of Object.entries(source)) {
-    target[key] = valueMap(key, value as Source[keyof Source]);
-  }
-  return target as Target;
+  return Object.fromEntries(
+    Object.entries(source).map(([key, value]) => [key, valueMap(key, value as Source[keyof Source])])
+  ) as Target;
 }
