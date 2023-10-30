191676bf4ff79afb207789bfb87b66b95fbb9b2b
alvrs <alvarius@lattice.xyz>
2023-10-30 21:21:53 +0100

    remove unused imports

--- packages/world-modules/src/modules/erc20-puppet/ERC20System.sol
+++ packages/world-modules/src/modules/erc20-puppet/ERC20System.sol
@@ -2,9 +2,8 @@
 pragma solidity >=0.8.21;
 
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
-import { RESOURCE_TABLE } from "@latticexyz/store/src/storeResourceTypes.sol";
 import { System } from "@latticexyz/world/src/System.sol";
-import { WorldResourceIdInstance, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
 import { NamespaceOwner } from "@latticexyz/world/src/codegen/tables/NamespaceOwner.sol";
 import { SystemRegistry } from "@latticexyz/world/src/codegen/tables/SystemRegistry.sol";
 import { ALLOWANCES_NAME, BALANCES_NAME, METADATA_NAME } from "./constants.sol";
