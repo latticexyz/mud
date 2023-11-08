d4f76236aff31748d19cf273630e7a8b1859aa29
dk1a <dk1a@protonmail.com>
2023-11-08 12:10:50 +0300

    narrow down the test

--- e2e/packages/contracts/test/StaticArray.t.sol
+++ e2e/packages/contracts/test/StaticArray.t.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
-import { PackedCounterLib } from "@latticexyz/store/src/PackedCounter.sol";
-import { EncodeArray } from "@latticexyz/store/src/tightcoder/EncodeArray.sol";
 import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 
-import { StaticArray, StaticArrayTableId } from "../src/codegen/index.sol";
+import { toStaticArray_uint256_2 } from "../src/codegen/tables/StaticArray.sol";
 
 contract StaticArrayTest is MudTest {
   uint256 internal memoryCorruptionCheck = uint256(keccak256("memoryCorruptionCheck"));
@@ -17,15 +14,7 @@ contract StaticArrayTest is MudTest {
     data[1] = 2;
     data[2] = memoryCorruptionCheck;
 
-    StoreSwitch.setRecord(
-      StaticArrayTableId,
-      new bytes32[](0),
-      "",
-      PackedCounterLib.pack(3),
-      abi.encodePacked(EncodeArray.encode(data))
-    );
-
-    uint256[2] memory result = StaticArray.get();
+    uint256[2] memory result = toStaticArray_uint256_2(data);
     assertEq(result[0], 0);
     assertEq(result[1], 0);
 
