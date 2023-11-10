781488bfb99e02177336f6bc826754a1ce8b1cbe
Kevin Ingersoll <kingersoll@gmail.com>
2023-11-10 15:34:41 +0300

    fix(store-indexer): build the full thing

--- packages/store-indexer/tsup.config.ts
+++ packages/store-indexer/tsup.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "bin/postgres-frontend.ts", "bin/postgres-indexer.ts", "bin/sqlite-indexer.ts"],
+  entry: {
+    "postgres-frontend": "bin/postgres-frontend.ts",
+    "postgres-indexer": "bin/postgres-indexer.ts",
+    "sqlite-indexer": "bin/sqlite-indexer.ts",
+  },
   target: "esnext",
   format: ["esm"],
   dts: false,
   sourcemap: true,
   clean: true,
   minify: true,
+  platform: "node",
+  noExternal: [/(.*)/],
+  splitting: false,
+  banner: {
+    js: `
+      // BANNER START
+      const require = (await import("node:module")).createRequire(import.meta.url);
+      const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+      const __dirname = (await import("node:path")).dirname(__filename);
+      // BANNER END
+    `,
+  },
 });
