a3aa8bbb6ba8fd8cdd516083153e54426a20c9be
Kevin Ingersoll <kingersoll@gmail.com>
2023-12-03 21:53:03 +0000

    start sse endpoint

--- packages/store-indexer/bin/postgres-frontend.ts
+++ packages/store-indexer/bin/postgres-frontend.ts
@@ -5,12 +5,17 @@ import Koa from "koa";
 import compress from "koa-compress";
 import cors from "@koa/cors";
 import Router from "@koa/router";
+import compressible from "compressible";
 import { createKoaMiddleware } from "trpc-koa-adapter";
-import { createAppRouter } from "@latticexyz/store-sync/trpc-indexer";
+import { createAppRouter, input } from "@latticexyz/store-sync/trpc-indexer";
 import { createQueryAdapter } from "../src/postgres/createQueryAdapter";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { frontendEnvSchema, parseEnv } from "./parseEnv";
+import { queryLogs } from "../src/postgres/queryLogs";
+import { PassThrough } from "node:stream";
+import { StorageAdapterLog } from "@latticexyz/store-sync";
+import { decodeDynamicField } from "@latticexyz/protocol-parser";
 
 const env = parseEnv(
   z.intersection(
@@ -21,10 +26,24 @@ const env = parseEnv(
   )
 );
 
-const database = drizzle(postgres(env.DATABASE_URL));
+const pg = postgres(env.DATABASE_URL, {
+  debug: (conn, query, params) => {
+    console.log("query", query);
+  },
+});
+const database = drizzle(pg);
 
 const server = new Koa();
-server.use(compress());
+server.use(
+  compress({
+    // TODO: figure out if we can force compression to flush during streaming
+    // TODO: this seems to be bypassed by trpc
+    filter: (mimeType) => {
+      if (/^application\/json$/.test(mimeType)) return true;
+      return compressible(mimeType) ?? false;
+    },
+  })
+);
 server.use(cors());
 
 const router = new Router();
@@ -35,6 +54,63 @@ router.get("/healthz", (ctx) => {
 });
 router.get("/readyz", (ctx) => {
   ctx.status = 200;
+});
+
+router.get("/sse/logs", async (ctx) => {
+  const opts = typeof ctx.query.input === "string" ? input.parse(JSON.parse(ctx.query.input)) : null;
+
+  ctx.request.socket.setTimeout(0);
+  ctx.req.socket.setNoDelay(true);
+  ctx.req.socket.setKeepAlive(true);
+
+  ctx.set({
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+
+  const stream = new PassThrough();
+
+  ctx.status = 200;
+  ctx.body = stream;
+
+  let hasEmittedConfig = false;
+
+  await queryLogs(pg, opts ?? {}).forEach((row) => {
+    if (!hasEmittedConfig) {
+      stream.write("event: config\n");
+      stream.write(
+        `data: ${JSON.stringify({
+          indexerVersion: row.indexerVersion,
+          chainId: row.chainId,
+          lastUpdatedBlockNumber: row.chainBlockNumber,
+        })}\n`
+      );
+      stream.write("\n");
+      hasEmittedConfig = true;
+    }
+
+    stream.write("event: log\n");
+    stream.write(
+      `data: ${JSON.stringify({
+        address: row.address,
+        eventName: "Store_SetRecord",
+        args: {
+          tableId: row.tableId,
+          keyTuple: decodeDynamicField("bytes32[]", row.keyBytes),
+          staticData: row.staticData ?? "0x",
+          encodedLengths: row.encodedLengths ?? "0x",
+          dynamicData: row.dynamicData ?? "0x",
+        },
+      } as const satisfies StorageAdapterLog)}\n`
+    );
+    stream.write("\n");
+  });
+
+  // TODO: subscribe + continue writing
+
+  stream.write(":end\n\n");
+  stream.end();
 });
 
 server.use(router.routes());
