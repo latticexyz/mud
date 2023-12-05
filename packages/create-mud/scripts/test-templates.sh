94553280d4aa5577f0bf6d0b0ed5c8d9b56d32c4
tash-2s <81064017+tash-2s@users.noreply.github.com>
2023-12-04 23:30:56 -0600

    create-mud: full test in templates might be redundant, skip `mud test`

--- packages/create-mud/scripts/test-templates.sh
+++ packages/create-mud/scripts/test-templates.sh
@@ -7,9 +7,9 @@ set -ex
 
 # Create a temporary directory and remove it at the end of this script regardless of the result.
 # This ensures that created projects work properly in user environments since the temporary
-# directory is outside of this repository. Node.js/TypeScript can resolve modules/types by looking
-# up in ancestor directories' `node_modules`. Hence, if a created project is anywhere within this
-# repository, it might miss errors.
+# directory is outside of this repository. TypeScript can resolve types by looking
+# up in ancestor directories' `node_modules`. Hence, if a created project is in this repository,
+# it might miss errors.
 tmp_dir="$(mktemp -d)"
 trap 'test -d "$tmp_dir" && pnpm rimraf "$tmp_dir"' EXIT
 
@@ -18,5 +18,6 @@ for template_name in "$@"; do
   project_path="$tmp_dir/$template_name"
 
   pnpm ./dist/cli.js "$project_path" --mud-version main --template "$template_name"
-  pnpm --dir "$project_path" run test
+  pnpm --dir "$project_path" --filter contracts run build # abi files necessary for client
+  pnpm --dir "$project_path" --filter contracts --filter client exec tsc --noEmit
 done
