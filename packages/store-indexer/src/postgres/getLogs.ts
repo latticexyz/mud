4973ebf8ad36a55e2a52fe3a06890adc3470d4d4
alvrs <alvarius@lattice.xyz>
2023-12-05 14:17:01 +0100

    experiment with different query layouts

--- packages/store-indexer/src/postgres/getLogs.ts
+++ packages/store-indexer/src/postgres/getLogs.ts
@@ -2,7 +2,7 @@ import { PgDatabase } from "drizzle-orm/pg-core";
 import { Hex } from "viem";
 import { StorageAdapterLog, SyncFilter } from "@latticexyz/store-sync";
 import { tables } from "@latticexyz/store-sync/postgres";
-import { and, asc, eq, inArray, or } from "drizzle-orm";
+import { and, asc, eq, or } from "drizzle-orm";
 import { decodeDynamicField } from "@latticexyz/protocol-parser";
 import { bigIntMax } from "@latticexyz/common/utils";
 import { createBenchmark } from "./createBenchmark";
@@ -22,40 +22,23 @@ export async function getLogs(
 ): Promise<{ blockNumber: bigint; logs: (StorageAdapterLog & { eventName: "Store_SetRecord" })[] }> {
   const benchmark = createBenchmark();
 
-  const tableOnlyFilters = filters.filter((filter) => filter.key0 == null && filter.key1 == null);
-  const multiFilters = filters.filter((filter) => filter.key0 != null || filter.key1 != null);
-
-  const tableOnlyConditions = tableOnlyFilters.length
-    ? [
+  const conditions = filters.length
+    ? filters.map((filter) =>
         and(
           address != null ? eq(tables.recordsTable.address, address) : undefined,
-          inArray(
-            tables.recordsTable.tableId,
-            tableOnlyFilters.map((filter) => filter.tableId)
-          )
-        ),
-      ]
+          eq(tables.recordsTable.tableId, filter.tableId),
+          filter.key0 != null ? eq(tables.recordsTable.key0, filter.key0) : undefined,
+          filter.key1 != null ? eq(tables.recordsTable.key1, filter.key1) : undefined
+        )
+      )
+    : address != null
+    ? [eq(tables.recordsTable.address, address)]
     : [];
-
-  const multiConditions = multiFilters.map((filter) =>
-    and(
-      address != null ? eq(tables.recordsTable.address, address) : undefined,
-      eq(tables.recordsTable.tableId, filter.tableId),
-      filter.key0 != null ? eq(tables.recordsTable.key0, filter.key0) : undefined,
-      filter.key1 != null ? eq(tables.recordsTable.key1, filter.key1) : undefined
-    )
-  );
-
-  const addressCondition = filters.length == 0 && address != null ? [eq(tables.recordsTable.address, address)] : [];
-
-  const conditions = [...tableOnlyConditions, ...multiConditions, ...addressCondition];
 
   benchmark("conditions");
 
   // debug("conditions:", filters);
-  debug("tableOnlyCondition:", tableOnlyConditions.length);
-  debug("multiConditions:", multiConditions.length);
-  debug("addressConditions:", addressCondition.length);
+  debug("first condition:", conditions[0]);
   debug("address", address);
 
   // Query for the block number that the indexer (i.e. chain) is at, in case the
@@ -79,10 +62,19 @@ export async function getLogs(
 
   benchmark("query chainState");
 
+  // const records = (
+  //   await Promise.all(
+  //     conditions.map(async (condition) => {
+  //       return database.select().from(tables.recordsTable).where(condition);
+  //     })
+  //   )
+  // ).flat();
+
   const records = await database
     .select()
     .from(tables.recordsTable)
     .where(or(...conditions));
+
   // .orderBy(
   //   asc(tables.recordsTable.lastUpdatedBlockNumber)
   //   // TODO: add logIndex (https://github.com/latticexyz/mud/issues/1979)
