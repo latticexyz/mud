003e2b757a572f5013bc865df5dd7d19b9702f3c
tash-2s <81064017+tash-2s@users.noreply.github.com>
2023-12-04 21:27:00 -0600

    create-mud: detect errors by running `pnpm test` in each template

---
+++ packages/create-mud/scripts/test-templates.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Usage example: `./script/test-templates.sh template1 template2`
+
+# Exit on error and print commands
+set -ex
+
+# Create a temporary directory and remove it at the end of this script regardless of the result.
+# This ensures that created projects work properly in user environments since the temporary
+# directory is outside of this repository. Node.js/TypeScript can resolve modules/types by looking
+# up in ancestor directories' `node_modules`. Hence, if a created project is anywhere within this
+# repository, it might miss errors.
+tmp_dir="$(mktemp -d)"
+trap 'test -d "$tmp_dir" && pnpm rimraf "$tmp_dir"' EXIT
+
+# Test each template specified as an argument
+for template_name in "$@"; do
+  project_path="$tmp_dir/$template_name"
+
+  pnpm ./dist/cli.js "$project_path" --mud-version main --template "$template_name"
+  pnpm --dir "$project_path" run test
+done
