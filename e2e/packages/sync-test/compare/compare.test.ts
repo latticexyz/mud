41b295683f8a522bb3c682c31ab9cb672593800a
alvrs <alvarius@lattice.xyz>
2023-12-06 22:28:50 +0100

    improve comparison

--- e2e/packages/sync-test/compare/compare.test.ts
+++ e2e/packages/sync-test/compare/compare.test.ts
@@ -1,47 +1,112 @@
 import { describe, expect, it } from "vitest";
 import { Hex } from "viem";
 import { createIndexerClient } from "@latticexyz/store-sync/indexer-client";
-import { indexerUrls, options } from "./config";
+import { IndexerConfig, indexerUrls, options } from "./config";
 import { unwrap } from "@latticexyz/common";
-import { StorageAdapterLog } from "@latticexyz/store-sync/src/common";
+import { writeFileSync } from "fs";
 
-type Row = { staticData: Hex; dynamicData: Hex; encodedLengths: Hex };
-type State = { blockNumber: bigint; rows: Map<string, Row> };
+type Address = string;
+type TableId = string;
+type Key = string;
 
-function logToKey(log: StorageAdapterLog): string {
-  return `${log.address}/${log.args.tableId}/${log.args.keyTuple.join(":")}`;
+type RecordData = { staticData: Hex; dynamicData: Hex; encodedLengths: Hex };
+
+type State = {
+  source: IndexerConfig;
+  blockNumber: bigint;
+  rows: Record<Address, Record<TableId, Record<Key, RecordData>>>;
+};
+
+type Comparison = {
+  sources: { first: IndexerConfig; second: IndexerConfig };
+  mismatchingRows: Record<Address, Record<TableId, Record<Key, Record<string, RecordData | null>>>>;
+};
+
+function setRecord<Data>(
+  state: Record<Address, Record<TableId, Record<Key, Data>>>,
+  address: Address,
+  tableId: TableId,
+  key: Key,
+  data: Data
+): void {
+  state[address] ??= {};
+  state[address][tableId] ??= {};
+  state[address][tableId][key] ??= data;
 }
 
-async function syncToObject(indexerUrl: string): Promise<State> {
-  console.log("Fetching from", indexerUrl);
-  const state: State = { blockNumber: -1n, rows: new Map() };
+function getRecord<Data>(
+  state: Record<Address, Record<TableId, Record<Key, Data>>>,
+  address: Address,
+  tableId: TableId,
+  key: Key
+): Data | undefined {
+  return state[address]?.[tableId]?.[key];
+}
 
-  const indexer = createIndexerClient({ url: indexerUrl });
+function countRows(nestedRows: Record<Address, Record<TableId, Record<Key, Record<string, unknown>>>>): number {
+  let num = 0;
+  for (const address of Object.keys(nestedRows)) {
+    for (const tableId of Object.keys(nestedRows[address])) {
+      for (const key of Object.keys(nestedRows[address][tableId])) {
+        num += Object.keys(nestedRows[address][tableId][key]).length;
+      }
+    }
+  }
+  return num;
+}
+
+async function syncToObject(indexerConfig: IndexerConfig): Promise<State> {
+  console.log("Fetching from", indexerConfig);
+  const state: State = { source: indexerConfig, blockNumber: -1n, rows: {} };
+
+  const indexer = createIndexerClient({ url: indexerConfig.url });
   const result = unwrap(await indexer.getLogs(options));
-  console.log(`Fetched ${result.logs.length} logs from`, indexerUrl);
+  console.log(`Fetched ${result.logs.length} logs from`, indexerConfig.name);
   state.blockNumber = result.blockNumber;
 
   for (const log of result.logs) {
     if (log.eventName !== "Store_SetRecord") {
-      throw new Error(`[${indexerUrl}]: Snapshots can only include Store_SetRecord logs`);
+      throw new Error(`[${indexerConfig.name}]: Snapshots can only include Store_SetRecord logs`);
     }
-    const key = logToKey(log);
-    const { staticData, dynamicData, encodedLengths } = log.args;
-    state.rows.set(key, { staticData, dynamicData, encodedLengths });
+    const { address } = log;
+    const { tableId, keyTuple, staticData, dynamicData, encodedLengths } = log.args;
+    setRecord(state.rows, address, tableId, keyTuple.join("-"), { staticData, dynamicData, encodedLengths });
   }
 
   return state;
 }
 
-function compare(received: State, expected: State): void {
-  expect(received.blockNumber).toBe(expected.blockNumber);
-  // expect(received.rows.size).toBe(expected.rows.size);
-  for (const key of expected.rows.keys()) {
-    const expectedValue = expected.rows.get(key);
-    const receivedValue = received.rows.get(key);
-    expect(receivedValue?.staticData, `key: ${key}`).toBe(expectedValue?.staticData);
-    expect(receivedValue?.encodedLengths, `key: ${key}`).toBe(expectedValue?.encodedLengths);
-    expect(receivedValue?.dynamicData, `key: ${key}`).toBe(expectedValue?.dynamicData);
+function compare(first: State, second: State): Comparison {
+  const comparison: Comparison = {
+    sources: { first: first.source, second: second.source },
+    mismatchingRows: {},
+  };
+
+  extendComparison(comparison, first, second);
+  extendComparison(comparison, second, first);
+  return comparison;
+}
+
+function extendComparison(comparison: Comparison, first: State, second: State) {
+  for (const address of Object.keys(second.rows)) {
+    for (const tableId of Object.keys(second.rows[address])) {
+      for (const key of Object.keys(second.rows[address][tableId])) {
+        const firstRecord = getRecord(first.rows, address, tableId, key);
+        const secondRecord = getRecord(second.rows, address, tableId, key);
+        if (
+          !firstRecord ||
+          !secondRecord ||
+          firstRecord.staticData != secondRecord.staticData ||
+          firstRecord.encodedLengths != secondRecord.encodedLengths ||
+          firstRecord.dynamicData != secondRecord.dynamicData
+        ) {
+          setRecord(comparison.mismatchingRows, address, tableId, key, {
+            [first.source.name]: firstRecord ?? null,
+            [second.source.name]: secondRecord ?? null,
+          });
+        }
+      }
+    }
   }
 }
 
@@ -53,12 +118,14 @@ describe("Compare indexer snapshots", async () => {
 
     const [first, ...rest] = states;
 
-    // Compare the first state (received) to the last state (expected)
-    compare(first, states[states.length - 1]);
-
-    // Compare each state (received) to the first state (expected)
+    // Compare each state to the first one
     for (const state of rest) {
-      compare(state, first);
+      const comparison = compare(state, first);
+      const report = JSON.stringify(comparison, null, 2);
+      const fileName = `compare-${comparison.sources.first.name}-${comparison.sources.second.name}.json`;
+      writeFileSync(fileName, report);
+
+      expect(countRows(comparison.mismatchingRows)).toBe(0);
     }
   });
 });
