244c42e58aa2cf968caf6376362e2aa0fa70b674
alvrs <alvarius@lattice.xyz>
2023-12-07 21:06:17 +0100

    add url normalization

--- packages/store-sync/src/indexer-client/createIndexerClient.ts
+++ packages/store-sync/src/indexer-client/createIndexerClient.ts
@@ -25,7 +25,8 @@ export function createIndexerClient({ url }: CreateIndexerClientOptions): Indexe
     getLogs: async (opts): Promise<Result<StorageAdapterBlock>> => {
       try {
         const input = encodeURIComponent(JSON.stringify(opts));
-        const response = await fetch(`${url}/api/logs?input=${input}`, { method: "GET" });
+        const urlOrigin = new URL(url).origin;
+        const response = await fetch(`${urlOrigin}/api/logs?input=${input}`, { method: "GET" });
 
         // TODO: return a readable stream instead of fetching the entire response at once
         const result = await response.json();
