3aa53382167bacfac18ec8ac19190ade4f0f9691
alvrs <alvarius@lattice.xyz>
2023-12-09 12:40:18 +0100

    chore(common): log benchmark to stderr

--- packages/common/src/createBenchmark.ts
+++ packages/common/src/createBenchmark.ts
@@ -1,7 +1,12 @@
 import createDebug from "debug";
 
+const parentDebug = createDebug("mud:benchmark");
+
+// Pipe debug output to stdout instead of stderr
+parentDebug.log = console.debug.bind(console);
+
 export function createBenchmark(namespace: string): (stepName: string) => void {
-  const debug = createDebug("mud:benchmark:" + namespace);
+  const debug = parentDebug.extend(namespace);
   let lastStep = performance.now();
 
   return (stepName: string) => {
