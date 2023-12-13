a59c36214c6fa4f82da72f240678297763cd5513
Ori Pomerantz <qbzzt1@gmail.com>
2023-12-13 16:00:05 -0600

    Added pwa

--- docs/pages/templates/_meta.js
+++ docs/pages/templates/_meta.js
@@ -2,6 +2,7 @@ export default {
   introduction: "Introduction",
   typescript: "TypeScript",
   godot: "Godot",
+  pwa: "Progressive Web App (for mobile)",
   swift: "Swift",
   svelte: "Svelte",
   unity: "Unity",
