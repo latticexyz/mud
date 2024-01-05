c32553e83b02bea0759a4a2918588ef953bacb07
alvarius <alvarius@lattice.xyz>
2024-01-05 14:11:32 +0100

    Apply suggestions from code review

--- packages/store/src/ResourceId.sol
+++ packages/store/src/ResourceId.sol
@@ -9,9 +9,9 @@ pragma solidity >=0.8.21;
 type ResourceId is bytes32;
 
 /// @dev Number of bits reserved for the type in the ResourceId.
-uint256 constant TYPE_BITS = 2 * 8; // BYTE_TO_BITS
+uint256 constant TYPE_BITS = 2 * 8; // 2 bytes * 8 bits per byte
 /// @dev Number of bits reserved for the name in the ResourceId.
-uint256 constant NAME_BITS = 32 * 8 - TYPE_BITS; // BYTE_TO_BITS
+uint256 constant NAME_BITS = 32 * 8 - TYPE_BITS; // 32 bytes * 8 bits per byte - type bits
 
 /// @dev Bitmask to extract the type from the ResourceId.
 bytes32 constant TYPE_MASK = bytes32(hex"ffff");
