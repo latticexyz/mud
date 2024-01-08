ce223de162292d989790bba3754645ab11f59128
Fraser Scott <fraserdscott@gmail.com>
2024-01-08 13:09:41 +0000

    refactor: extcodesize not needed in Create2

--- packages/world/src/Create2.sol
+++ packages/world/src/Create2.sol
@@ -29,7 +29,7 @@ library Create2 {
       // code for the constructor. So the code starts at creationCode+0x20, and is mload(creationCode)
       // bytes long.
       addr := create2(0, add(creationCode, 0x20), mload(creationCode), salt)
-      if iszero(extcodesize(addr)) {
+      if iszero(addr) {
         revert(0, 0)
       }
     }
