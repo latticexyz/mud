25ec1ff91da9797dbc053a460aeec77ad805f1ed
Fraser Scott <fraserdscott@gmail.com>
2024-01-08 12:08:47 +0000

    refactor: delete unused constants

--- packages/store/src/ResourceId.sol
+++ packages/store/src/ResourceId.sol
@@ -10,11 +10,6 @@ type ResourceId is bytes32;
 
 /// @dev Number of bits reserved for the type in the ResourceId.
 uint256 constant TYPE_BITS = 2 * 8;
-/// @dev Number of bits reserved for the name in the ResourceId.
-uint256 constant NAME_BITS = 32 * 8 - TYPE_BITS;
-
-/// @dev Bitmask to extract the type from the ResourceId.
-bytes32 constant TYPE_MASK = bytes32(hex"ffff");
 
 /**
  * @title ResourceIdLib Library
