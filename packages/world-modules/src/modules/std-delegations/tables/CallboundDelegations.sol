3bebc4455ea528ef39cea3dcb26621e34d576075
Kevin Ingersoll <kingersoll@gmail.com>
2024-01-11 11:45:21 +0000

    docs: fix codegen typos [N-07] (#2101)

--- packages/world-modules/src/modules/std-delegations/tables/CallboundDelegations.sol
+++ packages/world-modules/src/modules/std-delegations/tables/CallboundDelegations.sol
@@ -291,7 +291,7 @@ library CallboundDelegations {
    * @notice Encode all of a record's fields.
    * @return The static (fixed length) data, encoded into a sequence of bytes.
    * @return The lengths of the dynamic fields (packed into a single bytes32 value).
-   * @return The dyanmic (variable length) data, encoded into a sequence of bytes.
+   * @return The dynamic (variable length) data, encoded into a sequence of bytes.
    */
   function encode(uint256 availableCalls) internal pure returns (bytes memory, PackedCounter, bytes memory) {
     bytes memory _staticData = encodeStatic(availableCalls);
