55224b1730bec3807323a8ac7e772e28f3396d51
Kevin Ingersoll <kingersoll@gmail.com>
2024-01-18 18:17:44 +0000

    fix example

--- examples/minimal/packages/contracts/src/interfaces/IChatNamespacedSystem.sol
+++ examples/minimal/packages/contracts/src/interfaces/IChatNamespacedSystem.sol
@@ -3,5 +3,5 @@ pragma solidity >=0.8.21;
 
 // TODO allow overriding namespace per-system
 interface IChatNamespacedSystem {
-  function namespace_ChatNamespaced_sendMessage(string memory message) external;
+  function namespace_sendMessage(string memory message) external;
 }
