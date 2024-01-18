a74ff63b12a4dbe55edbd934c62946fe22e59b3d
Kevin Ingersoll <kingersoll@gmail.com>
2024-01-18 18:29:58 +0000

    this comment doesn't make sense

--- examples/minimal/packages/contracts/src/interfaces/IChatNamespacedSystem.sol
+++ examples/minimal/packages/contracts/src/interfaces/IChatNamespacedSystem.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-// TODO allow overriding namespace per-system
 interface IChatNamespacedSystem {
   function namespace_sendMessage(string memory message) external;
 }
