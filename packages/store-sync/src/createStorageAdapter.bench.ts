be59a4937e593213b028663fb84ff774b719942c
Fraser Scott <fraserdscott@gmail.com>
2024-01-23 16:29:44 +0000

    feat: compare sqlite

--- packages/store-sync/src/createStorageAdapter.bench.ts
+++ packages/store-sync/src/createStorageAdapter.bench.ts
@@ -1,36 +1,33 @@
 import { bench, describe } from "vitest";
-import mudConfig from "../../../e2e/packages/contracts/mud.config";
-import worldRpcLogs from "../../../test-data/world-logs.json";
-import { groupLogsByBlockNumber } from "@latticexyz/block-logs-stream";
-import { StoreEventsLog } from "./common";
-import { RpcLog, formatLog, decodeEventLog, Hex } from "viem";
-import { resolveConfig, storeEventsAbi } from "@latticexyz/store";
-
+import { resolveConfig } from "@latticexyz/store";
 import { createWorld } from "@latticexyz/recs";
+import { drizzle } from "drizzle-orm/sql-js";
+import { createPublicClient, http } from "viem";
+import { foundry } from "viem/chains";
+
+import mudConfig from "../../../e2e/packages/contracts/mud.config";
 import { recsStorage } from "./recs";
+import initSqlJs from "sql.js";
+import { sqliteStorage } from "./sqlite";
 import { createStorageAdapter } from "./zustand/createStorageAdapter";
 import { createStore } from "./zustand/createStore";
+import { blocks } from "./constants";
 
 const tables = resolveConfig(mudConfig).tables;
-
-// TODO: make test-data a proper package and export this
-const blocks = groupLogsByBlockNumber(
-  worldRpcLogs.map((log) => {
-    const { eventName, args } = decodeEventLog({
-      abi: storeEventsAbi,
-      data: log.data as Hex,
-      topics: log.topics as [Hex, ...Hex[]],
-      strict: true,
-    });
-    return formatLog(log as any as RpcLog, { args, eventName: eventName as string }) as StoreEventsLog;
-  })
-);
 
 const world = createWorld();
 const { storageAdapter: recsStorageAdapter } = recsStorage({ world, tables });
 
 const useStore = createStore({ tables });
 const zustandStorageAdapter = createStorageAdapter({ store: useStore });
+
+const SqlJs = await initSqlJs();
+const db = drizzle(new SqlJs.Database(), {});
+const publicClient = createPublicClient({
+  chain: foundry,
+  transport: http(),
+});
+const sqliteStorageAdapter = await sqliteStorage({ database: db, publicClient });
 
 describe("Storage Adapter", () => {
   bench("recs: `storageAdapter`", async () => {
@@ -42,6 +39,12 @@ describe("Storage Adapter", () => {
   bench("zustand: `storageAdapter`", async () => {
     for (const block of blocks) {
       await zustandStorageAdapter(block);
+    }
+  });
+
+  bench("sqlite: `storageAdapter`", async () => {
+    for (const block of blocks) {
+      await sqliteStorageAdapter(block);
     }
   });
 });
