3be5ba218746d6d0bc3da39fb0f95d1a592b8b08
Ori Pomerantz <qbzzt1@gmail.com>
2024-01-25 16:09:09 -0600

    docs(guides/extending-world): turn into a multi-page

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -1,5 +1,6 @@
 export default {
   "hello-world": "Hello World",
+  debug: "Debugging",
   "extending-world": "Extending World",
   emojimon: "Emojimon",
 };
---
+++ docs/pages/guides/extending-world/_meta.js
@@ -0,0 +1,4 @@
+export default {
+  "add-table": "Add a table",
+  "as-diff-user": "Permissionlessly extending a World"
+};
