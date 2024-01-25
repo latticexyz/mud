798a7bd5051fc7af3919393f83e48f07a0f79969
Kevin Ingersoll <kingersoll@gmail.com>
2024-01-25 20:28:54 +0000

    adjust max-age and stale-while-revalidate

--- packages/store-indexer/src/postgres/apiRoutes.ts
+++ packages/store-indexer/src/postgres/apiRoutes.ts
@@ -48,12 +48,19 @@ export function apiRoutes(database: Sql): Middleware {
       const blockNumber = records[0].chainBlockNumber;
       ctx.status = 200;
 
-      // client fetches logs 1000 blocks at a time, so we'll allow clients to get behind by 4 rpc requests worth
-      // this equates to ~2 hours, which seems like a reasonable refresh rate for an effective cache!
-      // we've also turned on `stale-while-revalidate`, which will refetch in the background once stale so responses are always snappy
-      const maxAgeInBlocks = 1000 * 4;
-      const secondsPerBlock = 2;
-      ctx.set("Cache-Control", `public, max-age=${maxAgeInBlocks * secondsPerBlock}, stale-while-revalidate`);
+      // max age is set to several multiples of the uncached response time (currently ~10s, but using 60s for wiggle room) to ensure only ~one origin request at a time
+      // and stale-while-revalidate below means that the cache is refreshed under the hood while still responding fast (cached)
+      const maxAgeSeconds = 60 * 5;
+      // we set stale-while-revalidate to the time elapsed by the number of blocks we can fetch from the RPC in the same amount of time as an uncached response
+      // meaning it would take ~the same about of time to get an uncached response from the origin as it would to catch up from the currently cached response
+      // if an uncached response takes ~10 seconds, we have ~10s to catch up, so let's say we can do enough RPC calls to fetch 4000 blocks
+      // with a block per 2 seconds, that means we can serve a stale/cached response for 8000 seconds before we should require the response be returned by the origin
+      const staleWhileRevalidateSeconds = 4000 * 2;
+
+      ctx.set(
+        "CDN-Cache-Control",
+        `public, max-age=${maxAgeSeconds}, stale-while-revalidate=${staleWhileRevalidateSeconds}`
+      );
 
       ctx.set("Content-Type", "application/json");
       ctx.body = JSON.stringify({ blockNumber, logs });
