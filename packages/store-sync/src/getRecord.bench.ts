1ad53b0742311302c4dca5d64ac0c909bf7fd365
Fraser Scott <fraserdscott@gmail.com>
2024-01-26 12:56:38 +0000

    test: add getRecord benchmark

---
+++ packages/store-sync/src/getRecord.bench.ts
@@ -0,0 +1,31 @@
+import { bench, describe } from "vitest";
+import { getComponentEntities, getComponentValue } from "@latticexyz/recs";
+import { eq } from "drizzle-orm";
+import { mudStoreTables } from "./sqlite";
+import { blocks } from "../test/blocks";
+import {
+  components,
+  db,
+  recsStorageAdapter,
+  sqliteStorageAdapter,
+  tables,
+  useStore,
+  zustandStorageAdapter,
+} from "../test/utils";
+import { singletonEntity } from "./recs";
+
+for (const block of blocks) {
+  await recsStorageAdapter(block);
+  await zustandStorageAdapter(block);
+  await sqliteStorageAdapter(block);
+}
+
+describe("Get Record", () => {
+  bench("recs: `getComponentValue`", async () => {
+    getComponentValue(components.NumberList, singletonEntity);
+  });
+
+  bench("zustand: `getRecord`", async () => {
+    useStore.getState().getRecord(tables.NumberList, {});
+  });
+});
