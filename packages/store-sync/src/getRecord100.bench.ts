739bd0d809659f0e94612dc85f524b9f6f34b190
Fraser Scott <fraserdscott@gmail.com>
2024-01-29 16:38:47 +0000

    chore: get record and records

---
+++ packages/store-sync/src/getRecord100.bench.ts
@@ -0,0 +1,29 @@
+import { bench, describe } from "vitest";
+import { getComponentValue } from "@latticexyz/recs";
+import {
+  components,
+  recsStorageAdapter,
+  sqliteStorageAdapter,
+  tables,
+  useStore,
+  zustandStorageAdapter,
+} from "../test/utils";
+import { encodeEntity } from "./recs";
+import worldRpcLogs from "../../../test-data/world-logs-10000.json";
+import { logsToBlocks } from "../test/logsToBlocks";
+
+const blocks = logsToBlocks(worldRpcLogs.slice(0, 100));
+
+for (const block of blocks) {
+  await Promise.all([recsStorageAdapter(block), zustandStorageAdapter(block), sqliteStorageAdapter(block)]);
+}
+
+describe("Get single record by key 100 logs", () => {
+  bench("recs: `getComponentValue`", async () => {
+    getComponentValue(components.Number, encodeEntity({ key: "uint256" }, { key: 0n }));
+  });
+
+  bench("zustand: `getRecord`", async () => {
+    useStore.getState().getRecord(tables.Number, { key: 0 });
+  });
+});
---
+++ packages/store-sync/src/getRecords100.bench.ts
@@ -0,0 +1,37 @@
+import { bench, describe } from "vitest";
+import { getComponentEntities, getComponentValue } from "@latticexyz/recs";
+import { eq } from "drizzle-orm";
+import { mudStoreTables } from "./sqlite";
+import {
+  components,
+  db,
+  recsStorageAdapter,
+  sqliteStorageAdapter,
+  tables,
+  useStore,
+  zustandStorageAdapter,
+} from "../test/utils";
+import worldRpcLogs from "../../../test-data/world-logs-10000.json";
+import { logsToBlocks } from "../test/logsToBlocks";
+
+const blocks = logsToBlocks(worldRpcLogs.slice(0, 100));
+
+for (const block of blocks) {
+  await Promise.all([recsStorageAdapter(block), zustandStorageAdapter(block), sqliteStorageAdapter(block)]);
+}
+
+describe("Get all records for table 100 logs", () => {
+  bench("recs: `getComponentValue`", async () => {
+    for (const entity of getComponentEntities(components.Number)) {
+      getComponentValue(components.Number, entity);
+    }
+  });
+
+  bench("zustand: `getRecords`", async () => {
+    useStore.getState().getRecords(tables.Number);
+  });
+
+  bench("sqlite: `select`", async () => {
+    db.select().from(mudStoreTables).where(eq(mudStoreTables.name, "Number")).all();
+  });
+});
