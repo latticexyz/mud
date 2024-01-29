23f61f98450b142944dbdd48890cf6f183101646
Fraser Scott <fraserdscott@gmail.com>
2024-01-29 11:10:57 +0000

    chore(common,store,world): upgrade to solidity 0.8.24

--- packages/store/test/RevertSubscriber.sol
+++ packages/store/test/RevertSubscriber.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.21;
+pragma solidity >=0.8.24;
 
 import { StoreHook } from "../src/StoreHook.sol";
 import { FieldLayout } from "../src/FieldLayout.sol";
--- packages/store/test/constants.t.sol
+++ packages/store/test/constants.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.21;
+pragma solidity >=0.8.24;
 
 import { Test, console, stdJson } from "forge-std/Test.sol";
 import "../src/constants.sol";
