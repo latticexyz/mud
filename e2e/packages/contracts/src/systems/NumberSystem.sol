81bcba1bac739fd93390b275915d56d4adfc7938
Fraser Scott <fraserdscott@gmail.com>
2024-02-01 15:05:22 +0000

    refactor: solidity

--- e2e/packages/contracts/src/systems/NumberSystem.sol
+++ e2e/packages/contracts/src/systems/NumberSystem.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.21;
+pragma solidity >=0.8.24;
 
-import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
-import { EncodeArray } from "@latticexyz/store/src/tightcoder/EncodeArray.sol";
 import { System } from "@latticexyz/world/src/System.sol";
 import { Number } from "../codegen/index.sol";
 
