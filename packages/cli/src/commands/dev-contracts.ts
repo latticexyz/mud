c98bd03f0f971a9d9473118c79469fbf002769ba
Kevin Ingersoll <kingersoll@gmail.com>
2024-02-06 14:49:57 +0000

    make salt consistent in dev-contracts

--- packages/cli/src/commands/dev-contracts.ts
+++ packages/cli/src/commands/dev-contracts.ts
@@ -89,7 +89,7 @@ const commandModule: CommandModule<typeof devOptions, InferredOptionTypes<typeof
             saveDeployment: true,
             worldAddress,
             srcDir,
-            salt: undefined,
+            salt: "0x",
           });
           worldAddress = deploy.address;
           // if there were changes while we were deploying, trigger it again
