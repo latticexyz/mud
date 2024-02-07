340c868b53477e4d743f3d194e4fb81434c890e6
Kevin Ingersoll <kingersoll@gmail.com>
2024-02-07 16:54:49 +0000

    add guide for how MUD models data

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -1,4 +1,6 @@
 export default {
+  "how-mud-models-data": "How MUD models data",
+  "replicating-onchain-state": "Replicating onchain state",
   "hello-world": "Hello World",
   "extending-a-world": "Extending a World",
   emojimon: "Emojimon",
