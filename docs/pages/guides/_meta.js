5dcb8e92101c9343e1bcd482a7c4088fd0210581
Kevin Ingersoll <kingersoll@gmail.com>
2024-02-07 17:19:57 +0000

    move files around

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -1,5 +1,4 @@
 export default {
-  "how-mud-models-data": "How MUD models data",
   "replicating-onchain-state": "Replicating onchain state",
   "hello-world": "Hello World",
   "extending-a-world": "Extending a World",
