0dd9158276ed1625571e518d468c495b39ab46b6
Fraser Scott <fraserdscott@gmail.com>
2024-02-13 17:22:41 +0000

    test: update benchmark

--- packages/store-sync/benchmarks/query.bench.ts
+++ packages/store-sync/benchmarks/query.bench.ts
@@ -2,18 +2,18 @@ import { bench, describe } from "vitest";
 import { Has, runQuery } from "@latticexyz/recs";
 import { createRecsStorage, createZustandStorage, tables } from "../test/utils";
 import { logsToBlocks } from "../test/logsToBlocks";
-import worldRpcLogs from "../../../test-data/world-logs.json.json";
+import worldRpcLogs from "../../../test-data/world-logs-query.json";
 
-describe.each("Get records with query: $numRecords records", async ({ logs }) => {
-  const blocks = logsToBlocks(logs);
+const blocks = logsToBlocks(worldRpcLogs);
 
-  const { components, storageAdapter: recsStorageAdapter } = createRecsStorage();
-  const { useStore, storageAdapter: zustandStorageAdapter } = createZustandStorage();
+const { components, storageAdapter: recsStorageAdapter } = createRecsStorage();
+const { useStore, storageAdapter: zustandStorageAdapter } = createZustandStorage();
 
-  for (const block of blocks) {
-    await Promise.all([recsStorageAdapter(block), zustandStorageAdapter(block)]);
-  }
+for (const block of blocks) {
+  await Promise.all([recsStorageAdapter(block), zustandStorageAdapter(block)]);
+}
 
+describe("Get records with query", async () => {
   bench("recs: `runQuery`", async () => {
     runQuery([Has(components.Number), Has(components.Vector)]);
   });
@@ -21,6 +21,6 @@ describe.each("Get records with query: $numRecords records", async ({ logs }) =>
   bench("zustand: `getRecords`", async () => {
     const records = useStore.getState().getRecords(tables.Number);
 
-    Object.keys(useStore.getState().getRecords(tables.Number)).filter((x) => x in records);
+    Object.keys(useStore.getState().getRecords(tables.Vector)).filter((x) => x in records);
   });
 });
