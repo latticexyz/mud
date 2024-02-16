863357b08bdaf281ed1d9b328e8ac1686b7f7569
Fraser Scott <fraserdscott@gmail.com>
2024-02-16 11:08:06 +0000

    chore: move unneeded test

--- e2e/packages/sync-test/data/waitForInitialSync.ts
+++ e2e/packages/sync-test/data/waitForInitialSync.ts
@@ -3,6 +3,4 @@ import { Page, expect } from "@playwright/test";
 export async function waitForInitialSync(page: Page) {
   const syncStep = page.locator("#sync-step");
   await expect(syncStep).toHaveText("live");
-  const syncProgress = page.locator("#sync-percentage");
-  await expect(syncProgress).toHaveText("100");
 }
