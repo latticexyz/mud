936b72c6149a87ff72e9dafe6424cda00a02a8d4
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-02-18 18:43:48 -0600

    fix inaccurate comment

--- examples/minimal/packages/client-react-external-wallet/src/index.tsx
+++ examples/minimal/packages/client-react-external-wallet/src/index.tsx
@@ -10,7 +10,7 @@ const rootElement = document.getElementById("react-root");
 if (!rootElement) throw new Error("React root not found");
 const root = ReactDOM.createRoot(rootElement);
 
-// TODO: This setup function can be synchronous.
+// TODO: figure out if we actually want this to be async or if we should render something else in the meantime
 setup().then(async ({ mud, wagmiConfig }) => {
   root.render(
     <WagmiConfig config={wagmiConfig}>
