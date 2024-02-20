ff57e7568e807b3ef6141b9deaea7e3dc6a56f74
PWR <cell@pwr.site>
2024-02-20 18:18:29 +0100

    Update packages/gas-report/ts/index.ts
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/gas-report/ts/index.ts
+++ packages/gas-report/ts/index.ts
@@ -130,7 +130,8 @@ async function runGasReport(options: Options): Promise<GasReport> {
   const lines = logs.split("\n").map(stripAnsi);
   const gasReportPattern = /^\s*GAS REPORT: (\d+) (.*)$/;
   const testFunctionPattern = /^\[(?:PASS|FAIL).*\] (\w+)\(\)/;
-  // Match both old (before 2024-02-15) and new forge test output format
+  // Matches "Running" for forge versions before 2024-02-15
+  // And "Ran" for forge versions after 2024-02-15
   const testFilePattern = /^(?:Running|Ran) \d+ tests? for (.*):(.*)$/;
 
   function nearestLine(pattern: RegExp, startIndex: number = lines.length - 1): number {
