995a8b5c20606ad8c136c552c9a51b2bbcf39021
Fraser Scott <fraserdscott@gmail.com>
2024-02-22 14:10:02 +0000

    refactor: use system ID's

--- packages/world/test/LimitedCallContext.t.sol
+++ packages/world/test/LimitedCallContext.t.sol
@@ -6,22 +6,23 @@ import { Test } from "forge-std/Test.sol";
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
-import { IBaseWorld } from "../src/codegen/interfaces/IBaseWorld.sol";
 import { createWorld } from "./createWorld.sol";
+
 import { SystemCallData, SystemCallFromData } from "../src/modules/init/types.sol";
-import { BalanceTransferSystem } from "../src/modules/init/implementations/BalanceTransferSystem.sol";
-import { BatchCallSystem } from "../src/modules/init/implementations/BatchCallSystem.sol";
-import { WorldResourceIdLib } from "../src/WorldResourceId.sol";
-import { RESOURCE_SYSTEM } from "../src/worldResourceTypes.sol";
-import { Systems } from "../src/codegen/tables/Systems.sol";
 import { LimitedCallContext } from "../src/modules/init/LimitedCallContext.sol";
 
-contract LimitedCallContextTest is Test {
-  IBaseWorld world;
+import { AccessManagementSystem } from "../src/modules/init/implementations/AccessManagementSystem.sol";
+import { BalanceTransferSystem } from "../src/modules/init/implementations/BalanceTransferSystem.sol";
+import { BatchCallSystem } from "../src/modules/init/implementations/BatchCallSystem.sol";
 
+import { RegistrationSystem } from "../src/modules/init/RegistrationSystem.sol";
+import { ACCESS_MANAGEMENT_SYSTEM_ID, BALANCE_TRANSFER_SYSTEM_ID, BATCH_CALL_SYSTEM_ID, REGISTRATION_SYSTEM_ID } from "../src/modules/init/constants.sol";
+
+import { Systems } from "../src/codegen/tables/Systems.sol";
+
+contract LimitedCallContextTest is Test {
   function setUp() public {
-    world = createWorld();
-    StoreSwitch.setStoreAddress(address(world));
+    StoreSwitch.setStoreAddress(address(createWorld()));
   }
 
   function callSystem(ResourceId resourceId, bytes memory callData) internal {
@@ -35,52 +36,36 @@ contract LimitedCallContextTest is Test {
   }
 
   function testTransferBalanceToNamespace() public {
-    ResourceId resourceId = WorldResourceIdLib.encode({
-      typeId: RESOURCE_SYSTEM,
-      namespace: "",
-      name: "BalanceTransfer"
-    });
-
     ResourceId fromNamespaceId;
     ResourceId toNamespaceId;
     uint256 amount;
 
     callSystem(
-      resourceId,
+      BALANCE_TRANSFER_SYSTEM_ID,
       abi.encodeCall(BalanceTransferSystem.transferBalanceToNamespace, (fromNamespaceId, toNamespaceId, amount))
     );
   }
 
   function testTransferBalanceToAddress() public {
-    ResourceId resourceId = WorldResourceIdLib.encode({
-      typeId: RESOURCE_SYSTEM,
-      namespace: "",
-      name: "BalanceTransfer"
-    });
-
     ResourceId fromNamespaceId;
     address toAddress;
     uint256 amount;
 
     callSystem(
-      resourceId,
+      BALANCE_TRANSFER_SYSTEM_ID,
       abi.encodeCall(BalanceTransferSystem.transferBalanceToAddress, (fromNamespaceId, toAddress, amount))
     );
   }
 
   function testBatchCall() public {
-    ResourceId resourceId = WorldResourceIdLib.encode({ typeId: RESOURCE_SYSTEM, namespace: "", name: "BatchCall" });
-
     SystemCallData[] memory systemCalls;
 
-    callSystem(resourceId, abi.encodeCall(BatchCallSystem.batchCall, (systemCalls)));
+    callSystem(BATCH_CALL_SYSTEM_ID, abi.encodeCall(BatchCallSystem.batchCall, (systemCalls)));
   }
 
   function testBatchCallFrom() public {
-    ResourceId resourceId = WorldResourceIdLib.encode({ typeId: RESOURCE_SYSTEM, namespace: "", name: "BatchCall" });
-
     SystemCallFromData[] memory systemCalls;
 
-    callSystem(resourceId, abi.encodeCall(BatchCallSystem.batchCallFrom, (systemCalls)));
+    callSystem(BATCH_CALL_SYSTEM_ID, abi.encodeCall(BatchCallSystem.batchCallFrom, (systemCalls)));
   }
 }
