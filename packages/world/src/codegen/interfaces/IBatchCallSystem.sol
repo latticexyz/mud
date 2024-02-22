00ee73b6856c5e1d06c20ddfd7dc45111165d073
alvrs <alvarius@lattice.xyz>
2024-02-22 23:15:24 +0000

    add failing test case for sending value to batchCall

--- packages/world/src/codegen/interfaces/IBatchCallSystem.sol
+++ packages/world/src/codegen/interfaces/IBatchCallSystem.sol
@@ -10,7 +10,9 @@ import { SystemCallData, SystemCallFromData } from "./../../modules/init/types.s
  * @dev This interface is automatically generated from the corresponding system contract. Do not edit manually.
  */
 interface IBatchCallSystem {
-  function batchCall(SystemCallData[] calldata systemCalls) external returns (bytes[] memory returnDatas);
+  function batchCall(SystemCallData[] calldata systemCalls) external payable returns (bytes[] memory returnDatas);
 
-  function batchCallFrom(SystemCallFromData[] calldata systemCalls) external returns (bytes[] memory returnDatas);
+  function batchCallFrom(
+    SystemCallFromData[] calldata systemCalls
+  ) external payable returns (bytes[] memory returnDatas);
 }
