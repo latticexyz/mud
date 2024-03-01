c991c71ae2eed8ec3c8a328962439710cffc135d
yonada <fraserdscott@gmail.com>
2024-03-01 11:22:09 +0000

    refactor(store): event interfaces for Store libraries  (#2348)

--- packages/store/src/IStore.sol
+++ packages/store/src/IStore.sol
@@ -4,9 +4,22 @@ pragma solidity >=0.8.24;
 import { IStoreErrors } from "./IStoreErrors.sol";
 import { IStoreData } from "./IStoreData.sol";
 import { IStoreRegistration } from "./IStoreRegistration.sol";
+import { IFieldLayoutErrors } from "./IFieldLayoutErrors.sol";
+import { IPackedCounterErrors } from "./IPackedCounterErrors.sol";
+import { ISchemaErrors } from "./ISchemaErrors.sol";
+import { ISliceErrors } from "./ISliceErrors.sol";
 
 /**
  * @title IStore
  * @author MUD (https://mud.dev) by Lattice (https://lattice.xyz)
+ * @notice IStore implements the error interfaces for each library that it uses.
  */
-interface IStore is IStoreData, IStoreRegistration, IStoreErrors {}
+interface IStore is
+  IStoreData,
+  IStoreRegistration,
+  IStoreErrors,
+  IFieldLayoutErrors,
+  IPackedCounterErrors,
+  ISchemaErrors,
+  ISliceErrors
+{}
