db9528207c13100a54317f0d01c58f663d4cebd9
Fraser Scott <fraserdscott@gmail.com>
2024-03-01 10:26:45 +0000

    docs: better comment

--- packages/store/src/IFieldLayoutErrors.sol
+++ packages/store/src/IFieldLayoutErrors.sol
@@ -5,6 +5,8 @@ pragma solidity >=0.8.24;
  * @title IFieldLayoutErrors
  * @author MUD (https://mud.dev) by Lattice (https://lattice.xyz)
  * @notice This interface includes errors for the FieldLayout library.
+ * @dev We bundle these errors in an interface (instead of at the file-level or in their corresponding libraries) so they can be inherited by IStore.
+ * This ensures that all possible errors are included in the IStore ABI for proper decoding in the frontend.
  */
 interface IFieldLayoutErrors {
   error FieldLayout_TooManyFields(uint256 numFields, uint256 maxFields);
--- packages/store/src/ISliceErrors.sol
+++ packages/store/src/ISliceErrors.sol
@@ -5,6 +5,8 @@ pragma solidity >=0.8.24;
  * @title ISliceErrors
  * @author MUD (https://mud.dev) by Lattice (https://lattice.xyz)
  * @notice This interface includes errors for the Slice library.
+ * @dev We bundle these errors in an interface (instead of at the file-level or in their corresponding libraries) so they can be inherited by IStore.
+ * This ensures that all possible errors are included in the IStore ABI for proper decoding in the frontend.
  */
 interface ISliceErrors {
   error Slice_OutOfBounds(bytes data, uint256 start, uint256 end);
