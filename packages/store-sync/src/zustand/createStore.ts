bbfbe92d68c2357c5a7e6ef0fc4b6b8b24cdbe13
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-01 18:14:09 +0000

    add list of last updated records

--- packages/store-sync/src/zustand/createStore.ts
+++ packages/store-sync/src/zustand/createStore.ts
@@ -33,6 +33,11 @@ export type ZustandState<tables extends Tables> = {
   readonly records: {
     readonly [id: string]: TableRecord<tables[keyof tables]>;
   };
+  /** Records that most recently updated. We subscribe to this for streaming updates. */
+  readonly lastUpdatedRecords: readonly {
+    readonly previousRecord: TableRecord<tables[keyof tables]>;
+    readonly nextRecord: TableRecord<tables[keyof tables]>;
+  }[];
   readonly getRecords: <table extends Table>(table: table) => TableRecords<table>;
   readonly getRecord: <table extends Table>(
     table: table,
@@ -62,6 +67,7 @@ export function createStore<tables extends Tables>(opts: CreateStoreOptions<tabl
     tables: Object.fromEntries(Object.entries(opts.tables).map(([, table]) => [table.tableId, table])),
     rawRecords: {},
     records: {},
+    lastUpdatedRecords: [],
     getRecords: <table extends Table>(table: table): TableRecords<table> => {
       const records = get().records;
       return Object.fromEntries(
