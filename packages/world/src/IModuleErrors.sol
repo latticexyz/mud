32db9958ac2c8050d6634b05d68843c233adf35a
Fraser Scott <fraserdscott@gmail.com>
2024-03-06 16:36:05 +0000

    refactor(world): add IModuleErrors

---
+++ packages/world/src/IModuleErrors.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { IERC165 } from "./IERC165.sol";
+
+/**
+ * @title ISliceErrors
+ * @author MUD (https://mud.dev) by Lattice (https://lattice.xyz)
+ * @notice This interface includes errors for the Slice library.
+ * @dev We bundle these errors in an interface (instead of at the file-level or in their corresponding library) so they can be inherited by IWorldKernel.
+ * This ensures that all errors are included in the IWorldKernel ABI for proper decoding in the frontend.
+ */
+interface IModuleErrors {
+  /// @dev Errors to represent non-support of specific installation types.
+  error Module_RootInstallNotSupported();
+  error Module_NonRootInstallNotSupported();
+  error Module_AlreadyInstalled();
+  error Module_MissingDependency(address dependency);
+}
