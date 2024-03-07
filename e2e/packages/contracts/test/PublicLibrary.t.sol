85bb8eb80150eea59b91306f251343d07bbe8286
Fraser Scott <fraserdscott@gmail.com>
2024-03-07 15:37:22 +0000

    test: document tests

--- e2e/packages/contracts/test/PublicLibrary.t.sol
+++ e2e/packages/contracts/test/PublicLibrary.t.sol
@@ -6,7 +6,9 @@ import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 
 contract PublicLibraryTest is MudTest {
-  // Test that the deployer can handle deeply nested public libraries
+  /**
+   * @dev Test that the deployer can handle deeply nested public libraries.
+   */
   function testNesting() public {
     assertEq(IWorld(worldAddress).callLib(), "success");
     assertEq(IWorld(worldAddress).callFreeFunc(), "success");
