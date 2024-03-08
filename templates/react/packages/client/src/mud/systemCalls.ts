c727e70910cc8275030d2869b67385276f491d63
tash-2s <81064017+tash-2s@users.noreply.github.com>
2024-03-07 20:30:08 -0600

    Reapply "use worldContract as it's convenient and it doesn't affect js size much"
    This reverts commit 7fdd3dee5ef7c9315f2629cb8318ea6071fb1a69.

--- templates/react/packages/client/src/mud/systemCalls.ts
+++ templates/react/packages/client/src/mud/systemCalls.ts
@@ -1,35 +1,20 @@
-import type { Hex, WalletClient, Transport, Chain, Account } from "viem";
+import { type Hex } from "viem";
 import { type Network } from "./setupNetwork";
-import IWorldAbi from "contracts/out/IWorld.sol/IWorld.abi.json";
+import { type Burner } from "./wallet/createBurner";
 
-export async function addTask(network: Network, walletClient: WalletClient<Transport, Chain, Account>, label: string) {
-  const tx = await walletClient.writeContract({
-    address: network.worldAddress,
-    abi: IWorldAbi,
-    functionName: "addTask",
-    args: [label],
-  });
+export async function addTask(network: Network, worldContract: Burner["worldContract"], label: string) {
+  const tx = await worldContract.write.addTask([label]);
   await network.waitForTransaction(tx);
 }
 
-export async function toggleTask(network: Network, walletClient: WalletClient<Transport, Chain, Account>, key: Hex) {
+export async function toggleTask(network: Network, worldContract: Burner["worldContract"], key: Hex) {
   const isComplete = (network.useStore.getState().getValue(network.tables.Tasks, { key })?.completedAt ?? 0n) > 0n;
 
-  const tx = await walletClient.writeContract({
-    address: network.worldAddress,
-    abi: IWorldAbi,
-    functionName: isComplete ? "resetTask" : "completeTask",
-    args: [key],
-  });
+  const tx = isComplete ? await worldContract.write.resetTask([key]) : await worldContract.write.completeTask([key]);
   await network.waitForTransaction(tx);
 }
 
-export async function deleteTask(network: Network, walletClient: WalletClient<Transport, Chain, Account>, key: Hex) {
-  const tx = await walletClient.writeContract({
-    address: network.worldAddress,
-    abi: IWorldAbi,
-    functionName: "deleteTask",
-    args: [key],
-  });
+export async function deleteTask(network: Network, worldContract: Burner["worldContract"], key: Hex) {
+  const tx = await worldContract.write.deleteTask([key]);
   await network.waitForTransaction(tx);
 }
--- templates/react/packages/client/src/mud/wallet/createBurner.ts
+++ templates/react/packages/client/src/mud/wallet/createBurner.ts
@@ -1,13 +1,15 @@
-import { createWalletClient } from "viem";
+import { createWalletClient, getContract } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { Subject, share } from "rxjs";
 import { getBurnerPrivateKey, type ContractWrite } from "@latticexyz/common";
 import { transactionQueue, writeObserver } from "@latticexyz/common/actions";
-import { type MUDChain } from "@latticexyz/common/chains";
-import { type Burner } from "./BurnerContext";
 import { createClientConfig } from "../createClientConfig";
+import { type Network } from "../setupNetwork";
+import IWorldAbi from "contracts/out/IWorld.sol/IWorld.abi.json";
 
-export function createBurner(chain: MUDChain): Burner {
+export type Burner = ReturnType<typeof createBurner>;
+
+export function createBurner(network: Network) {
   /*
    * Create an observable for contract writes that we can
    * pass into MUD dev tools for transaction observability.
@@ -15,11 +17,20 @@ export function createBurner(chain: MUDChain): Burner {
   const write$ = new Subject<ContractWrite>();
 
   const walletClient = createWalletClient({
-    ...createClientConfig(chain),
+    ...createClientConfig(network.publicClient.chain),
     account: privateKeyToAccount(getBurnerPrivateKey()),
   })
     .extend(transactionQueue())
     .extend(writeObserver({ onWrite: (write) => write$.next(write) }));
 
-  return { walletClient, write$: write$.asObservable().pipe(share()) };
+  /*
+   * Create an object for communicating with the deployed World.
+   */
+  const worldContract = getContract({
+    address: network.worldAddress,
+    abi: IWorldAbi,
+    client: { public: network.publicClient, wallet: walletClient },
+  });
+
+  return { walletClient, worldContract, write$: write$.asObservable().pipe(share()) };
 }
