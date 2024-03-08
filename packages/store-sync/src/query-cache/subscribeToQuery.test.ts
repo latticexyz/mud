cefc3e4c0fe60c719a7c9a2cfc41bd99ab650dcb
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-08 16:30:20 +0000

    try setting interval mining to zero

--- packages/store-sync/src/query-cache/subscribeToQuery.test.ts
+++ packages/store-sync/src/query-cache/subscribeToQuery.test.ts
@@ -14,6 +14,7 @@ describe("subscribeToQuery", async () => {
   let worldAddress: Address;
 
   beforeAll(async () => {
+    await testClient.setIntervalMining({ interval: 0 });
     await testClient.setBalance({ address: henryAccount.address, value: parseEther("1") });
     worldAddress = await deployMockGame();
   });
