8c7388f4b14ac29064e11671c4a40d6238de2dce
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-08 15:53:41 +0000

    shorter teardown timeout

--- packages/store-sync/vitest.config.ts
+++ packages/store-sync/vitest.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globalSetup: ["test/globalSetup.ts"],
+    // Temporarily set a low teardown timeout because anvil hangs otherwise
+    // Could move this timeout to anvil setup after https://github.com/wevm/anvil.js/pull/46
+    teardownTimeout: 500,
   },
 });
