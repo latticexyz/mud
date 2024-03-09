323f785c5bb04e94609f3d2d83f4e23e5aa2cf0e
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-09 15:59:25 +0000

    fix(common): remove underscore prefix from root namespace labels

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -8,7 +8,7 @@ export * from "./getNonceManager";
 export * from "./getNonceManagerId";
 export * from "./hexToResource";
 export * from "./readHex";
-export * from "./resourceLabel";
+export * from "./resourceToLabel";
 export * from "./resourceToHex";
 export * from "./resourceTypes";
 export * from "./result";
