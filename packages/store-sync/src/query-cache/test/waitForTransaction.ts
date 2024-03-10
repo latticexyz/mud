f4a6ae0f2e5a5f0538d9ff6d18a727e8ee1b0532
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-10 16:33:25 +0000

    log tx pool status

--- packages/store-sync/src/query-cache/test/waitForTransaction.ts
+++ packages/store-sync/src/query-cache/test/waitForTransaction.ts
@@ -1,10 +1,11 @@
 import { Hex } from "viem";
-import { waitForTransactionReceipt } from "viem/actions";
+import { getTransactionReceipt } from "viem/actions";
 import { testClient } from "../../../test/common";
 
 export async function waitForTransaction(hash: Hex): Promise<void> {
+  console.log("txpool", await testClient.getTxpoolContent());
   await testClient.mine({ blocks: 1 });
-  const receipt = await waitForTransactionReceipt(testClient, { hash });
+  const receipt = await getTransactionReceipt(testClient, { hash });
   if (receipt.status === "reverted") {
     // TODO: better error
     throw new Error(`Transaction reverted (${hash})`);
