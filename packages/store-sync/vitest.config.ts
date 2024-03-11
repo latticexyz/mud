488cd7459a19cc48457fdebe4915cbcfdfda7802
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-11 11:15:38 +0000

    bump timeout

--- packages/store-sync/vitest.config.ts
+++ packages/store-sync/vitest.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
     // Temporarily set a low teardown timeout because anvil hangs otherwise
     // Could move this timeout to anvil setup after https://github.com/wevm/anvil.js/pull/46
     teardownTimeout: 500,
+    hookTimeout: 15000,
   },
 });
