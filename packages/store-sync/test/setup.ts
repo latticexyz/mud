38e0707697c6922baf8726ef770dc8d7596ba712
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-11 11:02:48 +0000

    add comment about setup handlers

--- packages/store-sync/test/setup.ts
+++ packages/store-sync/test/setup.ts
@@ -1,6 +1,7 @@
 import { beforeAll, beforeEach } from "vitest";
 import { testClient } from "./common";
 
+// Some test suites deploy contracts in a `beforeAll` handler, so we restore chain state here.
 beforeAll(async () => {
   const state = await testClient.dumpState();
   return async (): Promise<void> => {
@@ -8,6 +9,7 @@ beforeAll(async () => {
   };
 });
 
+// Some tests execute transactions, so we restore chain state here.
 beforeEach(async () => {
   const state = await testClient.dumpState();
   return async (): Promise<void> => {
