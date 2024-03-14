b33c3f6ee988246a2d7b1dd513b30ec029d52f19
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-14 11:20:30 +0000

    clarify key names

--- packages/world/ts/config/v2/output.ts
+++ packages/world/ts/config/v2/output.ts
@@ -2,9 +2,9 @@ import { Config as StoreConfig, Table } from "@latticexyz/store/config/v2";
 
 export type Config = StoreConfig & {
   readonly namespaces: {
-    readonly [key: string]: {
+    readonly [namespace: string]: {
       readonly tables: {
-        readonly [key: string]: Table;
+        readonly [tableName: string]: Table;
       };
     };
   };
