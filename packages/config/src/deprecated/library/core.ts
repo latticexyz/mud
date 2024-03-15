e4c0dea8eb10ace95e3adbf62ab1ab275363754c
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-15 14:05:38 +0000

    refactor(config): deprecate old helpers, lift common types (#2445)

--- packages/config/src/deprecated/library/core.ts
+++ packages/config/src/deprecated/library/core.ts
@@ -1,14 +1,20 @@
 import { MUDCoreContext } from "./context";
 
+/** @deprecated */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface MUDCoreUserConfig {}
 
+/** @deprecated */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface MUDCoreConfig {}
 
+/** @deprecated */
 export type MUDConfigExtender = (config: MUDCoreConfig) => Record<string, unknown>;
 
-/** Resolver that sequentially passes the config through all the plugins */
+/**
+ * Resolver that sequentially passes the config through all the plugins
+ * @deprecated
+ */
 export function mudCoreConfig(config: MUDCoreUserConfig): MUDCoreConfig {
   // config types can change with plugins, `any` helps avoid errors when typechecking dependencies
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -20,7 +26,10 @@ export function mudCoreConfig(config: MUDCoreUserConfig): MUDCoreConfig {
   return configAsAny;
 }
 
-/** Utility for plugin developers to extend the core config */
+/**
+ * Utility for plugin developers to extend the core config
+ * @deprecated
+ */
 export function extendMUDCoreConfig(extender: MUDConfigExtender) {
   const context = MUDCoreContext.getContext();
   context.configExtenders.push(extender);
--- packages/store/ts/config/v2/output.ts
+++ packages/store/ts/config/v2/output.ts
@@ -1,11 +1,6 @@
-import { DynamicAbiType, StaticAbiType } from "@latticexyz/schema-type";
-import { Hex } from "viem";
+import { AbiType, StaticAbiType, Schema, Table as BaseTable } from "@latticexyz/config";
 
-// Although we could import `SchemaAbiType` from `@latticexyz/schema-type` here, we "redefine" this here
-// so that our downstream type errors give back `AbiType` instead of the union of all possible ABI types.
-//
-// This is a bit of a TS compiler hack and we should figure out a better long-term approach.
-export type AbiType = StaticAbiType | DynamicAbiType;
+export type { AbiType, Schema };
 
 export type UserTypes = {
   readonly [userTypeName: string]: { type: AbiType; filePath: string };
@@ -13,15 +8,6 @@ export type UserTypes = {
 
 export type Enums = {
   readonly [enumName: string]: readonly [string, ...string[]];
-};
-
-export type Schema = {
-  readonly [fieldName: string]: {
-    /** the Solidity primitive ABI type */
-    readonly type: AbiType;
-    /** the user defined type or Solidity primitive ABI type */
-    readonly internalType: string;
-  };
 };
 
 export type KeySchema = {
@@ -40,15 +26,9 @@ export type TableCodegenOptions = {
   readonly dataStruct: boolean;
 };
 
-export type Table = {
-  readonly tableId: Hex;
-  readonly primaryKey: readonly string[];
-  readonly schema: Schema;
+export type Table = BaseTable & {
   readonly keySchema: KeySchema;
   readonly valueSchema: Schema;
-  readonly type: "table" | "offchainTable";
-  readonly name: string;
-  readonly namespace: string;
   readonly codegen: TableCodegenOptions;
 };
 
