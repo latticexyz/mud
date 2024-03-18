460fdd1501681627eb45d10c621b0f4b7a2edaa3
mous <97399882+mouseless-eth@users.noreply.github.com>
2024-03-18 02:20:09 +0200

    feat(templates/react): setup aa infra

---
+++ templates/react/packages/account-abstraction/src/index.ts
@@ -0,0 +1,28 @@
+import { createWalletClient, http } from "viem";
+import { mnemonicToAccount } from "viem/accounts";
+import { foundry } from "viem/chains";
+import { DETERMINISTIC_DEPLOYER, ENTRY_POINT_CREATE_CALL, SIMPLE_ACCOUNT_FACTORY_CREATE_CALL } from "./constants";
+
+const account = mnemonicToAccount("test test test test test test test test test test test junk");
+
+const walletClient = createWalletClient({
+  account,
+  chain: foundry,
+  transport: http("http://127.0.0.1:8545"),
+});
+
+(async () => {
+  const entryPointHash = await walletClient.sendTransaction({
+    to: DETERMINISTIC_DEPLOYER,
+    data: ENTRY_POINT_CREATE_CALL,
+  });
+
+  console.log("entryPointHash deployed", entryPointHash);
+
+  const simpleAccountFactoryHash = await walletClient.sendTransaction({
+    to: DETERMINISTIC_DEPLOYER,
+    data: SIMPLE_ACCOUNT_FACTORY_CREATE_CALL,
+  });
+
+  console.log("simpleAccountFactory deployed", simpleAccountFactoryHash);
+})();
