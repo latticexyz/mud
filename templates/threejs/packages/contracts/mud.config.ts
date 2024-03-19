b367cee6a7844c1906958ae443a2cc9292010c20
Fraser Scott <fraserdscott@gmail.com>
2024-03-19 17:07:10 +0000

    refactor: threejs id

--- templates/threejs/packages/contracts/mud.config.ts
+++ templates/threejs/packages/contracts/mud.config.ts
@@ -4,12 +4,11 @@ export default defineWorld({
   tables: {
     Position: {
       schema: {
-        key: "bytes32",
+        id: "bytes32",
         x: "int32",
         y: "int32",
         z: "int32",
       },
-      key: ["key"],
     },
   },
 });
