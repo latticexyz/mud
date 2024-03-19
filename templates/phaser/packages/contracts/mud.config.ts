b9cd8c94064c5befefd4e7e4b423b56f3916a1e3
Fraser Scott <fraserdscott@gmail.com>
2024-03-19 17:15:29 +0000

    refactor: multi-line

--- templates/phaser/packages/contracts/mud.config.ts
+++ templates/phaser/packages/contracts/mud.config.ts
@@ -3,7 +3,9 @@ import { defineWorld } from "@latticexyz/world/config/v2";
 export default defineWorld({
   tables: {
     Counter: {
-      schema: { value: "uint32" },
+      schema: {
+        value: "uint32",
+      },
       key: [],
     },
   },
