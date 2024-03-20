81aa75e3871db7736f2cc941e25cc51959be614f
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-20 20:05:12 +0000

    more fixes

--- examples/minimal/packages/contracts/mud.config.ts
+++ examples/minimal/packages/contracts/mud.config.ts
@@ -1,15 +1,47 @@
 import { defineWorld } from "@latticexyz/world";
+import { resolveTableId } from "@latticexyz/config/library";
 
 export default defineWorld({
-  namespace: "LastCall",
-  tables: {
-    LastCall: {
-      schema: {
-        caller: "address",
-        callTime: "uint256",
-        sender: "address",
-      },
-      key: ["caller"],
+  systems: {
+    IncrementSystem: {
+      name: "increment",
+      openAccess: true,
     },
   },
+  excludeSystems: [
+    // Until namespace overrides, this system must be manually deployed in PostDeploy
+    "ChatNamespacedSystem",
+  ],
+  tables: {
+    CounterTable: {
+      schema: {
+        value: "uint32",
+      },
+      key: [],
+      codegen: { storeArgument: true },
+    },
+    MessageTable: {
+      type: "offchainTable",
+      schema: {
+        value: "string",
+      },
+      key: [],
+    },
+    Inventory: {
+      schema: {
+        owner: "address",
+        item: "uint32",
+        itemVariant: "uint32",
+        amount: "uint32",
+      },
+      key: ["owner", "item", "itemVariant"],
+    },
+  },
+  modules: [
+    {
+      name: "KeysWithValueModule",
+      root: true,
+      args: [resolveTableId("Inventory")],
+    },
+  ],
 });
