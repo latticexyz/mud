e9a578ff7c9d220c84cf2719e73994603cc36e13
Ori Pomerantz <qbzzt1@gmail.com>
2024-03-21 11:24:27 -0500

    add redirect

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -221,6 +221,11 @@ export default withNextra({
         destination: "/guides/extending-a-world",
         permanent: false,
       },
+      {
+        source: "/templates/typescript/getting-started",
+        destination: "/quickstart",
+        permanent: true,
+      },
     ];
   },
 });
