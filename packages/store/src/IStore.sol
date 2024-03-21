794f428079723f86b9788b2032b2360f3e4353d6
alvarius <alvarius@lattice.xyz>
2024-03-21 12:08:31 +0000

    Update packages/store/src/IStore.sol
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/store/src/IStore.sol
+++ packages/store/src/IStore.sol
@@ -7,6 +7,5 @@ import { IStoreRegistration } from "./IStoreRegistration.sol";
 /**
  * @title IStore
  * @author MUD (https://mud.dev) by Lattice (https://lattice.xyz)
- * @notice IStore includes the error interfaces for each library that it uses.
  */
 interface IStore is IStoreKernel, IStoreRegistration {}
