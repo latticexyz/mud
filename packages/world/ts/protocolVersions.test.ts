b0253b84c00c8c5c09de2d656d887efd30f44c6a
Kevin Ingersoll <kingersoll@gmail.com>
2024-03-21 11:33:03 +0000

    update protocol version tests

--- packages/world/ts/protocolVersions.test.ts
+++ packages/world/ts/protocolVersions.test.ts
@@ -1,8 +1,20 @@
 import { describe, expect, it } from "vitest";
 import fs from "node:fs";
-import WorldAbi from "../out/World.sol/World.abi.json";
+import WorldAbi from "../out/IBaseWorld.sol/IBaseWorld.abi.json";
 import { formatAbi } from "abitype";
 import { protocolVersions } from "./protocolVersions";
+
+/**
+ * If these tests fail, it's because the protocol changed since the last ABI + version snapshot.
+ *
+ * The fix for this depends on your needs:
+ *
+ * - If the change was an intentional iteration of the protocol, increment the `version.sol` file according to semver.
+ *   A new protocol snapshot will be created when you run this test again, which you should check into git.
+ *
+ * - If the protocol change is for a yet-to-be-release version (i.e. the version was bumped but packages not yet published),
+ *   then it's safe to just update the protocol snapshot for this version (`vitest -u`).
+ */
 
 const [, currentVersion] = fs.readFileSync(`${__dirname}/../src/version.sol`, "utf8").match(/VERSION = "(.*?)"/) ?? [];
 
