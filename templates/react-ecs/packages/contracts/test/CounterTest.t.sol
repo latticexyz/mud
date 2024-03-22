7932866bcf9d9fa8c4d593e8ab62d2f64d1b14f4
Fraser Scott <fraserdscott@gmail.com>
2024-03-22 11:43:56 +0000

    refactor: set counter tests

--- templates/react-ecs/packages/contracts/test/CounterTest.t.sol
+++ templates/react-ecs/packages/contracts/test/CounterTest.t.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.24;
 
 import "forge-std/Test.sol";
 import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
+import { IWorldErrors } from "@latticexyz/world/src/IWorldErrors.sol";
+import { ResourceId, WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
 import { getKeysWithValue } from "@latticexyz/world-modules/src/modules/keyswithvalue/getKeysWithValue.sol";
 
 import { IWorld } from "../src/codegen/world/IWorld.sol";
@@ -29,17 +31,32 @@ contract CounterTest is MudTest {
     assertEq(counter, 2);
   }
 
-  function testFailCounterSet() public {
-    // These tests are sent by FOUNDRY_SENDER by default.
-    // FOUNDRY_SENDER is not the deployer, so it does not have the right to set the counter.
-    Counter.set(0);
+  function testCounterGet() public {
+    // Expect the counter to be 1 because it was incremented in the PostDeploy script.
+    uint32 counter = Counter.get();
+    assertEq(counter, 1);
+
+    // Expect the counter to be 2 after calling increment.
+    IWorld(worldAddress).increment();
+    counter = Counter.get();
+    assertEq(counter, 2);
   }
 
   function testCounterSet() public {
+    // alice is not the deployer, so it does not have the right to set the counter.
+    address alice = makeAddr("alice");
+    vm.prank(alice);
+    vm.expectRevert(
+      abi.encodeWithSelector(IWorldErrors.World_AccessDenied.selector, Counter._tableId.toString(), alice)
+    );
+    Counter.set(42);
+
     // Run as the deployer address, which is allowed to set the counter.
     vm.prank(address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266));
-    Counter.set(0);
+    Counter.set(42);
+
+    // Expect the counter value to have been set.
     uint32 counter = Counter.get();
-    assertEq(counter, 0);
+    assertEq(counter, 42);
   }
 }
