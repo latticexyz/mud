b141f621dd3fe8eb20e64c185913b4f60631429f
Fraser Scott <fraserdscott@gmail.com>
2024-03-26 11:58:21 +0000

    test: tidy test naming

--- e2e/packages/sync-test/data/callRegisterDelegationWithSignature.ts
+++ e2e/packages/sync-test/data/callRegisterDelegationWithSignature.ts
@@ -1,21 +1,56 @@
 import { Page } from "@playwright/test";
-import IWorldAbi from "../../contracts/out/IWorld.sol/IWorld.abi.json";
 import { GetContractReturnType, PublicClient, WalletClient } from "viem";
 import { AbiParametersToPrimitiveTypes, ExtractAbiFunction, ExtractAbiFunctionNames } from "abitype";
 
-type WorldAbi = typeof IWorldAbi;
+const DelegationAbi = [
+  {
+    type: "function",
+    name: "registerDelegationWithSignature",
+    inputs: [
+      {
+        name: "delegatee",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "delegationControlId",
+        type: "bytes32",
+        internalType: "ResourceId",
+      },
+      {
+        name: "initCallData",
+        type: "bytes",
+        internalType: "bytes",
+      },
+      {
+        name: "delegator",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "signature",
+        type: "bytes",
+        internalType: "bytes",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+] as const;
 
-type WorldContract = GetContractReturnType<WorldAbi, PublicClient, WalletClient>;
+type DelegationAbi = typeof DelegationAbi;
 
-type WriteMethodName = ExtractAbiFunctionNames<WorldAbi>;
-type WriteMethod<TMethod extends WriteMethodName> = ExtractAbiFunction<WorldAbi, TMethod>;
+type WorldContract = GetContractReturnType<DelegationAbi, PublicClient, WalletClient>;
+
+type WriteMethodName = ExtractAbiFunctionNames<DelegationAbi>;
+type WriteMethod<TMethod extends WriteMethodName> = ExtractAbiFunction<DelegationAbi, TMethod>;
 type WriteArgs<TMethod extends WriteMethodName> = AbiParametersToPrimitiveTypes<WriteMethod<TMethod>["inputs"]>;
 
 export function callRegisterDelegationWithSignature(page: Page, args?: WriteArgs<"registerDelegationWithSignature">) {
   return page.evaluate(
     ([_args]) => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const walletClient = (window as any).walletClient;
+      const walletClient = (window as any).walletClient as WalletClient;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const worldContract = (window as any).worldContract as WorldContract;
 
--- e2e/packages/sync-test/registerDelegationWithSignature.test.ts
+++ e2e/packages/sync-test/registerDelegationWithSignature.test.ts
@@ -16,7 +16,7 @@ import { types } from "@latticexyz/world/internal";
 import { getWorld } from "./data/getWorld";
 import { callRegisterDelegationWithSignature } from "./data/callRegisterDelegationWithSignature";
 
-const PRIVATE_KEY = "0x67bbd1575ecc79b3247c7d7b87a5bc533ccb6a63955a9fefdfaf75853f7cd543";
+const DELEGATOR_PRIVATE_KEY = "0x67bbd1575ecc79b3247c7d7b87a5bc533ccb6a63955a9fefdfaf75853f7cd543";
 
 const worldConfigV1 = worldToV1(worldConfig);
 
@@ -53,23 +53,24 @@ describe("registerDelegationWithSignature", async () => {
       transport: transportObserver(http(mudFoundry.rpcUrls.default.http[0] ?? undefined)),
     } as const satisfies ClientConfig;
 
-    const account = createBurnerAccount(PRIVATE_KEY);
-    const walletClient = createWalletClient({
+    const delegator = createBurnerAccount(DELEGATOR_PRIVATE_KEY);
+    const delegatorWalletClient = createWalletClient({
       ...clientOptions,
-      account,
+      account: delegator,
     });
 
     const worldContract = await getWorld(page);
 
-    // Sign registration message
+    // Declare delegation parameters
     const delegatee = "0x7203e7ADfDF38519e1ff4f8Da7DCdC969371f377";
     const delegationControlId = resourceToHex({ type: "system", namespace: "", name: "unlimited" });
     const initCallData = "0x";
     const nonce = 0n;
 
-    const signature = await walletClient.signTypedData({
+    // Sign registration message
+    const signature = await delegatorWalletClient.signTypedData({
       domain: {
-        chainId: walletClient.chain.id,
+        chainId: delegatorWalletClient.chain.id,
         verifyingContract: worldContract.address,
       },
       types,
@@ -87,7 +88,7 @@ describe("registerDelegationWithSignature", async () => {
       delegatee,
       delegationControlId,
       initCallData,
-      walletClient.account.address,
+      delegator.address,
       signature,
     ]);
 
@@ -95,7 +96,7 @@ describe("registerDelegationWithSignature", async () => {
     const value = await callPageFunction(page, "getComponentValue", [
       "UserDelegationControl",
       encodeEntity(worldConfigV1.tables.UserDelegationControl.keySchema, {
-        delegator: account.address,
+        delegator: delegator.address,
         delegatee,
       }),
     ]);
