50056834aac1137e4968f8f7f645b31a6bd8fe94
Fraser Scott <fraserdscott@gmail.com>
2024-03-27 12:16:30 +0000

    wip

--- packages/world/test/upgrade.t.sol
+++ packages/world/test/upgrade.t.sol
@@ -3,10 +3,15 @@ pragma solidity >=0.8.24;
 
 import { Test } from "forge-std/Test.sol";
 import { GasReporter } from "@latticexyz/gas-report/src/GasReporter.sol";
+import { World } from "../src/World.sol";
+
+import { Upgrades } from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract UpgradeTest is Test, GasReporter {
   function testUpgrade() public {
     uint256 one = 1;
     assertEq(one, 1);
+
+    address proxy = Upgrades.deployUUPSProxy("World.sol", abi.encodeCall(World.initialize, ()));
   }
 }
