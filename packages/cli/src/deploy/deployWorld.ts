92a61e6daa875c6e43b4ed25773b998f16311585
Fraser Scott <fraserdscott@gmail.com>
2024-04-10 12:53:37 +0100

    feat: cli

--- packages/cli/src/deploy/deployWorld.ts
+++ packages/cli/src/deploy/deployWorld.ts
@@ -1,18 +1,23 @@
 import { Account, Chain, Client, Hex, Log, Transport } from "viem";
 import { waitForTransactionReceipt } from "viem/actions";
 import { ensureWorldFactory } from "./ensureWorldFactory";
+import { ensureWorldProxyFactory } from "./ensureWorldProxyFactory";
 import WorldFactoryAbi from "@latticexyz/world/out/WorldFactory.sol/WorldFactory.abi.json" assert { type: "json" };
 import { writeContract } from "@latticexyz/common";
 import { debug } from "./debug";
 import { logsToWorldDeploy } from "./logsToWorldDeploy";
 import { WorldDeploy } from "./common";
 
+const PROXY = true;
+
 export async function deployWorld(
   client: Client<Transport, Chain | undefined, Account>,
   deployerAddress: Hex,
   salt: Hex,
 ): Promise<WorldDeploy> {
-  const worldFactory = await ensureWorldFactory(client, deployerAddress);
+  const worldFactory = PROXY
+    ? await ensureWorldProxyFactory(client, deployerAddress)
+    : await ensureWorldFactory(client, deployerAddress);
 
   debug("deploying world");
   const tx = await writeContract(client, {
