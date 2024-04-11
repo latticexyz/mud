c4a000701e45fbc5de3e494c9906e153dc0b153a
Fraser Scott <fraserdscott@gmail.com>
2024-04-11 16:35:18 +0100

    chore: remove comment

--- packages/world/src/WorldProxy.sol
+++ packages/world/src/WorldProxy.sol
@@ -15,7 +15,6 @@ import { StorageSlot } from "./StorageSlot.sol";
  * @dev Storage slot with the address of the current implementation.
  * This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1.
  */
-// solhint-disable-next-line private-vars-leading-underscore
 bytes32 constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
 /**
