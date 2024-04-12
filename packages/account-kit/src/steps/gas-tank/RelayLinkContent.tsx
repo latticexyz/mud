1e7f9d3f30f07a263030cafe8557d889f2164767
karooolis <hello@karolisram.com>
2024-04-12 14:02:18 +0300

    add createRelayClient, trigger switch chain

--- packages/account-kit/src/steps/gas-tank/RelayLinkContent.tsx
+++ packages/account-kit/src/steps/gas-tank/RelayLinkContent.tsx
@@ -7,6 +7,10 @@ import { Button } from "../../ui/Button";
 import { holesky } from "viem/chains";
 import { createPublicClient, formatEther, http, parseEther } from "viem";
 import { CHAIN_FROM, CHAIN_TO, AMOUNT_STEP, PAYMASTER_ADDRESS } from "./consts";
+import { createRelayCLient } from "./utils/createRelayClient";
+import { switchChain } from "viem/actions";
+
+createRelayCLient();
 
 // TODO: move to utils, or check if available already
 const publicClient = createPublicClient({
@@ -62,6 +66,9 @@ export function RelayLinkContent() {
 
   const executeDeposit = useCallback(async () => {
     if (!wallet.data || !amount) return;
+    if (wallet.data.chain.id !== holesky.id) {
+      await switchChain(wallet.data, { id: holesky.id });
+    }
 
     const { request } = await publicClient.simulateContract({
       address: PAYMASTER_ADDRESS,
