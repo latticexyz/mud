d0fd1012df403c75e06b3f1575228c0b0bdaa76f
alvrs <alvarius@lattice.xyz>
2024-04-12 20:40:28 +0100

    fix: storage slot computation

--- packages/account-kit/src/utils/getStaticDataLocation.ts
+++ packages/account-kit/src/utils/getStaticDataLocation.ts
@@ -1,8 +1,8 @@
-import { Hex, concatHex, hexToBigInt, keccak256, toBytes, toHex } from "viem";
+import { Hex, concatHex, hexToBigInt, keccak256, padHex, toBytes, toHex } from "viem";
 
 // TODO: move this to store package or similar (equivalent of StoreCore._getStaticDataLocation)
 
 const SLOT = hexToBigInt(keccak256(toBytes("mud.store")));
 export function getStaticDataLocation(tableId: Hex, keyTuple: Hex[]): Hex {
-  return toHex(SLOT ^ hexToBigInt(keccak256(concatHex([tableId, ...keyTuple]))));
+  return padHex(toHex(SLOT ^ hexToBigInt(keccak256(concatHex([tableId, ...keyTuple])))), { dir: "left", size: 32 });
 }
