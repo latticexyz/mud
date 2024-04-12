cfd7637cf296c6a855b01df434ac9d4bc2b89286
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-12 20:44:10 +0100

    simplify

--- packages/account-kit/src/utils/getStaticDataLocation.ts
+++ packages/account-kit/src/utils/getStaticDataLocation.ts
@@ -1,8 +1,9 @@
-import { Hex, concatHex, hexToBigInt, keccak256, padHex, toBytes, toHex } from "viem";
+import { Hex, concatHex, hexToBigInt, keccak256, toBytes, toHex } from "viem";
 
 // TODO: move this to store package or similar (equivalent of StoreCore._getStaticDataLocation)
 
 const SLOT = hexToBigInt(keccak256(toBytes("mud.store")));
+
 export function getStaticDataLocation(tableId: Hex, keyTuple: Hex[]): Hex {
-  return padHex(toHex(SLOT ^ hexToBigInt(keccak256(concatHex([tableId, ...keyTuple])))), { dir: "left", size: 32 });
+  return toHex(SLOT ^ hexToBigInt(keccak256(concatHex([tableId, ...keyTuple]))), { size: 32 });
 }
