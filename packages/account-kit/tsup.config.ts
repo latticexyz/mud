016903518b392bde87a6297ac95489319bbcde09
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-16 15:04:36 +0100

    don't bundle deps

--- packages/account-kit/tsup.config.ts
+++ packages/account-kit/tsup.config.ts
@@ -1,24 +1,22 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    outDir: "dist/tsup",
-    entry: {
-      index: "src/exports/index.ts",
-      internal: "src/exports/internal.ts",
-      "bin/local-bundler": "cli/local-bundler.ts",
-    },
-    target: "esnext",
-    format: ["esm"],
-    dts: false, // TODO: figure out how to reenable
-    sourcemap: true,
-    clean: true,
-    minify: true,
-    // Because we're injecting CSS via shadow DOM, we'll disable style injection and load CSS as a base64 string.
-    // TODO: figure out how to do this conditionally for only specific imports?
-    injectStyle: false,
-    loader: {
-      ".css": "text",
-    },
+export default defineConfig({
+  outDir: "dist/tsup",
+  entry: {
+    index: "src/exports/index.ts",
+    internal: "src/exports/internal.ts",
+    "bin/local-bundler": "cli/local-bundler.ts",
   },
-]);
+  target: "esnext",
+  format: ["esm"],
+  dts: false, // TODO: figure out how to reenable
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  // Because we're injecting CSS via shadow DOM, we'll disable style injection and load CSS as a base64 string.
+  // TODO: figure out how to do this conditionally for only specific imports?
+  injectStyle: false,
+  loader: {
+    ".css": "text",
+  },
+});
--- packages/account-kit/vite.config.ts
+++ packages/account-kit/vite.config.ts
@@ -1,9 +1,4 @@
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-
-import packageJson from "./package.json";
-
-const deps = Object.keys(packageJson.dependencies);
 
 export default defineConfig({
   build: {
@@ -16,10 +11,5 @@ export default defineConfig({
       // TODO: more formats?
       formats: ["es"],
     },
-    rollupOptions: {
-      // TODO: move these to peer deps?
-      external: [...deps, "vite-plugin-node-polyfills/shims/global"],
-    },
   },
-  plugins: [nodePolyfills({ include: ["events"] })],
 });
