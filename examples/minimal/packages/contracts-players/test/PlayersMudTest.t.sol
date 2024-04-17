88c695ca071cdd63cf5b39a25c2d88e054da5e3d
Fraser Scott <fraserdscott@gmail.com>
2024-04-17 14:13:06 +0100

    test: dynamic field length

--- examples/minimal/packages/contracts-players/test/PlayersMudTest.t.sol
+++ examples/minimal/packages/contracts-players/test/PlayersMudTest.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { ROOT_NAMESPACE_ID } from "@latticexyz/world/src/constants.sol";
 import { NamespaceOwner } from "@latticexyz/world/src/codegen/tables/NamespaceOwner.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { Players } from "../src/codegen/index.sol";
@@ -32,5 +33,9 @@ contract PlayersMudTest is MudTest {
 
     assertEq(Players.getItemResources(alice, 0), 150);
     assertEq(Players.getResources(alice)[0], 150);
+
+    bytes32[] memory _keyTuple = new bytes32[](1);
+    _keyTuple[0] = bytes32(uint256(uint160(alice)));
+    assertEq(StoreSwitch.getDynamicFieldLength(Players._tableId, _keyTuple, 2), 160);
   }
 }
--- examples/minimal/packages/contracts-players/test/PlayersTest.t.sol
+++ examples/minimal/packages/contracts-players/test/PlayersTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.24;
 
 import "forge-std/Test.sol";
 import { StoreMock } from "@latticexyz/store/test/StoreMock.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 
 import { Players } from "../src/codegen/index.sol";
 
@@ -22,5 +23,9 @@ contract PlayersTest is Test, StoreMock {
 
     assertEq(Players.getItemResources(alice, 0), 150);
     assertEq(Players.getResources(alice)[0], 150);
+
+    bytes32[] memory _keyTuple = new bytes32[](1);
+    _keyTuple[0] = bytes32(uint256(uint160(alice)));
+    assertEq(StoreSwitch.getDynamicFieldLength(Players._tableId, _keyTuple, 2), 160);
   }
 }
