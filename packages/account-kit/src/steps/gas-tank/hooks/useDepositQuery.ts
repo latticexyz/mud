aafbd8894d79e86b824a9d945d39eb0d7b79261f
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-17 15:27:24 +0100

    fix query invalidation after rebase

--- packages/account-kit/src/steps/gas-tank/hooks/useDepositQuery.ts
+++ packages/account-kit/src/steps/gas-tank/hooks/useDepositQuery.ts
@@ -1,26 +1,19 @@
 import { useQueryClient } from "@tanstack/react-query";
-import { useAccount, useConfig as useWagmiConfig, useWriteContract } from "wagmi";
-import { useConfig } from "../../../AccountKitProvider";
-import { getGasTankBalanceQueryKey } from "../../../useGasTankBalance";
+import { useConfig as useWagmiConfig, useWriteContract } from "wagmi";
 import { waitForTransactionReceipt } from "wagmi/actions";
 import { useOnboardingSteps } from "../../../useOnboardingSteps";
 
 export const useDepositQuery = () => {
   const queryClient = useQueryClient();
   const wagmiConfig = useWagmiConfig();
-  const { chain, gasTankAddress } = useConfig();
   const { resetStep } = useOnboardingSteps();
-  const userAccount = useAccount();
-  const userAccountAddress = userAccount.address;
 
   return useWriteContract({
     mutation: {
       onSuccess: async (hash) => {
         const receipt = await waitForTransactionReceipt(wagmiConfig, { hash });
         if (receipt.status === "success") {
-          queryClient.invalidateQueries({
-            queryKey: getGasTankBalanceQueryKey({ chainId: chain.id, gasTankAddress, userAccountAddress }),
-          });
+          queryClient.invalidateQueries();
           resetStep();
         }
       },
