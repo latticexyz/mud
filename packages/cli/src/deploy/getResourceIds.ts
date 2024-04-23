fb97fc933370cd73713ff02689a6d0deb9f26124
Fraser Scott <fraserdscott@gmail.com>
2024-04-23 13:46:20 +0100

    fix(cli): add retry to getLogs when getting resource ID's

--- packages/cli/src/deploy/getResourceIds.ts
+++ packages/cli/src/deploy/getResourceIds.ts
@@ -3,6 +3,8 @@ import { getLogs } from "viem/actions";
 import { storeSpliceStaticDataEvent } from "@latticexyz/store";
 import { WorldDeploy, storeTables } from "./common";
 import { debug } from "./debug";
+import pRetry from "p-retry";
+import { wait } from "@latticexyz/common/utils";
 
 export async function getResourceIds({
   client,
@@ -15,15 +17,25 @@ export async function getResourceIds({
   // TODO: PR to viem's getLogs to accept topics array so we can filter on all store events and quickly recreate this table's current state
 
   debug("looking up resource IDs for", worldDeploy.address);
-  const logs = await getLogs(client, {
-    strict: true,
-    address: worldDeploy.address,
-    fromBlock: worldDeploy.deployBlock,
-    toBlock: worldDeploy.stateBlock,
-    event: parseAbiItem(storeSpliceStaticDataEvent),
-    args: { tableId: storeTables.store_ResourceIds.tableId },
-  });
-
+  const logs = await pRetry(
+    () =>
+      getLogs(client, {
+        strict: true,
+        address: worldDeploy.address,
+        fromBlock: worldDeploy.deployBlock,
+        toBlock: worldDeploy.stateBlock,
+        event: parseAbiItem(storeSpliceStaticDataEvent),
+        args: { tableId: storeTables.store_ResourceIds.tableId },
+      }),
+    {
+      retries: 3,
+      onFailedAttempt: async (error) => {
+        const delay = error.attemptNumber * 500;
+        debug(`failed to get logs, retrying in ${delay}ms...`);
+        await wait(delay);
+      },
+    },
+  );
   const resourceIds = logs.map((log) => log.args.keyTuple[0]);
   debug("found", resourceIds.length, "resource IDs for", worldDeploy.address);
 
