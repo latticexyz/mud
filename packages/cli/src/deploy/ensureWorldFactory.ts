0e5fdb77886a2d527a8b7e05d2b92fa86f9df8a1
yonada <fraserdscott@gmail.com>
2024-04-24 18:53:59 +0100

    refactor: early return
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/cli/src/deploy/ensureWorldFactory.ts
+++ packages/cli/src/deploy/ensureWorldFactory.ts
@@ -8,27 +8,23 @@ export async function ensureWorldFactory(
   deployerAddress: Hex,
   withWorldProxy?: boolean,
 ): Promise<Address> {
-  if (!withWorldProxy) {
-    const contracts = getWorldFactoryContracts(deployerAddress);
-
-    // WorldFactory constructor doesn't call InitModule, only sets its address, so we can do these in parallel since the address is deterministic
-    await ensureContractsDeployed({
-      client,
-      deployerAddress,
-      contracts: Object.values(contracts),
-    });
-
-    return contracts.WorldFactory.address;
-  } else {
+  if (withWorldProxy) {
     const contracts = getWorldProxyFactoryContracts(deployerAddress);
-
     // WorldFactory constructor doesn't call InitModule, only sets its address, so we can do these in parallel since the address is deterministic
     await ensureContractsDeployed({
       client,
       deployerAddress,
       contracts: Object.values(contracts),
     });
-
     return contracts.WorldProxyFactory.address;
   }
+  
+  const contracts = getWorldFactoryContracts(deployerAddress);
+  // WorldFactory constructor doesn't call InitModule, only sets its address, so we can do these in parallel since the address is deterministic
+  await ensureContractsDeployed({
+    client,
+    deployerAddress,
+    contracts: Object.values(contracts),
+  });
+  return contracts.WorldFactory.address;
 }
