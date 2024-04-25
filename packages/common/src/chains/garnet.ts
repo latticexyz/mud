8970b3e87b3c052af05828a414de560030a2a25e
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-25 15:28:29 +0100

    add other contracts

--- packages/common/src/chains/garnet.ts
+++ packages/common/src/chains/garnet.ts
@@ -27,9 +27,25 @@ export const garnet = {
     },
   },
   contracts: {
+    multicall3: {
+      address: "0xca11bde05977b3631167028862be2a173976ca11",
+    },
     portal: {
       [sourceId]: {
         address: "0x57ee40586fbE286AfC75E67cb69511A6D9aF5909",
+        blockCreated: 1274684,
+      },
+    },
+    l2OutputOracle: {
+      [sourceId]: {
+        address: "0xCb8E7AC561b8EF04F2a15865e9fbc0766FEF569B",
+        blockCreated: 1274684,
+      },
+    },
+    l1StandardBridge: {
+      [sourceId]: {
+        address: "0x09bcDd311FE398F80a78BE37E489f5D440DB95DE",
+        blockCreated: 1274684,
       },
     },
   },
