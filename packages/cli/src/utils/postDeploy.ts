cff1ad1aad5a668c173be137af08a1ddf7c6bcdc
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-25 10:09:23 +0100

    clean up

--- packages/cli/src/utils/postDeploy.ts
+++ packages/cli/src/utils/postDeploy.ts
@@ -3,7 +3,7 @@ import path from "path";
 import chalk from "chalk";
 import { getScriptDirectory, forge } from "@latticexyz/common/foundry";
 import { execSync } from "child_process";
-import { formatEther } from "viem";
+import { formatGwei } from "viem";
 
 export async function postDeploy(
   postDeployScript: string,
@@ -14,17 +14,14 @@ export async function postDeploy(
   // Execute postDeploy forge script
   const postDeployPath = path.join(await getScriptDirectory(), postDeployScript + ".s.sol");
   if (existsSync(postDeployPath)) {
-    const gasPriceWei = parseInt(execSync(`cast gas-price --rpc-url ${rpc}`).toString().trim());
-    if (isNaN(gasPriceWei)) {
-      throw new Error("Unabled to fetch gas price from RPC.");
-    }
-    console.log(chalk.green(`Gas price in gwei: ${formatEther(BigInt(gasPriceWei), "gwei")}`));
+    const gasPrice = BigInt(execSync(`cast gas-price --rpc-url ${rpc}`, { encoding: "utf-8" }).trim());
     console.log(chalk.blue(`Executing post deploy script at ${postDeployPath}`));
+    console.log(chalk.blue(`  using gas price of ${formatGwei(gasPrice)} gwei`));
     await forge(
       [
         "script",
         "--with-gas-price",
-        gasPriceWei.toString(),
+        gasPrice.toString(),
         postDeployScript,
         "--sig",
         "run(address)",
