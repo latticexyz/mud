189050bd2c61ba645325d45a6a4040153d361412
Andy Cernera <kooshaba@gmail.com>
2024-04-25 18:49:34 +0900

    feat(cli): manually fetch gas price from rpc before PostDeploy runs (#2638)
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/cli/src/utils/postDeploy.ts
+++ packages/cli/src/utils/postDeploy.ts
@@ -2,6 +2,8 @@ import { existsSync } from "fs";
 import path from "path";
 import chalk from "chalk";
 import { getScriptDirectory, forge } from "@latticexyz/common/foundry";
+import { execSync } from "child_process";
+import { formatGwei } from "viem";
 
 export async function postDeploy(
   postDeployScript: string,
@@ -9,17 +11,35 @@ export async function postDeploy(
   rpc: string,
   profile: string | undefined,
 ): Promise<void> {
-  // Execute postDeploy forge script
   const postDeployPath = path.join(await getScriptDirectory(), postDeployScript + ".s.sol");
-  if (existsSync(postDeployPath)) {
-    console.log(chalk.blue(`Executing post deploy script at ${postDeployPath}`));
-    await forge(
-      ["script", postDeployScript, "--sig", "run(address)", worldAddress, "--broadcast", "--rpc-url", rpc, "-vvv"],
-      {
-        profile: profile,
-      },
-    );
-  } else {
+  if (!existsSync(postDeployPath)) {
     console.log(`No script at ${postDeployPath}, skipping post deploy hook`);
+    return;
   }
+
+  // TODO: replace this with a viem call
+  const gasPrice = BigInt(execSync(`cast gas-price --rpc-url ${rpc}`, { encoding: "utf-8" }).trim());
+
+  console.log(chalk.blue(`Executing post deploy script at ${postDeployPath}`));
+  console.log(chalk.blue(`  using gas price of ${formatGwei(gasPrice)} gwei`));
+
+  await forge(
+    [
+      "script",
+      postDeployScript,
+      "--broadcast",
+      "--sig",
+      "run(address)",
+      worldAddress,
+      // TODO: also set priority fee?
+      "--with-gas-price",
+      gasPrice.toString(),
+      "--rpc-url",
+      rpc,
+      "-vvv",
+    ],
+    {
+      profile: profile,
+    },
+  );
 }
