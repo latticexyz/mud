18e24dcff0eb2398bd50d2d65f14cd6261492ae5
Fraser Scott <fraserdscott@gmail.com>
2024-04-25 11:25:42 +0100

    chore: remove unused arg

--- packages/cli/src/verify.ts
+++ packages/cli/src/verify.ts
@@ -22,7 +22,6 @@ type VerifyOptions = {
    * If it is not deployed or the target chain does not support legacy transactions, the user must set the deployer manually.
    */
   deployerAddress?: Hex;
-  useProxy?: boolean;
 };
 
 const ERC1967_IMPLEMENTATION_SLOT = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc";
