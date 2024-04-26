5b24a535fbaa508458c2315f53c3d1d79c5ecb26
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-26 19:22:41 +0100

    add back in chain types

--- packages/common/src/chains/types.ts
+++ packages/common/src/chains/types.ts
@@ -1,5 +1,22 @@
+import { ChainContract } from "viem";
 import type { Chain } from "viem/chains";
+
+// TODO: import from viem once available
+export type RpcUrls = {
+  http: readonly [string, ...string[]];
+  webSocket?: readonly [string, ...string[]] | undefined;
+};
 
 export type MUDChain = Chain & {
   faucetUrl?: string;
+  iconUrls?: readonly string[];
+  rpcUrls?: Chain["rpcUrls"] & {
+    erc4337Bundler?: RpcUrls | undefined;
+  };
+  contracts?: Chain["contracts"] & {
+    gasTank?: ChainContract | undefined;
+    portal?: {
+      [sourceId: number]: ChainContract | undefined;
+    };
+  };
 };
