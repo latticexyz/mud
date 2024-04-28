074ed66eb64df377e37684c47d7ff15ced16885b
Kevin Ingersoll <kingersoll@gmail.com>
2024-04-28 23:41:07 +0100

    fix(cli): remove postdeploy gas setting in favor of script options (#2756)

--- packages/cli/src/utils/postDeploy.ts
+++ packages/cli/src/utils/postDeploy.ts
@@ -2,8 +2,6 @@ import { existsSync } from "fs";
 import path from "path";
 import chalk from "chalk";
 import { getScriptDirectory, forge } from "@latticexyz/common/foundry";
-import { execSync } from "child_process";
-import { formatGwei } from "viem";
 
 export async function postDeploy(
   postDeployScript: string,
@@ -20,11 +18,7 @@ export async function postDeploy(
     return;
   }
 
-  // TODO: replace this with a viem call
-  const gasPrice = BigInt(execSync(`cast gas-price --rpc-url ${rpc}`, { encoding: "utf-8" }).trim());
-
   console.log(chalk.blue(`Executing post deploy script at ${postDeployPath}`));
-  console.log(chalk.blue(`  using gas price of ${formatGwei(gasPrice)} gwei`));
 
   await forge(
     [
@@ -34,9 +28,6 @@ export async function postDeploy(
       "--sig",
       "run(address)",
       worldAddress,
-      // TODO: also set priority fee?
-      "--with-gas-price",
-      gasPrice.toString(),
       "--rpc-url",
       rpc,
       "-vvv",
