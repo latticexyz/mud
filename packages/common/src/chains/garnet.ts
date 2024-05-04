6bf442e8f674e6df2fc55e38586eff00a9085abd
Kevin Ingersoll <kingersoll@gmail.com>
2024-05-04 13:46:21 +0100

    extend OP chain contracts

--- packages/common/src/chains/garnet.ts
+++ packages/common/src/chains/garnet.ts
@@ -23,6 +23,7 @@ export const garnet = {
     },
   },
   contracts: {
+    ...chainConfig.contracts,
     multicall3: {
       address: "0xca11bde05977b3631167028862be2a173976ca11",
     },
--- packages/common/src/chains/redstone.ts
+++ packages/common/src/chains/redstone.ts
@@ -22,6 +22,7 @@ export const redstone = {
     },
   },
   contracts: {
+    ...chainConfig.contracts,
     multicall3: {
       address: "0xca11bde05977b3631167028862be2a173976ca11",
     },
