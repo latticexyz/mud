3c59a7385c2a38702ec7518e9d3d67bc5975c6e9
Fraser Scott <fraserdscott@gmail.com>
2024-05-22 11:22:34 +0100

    refactor: add helper

--- packages/cli/src/utils/systemToAbi.ts
+++ packages/cli/src/utils/systemToAbi.ts
@@ -1,10 +1,14 @@
-import { Abi, parseAbiItem } from "viem";
+import { Abi, AbiItem, parseAbiItem } from "viem";
 import { DeployedSystem } from "../deploy/common";
+
+export function functionSignatureToAbiItem(functionSignature: string): AbiItem {
+  const formattedSignature = `function ${functionSignature}`;
+  return parseAbiItem(formattedSignature);
+}
 
 export function systemToAbi(system: DeployedSystem): Abi {
   const abi = system.functions.map((func) => {
-    const formattedSignature = `function ${func.systemFunctionSignature}`;
-    return parseAbiItem(formattedSignature);
+    return functionSignatureToAbiItem(func.systemFunctionSignature);
   });
 
   return abi;
