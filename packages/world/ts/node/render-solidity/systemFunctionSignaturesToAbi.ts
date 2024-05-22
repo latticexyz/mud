6844e86653daf109e98757900c32c1b65ff5a6ab
Fraser Scott <fraserdscott@gmail.com>
2024-05-22 09:42:04 +0100

    refactor: use parseAbiItem

--- packages/world/ts/node/render-solidity/systemFunctionSignaturesToAbi.ts
+++ packages/world/ts/node/render-solidity/systemFunctionSignaturesToAbi.ts
@@ -1,52 +1,9 @@
-import { Abi } from "viem";
+import { Abi, parseAbiItem } from "viem";
 
 export function systemFunctionSignaturesToAbi(systemFunctionSignatures: readonly string[]): Abi {
   const abi = systemFunctionSignatures.map((systemFunctionSignature) => {
-    const match = systemFunctionSignature.match(/^([a-zA-Z_]\w*)\((.*)\)$/);
-    if (!match) {
-      throw new Error("Invalid signature");
-    }
-
-    const name = match[1];
-    const argsString = match[2];
-
-    const args = argsString.split(",");
-
-    const item = {
-      type: "function" as const,
-      name,
-      inputs: args.map((type, i) => {
-        const isTuple = type[0] === "(";
-        if (!isTuple) {
-          return {
-            name: "",
-            type,
-            internalType: type,
-          };
-        } else {
-          return {
-            name: "",
-            type: "tuple[]",
-            internalType: `${name}${i}Struct[]`,
-            components: type
-              .replace("(", "")
-              .replace(")", "")
-              .split(",")
-              .map((member, j) => ({
-                name: `value${j}`,
-                type: member,
-                internalType: member,
-              })),
-          };
-        }
-      }),
-      outputs: [],
-      stateMutability: "nonpayable" as const,
-    };
-
-    console.log(argsString, item);
-
-    return item;
+    const formattedSignature = "function " + systemFunctionSignature;
+    return parseAbiItem(formattedSignature);
   });
 
   return abi;
