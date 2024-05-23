b3a3dbeffc757e2e4494f006d78381ad67fd8999
Ori Pomerantz <qbzzt1@gmail.com>
2024-05-23 13:17:49 -0500

    docs(templates): remove out of date links to docs 🚗  (#2863)
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- templates/phaser/packages/client/src/mud/getNetworkConfig.ts
+++ templates/phaser/packages/client/src/mud/getNetworkConfig.ts
@@ -6,9 +6,6 @@
 /*
  * By default the template just creates a temporary wallet
  * (called a burner wallet).
- *
- * See https://mud.dev/tutorials/minimal/deploy#wallet-managed-address
- * for how to use the user's own address instead.
  */
 import { getBurnerPrivateKey } from "@latticexyz/common";
 
@@ -21,16 +18,6 @@ import worlds from "contracts/worlds.json";
 
 /*
  * The supported chains.
- * By default, there are only two chains here:
- *
- * - mudFoundry, the chain running on anvil that pnpm dev
- *   starts by default. It is similar to the viem anvil chain
- *   (see https://viem.sh/docs/clients/test.html), but with the
- *   basefee set to zero to avoid transaction fees.
- * - latticeTestnet, our public test network.
- *
- * See https://mud.dev/tutorials/minimal/deploy#run-the-user-interface
- * for instructions on how to add networks.
  */
 import { supportedChains } from "./supportedChains";
 
