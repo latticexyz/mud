6efa430c43b3fb9f5a4b5b0cd542da91576a5e01
karooolis <hello@karolisram.com>
2024-05-28 23:02:12 +0400

    react-ecs template setup

--- packages/worlds-explorer-next/src/app/page.tsx
+++ packages/worlds-explorer-next/src/app/page.tsx
@@ -1,79 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// "use client";
 
-export default function Home() {
+import Database from "better-sqlite3";
+
+export default async function Home() {
+  const db = new Database("/Users/karolis/Code/Lattice.xyz/mud/templates/react-ecs/packages/explorer/indexer.db", {
+    verbose: console.log,
+  });
+
+  const rows = db.prepare("SELECT * FROM tablename;").all();
+  console.log(rows);
+
+  const dirname = __dirname;
+  const filename = __filename;
+  const processCwd = process.cwd().split("node_modules")[0];
+  const processExec = process.execPath;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-          </a>
-        </div>
-      </div>
+    <main>
+      <h1>{dirname}</h1>
+      <h2>{filename}</h2>
+      <h3>{processCwd}</h3>
+      <h4>{processExec}</h4>
+      <h4>Hello, world</h4>
 
-      <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
-      </div>
+      <h5>Rows: {rows.map((row) => row.value).join(", ")}</h5>
     </main>
   );
 }
