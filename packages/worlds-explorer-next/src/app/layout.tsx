ae291c6031ac5d1e7d4a1d23e4a75642d28ac6bc
karooolis <hello@karolisram.com>
2024-05-29 13:09:04 +0400

    initial tables viewer setup

--- packages/worlds-explorer-next/src/app/layout.tsx
+++ packages/worlds-explorer-next/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
--- packages/worlds-explorer-next/src/app/page.tsx
+++ packages/worlds-explorer-next/src/app/page.tsx
@@ -1,29 +1,92 @@
-// "use client";
-
 import Database from "better-sqlite3";
+import { Select, Flex, Button, Table, TextArea } from "@radix-ui/themes";
+
+const db = new Database("/Users/karolis/Code/Lattice.xyz/mud/templates/react-ecs/packages/explorer/indexer.db");
+
+export function TableSelector() {
+  return (
+    <Select.Root defaultValue="apple">
+      <Select.Trigger>Select table</Select.Trigger>
+
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Fruits</Select.Label>
+          <Select.Item value="orange">Orange</Select.Item>
+          <Select.Item value="apple">Apple</Select.Item>
+          <Select.Item value="grape" disabled>
+            Grape
+          </Select.Item>
+        </Select.Group>
+        <Select.Separator />
+        <Select.Group>
+          <Select.Label>Vegetables</Select.Label>
+          <Select.Item value="carrot">Carrot</Select.Item>
+          <Select.Item value="potato">Potato</Select.Item>
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+}
+
+export function SQLEditor() {
+  return (
+    <>
+      <TextArea placeholder="SQL query…" />
+      <Button>Let&apos;s go</Button>
+    </>
+  );
+}
+
+export function TablesViewer() {
+  return (
+    <Table.Root>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+          <Table.Cell>danilo@example.com</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+
+        <Table.Row>
+          <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+          <Table.Cell>zahra@example.com</Table.Cell>
+          <Table.Cell>Admin</Table.Cell>
+        </Table.Row>
+
+        <Table.Row>
+          <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
+          <Table.Cell>jasper@example.com</Table.Cell>
+          <Table.Cell>Developer</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table.Root>
+  );
+}
 
 export default async function Home() {
-  const db = new Database("/Users/karolis/Code/Lattice.xyz/mud/templates/react-ecs/packages/explorer/indexer.db", {
-    verbose: console.log,
-  });
+  const tables = db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all();
+  console.log(tables);
 
-  const rows = db.prepare("SELECT * FROM tablename;").all();
-  console.log(rows);
-
-  const dirname = __dirname;
-  const filename = __filename;
   const processCwd = process.cwd().split("node_modules")[0];
-  const processExec = process.execPath;
+  console.log(processCwd);
+
+  // const rows = db.prepare("SELECT * FROM '0x8d8b6b8414e1e3dcfd4168561b9be6bd3bf6ec4b__app__counter';").all();
+  // console.log(rows);
 
   return (
-    <main>
-      <h1>{dirname}</h1>
-      <h2>{filename}</h2>
-      <h3>{processCwd}</h3>
-      <h4>{processExec}</h4>
-      <h4>Hello, world</h4>
+    <Flex direction="column" gap="2">
+      <SQLEditor />
 
-      <h5>Rows: {rows.map((row) => row.value).join(", ")}</h5>
-    </main>
+      <TableSelector />
+      <TablesViewer />
+    </Flex>
   );
 }
