78efb57c347e61374e62a8fe16a75494933e4872
Fraser Scott <fraserdscott@gmail.com>
2024-05-31 18:34:51 +0100

    refactor: regex

--- packages/world/ts/config/resolveWorldConfig.ts
+++ packages/world/ts/config/resolveWorldConfig.ts
@@ -17,7 +17,9 @@ export function resolveWorldConfig(
 ) {
   // Include contract names ending in "System", but not the base "System" contract, and not Interfaces
   const defaultSystemNames =
-    existingContracts?.filter((name) => name.match(/^(?!I[A-Z]).*System$/) && !name.match(/^.+__I.+System$/)) ?? [];
+    existingContracts?.filter(
+      (name) => name.endsWith("System") && name !== "System" && !name.match(/^I[A-Z]/) && !name.match(/__I/),
+    ) ?? [];
   const overriddenSystemNames = Object.keys(config.systems);
 
   // Validate every key in systems refers to an existing system contract (and is not called "World")
