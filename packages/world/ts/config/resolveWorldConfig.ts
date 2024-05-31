fffe976b235b2d940642c13f1a5ebc0e66f2e435
Fraser Scott <fraserdscott@gmail.com>
2024-05-31 17:27:45 +0100

    fix: exclude namespace interfaces

--- packages/world/ts/config/resolveWorldConfig.ts
+++ packages/world/ts/config/resolveWorldConfig.ts
@@ -16,7 +16,8 @@ export function resolveWorldConfig(
   existingContracts?: string[],
 ) {
   // Include contract names ending in "System", but not the base "System" contract, and not Interfaces
-  const defaultSystemNames = existingContracts?.filter((name) => name.match(/^(?!I[A-Z]).*System$/)) ?? [];
+  const defaultSystemNames =
+    existingContracts?.filter((name) => name.match(/^(?!I[A-Z]).*System$/) && !name.match(/^I.+__.+System$/)) ?? [];
   const overriddenSystemNames = Object.keys(config.systems);
 
   // Validate every key in systems refers to an existing system contract (and is not called "World")
