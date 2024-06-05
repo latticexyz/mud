50a715e2199fb74051336130eef6b22437b13ffc
Fraser Scott <fraserdscott@gmail.com>
2024-06-05 18:42:28 +0100

    refactor(world): add validateNamespace helper

--- packages/world/ts/config/v2/namespaces.ts
+++ packages/world/ts/config/v2/namespaces.ts
@@ -9,7 +9,8 @@ import {
   extendedScope,
   getPath,
 } from "@latticexyz/store/config/v2";
-import { NamespacesInput } from "./input";
+import { NamespaceInput, NamespacesInput } from "./input";
+import { ErrorMessage, conform } from "@arktype/util";
 
 export type namespacedTableKeys<world> = world extends { namespaces: infer namespaces }
   ? {
@@ -19,12 +20,16 @@ export type namespacedTableKeys<world> = world extends { namespaces: infer names
     }[keyof namespaces]
   : never;
 
+export type validateNamespace<namespace, scope extends Scope = AbiTypeScope> = {
+  readonly [key in keyof namespace]: key extends "tables"
+    ? validateTables<namespace[key], scope>
+    : key extends keyof NamespaceInput
+      ? conform<namespace[key], NamespaceInput[key]>
+      : ErrorMessage<`\`${key & string}\` is not a valid Namespace config option.`>;
+};
+
 export type validateNamespaces<namespaces, scope extends Scope = AbiTypeScope> = {
-  [namespace in keyof namespaces]: {
-    [key in keyof namespaces[namespace]]: key extends "tables"
-      ? validateTables<namespaces[namespace][key], scope>
-      : namespaces[namespace][key];
-  };
+  [namespace in keyof namespaces]: validateNamespace<namespace, scope>;
 };
 
 export function validateNamespaces<scope extends Scope = AbiTypeScope>(
