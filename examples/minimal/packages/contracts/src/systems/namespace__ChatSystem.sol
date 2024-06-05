8e650781710b16adaac9e40a2f42e345dfdcc325
Fraser Scott <fraserdscott@gmail.com>
2024-06-05 11:12:51 +0100

    chore: update comment

--- examples/minimal/packages/contracts/src/systems/namespace__ChatSystem.sol
+++ examples/minimal/packages/contracts/src/systems/namespace__ChatSystem.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.24;
 import { System } from "@latticexyz/world/src/System.sol";
 import { MessageTable } from "../codegen/index.sol";
 
-// This system is supposed to have a different namespace, but otherwise be identical to ChatSystem
+// This system has a different namespace, but is otherwise identical to ChatSystem
 contract namespace__ChatSystem is System {
   function sendMessage(string memory message) public {
     MessageTable.set(message);
