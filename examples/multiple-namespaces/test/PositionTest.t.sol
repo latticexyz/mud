5bd192b5634cec8a2b97d68c333835db29ac6377
Fraser Scott <fraserdscott@gmail.com>
2024-06-06 13:49:07 +0100

    refactor: address constant

--- examples/multiple-namespaces/test/PositionTest.t.sol
+++ examples/multiple-namespaces/test/PositionTest.t.sol
@@ -7,14 +7,14 @@ import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { Position, PositionData } from "../src/codegen/game/tables/Position.sol";
 
-address constant player = 0x44bDf8F1ada1D72c2CA157563b8deee97c0C8847;
+address constant alice = address(1);
 
 contract PositionTest is MudTest {
   function testPosition(int32 x, int32 y) public {
-    vm.prank(player);
+    vm.prank(alice);
     IWorld(worldAddress).game__move(x, y);
 
-    PositionData memory position = Position.get(player);
+    PositionData memory position = Position.get(alice);
     assertEq(position.x, x);
     assertEq(position.y, y);
   }
--- examples/multiple-namespaces/test/ScoreTest.t.sol
+++ examples/multiple-namespaces/test/ScoreTest.t.sol
@@ -7,28 +7,28 @@ import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
 import { IWorld } from "../src/codegen/world/IWorld.sol";
 import { Score } from "../src/codegen/somePlugin/tables/Score.sol";
 
-address constant player = 0x44bDf8F1ada1D72c2CA157563b8deee97c0C8847;
+address constant alice = address(1);
 
 contract ScoreTest is MudTest {
   function testScore() public {
-    uint256 score = Score.get(player);
+    uint256 score = Score.get(alice);
     assertEq(score, 0);
 
-    vm.prank(player);
+    vm.prank(alice);
     IWorld(worldAddress).game__move(3, 2);
-    vm.prank(player);
+    vm.prank(alice);
     vm.expectRevert("must be at goal");
     IWorld(worldAddress).somePlugin__increaseScore();
 
-    score = Score.get(player);
+    score = Score.get(alice);
     assertEq(score, 0);
 
-    vm.prank(player);
+    vm.prank(alice);
     IWorld(worldAddress).game__move(4, 2);
-    vm.prank(player);
+    vm.prank(alice);
     IWorld(worldAddress).somePlugin__increaseScore();
 
-    score = Score.get(player);
+    score = Score.get(alice);
     assertEq(score, 1);
   }
 }
