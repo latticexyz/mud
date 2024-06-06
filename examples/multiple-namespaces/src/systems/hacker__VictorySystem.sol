869a12076e32265a473f5b0264321aa26bbfd78c
Fraser Scott <fraserdscott@gmail.com>
2024-06-06 13:34:50 +0100

    chore: comment

--- examples/multiple-namespaces/src/systems/hacker__VictorySystem.sol
+++ examples/multiple-namespaces/src/systems/hacker__VictorySystem.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.24;
 import { System } from "@latticexyz/world/src/System.sol";
 import { Victory } from "../codegen/game/tables/Victory.sol";
 
+// This system has a different namespace, but is otherwise identical to game__VictorySystem
 contract hacker__VictorySystem is System {
   function win() public {
     Victory.set(true);
