1ca67c3d645c2c82110b77d5a9d1c43f3009ba44
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-04 10:59:25 +0100

    refactor

--- packages/world/ts/node/generateSystemManifest.ts
+++ packages/world/ts/node/generateSystemManifest.ts
@@ -2,28 +2,26 @@ import path from "node:path";
 import fs from "node:fs/promises";
 import { World } from "../config/v2";
 import { getSystemContracts } from "./getSystemContracts";
+import { getSystemManifest } from "./getSystemManifest";
 
 export type GenerateSystemManifestOptions = {
-  configPath: string;
-  config: World;
+  readonly configPath: string;
+  readonly config: World;
 };
 
 export async function generateSystemManifest({ configPath, config }: GenerateSystemManifestOptions): Promise<string> {
-  const systems = await getSystemContracts({ configPath, config });
+  const systemContracts = await getSystemContracts({ configPath, config });
+  const systemManifest = getSystemManifest({ config, systemContracts });
 
-  // TODO: iterate through config systems and adjust output accordingly
-  // TODO: throw if any systems in config are missing from filesystem
-  // TODO: exclude systems specified by config
-
-  // TODO: should this be a .ts file? or a .json file with corresponding .json.d.ts?
+  // TODO: generate corresponding .json.d.ts?
   const outputPath = path.join(path.dirname(configPath), config.metadataDirectory, "systems.json");
 
-  // TODO: transform system file paths to be relative to manifest output path?
+  // TODO: transform system source paths to be relative to manifest output path?
 
   // TODO: move to debug?
   console.log(`Writing system manifest to ${outputPath}`);
   await fs.mkdir(path.dirname(outputPath), { recursive: true });
-  await fs.writeFile(outputPath, JSON.stringify(systems, null, 2) + "\n");
+  await fs.writeFile(outputPath, JSON.stringify(systemManifest, null, 2) + "\n");
 
   return outputPath;
 }
--- packages/world/ts/node/getSystemContracts.ts
+++ packages/world/ts/node/getSystemContracts.ts
@@ -1,13 +1,21 @@
-import { resourceToHex } from "@latticexyz/common";
 import { World } from "../config/v2/output";
 import { findSolidityFiles } from "./findSolidityFiles";
 
-const namespacePattern = /\/namespaces\/(?<namespace>[^/]+)\//;
+export type SolidityContract = {
+  readonly sourcePath: string;
+  readonly name: string;
+};
 
-export async function getSystemContracts({ configPath, config }: { configPath: string; config: World }) {
+export type GetSystemContractsOptions = {
+  readonly configPath: string;
+  readonly config: World;
+};
+
+export async function getSystemContracts({
+  configPath,
+  config,
+}: GetSystemContractsOptions): Promise<readonly SolidityContract[]> {
   const solidityFiles = await findSolidityFiles({ configPath, config });
-
-  // TODO: validate alignment between config systems (namespaces, system names)
 
   return solidityFiles
     .map((file) => ({
@@ -21,36 +29,5 @@ export async function getSystemContracts({ configPath, config }: { configPath: s
         file.name !== "System" &&
         // exclude interfaces
         !/^I[A-Z]/.test(file.name),
-    )
-    .map((system) => {
-      if (config.internal.multipleNamespaces) {
-        const match = system.sourcePath.match(namespacePattern);
-        const namespace = match?.groups?.namespace;
-        if (namespace == null) {
-          throw new Error(
-            `This MUD app is configured to use multiple namespaces, but system contract at \`${system.sourcePath}\` was not in a namespace directory.`,
-          );
-        }
-        // TODO: look up namespace in config in case this is a label
-        return { source: system.sourcePath, namespace, name: system.name };
-      }
-
-      if (namespacePattern.test(system.sourcePath)) {
-        throw new Error(
-          `This MUD app is configured to use a single namespace, but system contract at \`${system.sourcePath}\` was in a namespace directory.`,
-        );
-      }
-      return { source: system.sourcePath, namespace: config.namespace, name: system.name };
-    })
-    .map((system) => {
-      console.log("system", system);
-      return {
-        ...system,
-        systemId: resourceToHex({
-          type: "system",
-          namespace: system.namespace,
-          name: system.name,
-        }),
-      };
-    });
+    );
 }
