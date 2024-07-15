e554b686c1c98a6c166ccf106a59f824b58cc982
karooolis <hello@karolisram.com>
2024-07-15 14:17:39 +0300

    export world abi fns from cli package

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,6 @@
 // nothing to export
+
+import { functionSignatureToAbiItem } from "./utils/functionSignatureToAbiItem";
+import { getWorldAbi } from "./utils/getWorldAbi";
+
+export { functionSignatureToAbiItem, getWorldAbi };
