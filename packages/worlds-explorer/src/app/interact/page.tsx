2fb01b49b441ccdcbe51c705532bb845c3321691
karooolis <hello@karolisram.com>
2024-07-16 15:50:53 +0300

    dynamic fetch url

--- packages/worlds-explorer/src/app/interact/page.tsx
+++ packages/worlds-explorer/src/app/interact/page.tsx
@@ -1,8 +1,14 @@
 import { Flex, Container } from "@radix-ui/themes";
 import { Form } from "./Form";
 
+import { headers } from "next/headers";
+
 async function getABI() {
-  const res = await fetch("http://localhost:1333/api/world");
+  const headersList = headers();
+  const protocol = headersList.get("x-forwarded-proto");
+  const host = headersList.get("host");
+
+  const res = await fetch(`${protocol}://${host}/api/world`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
