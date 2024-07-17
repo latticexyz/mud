70b689fc92e31692c8d7dfa583edc80113bcc3ca
karooolis <hello@karolisram.com>
2024-07-17 13:18:36 +0300

    wagmi setup

--- packages/worlds-explorer/src/app/_providers.tsx
+++ packages/worlds-explorer/src/app/_providers.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import { ReactNode } from "react";
+import { WagmiProvider } from "wagmi";
+import { http, createConfig } from "@wagmi/core";
+import { localhost } from "@wagmi/core/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
+export const wagmiConfig = createConfig({
+  chains: [localhost],
+  transports: {
+    [localhost.id]: http(),
+  },
+});
+
 export function Providers({ children }: { children: ReactNode }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+    </QueryClientProvider>
+  );
 }
