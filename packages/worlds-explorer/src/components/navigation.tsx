00bd542205cd77b318873c91cd6bc9b2533ee0bd
karooolis <hello@karolisram.com>
2024-07-17 17:40:09 +0300

    add latest block display

--- packages/worlds-explorer/src/components/navigation.tsx
+++ packages/worlds-explorer/src/components/navigation.tsx
@@ -3,17 +3,40 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useBlockNumber } from "wagmi";
 
 export function Navigation() {
   const pathname = usePathname();
+  const { data: block } = useBlockNumber();
+
+  console.log(block);
+
   return (
-    <div className="flex gap-x-4 py-4">
-      <Link href="/" className={cn(pathname === "/" && "font-semibold underline")}>
-        Home
-      </Link>
-      <Link href="/interact" className={cn(pathname === "/interact" && "font-semibold underline")}>
-        Interact
-      </Link>
+    <div className="flex justify-between items-center">
+      <div className="flex gap-x-4 py-4">
+        <Link href="/" className={cn(pathname === "/" && "font-semibold underline")}>
+          Home
+        </Link>
+        <Link href="/interact" className={cn(pathname === "/interact" && "font-semibold underline")}>
+          Interact
+        </Link>
+      </div>
+
+      <div>
+        {block != null && block > BigInt(0) && (
+          <div className="inline-block mr-4">
+            <div className="flex items-center justify-between text-xs font-extrabold text-green-600">
+              <span
+                className="mr-2 inline-block h-[8px] w-[8px] rounded-full animate-pulse"
+                style={{
+                  background: "rgb(64, 182, 107)",
+                }}
+              ></span>
+              <span className="opacity-70">{block.toString()}</span>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
