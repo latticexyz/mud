886252ef55fa64e1da43b374704a74dce786e02b
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-19 09:43:03 +0100

    add ts-benchmarks test package

---
+++ test/ts-benchmarks/bench.ts
@@ -0,0 +1,55 @@
+import { bench } from "@ark/attest";
+import { syncToRecs } from "@latticexyz/store-sync/recs";
+import { syncToZustand } from "@latticexyz/store-sync/zustand";
+import { Hex, type PublicClient } from "viem";
+import { defineWorld } from "@latticexyz/world";
+import { createWorld } from "@latticexyz/recs";
+
+bench("syncToRecs", async () => {
+  const config = defineWorld({
+    namespace: "demo",
+    tables: {
+      Position: {
+        schema: {
+          entity: "address",
+          x: "int32",
+          y: "int32",
+        },
+        key: ["entity"],
+      },
+    },
+  });
+
+  const t = await syncToRecs({
+    world: createWorld(),
+    config,
+    address: {} as Hex,
+    publicClient: {} as PublicClient,
+  });
+
+  return t;
+}).types([43529, "instantiations"]);
+
+bench("syncToZustand", async () => {
+  const config = defineWorld({
+    namespace: "demo",
+    tables: {
+      Position: {
+        schema: {
+          entity: "address",
+          x: "int32",
+          y: "int32",
+        },
+        key: ["entity"],
+      },
+    },
+  });
+
+  const t = await syncToZustand({
+    config,
+    address: {} as Hex,
+    publicClient: {} as PublicClient,
+  });
+
+  return t;
+}).types([43419, "instantiations"]);
