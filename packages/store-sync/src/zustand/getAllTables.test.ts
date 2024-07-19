47a63918c429a8c408a185b2b052f3b1fd4555e0
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-19 18:23:38 +0100

    add a generic test, align attest versions

--- packages/store-sync/src/zustand/getAllTables.test.ts
+++ packages/store-sync/src/zustand/getAllTables.test.ts
@@ -1,10 +1,14 @@
 import { describe, it } from "vitest";
-import { defineWorld } from "@latticexyz/world";
+import { World, defineWorld } from "@latticexyz/world";
 import { attest } from "@arktype/attest";
 import { getAllTables } from "./getAllTables";
 import { Tables } from "@latticexyz/config";
 
 describe("getAllTables", () => {
+  it("type satisfies Tables", () => {
+    attest<Tables, getAllTables<World, {}>>();
+  });
+
   it("returns combined tables", () => {
     const config = defineWorld({
       namespace: "app",
