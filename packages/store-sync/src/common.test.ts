95256588fb82401df8ea645267872ae338967290
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-20 16:56:57 +0100

    better test

--- packages/store-sync/src/common.test.ts
+++ packages/store-sync/src/common.test.ts
@@ -1,15 +1,12 @@
 import { describe, it } from "vitest";
 import { attest } from "@arktype/attest";
+import { isDisjoint } from "@arktype/util";
 import storeConfig from "@latticexyz/store/mud.config";
 import worldConfig from "@latticexyz/world/mud.config";
 import { configToTables } from "./configToTables";
 
 describe("mudTables", () => {
   it("has no overlapping table labels", () => {
-    // TODO: is there a better way to write this test?
-    attest<
-      true,
-      keyof configToTables<typeof storeConfig> & keyof configToTables<typeof worldConfig> extends never ? true : false
-    >();
+    attest<true, isDisjoint<keyof configToTables<typeof storeConfig>, keyof configToTables<typeof worldConfig>>>();
   });
 });
