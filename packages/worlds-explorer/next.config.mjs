2485c042a681c54ffd837c447a75d85da8378161
karooolis <hello@karolisram.com>
2024-07-22 15:31:28 +0300

    pino-pretty webpack config

--- packages/worlds-explorer/next.config.mjs
+++ packages/worlds-explorer/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
