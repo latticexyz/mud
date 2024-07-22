78e2746e34485df04bd0784ebd6d2a406585a77b
alvrs <alvarius@lattice.xyz>
2024-07-22 16:51:33 -0700

    add storage adapter

---
+++ packages/store-sync/src/zustand-query/createStorageAdapter.ts
@@ -0,0 +1,141 @@
+import { hexToResource, resourceToLabel, spliceHex } from "@latticexyz/common";
+import {
+  KeySchema,
+  decodeKey,
+  decodeValueArgs,
+  encodeValueArgs,
+  getKeySchema,
+  getSchemaTypes,
+  getValueSchema,
+} from "@latticexyz/protocol-parser/internal";
+import { size } from "viem";
+import { isTableRegistrationLog } from "../isTableRegistrationLog";
+import { logToTable } from "../logToTable";
+import { StorageAdapter, StorageAdapterBlock } from "../common";
+import { Store } from "@latticexyz/zustand-query/internal";
+import { debug } from "./debug";
+
+export type CreateStorageAdapterOptions = {
+  store: Store;
+};
+
+export type CreateStorageAdapterResult = {
+  storageAdapter: StorageAdapter;
+};
+
+export function createStorageAdapter({ store }: CreateStorageAdapterOptions): CreateStorageAdapterResult {
+  async function storageAdapter({ logs }: StorageAdapterBlock): Promise<void> {
+    const newTables = logs.filter(isTableRegistrationLog).map(logToTable);
+
+    for (const newTable of newTables) {
+      // TODO: switch this to `newTable.label` once available
+      const existingTable = store.getState().config[newTable.namespace][newTable.name];
+      if (existingTable) {
+        console.warn("table already registered, ignoring", {
+          newTable,
+          existingTable,
+        });
+      } else {
+        // TODO: create util for converting table config
+        store.getState().actions.registerTable({
+          ...newTable,
+          key: newTable.key as string[],
+          label: newTable.name,
+          schema: Object.fromEntries(Object.entries(newTable.schema).map(([field, { type }]) => [field, type])),
+        });
+      }
+    }
+
+    // Convert logs in (partial) table updates
+    for (const log of logs) {
+      const { namespace, name } = hexToResource(log.args.tableId);
+      // TODO: use label once available
+      const tableConfig = store.getState().config[namespace][name];
+      const boundTable = store.getState().actions.getTable({ namespace, label: name });
+
+      if (!tableConfig || !boundTable) {
+        debug(`skipping update for unknown table: ${resourceToLabel({ namespace, name })} at ${log.address}`);
+        continue;
+      }
+
+      const valueSchema = getSchemaTypes(getValueSchema(tableConfig));
+      const keySchema = getSchemaTypes(getKeySchema(tableConfig)) as KeySchema;
+      const key = decodeKey(keySchema, log.args.keyTuple);
+
+      if (log.eventName === "Store_SetRecord") {
+        // TODO: is there a more elegant way to do this (converting table config to expected schema)?
+        const record = decodeValueArgs(valueSchema, log.args);
+        debug("setting table record", {
+          namespace: tableConfig.namespace,
+          name: tableConfig.name,
+          key,
+          record,
+        });
+
+        boundTable.setRecord({ key, record });
+      } else if (log.eventName === "Store_SpliceStaticData") {
+        // TODO: add tests that this works when no record had been set before
+        const previousRecord = boundTable.getRecord({ key }) ?? {};
+
+        // TODO: maybe better to also store the static data than to re-encode here?
+        const {
+          staticData: previousStaticData,
+          encodedLengths,
+          dynamicData,
+        } = encodeValueArgs(valueSchema, previousRecord);
+        const newStaticData = spliceHex(previousStaticData, log.args.start, size(log.args.data), log.args.data);
+        const newValue = decodeValueArgs(valueSchema, {
+          staticData: newStaticData,
+          encodedLengths,
+          dynamicData,
+        });
+
+        debug("setting record via splice static", {
+          namespace,
+          name,
+          key,
+          previousStaticData,
+          newStaticData,
+          previousRecord,
+          newValue,
+        });
+        boundTable.setRecord({ key, record: newValue });
+      } else if (log.eventName === "Store_SpliceDynamicData") {
+        // TODO: add tests that this works when no record had been set before
+        const previousRecord = boundTable.getRecord({ key }) ?? {};
+
+        // TODO: maybe better to also store the dynamic data than to re-encode here?
+        const { staticData, dynamicData: previousDynamicData } = encodeValueArgs(valueSchema, previousRecord);
+
+        const newDynamicData = spliceHex(previousDynamicData, log.args.start, log.args.deleteCount, log.args.data);
+        const newValue = decodeValueArgs(valueSchema, {
+          staticData,
+          // TODO: handle unchanged encoded lengths (comment taken from recs sync, what does this mean?)
+          encodedLengths: log.args.encodedLengths,
+          dynamicData: newDynamicData,
+        });
+
+        debug("setting record via splice dynamic", {
+          namespace,
+          name,
+          key,
+          previousDynamicData,
+          newDynamicData,
+          previousRecord,
+          newValue,
+        });
+
+        boundTable.setRecord({ key, record: newValue });
+      } else if (log.eventName === "Store_DeleteRecord") {
+        debug("deleting record", {
+          namespace,
+          name,
+          key,
+        });
+        boundTable.deleteRecord({ key });
+      }
+    }
+  }
+
+  return { storageAdapter };
+}
