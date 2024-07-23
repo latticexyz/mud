ce2bca918dddb61e2dcd16df73520e1650a7eeec
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-23 20:12:22 +0100

    bring back shorthand tests

--- packages/store/ts/config/v2/store.test.ts
+++ packages/store/ts/config/v2/store.test.ts
@@ -720,4 +720,292 @@ describe("defineStore", () => {
 
     attest<typeof expectedConfig>(config).equals(expectedConfig);
   });
+
+  describe("shorthands", () => {
+    it("should accept a shorthand store config as input and expand it", () => {
+      const config = defineStore({ tables: { Name: "address" } });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Name: {
+            label: "Name",
+            type: "table",
+            namespace: "" as string,
+            name: "Name" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Name" }),
+            schema: {
+              id: {
+                type: "bytes32",
+                internalType: "bytes32",
+              },
+              value: {
+                type: "address",
+                internalType: "address",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: false as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        sourceDirectory: "src",
+        userTypes: {},
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+    });
+
+    it("should satisfy the output type", () => {
+      const config = defineStore({
+        tables: { Name: { schema: { id: "address" }, key: ["id"] } },
+        userTypes: { CustomType: { type: "address", filePath: "path/to/file" } },
+      });
+
+      attest<typeof config, satisfy<Store, typeof config>>();
+    });
+
+    it("should accept an empty input", () => {
+      const config = defineStore({});
+      attest<typeof config, satisfy<Store, typeof config>>();
+    });
+
+    it("should accept a user type as input and expand it", () => {
+      const config = defineStore({
+        tables: { Name: "CustomType" },
+        userTypes: { CustomType: { type: "address", filePath: "path/to/file" } },
+      });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Name: {
+            label: "Name",
+            type: "table",
+            namespace: "" as string,
+            name: "Name" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Name" }),
+            schema: {
+              id: {
+                type: "bytes32",
+                internalType: "bytes32",
+              },
+              value: {
+                type: "address",
+                internalType: "CustomType",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: false as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        sourceDirectory: "src",
+        userTypes: { CustomType: { type: "address", filePath: "path/to/file" } },
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+      attest<typeof config>(expectedConfig);
+    });
+
+    it("given a schema with a key field with static ABI type, it should use `id` as single key", () => {
+      const config = defineStore({
+        tables: { Example: { id: "address", name: "string", age: "uint256" } },
+      });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Example: {
+            label: "Example",
+            type: "table",
+            namespace: "" as string,
+            name: "Example" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Example" }),
+            schema: {
+              id: {
+                type: "address",
+                internalType: "address",
+              },
+              name: {
+                type: "string",
+                internalType: "string",
+              },
+              age: {
+                type: "uint256",
+                internalType: "uint256",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: true as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        sourceDirectory: "src",
+        userTypes: {},
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+    });
+
+    it("given a schema with a key field with static custom type, it should use `id` as single key", () => {
+      const config = defineStore({
+        tables: { Example: { id: "address", name: "string", age: "uint256" } },
+      });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Example: {
+            label: "Example",
+            type: "table",
+            namespace: "" as string,
+            name: "Example" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Example" }),
+            schema: {
+              id: {
+                type: "address",
+                internalType: "address",
+              },
+              name: {
+                type: "string",
+                internalType: "string",
+              },
+              age: {
+                type: "uint256",
+                internalType: "uint256",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: true as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        sourceDirectory: "src",
+        userTypes: {},
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+    });
+
+    it("should pass through full table config inputs", () => {
+      const config = defineStore({
+        tables: {
+          Example: {
+            schema: { id: "address", name: "string", age: "uint256" },
+            key: ["age", "id"],
+          },
+        },
+      });
+      const expected = defineStore({
+        tables: {
+          Example: {
+            schema: { id: "address", name: "string", age: "uint256" },
+            key: ["age", "id"],
+          },
+        },
+      });
+
+      attest<typeof expected>(config).equals(expected);
+    });
+
+    it("should throw if the shorthand doesn't include a key field", () => {
+      attest(() =>
+        defineStore({
+          tables: {
+            // @ts-expect-error Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.
+            Example: {
+              name: "string",
+              age: "uint256",
+            },
+          },
+        }),
+      ).throwsAndHasTypeError(
+        "Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.",
+      );
+    });
+
+    it("should throw if the shorthand config includes a non-static key field", () => {
+      attest(() =>
+        // @ts-expect-error Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.
+        defineStore({ tables: { Example: { id: "string", name: "string", age: "uint256" } } }),
+      ).throwsAndHasTypeError(
+        "Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.",
+      );
+    });
+
+    it("should throw if the shorthand config includes a non-static user type as key field", () => {
+      attest(() =>
+        defineStore({
+          // @ts-expect-error Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.
+          tables: { Example: { id: "dynamic", name: "string", age: "uint256" } },
+          userTypes: {
+            dynamic: { type: "string", filePath: "path/to/file" },
+            static: { type: "address", filePath: "path/to/file" },
+          },
+        }),
+      ).throwsAndHasTypeError(
+        "Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.",
+      );
+    });
+
+    it("should throw if the shorthand key is neither a custom nor ABI type", () => {
+      // @ts-expect-error Type '"NotAnAbiType"' is not assignable to type 'AbiType'
+      attest(() => defineStore({ tables: { Invalid: "NotAnAbiType" } }))
+        .throws("Invalid ABI type. `NotAnAbiType` not found in scope.")
+        .type.errors(`Type '"NotAnAbiType"' is not assignable to type 'AbiType'`);
+    });
+  });
 });
--- packages/world/ts/config/v2/world.test.ts
+++ packages/world/ts/config/v2/world.test.ts
@@ -883,4 +883,425 @@ describe("defineWorld", () => {
       }),
     ).type.errors("`invalidOption` is not a valid World config option.");
   });
+
+  describe("shorthands", () => {
+    it.skip("should resolve namespaced shorthand table config with user types and enums", () => {
+      const config = defineWorld({
+        // @ts-expect-error TODO
+        namespaces: {
+          ExampleNS: {
+            tables: {
+              ExampleTable: "Static",
+            },
+          },
+        },
+        userTypes: {
+          Static: { type: "address", filePath: "path/to/file" },
+          Dynamic: { type: "string", filePath: "path/to/file" },
+        },
+        enums: {
+          MyEnum: ["First", "Second"],
+        },
+      });
+
+      const expected = {
+        ...CONFIG_DEFAULTS,
+        codegen: CODEGEN_DEFAULTS,
+        tables: {
+          ExampleNS__ExampleTable: {
+            label: "ExampleTable",
+            type: "table",
+            namespace: "ExampleNS",
+            name: "ExampleTable" as string,
+            tableId: resourceToHex({ type: "table", namespace: "ExampleNS", name: "ExampleTable" }),
+            schema: {
+              id: {
+                type: "bytes32",
+                internalType: "bytes32",
+              },
+              value: {
+                type: "address",
+                internalType: "Static",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: false as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+        userTypes: {
+          Static: { type: "address", filePath: "path/to/file" as string },
+          Dynamic: { type: "string", filePath: "path/to/file" as string },
+        },
+        enums: {
+          MyEnum: ["First", "Second"],
+        },
+        enumValues: {
+          MyEnum: {
+            First: 0,
+            Second: 1,
+          },
+        },
+        namespace: "" as string,
+      } as const;
+
+      attest<typeof expected>(config).equals(expected);
+    });
+
+    it.skip("should resolve namespaced shorthand schema table config with user types and enums", () => {
+      const config = defineWorld({
+        // @ts-expect-error TODO
+        namespaces: {
+          ExampleNS: {
+            tables: {
+              ExampleTable: {
+                id: "Static",
+                value: "MyEnum",
+                dynamic: "Dynamic",
+              },
+            },
+          },
+        },
+        userTypes: {
+          Static: { type: "address", filePath: "path/to/file" },
+          Dynamic: { type: "string", filePath: "path/to/file" },
+        },
+        enums: {
+          MyEnum: ["First", "Second"],
+        },
+      });
+
+      const expected = {
+        ...CONFIG_DEFAULTS,
+        codegen: CODEGEN_DEFAULTS,
+        tables: {
+          ExampleNS__ExampleTable: {
+            label: "ExampleTable",
+            type: "table",
+            namespace: "ExampleNS",
+            name: "ExampleTable" as string,
+            tableId: resourceToHex({ type: "table", namespace: "ExampleNS", name: "ExampleTable" }),
+            schema: {
+              id: {
+                type: "address",
+                internalType: "Static",
+              },
+              value: {
+                type: "uint8",
+                internalType: "MyEnum",
+              },
+              dynamic: {
+                type: "string",
+                internalType: "Dynamic",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: true as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+        userTypes: {
+          Static: { type: "address", filePath: "path/to/file" as string },
+          Dynamic: { type: "string", filePath: "path/to/file" as string },
+        },
+        enums: {
+          MyEnum: ["First", "Second"],
+        },
+        enumValues: {
+          MyEnum: {
+            First: 0,
+            Second: 1,
+          },
+        },
+        namespace: "" as string,
+      } as const;
+
+      attest<typeof expected>(config).equals(expected);
+    });
+
+    it("should accept a shorthand store config as input and expand it", () => {
+      const config = defineWorld({ tables: { Name: "address" } });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Name: {
+            label: "Name",
+            type: "table",
+            namespace: "" as string,
+            name: "Name" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Name" }),
+            schema: {
+              id: {
+                type: "bytes32",
+                internalType: "bytes32",
+              },
+              value: {
+                type: "address",
+                internalType: "address",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: false as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...CONFIG_DEFAULTS,
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        userTypes: {},
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+      attest<typeof config>(expectedConfig);
+    });
+
+    it("should accept a user type as input and expand it", () => {
+      const config = defineWorld({
+        tables: { Name: "CustomType" },
+        userTypes: { CustomType: { type: "address", filePath: "path/to/file" } },
+      });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Name: {
+            label: "Name",
+            type: "table",
+            namespace: "" as string,
+            name: "Name" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Name" }),
+            schema: {
+              id: {
+                type: "bytes32",
+                internalType: "bytes32",
+              },
+              value: {
+                type: "address",
+                internalType: "CustomType",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: false as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...CONFIG_DEFAULTS,
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        userTypes: { CustomType: { type: "address", filePath: "path/to/file" as string } },
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+    });
+
+    it("given a schema with a key field with static ABI type, it should use `id` as single key", () => {
+      const config = defineWorld({
+        tables: { Example: { id: "address", name: "string", age: "uint256" } },
+      });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Example: {
+            label: "Example",
+            type: "table",
+            namespace: "" as string,
+            name: "Example" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Example" }),
+            schema: {
+              id: {
+                type: "address",
+                internalType: "address",
+              },
+              name: {
+                type: "string",
+                internalType: "string",
+              },
+              age: {
+                type: "uint256",
+                internalType: "uint256",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: true as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...CONFIG_DEFAULTS,
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        userTypes: {},
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+    });
+
+    it("given a schema with a key field with static custom type, it should use `id` as single key", () => {
+      const config = defineWorld({
+        tables: { Example: { id: "address", name: "string", age: "uint256" } },
+      });
+
+      const expectedBaseNamespace = {
+        namespace: "" as string,
+        tables: {
+          Example: {
+            label: "Example",
+            type: "table",
+            namespace: "" as string,
+            name: "Example" as string,
+            tableId: resourceToHex({ type: "table", namespace: "", name: "Example" }),
+            schema: {
+              id: {
+                type: "address",
+                internalType: "address",
+              },
+              name: {
+                type: "string",
+                internalType: "string",
+              },
+              age: {
+                type: "uint256",
+                internalType: "uint256",
+              },
+            },
+            key: ["id"],
+            codegen: { ...TABLE_CODEGEN_DEFAULTS, dataStruct: true as boolean },
+            deploy: TABLE_DEPLOY_DEFAULTS,
+          },
+        },
+      } as const;
+
+      const expectedConfig = {
+        ...CONFIG_DEFAULTS,
+        ...expectedBaseNamespace,
+        namespaces: {
+          "": {
+            label: "",
+            ...expectedBaseNamespace,
+          },
+        },
+        userTypes: {},
+        enums: {},
+        enumValues: {},
+        codegen: CODEGEN_DEFAULTS,
+      } as const;
+
+      attest<typeof expectedConfig>(config).equals(expectedConfig);
+    });
+
+    it("throw an error if the shorthand doesn't include a key field", () => {
+      attest(() =>
+        defineWorld({
+          tables: {
+            // @ts-expect-error Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.
+            Example: {
+              name: "string",
+              age: "uint256",
+            },
+          },
+        }),
+      ).throwsAndHasTypeError(
+        "Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.",
+      );
+    });
+
+    it("throw an error if the shorthand config includes a non-static key field", () => {
+      attest(() =>
+        // @ts-expect-error Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.
+        defineWorld({ tables: { Example: { id: "string", name: "string", age: "uint256" } } }),
+      ).throwsAndHasTypeError(
+        "Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.",
+      );
+    });
+
+    it("throw an error if the shorthand config includes a non-static user type as key field", () => {
+      attest(() =>
+        defineWorld({
+          // @ts-expect-error Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.
+          tables: { Example: { id: "dynamic", name: "string", age: "uint256" } },
+          userTypes: {
+            dynamic: { type: "string", filePath: "path/to/file" },
+            static: { type: "address", filePath: "path/to/file" },
+          },
+        }),
+      ).throwsAndHasTypeError(
+        "Invalid schema. Expected an `id` field with a static ABI type or an explicit `key` option.",
+      );
+    });
+
+    it("should allow a const config as input", () => {
+      const config = {
+        tables: {
+          Example: {
+            schema: { id: "address", name: "string", age: "uint256" },
+            key: ["age"],
+          },
+        },
+      } as const;
+
+      defineWorld(config);
+    });
+
+    it.skip("should throw with an invalid namespace config option", () => {
+      attest(() =>
+        defineWorld({
+          // @ts-expect-error TODO
+          namespaces: {
+            ExampleNS: {
+              tables: {
+                ExampleTable: "number",
+              },
+            },
+          },
+        }),
+      ).type.errors(`Type '"number"' is not assignable to type 'AbiType'.`);
+    });
+
+    it.skip("should throw with a non-existent namespace config option", () => {
+      attest(() =>
+        defineWorld({
+          // @ts-expect-error TODO
+          namespaces: {
+            ExampleNS: {
+              invalidProperty: true,
+            },
+          },
+        }),
+      ).type.errors("`invalidProperty` is not a valid namespace config option.");
+    });
+  });
 });
