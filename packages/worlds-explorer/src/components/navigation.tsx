b72425c57a2c9b8d671ccd90dd8c01c9d2895740
karooolis <hello@karolisram.com>
2024-07-29 15:14:52 +0300

    show currently active interact fn

--- packages/worlds-explorer/src/components/navigation.tsx
+++ packages/worlds-explorer/src/components/navigation.tsx
@@ -5,25 +5,41 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { LatestBlock } from "@/components/latest-block";
 import { AccountSelect } from "./account-select";
+import { Separator } from "./ui/separator";
 
 export function Navigation() {
   const pathname = usePathname();
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex gap-x-4 py-4">
-        <Link href="/" className={cn(pathname === "/" && "font-semibold underline")}>
-          Data explorer
-        </Link>
-        <Link href="/interact" className={cn(pathname === "/interact" && "font-semibold underline")}>
-          Interact
-        </Link>
+    <div className="mb-8">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-x-6 py-4">
+          <Link
+            href="/"
+            className={cn("underline-offset-[16px] uppercase", {
+              "font-semibold underline decoration-orange-500 decoration-4": pathname === "/",
+            })}
+          >
+            Data explorer
+          </Link>
+
+          <Link
+            href="/interact"
+            className={cn("underline-offset-[16px] uppercase", {
+              "font-semibold underline decoration-orange-500 decoration-4": pathname === "/interact",
+            })}
+          >
+            Interact
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-x-4">
+          <LatestBlock />
+          <AccountSelect />
+        </div>
       </div>
 
-      <div className="flex items-center gap-x-4">
-        <LatestBlock />
-        <AccountSelect />
-      </div>
+      <Separator />
     </div>
   );
 }
---
+++ packages/worlds-explorer/src/hooks/useHash.ts
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import queryString from "query-string";
+
+// How do I get the pathname with hash.
+// source: https://github.com/vercel/next.js/discussions/49465
+export const useHashState = () => {
+  const getCurrentHash = useMemo(
+    () => () => (typeof window !== "undefined" ? window.location.hash.replace(/^#!?/, "") : ""),
+    [],
+  );
+  const router = useRouter();
+  const params = useParams();
+  const [hash, _setHash] = useState<string>(getCurrentHash());
+
+  const setHash = (newHash: string) => {
+    let updatedUrl = window.location.href;
+    updatedUrl = queryString.stringifyUrl({ url: updatedUrl.split("#")[0], fragmentIdentifier: newHash });
+
+    _setHash(newHash);
+    router.replace(updatedUrl);
+  };
+  useEffect(() => {
+    const currentHash = getCurrentHash();
+    _setHash(currentHash);
+  }, [params]);
+
+  const handleHashChange = () => {
+    const currentHash = getCurrentHash();
+    _setHash(currentHash);
+  };
+
+  useEffect(() => {
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return [hash, setHash] as const;
+};
