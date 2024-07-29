55a86ce8c2a3c1f01f69c86e72857f936da551a8
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-29 10:25:19 +0100

    label -> debugLabel

--- packages/cli/src/deploy/ensureContract.ts
+++ packages/cli/src/deploy/ensureContract.ts
@@ -9,7 +9,7 @@ import { wait } from "@latticexyz/common/utils";
 export type Contract = {
   bytecode: Hex;
   deployedBytecodeSize: number;
-  label?: string;
+  debugLabel?: string;
 };
 
 export async function ensureContract({
@@ -17,34 +17,35 @@ export async function ensureContract({
   deployerAddress,
   bytecode,
   deployedBytecodeSize,
-  label = "contract",
+  debugLabel = "contract",
 }: {
   readonly client: Client<Transport, Chain | undefined, Account>;
   readonly deployerAddress: Hex;
 } & Contract): Promise<readonly Hex[]> {
   if (bytecode.includes("__$")) {
-    throw new Error(`Found unlinked public library in ${label} bytecode`);
+    throw new Error(`Found unlinked public library in ${debugLabel} bytecode`);
   }
 
   const address = getCreate2Address({ from: deployerAddress, salt, bytecode });
 
   const contractCode = await getBytecode(client, { address, blockTag: "pending" });
   if (contractCode) {
-    debug("found", label, "at", address);
+    debug("found", debugLabel, "at", address);
     return [];
   }
 
   if (deployedBytecodeSize > contractSizeLimit) {
     console.warn(
-      `\nBytecode for ${label} (${deployedBytecodeSize} bytes) is over the contract size limit (${contractSizeLimit} bytes). Run \`forge build --sizes\` for more info.\n`,
+      `\nBytecode for ${debugLabel} (${deployedBytecodeSize} bytes) is over the contract size limit (${contractSizeLimit} bytes). Run \`forge build --sizes\` for more info.\n`,
     );
   } else if (deployedBytecodeSize > contractSizeLimit * 0.95) {
     console.warn(
-      `\nBytecode for ${label} (${deployedBytecodeSize} bytes) is almost over the contract size limit (${contractSizeLimit} bytes). Run \`forge build --sizes\` for more info.\n`,
+      // eslint-disable-next-line max-len
+      `\nBytecode for ${debugLabel} (${deployedBytecodeSize} bytes) is almost over the contract size limit (${contractSizeLimit} bytes). Run \`forge build --sizes\` for more info.\n`,
     );
   }
 
-  debug("deploying", label, "at", address);
+  debug("deploying", debugLabel, "at", address);
   return [
     await pRetry(
       () =>
@@ -57,7 +58,7 @@ export async function ensureContract({
         retries: 3,
         onFailedAttempt: async (error) => {
           const delay = error.attemptNumber * 500;
-          debug(`failed to deploy ${label}, retrying in ${delay}ms...`);
+          debug(`failed to deploy ${debugLabel}, retrying in ${delay}ms...`);
           await wait(delay);
         },
       },
