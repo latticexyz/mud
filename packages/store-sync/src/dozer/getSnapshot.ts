9c398d852185d46db6194c3c27708cde09209e03
alvrs <alvarius@lattice.xyz>
2024-08-01 21:38:02 +0100

    update logic in dozer/getSnapshot

--- packages/store-sync/src/dozer/getSnapshot.ts
+++ packages/store-sync/src/dozer/getSnapshot.ts
@@ -25,8 +25,6 @@ export async function getSnapshot({
   startBlock = 0n,
   chainId,
 }: GetSnapshotArgs): Promise<GetSnapshotResult> {
-  const initialBlockLogs: StorageAdapterBlock = { blockNumber: startBlock, logs: [] };
-
   // We execute the list of provided SQL queries for hydration. For performance
   // reasons the queries are not executed against a fixed block height, but against
   // the latest state. We therefore pass the min block number of all query results
@@ -38,47 +36,53 @@ export async function getSnapshot({
 
   const sqlFilters = filters ? (filters.filter((filter) => "sql" in filter) as TableQuery[]) : [];
 
+  const fetchLogs = async (): Promise<StorageAdapterBlock | undefined> => {
+    // Fetch the tables without SQL filter from the snapshot logs API for better performance.
+    const logsFilters =
+      filters &&
+      filters
+        .filter((filter) => !("sql" in filter))
+        .map((filter) => {
+          const { table, key0, key1 } = filter as LogFilter;
+          return { tableId: table.tableId, key0, key1 } as LegacyLogFilter;
+        });
+
+    if (logsFilters && logsFilters.length === 0) {
+      return undefined;
+    }
+
+    return getSnapshotLogs({
+      chainId,
+      address: storeAddress,
+      filters: logsFilters,
+      indexerUrl: dozerUrl,
+    });
+  };
+
+  const fetchSql = (): Promise<StorageAdapterBlock | undefined>[] => {
+    return sqlFilters.map(async (filter) => {
+      const result = await fetchRecordsSql({ dozerUrl, storeAddress, queries: [filter] });
+      return (
+        result && {
+          blockNumber: result.blockHeight,
+          logs: result.result.flatMap(({ table, records }) =>
+            records.map((record) => recordToLog({ table, record, address: storeAddress })),
+          ),
+        }
+      );
+    });
+  };
+
   // Execute individual SQL queries as separate requests to parallelize on the backend.
   // Each individual request is expected to be executed against the same db state so it
   // can't be parallelized.
-  const dozerTables = (
-    await Promise.all(sqlFilters.map((filter) => fetchRecordsSql({ dozerUrl, storeAddress, queries: [filter] })))
-  ).filter(isDefined);
-
-  if (dozerTables.length > 0) {
-    // Use the minimum block number of all query results as the block number to start syncing from.
-    initialBlockLogs.blockNumber = bigIntMin(...dozerTables.map((result) => result.blockHeight));
-    initialBlockLogs.logs = dozerTables.flatMap(({ result: [{ table, records }] }) =>
-      records.map((record) => recordToLog({ table, record, address: storeAddress })),
-    );
-  }
-
-  // Fetch the tables without SQL filter from the snapshot logs API for better performance.
-  const logsFilters =
-    filters &&
-    filters
-      .filter((filter) => !("sql" in filter))
-      .map((filter) => {
-        const { table, key0, key1 } = filter as LogFilter;
-        return { tableId: table.tableId, key0, key1 } as LegacyLogFilter;
-      });
-
-  const logs =
-    // If no filters are provided, the entire state is fetched
-    !logsFilters || logsFilters.length > 0
-      ? await getSnapshotLogs({
-          chainId,
-          address: storeAddress,
-          filters: logsFilters,
-          indexerUrl: dozerUrl,
-        })
-      : undefined;
+  const results = (await Promise.all([fetchLogs(), ...fetchSql()])).filter(isDefined);
 
   // The block number passed in the overall result will be the min of all queries and the logs.
-  if (logs) {
-    initialBlockLogs.blockNumber = bigIntMin(initialBlockLogs.blockNumber, logs.blockNumber);
-    initialBlockLogs.logs = [...initialBlockLogs.logs, ...logs.logs];
-  }
+  const initialBlockLogs = {
+    blockNumber: results.length > 0 ? bigIntMin(...results.map((result) => result.blockNumber)) : startBlock,
+    logs: results.flatMap((result) => result.logs),
+  };
 
   return { initialBlockLogs };
 }
