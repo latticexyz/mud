e730b263471479168a37cc9544e1f82b3ab07eef
alvrs <alvarius@lattice.xyz>
2024-08-02 19:03:03 +0100

    50% of instantiations come from createClientComponents

--- templates/react-ecs/packages/client/src/mud/setup.ts
+++ templates/react-ecs/packages/client/src/mud/setup.ts
@@ -2,7 +2,7 @@
  * This file sets up all the definitions required for a MUD client.
  */
 
-import { createClientComponents } from "./createClientComponents";
+// import { createClientComponents } from "./createClientComponents";
 import { createSystemCalls } from "./createSystemCalls";
 import { setupNetwork } from "./setupNetwork";
 
@@ -10,12 +10,12 @@ export type SetupResult = Awaited<ReturnType<typeof setup>>;
 
 export async function setup() {
   const network = await setupNetwork();
-  const components = createClientComponents(network);
+  // const components = createClientComponents(network);
   const systemCalls = createSystemCalls(network, components);
 
   return {
     network,
-    components,
+    // components,
     systemCalls,
   };
 }
