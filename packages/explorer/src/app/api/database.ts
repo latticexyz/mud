04fd76bfe1195453e43241f538009cc40870a860
Kevin Ingersoll <kingersoll@gmail.com>
2024-08-09 14:46:42 +0100

    fix export type

--- packages/explorer/src/app/api/database.ts
+++ packages/explorer/src/app/api/database.ts
@@ -1,10 +1,10 @@
-import Database from "better-sqlite3";
+import sqlite from "better-sqlite3";
 import fs from "fs";
 import path from "path";
 
 const DEFAULT_DB_PATH = "indexer.db";
 
-export const getDatabase = () => {
+export function getDatabase(): sqlite.Database | null {
   let dbPath = process.env.INDEXER_DB_PATH_ABSOLUTE;
   if (!dbPath) {
     dbPath = path.join(
@@ -17,10 +17,10 @@ export const getDatabase = () => {
     return null;
   }
 
-  const db = new Database(dbPath);
+  const db = new sqlite(dbPath);
   if (!db) {
     return null;
   }
 
   return db;
-};
+}
