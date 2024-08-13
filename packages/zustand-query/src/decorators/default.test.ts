fd4a4cc857107a46d73ee0271349a41467355c15
alvrs <alvarius@lattice.xyz>
2024-08-13 13:45:40 +0100

    change test order

--- packages/zustand-query/src/decorators/default.test.ts
+++ packages/zustand-query/src/decorators/default.test.ts
@@ -4,6 +4,77 @@ import { describe, it } from "vitest";
 import { createStore } from "../createStore";
 
 describe("store with default actions", () => {
+  describe("getRecord", () => {
+    it("should get a record by key from the table", () => {
+      const config = defineStore({
+        namespace: "namespace1",
+        tables: {
+          table1: {
+            schema: {
+              field1: "string",
+              field2: "uint32",
+              field3: "int32",
+            },
+            key: ["field2", "field3"],
+          },
+        },
+      });
+
+      const table = config.namespaces.namespace1.tables.table1;
+
+      const store = createStore(config);
+
+      store.setRecord({
+        table,
+        key: { field2: 2, field3: 1 },
+        record: { field1: "world" },
+      });
+
+      attest<{ field1: string; field2: number; field3: number }>(
+        store.getRecord({
+          table,
+          key: { field2: 2, field3: 1 },
+        }),
+      ).snap({ field1: "world", field2: 2, field3: 1 });
+    });
+
+    it("should throw a type error if the key type doesn't match", () => {
+      const config = defineStore({
+        namespace: "namespace1",
+        tables: {
+          table1: {
+            schema: {
+              field1: "string",
+              field2: "uint32",
+              field3: "int32",
+            },
+            key: ["field2", "field3"],
+          },
+        },
+      });
+
+      const table = config.namespaces.namespace1.tables.table1;
+
+      const store = createStore(config);
+
+      attest(() =>
+        store.getRecord({
+          table,
+          // @ts-expect-error Property 'field3' is missing in type '{ field2: number; }'
+          key: { field2: 1 },
+        }),
+      ).type.errors(`Property 'field3' is missing in type '{ field2: number; }'`);
+
+      attest(() =>
+        store.getRecord({
+          table,
+          // @ts-expect-error Type 'string' is not assignable to type 'number'
+          key: { field2: 1, field3: "invalid" },
+        }),
+      ).type.errors(`Type 'string' is not assignable to type 'number'`);
+    });
+  });
+
   describe("setRecord", () => {
     it("should show a type warning if an invalid table, key or record is used", () => {
       const config = defineStore({
@@ -90,77 +161,6 @@ describe("store with default actions", () => {
           records: [{ field1: 1, field2: 1, field3: 2 }],
         }),
       ).type.errors(`Type 'number' is not assignable to type 'string'.`);
-    });
-  });
-
-  describe("getRecord", () => {
-    it("should get a record by key from the table", () => {
-      const config = defineStore({
-        namespace: "namespace1",
-        tables: {
-          table1: {
-            schema: {
-              field1: "string",
-              field2: "uint32",
-              field3: "int32",
-            },
-            key: ["field2", "field3"],
-          },
-        },
-      });
-
-      const table = config.namespaces.namespace1.tables.table1;
-
-      const store = createStore(config);
-
-      store.setRecord({
-        table,
-        key: { field2: 2, field3: 1 },
-        record: { field1: "world" },
-      });
-
-      attest<{ field1: string; field2: number; field3: number }>(
-        store.getRecord({
-          table,
-          key: { field2: 2, field3: 1 },
-        }),
-      ).snap({ field1: "world", field2: 2, field3: 1 });
-    });
-
-    it("should throw a type error if the key type doesn't match", () => {
-      const config = defineStore({
-        namespace: "namespace1",
-        tables: {
-          table1: {
-            schema: {
-              field1: "string",
-              field2: "uint32",
-              field3: "int32",
-            },
-            key: ["field2", "field3"],
-          },
-        },
-      });
-
-      const table = config.namespaces.namespace1.tables.table1;
-
-      const store = createStore(config);
-
-      attest(() =>
-        store.getRecord({
-          table,
-          // @ts-expect-error Property 'field3' is missing in type '{ field2: number; }'
-          key: { field2: 1 },
-        }),
-      ).type.errors(`Property 'field3' is missing in type '{ field2: number; }'`);
-
-      attest(() =>
-        store.getRecord({
-          table,
-          // @ts-expect-error Type 'string' is not assignable to type 'number'
-          key: { field2: 1, field3: "invalid" },
-        }),
-      ).type.errors(`Type 'string' is not assignable to type 'number'`);
     });
   });
 });
