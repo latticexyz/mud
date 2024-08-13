51cd0b7da6f6522d59184d9de50f6cbc5b9da723
Kevin Ingersoll <kingersoll@gmail.com>
2024-08-13 18:57:36 +0100

    add module tests, fix bugs

--- packages/world-modules/src/modules/metadata/MetadataSystem.sol
+++ packages/world-modules/src/modules/metadata/MetadataSystem.sol
@@ -3,13 +3,13 @@ pragma solidity >=0.8.24;
 
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { System } from "@latticexyz/world/src/System.sol";
-import { AccessControl } from "@latticexyz/world/src/AccessControl.sol";
-import { Metadata } from "./codegen/tables/Metadata.sol";
+import { AccessControlLib } from "../../utils/AccessControlLib.sol";
+import { Resource } from "./codegen/tables/Resource.sol";
 
 contract MetadataSystem is System {
-  function setMetadata(ResourceId resource, bytes32 name, string memory value) external {
-    AccessControl.requireExistence(resource);
-    AccessControl.requireOwner(resource, _msgSender());
-    Metadata.set(resource, name, value);
+  function setResource(ResourceId resource, bytes32 name, string memory value) public {
+    AccessControlLib.requireExistence(resource);
+    AccessControlLib.requireOwner(resource, _msgSender());
+    Resource.set(resource, name, value);
   }
 }
---
+++ packages/world-modules/test/MetadataModule.t.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { Test } from "forge-std/Test.sol";
+import { GasReporter } from "@latticexyz/gas-report/src/GasReporter.sol";
+
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { createWorld } from "@latticexyz/world/test/createWorld.sol";
+import { ResourceId, WorldResourceIdLib, WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
+import { IWorldErrors } from "@latticexyz/world/src/IWorldErrors.sol";
+import { NamespaceOwner } from "@latticexyz/world/src/codegen/tables/NamespaceOwner.sol";
+
+import { MetadataModule } from "../src/modules/metadata/MetadataModule.sol";
+import { IWorld } from "../src/modules/metadata/codegen/world/IWorld.sol";
+import { Resource as ResourceMetadata } from "../src/modules/metadata/codegen/tables/Resource.sol";
+
+contract MetadataModuleTest is Test, GasReporter {
+  using WorldResourceIdInstance for ResourceId;
+
+  IWorld world;
+  MetadataModule metadataModule = new MetadataModule();
+
+  function setUp() public {
+    world = IWorld(address(createWorld()));
+    StoreSwitch.setStoreAddress(address(world));
+  }
+
+  function testInstall() public {
+    startGasReport("install metadata module");
+    world.installModule(metadataModule, new bytes(0));
+    endGasReport();
+
+    assertEq(NamespaceOwner.get(ResourceMetadata._tableId.getNamespaceId()), address(this));
+  }
+
+  function testSetResourceMetadata() public {
+    world.installModule(metadataModule, new bytes(0));
+
+    assertEq(ResourceMetadata.get(ResourceMetadata._tableId, "label"), "");
+
+    startGasReport("set resource metadata");
+    world.metadata__setResource(ResourceMetadata._tableId, "label", "ResourceMetadata");
+    endGasReport();
+
+    assertEq(ResourceMetadata.get(ResourceMetadata._tableId, "label"), "ResourceMetadata");
+  }
+}
