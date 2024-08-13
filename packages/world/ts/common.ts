ffe64036ad6974eb73294540c4620203ec970e91
karooolis <hello@karolisram.com>
2024-08-13 14:52:25 +0200

    remove concatBaseApi helper function

--- packages/world/ts/common.ts
+++ packages/world/ts/common.ts
@@ -1,4 +1,4 @@
-import { Hex } from "viem";
+import { Hex, createTestClient, http, publicActions, walletActions } from "viem";
 
 export type WorldFunction = {
   readonly signature: string;
@@ -7,3 +7,19 @@ export type WorldFunction = {
   readonly systemFunctionSignature: string;
   readonly systemFunctionSelector: Hex;
 };
+
+export const anvilHost = "127.0.0.1";
+export const anvilPort = 8555;
+
+// ID of the current test worker. Used by the `@viem/anvil` proxy server.
+export const poolId = Number(process.env.VITEST_POOL_ID ?? 1);
+
+export const anvilRpcUrl = `http://${anvilHost}:${anvilPort}/${poolId}`;
+
+export const testClient = createTestClient({
+  mode: "anvil",
+  transport: http(anvilRpcUrl),
+  pollingInterval: 10,
+})
+  .extend(publicActions)
+  .extend(walletActions);
