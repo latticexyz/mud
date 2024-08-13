648be0c19f71c1d19a118732e5b460907113e442
Kevin Ingersoll <kingersoll@gmail.com>
2024-08-13 16:27:49 +0100

    build world-modules without cli

--- packages/cli/src/utils/knownModuleArtifacts.ts
+++
@@ -1,8 +0,0 @@
-/** @deprecated Please don't add to this list! These are kept for backwards compatibility and assumes the downstream project has this module installed as a dependency. */
-export const knownModuleArtifacts = {
-  KeysWithValueModule: "@latticexyz/world-modules/out/KeysWithValueModule.sol/KeysWithValueModule.json",
-  KeysInTableModule: "@latticexyz/world-modules/out/KeysInTableModule.sol/KeysInTableModule.json",
-  UniqueEntityModule: "@latticexyz/world-modules/out/UniqueEntityModule.sol/UniqueEntityModule.json",
-  Unstable_CallWithSignatureModule:
-    "@latticexyz/world-modules/out/Unstable_CallWithSignatureModule.sol/Unstable_CallWithSignatureModule.json",
-};
