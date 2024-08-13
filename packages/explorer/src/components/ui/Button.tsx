dd8e9220f6094259c1902479beac854b96562873
Karolis Ramanauskas <hello@karolisram.com>
2024-08-13 10:54:07 +0300

    Update packages/explorer/src/components/ui/Button.tsx
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- packages/explorer/src/components/ui/Button.tsx
+++ packages/explorer/src/components/ui/Button.tsx
@@ -33,11 +33,7 @@ const buttonVariants = cva(
   },
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<typeof buttonVariants> & { asChild?: boolean; }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
