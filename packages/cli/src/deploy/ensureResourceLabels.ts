bb069fbbbd46c3b556ca357cf6ff81dce87d8f91
Kevin Ingersoll <kingersoll@gmail.com>
2024-08-15 10:43:25 +0100

    check for set tag function and check for current labels

--- packages/cli/src/deploy/ensureResourceLabels.ts
+++ packages/cli/src/deploy/ensureResourceLabels.ts
@@ -1,14 +1,38 @@
-import { Hex, Client, Transport, Chain, Account, stringToHex, BaseError } from "viem";
+import {
+  Hex,
+  Client,
+  Transport,
+  Chain,
+  Account,
+  stringToHex,
+  BaseError,
+  hexToString,
+  toFunctionSelector,
+  AbiFunction,
+  AbiItem,
+  toFunctionSignature,
+} from "viem";
 import { WorldDeploy } from "./common";
 import { debug } from "./debug";
 import { hexToResource, writeContract } from "@latticexyz/common";
 import { isDefined } from "@latticexyz/common/utils";
+import worldConfig from "@latticexyz/world/mud.config";
+import metadataConfig from "@latticexyz/world-module-metadata/mud.config";
 import metadataAbi from "@latticexyz/world-module-metadata/out/IMetadataSystem.sol/IMetadataSystem.abi.json" assert { type: "json" };
+import { getTableValue } from "./getTableValue";
 
 type LabeledResource = {
   readonly resourceId: Hex;
   readonly label: string;
 };
+
+function isAbiFunction(item: AbiItem): item is AbiFunction {
+  return item.type === "function";
+}
+
+function isEmptyHex(hex: Hex): boolean {
+  return /^0x0*/.test(hex);
+}
 
 export async function ensureResourceLabels({
   client,
@@ -19,13 +43,43 @@ export async function ensureResourceLabels({
   readonly worldDeploy: WorldDeploy;
   readonly resources: readonly LabeledResource[];
 }): Promise<readonly Hex[]> {
-  // TODO: check if `metadata__setResourceTag` function exists
-  // TODO: filter already registered labels
+  // it's safe to use ! here because the abi is strongly typed and TS will show an error if the name used below is not in the ABI
+  const functionAbi = metadataAbi.filter(isAbiFunction).find((fn) => fn.name === "metadata__setResourceTag")!;
+  const worldFunction = await getTableValue({
+    client,
+    worldDeploy,
+    table: worldConfig.tables.world__FunctionSelectors,
+    key: { worldFunctionSelector: toFunctionSelector(functionAbi) },
+  });
+  console.log("world function", worldFunction);
+  if (isEmptyHex(worldFunction.systemId)) {
+    debug(
+      `Skipping resource labels because \`${toFunctionSignature(functionAbi)}\` world function was missing. Is the metadata module not installed or outdated?`,
+    );
+    return [];
+  }
+
+  const currentLabels = await Promise.all(
+    resources.map(async (resource) => {
+      const { value } = await getTableValue({
+        client,
+        worldDeploy,
+        table: metadataConfig.tables.metadata__ResourceTag,
+        key: { resource: resource.resourceId, tag: stringToHex("label", { size: 32 }) },
+      });
+      return hexToString(value);
+    }),
+  );
+
+  const resourcesToSet = resources.filter((resource, i) => resource.label !== currentLabels[i]);
+  if (resourcesToSet.length === 0) {
+    return [];
+  }
 
   debug(`setting ${resources.length} resource labels`);
   return (
     await Promise.all(
-      resources.map(async ({ resourceId, label }) => {
+      resourcesToSet.map(async ({ resourceId, label }) => {
         const resource = hexToResource(resourceId);
         // TODO: move to resourceToDebugString
         const resourceString = `${resource.type}:${resource.namespace}:${resource.name}`;
