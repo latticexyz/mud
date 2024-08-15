9a02d5be6e39d6decf5ee00d2a3f5ec4313070d7
alvrs <alvarius@lattice.xyz>
2024-08-15 11:17:28 +0100

    update useSelector type

--- packages/zustand-query/src/react/useSelector.ts
+++ packages/zustand-query/src/react/useSelector.ts
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-import { State, Store } from "../common";
+import { State, Store, StoreConfig } from "../common";
 import { subscribeStore } from "../actions";
 
-export function useSelector<store extends Store, T>(
-  store: store,
-  selector: (store: State) => T,
+export function useSelector<config extends StoreConfig, T>(
+  store: Store<config>,
+  selector: (store: State<config>) => T,
   equals: (a: T, b: T) => boolean = (a, b) => a === b,
 ): T {
   const state = useRef(selector(store.get()));
