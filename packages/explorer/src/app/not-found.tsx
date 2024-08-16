3c669644474750272e8a194c9dbcf59bf0e1da79
karooolis <hello@karolisram.com>
2024-08-16 13:53:10 +0200

    add 404 page

---
+++ packages/explorer/src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../components/ui/Button";
+
+export default function NotFound() {
+  return (
+    <main className="py-24 px-6 text-center">
+      <p className="text-3xl font-semibold text-orange-600">404</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
+      <p className="mt-6 text-base leading-7 text-white/70">Sorry, we couldn’t find the page you’re looking for.</p>
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <Button asChild>
+          <Link href="/">Go back to explorer</Link>
+        </Button>
+
+        <Button variant="ghost" asChild>
+          <a href="https://lattice.xyz/discord" target="_blank" rel="noopener noreferrer">
+            Contact on Discord <ExternalLink className="h-4 w-4 ml-2 -mt-0.5" />
+          </a>
+        </Button>
+      </div>
+    </main>
+  );
+}
