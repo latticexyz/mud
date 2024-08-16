4ca9f5a63831f87727d8c96db8c35078bf22fe2a
karooolis <hello@karolisram.com>
2024-08-16 13:20:17 +0200

    watch for worlds.json change

--- examples/local-explorer/explorer-watcher.mjs
+++ examples/local-explorer/explorer-watcher.mjs
@@ -1,4 +1,5 @@
-import { readFile, watch } from "fs/promises";
+import { watchFile } from "fs";
+import { readFile } from "fs/promises";
 import { spawn } from "child_process";
 import { fileURLToPath } from "url";
 import process from "process";
@@ -9,34 +10,32 @@ const __dirname = path.dirname(__filename);
 const EXPLORER_DIR = path.resolve(__dirname, "..", "..", "packages", "explorer");
 const WORLDS_JSON_PATH = path.resolve(__dirname, "packages", "contracts", "worlds.json");
 
-const MODE = process.env.MODE || "production";
+const MODE = process.env.MODE || "development";
+const CHAIN_ID = parseInt(process.env.CHAIN_ID || 31337);
 let explorerProcess = null;
-
-async function readWorldsJson() {
-  try {
-    const data = await readFile(WORLDS_JSON_PATH, "utf8");
-    return data;
-  } catch (error) {
-    console.error("Error reading worlds.json:", error);
-    return null;
-  }
-}
 
 async function startExplorer() {
   process.env.INIT_PWD = process.cwd();
-  process.env.NEXT_PUBLIC_WORLDS_CONFIG = await readWorldsJson();
+  process.env.NEXT_PUBLIC_CHAIN_ID = CHAIN_ID;
 
-  console.log("Starting explorer");
+  const currentWorldAddress = process.env.NEXT_PUBLIC_WORLD_ADDRESS;
+  const worldAddress = await readWorldsJson();
+  process.env.NEXT_PUBLIC_WORLD_ADDRESS = worldAddress;
+  if (currentWorldAddress) {
+    if (currentWorldAddress === worldAddress) {
+      return;
+    }
+
+    console.log("World address changed, restarting explorer...");
+  }
 
   let command, args, workingDir;
 
   if (MODE === "production") {
-    console.log("Running explorer in production mode");
     command = "pnpm";
     args = ["explorer"];
     workingDir = process.cwd();
   } else {
-    console.log("Running explorer in development mode");
     command = "pnpm";
     args = ["dev"];
     workingDir = EXPLORER_DIR;
@@ -47,10 +46,25 @@ async function startExplorer() {
     stdio: "inherit",
     env: { ...process.env },
   });
+}
 
-  explorerProcess.on("close", (code) => {
-    console.log(`Explorer process exited with code ${code}`);
-  });
+async function readWorldsJson() {
+  try {
+    const data = await readFile(WORLDS_JSON_PATH, "utf8");
+    if (data) {
+      const worlds = JSON.parse(data);
+      const world = worlds[CHAIN_ID];
+      if (world) {
+        return world.address;
+      } else {
+        console.error(`World not found for chain ID ${CHAIN_ID}`);
+        return null;
+      }
+    }
+  } catch (error) {
+    console.error("Error reading worlds.json:", error);
+    return null;
+  }
 }
 
 async function restartExplorer() {
@@ -60,21 +74,12 @@ async function restartExplorer() {
   await startExplorer();
 }
 
-async function watchWorldsJson() {
-  try {
-    const watcher = watch(WORLDS_JSON_PATH);
-    for await (const event of watcher) {
-      if (event.eventType === "change") {
-        console.log("worlds.json has changed. Restarting explorer...");
-        await restartExplorer();
-      }
-    }
-  } catch (error) {
-    console.error("Error watching worlds.json:", error);
-  }
+function watchWorldsJson() {
+  watchFile(WORLDS_JSON_PATH, async () => {
+    await restartExplorer();
+  });
 }
 
-// Handle script termination
 process.on("SIGINT", () => {
   if (explorerProcess) {
     explorerProcess.kill();
@@ -82,10 +87,9 @@ process.on("SIGINT", () => {
   process.exit();
 });
 
-// Start the explorer and watch for changes
 async function main() {
   await startExplorer();
-  await watchWorldsJson();
+  watchWorldsJson();
 }
 
 main().catch(console.error);
