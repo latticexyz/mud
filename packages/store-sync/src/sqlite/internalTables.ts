574e6083e16b025e16eccbf72240c6af09f19e5b
karooolis <hello@karolisram.com>
2024-08-19 13:31:21 +0300

    export internal table names

--- packages/store-sync/src/sqlite/internalTables.ts
+++ packages/store-sync/src/sqlite/internalTables.ts
@@ -2,7 +2,10 @@ import { blob, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { address, asHex, json } from "./columnTypes";
 import { PartialTable } from "./common";
 
-export const chainState = sqliteTable("__chainState", {
+export const CHAIN_STATE_TABLE_NAME = "__chainState";
+export const MUD_STORE_TABLES_NAME = "__mudStoreTables";
+
+export const chainState = sqliteTable(CHAIN_STATE_TABLE_NAME, {
   schemaVersion: integer("schema_version").notNull().primaryKey(),
   chainId: integer("chain_id").notNull().primaryKey(),
   lastUpdatedBlockNumber: blob("last_updated_block_number", { mode: "bigint" }),
@@ -10,7 +13,7 @@ export const chainState = sqliteTable("__chainState", {
   lastError: text("last_error"),
 });
 
-export const mudStoreTables = sqliteTable("__mudStoreTables", {
+export const mudStoreTables = sqliteTable(MUD_STORE_TABLES_NAME, {
   schemaVersion: integer("schema_version").primaryKey(),
   id: text("id").notNull().primaryKey(),
   address: address("address").notNull(),
