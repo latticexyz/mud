17de60a3ef123a62c95a2e27ed47685b838f9591
karooolis <hello@karolisram.com>
2024-08-19 08:17:59 +0300

    clarify private keys source

--- packages/explorer/src/consts.ts
+++ packages/explorer/src/consts.ts
@@ -3,6 +3,7 @@ import { privateKeyToAccount } from "viem/accounts";
 
 export const NON_EDITABLE_TABLES = ["__chainState", "__mudStoreTables"];
 
+// private keys for local development testnet (anvil)
 export const PRIVATE_KEYS: Hex[] = [
   "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
   "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
