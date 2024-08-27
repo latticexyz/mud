76d95a2b1b62d7f2423b79af593e0df02372001a
karooolis <hello@karolisram.com>
2024-08-27 11:27:09 +0300

    update bin script name

--- packages/explorer/tsup.config.ts
+++ packages/explorer/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["bin/explorer.ts"],
   target: "esnext",
   format: ["esm"],
   sourcemap: true,
