cb7624d93191185bec6157cdb3c32b9ffa107a25
karooolis <hello@karolisram.com>
2024-08-28 14:07:25 +0300

    update props

--- packages/explorer/src/store/createAppStore.ts
+++ packages/explorer/src/store/createAppStore.ts
@@ -2,13 +2,13 @@ import { Hex } from "viem";
 import { createStore } from "zustand";
 import { ACCOUNTS } from "../consts";
 
-export type AppStore = {
+export type AppStoreData = {
   account: Hex;
   setAccount: (account: Hex) => void;
 };
 
 export const createAppStore = () => {
-  return createStore<AppStore>()((set) => ({
+  return createStore<AppStoreData>()((set) => ({
     account: ACCOUNTS[0],
     setAccount: (account) => set({ account }),
   }));
