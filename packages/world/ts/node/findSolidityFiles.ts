4632b4b5b231fc8804410ad4a9419a9a63d06c0f
Kevin Ingersoll <kingersoll@gmail.com>
2024-08-28 21:40:18 +0100

    try an initial forward slash

--- packages/world/ts/node/findSolidityFiles.ts
+++ packages/world/ts/node/findSolidityFiles.ts
@@ -3,7 +3,7 @@ import { glob } from "glob";
 
 // TODO: move to common codegen?
 export async function findSolidityFiles({ cwd, pattern = "**" }: { cwd?: string; pattern: string }) {
-  const files = await glob(path.join(pattern, "*.sol"), { cwd, windowsPathsNoEscape: true });
+  const files = await glob(path.join("./", pattern, "*.sol"), { cwd, windowsPathsNoEscape: true });
   return files.sort().map((filename) => ({
     filename,
     basename: path.basename(filename, ".sol"),
