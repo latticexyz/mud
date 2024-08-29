d512f84f5e79f937370b3329363db480cfbad09f
Tamara Ringas <69479754+TamaraRingas@users.noreply.github.com>
2024-08-29 13:54:22 +0200

    update gasReport() implementation

--- packages/world-module-erc20-own-store/test/ERC20.t.sol
+++ packages/world-module-erc20-own-store/test/ERC20.t.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.24;
 
 import { Test } from "forge-std/Test.sol";
-import { console2 } from "forge-std/Console2.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { GasReporter } from "@latticexyz/gas-report/src/GasReporter.sol";
 
@@ -18,195 +17,173 @@ contract tokenTest is Test, GasReporter {
   address charlie = address(0x456);
 
   function setUp() public {
+    startGasReport("token constructor");
     token = new ERC20("token", "MUD", address(this), 18);
+    endGasReport();
     StoreSwitch.setStoreAddress(address(token));
   }
 
   function testTokenSetUp() public {
-    startGasReport("token constructor");
-
     assertTrue(address(token) != address(0));
 
     assertEq(Token.getDecimals(), 18);
     assertEq(Token.getTotalSupply(), 0);
     assertEq(Token.getName(), "token");
     assertEq(Token.getSymbol(), "MUD");
-
-    endGasReport();
   }
 
   function testTokenMint() public {
     startGasReport("token mint");
-
     token.mint(address(this), 1000);
+    endGasReport();
 
     assertEq(Balances.getBalance(address(this)), 1000);
     assertEq(Token.getTotalSupply(), 1000);
-
-    endGasReport();
   }
 
   function testTokenMintInvalidCaller() public {
-    startGasReport("token mint invalid caller");
     vm.expectRevert();
     vm.prank(alice);
+    startGasReport("token mint invalid caller");
     token.mint(address(alice), 1000);
-
     endGasReport();
   }
 
   function testTokenMintToZeroAddress() public {
-    startGasReport("token mint to zero address");
     vm.expectRevert();
+    startGasReport("token mint to zero address");
     token.mint(address(0), 1000);
-
     endGasReport();
   }
 
   function testFuzzTokenMint(uint256 mintAmount) public {
     startGasReport("fuzz token mint");
-
     token.mint(address(this), mintAmount);
+    endGasReport();
 
     assertEq(Balances.getBalance(address(this)), mintAmount);
     assertEq(Token.getTotalSupply(), mintAmount);
-
-    endGasReport();
   }
 
   function testTokenBurn() public {
-    startGasReport("token burn");
-
     token.mint(address(this), 1000);
+    startGasReport("token burn");
     token.burn(address(this), 500);
+    endGasReport();
 
     assertEq(Balances.getBalance(address(this)), 500);
     assertEq(Token.getTotalSupply(), 500);
-
-    endGasReport();
   }
 
   function testTokenBurnInvalidCaller() public {
-    startGasReport("token burn invalid caller");
     vm.expectRevert();
     vm.prank(alice);
+    startGasReport("token burn invalid caller");
     token.burn(address(alice), 1000);
-
     endGasReport();
   }
 
   function testTokenBurnZeroAddress() public {
-    startGasReport("token burn zero address");
     vm.expectRevert();
+    startGasReport("token burn zero address");
     token.burn(address(0), 1000);
     endGasReport();
   }
 
   function testFuzzTokenBurn(uint256 burnAmount) public {
-    startGasReport("fuzz token burn");
     burnAmount = bound(burnAmount, 1, 1000000000);
     token.mint(address(this), 1000000000);
 
+    startGasReport("fuzz token burn");
     token.burn(address(this), burnAmount);
+    endGasReport();
 
     assertEq(Balances.getBalance(address(this)), 1000000000 - burnAmount);
     assertEq(Token.getTotalSupply(), 1000000000 - burnAmount);
-
-    endGasReport();
   }
 
   function testTokenTransfer() public {
-    startGasReport("token transfer");
-
     token.mint(address(this), 1000);
+
+    startGasReport("token transfer");
     token.transfer(alice, 500);
+    endGasReport();
 
     assertEq(Balances.getBalance(address(this)), 500);
     assertEq(Balances.getBalance(alice), 500);
-
-    endGasReport();
   }
 
   function testTokenTransferInsufficientBalance() public {
-    startGasReport("token transfer insufficient balance");
     token.mint(address(this), 1000);
     vm.expectRevert();
-    token.transfer(alice, 1001);
 
+    startGasReport("token transfer insufficient balance");
+    token.transfer(alice, 1001);
     endGasReport();
   }
 
   function testFuzzTransfer(uint256 transferAmount) public {
-    startGasReport("fuzz transfer");
     transferAmount = bound(transferAmount, 1, 1000000);
     token.mint(address(this), 1000000);
+
+    startGasReport("fuzz transfer");
     token.transfer(alice, transferAmount);
+    endGasReport();
 
     assertEq(Balances.getBalance(address(this)), 1000000 - transferAmount);
     assertEq(Balances.getBalance(alice), transferAmount);
-
-    endGasReport();
   }
 
   function testApprove() public {
     startGasReport("token approve");
-
     token.approve(charlie, 1000);
-    assertEq(Allowances.getApproval(address(this), charlie), 1000);
-
     endGasReport();
+
+    assertEq(Allowances.getApproval(address(this), charlie), 1000);
   }
 
   function testTransferFrom() public {
-    startGasReport("token transferFrom");
-
     token.mint(alice, 1000);
 
     vm.prank(alice);
     token.approve(address(this), 500);
 
+    startGasReport("token transferFrom");
     token.transferFrom(alice, charlie, 500);
+    endGasReport();
 
     assertEq(Balances.getBalance(alice), 500);
     assertEq(Balances.getBalance(charlie), 500);
     assertEq(Allowances.getApproval(address(this), alice), 0);
-
-    endGasReport();
   }
 
   function testTransferFromInsufficientAllowance() public {
-    startGasReport("token transferFrom insufficient allowance");
     token.mint(address(this), 1000);
     vm.expectRevert();
-    token.transferFrom(charlie, alice, 1000);
 
+    startGasReport("token transferFrom insufficient allowance");
+    token.transferFrom(charlie, alice, 1000);
     endGasReport();
   }
 
   function testFuzzTransferFrom(uint256 transferAmount) public {
-    startGasReport("fuzz transferFrom");
     transferAmount = bound(transferAmount, 1, 1000000);
     token.mint(alice, 1000000);
 
     vm.prank(alice);
     token.approve(address(this), transferAmount);
 
+    startGasReport("fuzz transferFrom");
     token.transferFrom(alice, charlie, transferAmount);
+    endGasReport();
 
     assertEq(Balances.getBalance(alice), 1000000 - transferAmount);
     assertEq(Balances.getBalance(charlie), transferAmount);
     assertEq(Allowances.getApproval(address(this), alice), 0);
-
-    endGasReport();
   }
 
   function testBalanceOf() public {
-    startGasReport("token balanceOf");
-
     token.mint(address(this), 1000);
-
     assertEq(token.balanceOf(address(this)), 1000);
-
-    endGasReport();
   }
 }
