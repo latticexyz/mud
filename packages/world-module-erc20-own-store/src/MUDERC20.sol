914b0d5a72fea2750cf40d88af4360ea6e508a70
Tamara Ringas <69479754+TamaraRingas@users.noreply.github.com>
2024-08-29 13:24:08 +0200

    update natspec

--- packages/world-module-erc20-own-store/src/MUDERC20.sol
+++ packages/world-module-erc20-own-store/src/MUDERC20.sol
@@ -83,7 +83,19 @@ contract MUDERC20 is Store, IERC20Errors, IERC20Events {
   }
 
   /**
-   * ToDo: Natspec
+   * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
+   * caller's tokens.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * IMPORTANT: Beware that changing an allowance with this method brings the risk
+   * that someone may use both the old and the new allowance by unfortunate
+   * transaction ordering. One possible solution to mitigate this race
+   * condition is to first reduce the spender's allowance to 0 and set the
+   * desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   *
+   * Emits an {Approval} event.
    */
   function approve(address spender, uint256 value) public returns (bool) {
     _approve(msg.sender, spender, value);
@@ -91,7 +103,11 @@ contract MUDERC20 is Store, IERC20Errors, IERC20Events {
   }
 
   /**
-   * ToDo: Natspec
+   * @dev Moves a `value` amount of tokens from the caller's account to `to`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
    */
   function transfer(address to, uint256 value) public returns (bool) {
     _transfer(msg.sender, to, value);
@@ -99,7 +115,13 @@ contract MUDERC20 is Store, IERC20Errors, IERC20Events {
   }
 
   /**
-   * ToDo: Natspec
+   * @dev Moves a `value` amount of tokens from `from` to `to` using the
+   * allowance mechanism. `value` is then deducted from the caller's
+   * allowance.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
    */
   function transferFrom(address from, address to, uint256 value) public returns (bool) {
     _spendAllowance(from, msg.sender, value);
