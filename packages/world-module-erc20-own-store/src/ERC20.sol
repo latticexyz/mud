2f84632aa65f21e41fda3d960b2fba585f649132
Tamara Ringas <69479754+TamaraRingas@users.noreply.github.com>
2024-08-29 13:44:20 +0200

    bump pragma

--- packages/world-module-erc20-own-store/src/ERC20.sol
+++ packages/world-module-erc20-own-store/src/ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.24;
 
 import { IERC20Errors } from "./IERC20Errors.sol";
 import { IERC20Events } from "./IERC20Events.sol";
