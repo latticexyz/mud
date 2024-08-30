ef022a89a0427990fb7b283e8f509956cf1c6e60
Tamara Ringas <69479754+TamaraRingas@users.noreply.github.com>
2024-08-30 14:06:13 +0200

    implement Store contract

--- packages/world-module-erc20-external-store/src/Store.sol
+++ packages/world-module-erc20-external-store/src/Store.sol
@@ -1,18 +1,141 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
-import { IStore } from "@latticexyz/Store/src/IStore.sol";
-import { StoreRead } from "@latticexyz/Store/src/StoreRead.sol";
-import { StoreCore } from "@latticexyz/Store/src/StoreCore.sol";
-import { STORE_VERSION } from "@latticexyz/Store/src/version.sol";
-import { StoreKernel } from "@latticexyz/Store/src/StoreKernel.sol";
-import { IStoreEvents } from "@latticexyz/Store/src/IStoreEvents.sol";
+import { IStore } from "@latticexyz/store/src/IStore.sol";
+import { IStoreHook } from "@latticexyz/store/src/IStoreHook.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { EncodedLengths } from "@latticexyz/store/src/EncodedLengths.sol";
+import { StoreCore } from "@latticexyz/store/src/StoreCore.sol";
+import { Schema } from "@latticexyz/store/src/Schema.sol";
+import { FieldLayout } from "@latticexyz/store/src/FieldLayout.sol";
+import { StoreRead } from "@latticexyz/store/src/StoreRead.sol";
+import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 
-/**
- * @title Store Contract
- * @author MUD (https://mud.dev) by Lattice (https://lattice.xyz)
- * @notice This contract integrates the core storage functionalities and provides an interface for data storage.
- * @dev This abstract contract initializes `StoreCore`, implements `storeVersion`, and read methods,
- * but not write methods.
- */
-abstract contract Store is IStore, StoreKernel {}
+contract Store {
+  constructor() {
+    StoreCore.initialize();
+    StoreCore.registerInternalTables();
+    StoreSwitch.setStoreAddress(address(this));
+  }
+
+  // Set full record (including full dynamic data)
+  function setRecord(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    bytes calldata staticData,
+    EncodedLengths encodedLengths,
+    bytes calldata dynamicData
+  ) public {
+    StoreCore.setRecord(tableId, keyTuple, staticData, encodedLengths, dynamicData);
+  }
+
+  // Splice data in the static part of the record
+  function spliceStaticData(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint48 start,
+    bytes calldata data
+  ) public virtual {
+    StoreCore.spliceStaticData(tableId, keyTuple, start, data);
+  }
+
+  // Splice data in the dynamic part of the record
+  function spliceDynamicData(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 dynamicFieldIndex,
+    uint40 startWithinField,
+    uint40 deleteCount,
+    bytes calldata data
+  ) public virtual {
+    StoreCore.spliceDynamicData(tableId, keyTuple, dynamicFieldIndex, startWithinField, deleteCount, data);
+  }
+
+  // Set partial data at field index
+  function setField(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 fieldIndex,
+    bytes calldata data
+  ) public virtual {
+    StoreCore.setField(tableId, keyTuple, fieldIndex, data);
+  }
+
+  // Set partial data at field index
+  function setField(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 fieldIndex,
+    bytes calldata data,
+    FieldLayout fieldLayout
+  ) public virtual {
+    StoreCore.setField(tableId, keyTuple, fieldIndex, data, fieldLayout);
+  }
+
+  // Set partial data at field index
+  function setStaticField(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 fieldIndex,
+    bytes calldata data,
+    FieldLayout fieldLayout
+  ) public virtual {
+    StoreCore.setStaticField(tableId, keyTuple, fieldIndex, data, fieldLayout);
+  }
+
+  // Set partial data at dynamic field index
+  function setDynamicField(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 dynamicFieldIndex,
+    bytes calldata data
+  ) public virtual {
+    StoreCore.setDynamicField(tableId, keyTuple, dynamicFieldIndex, data);
+  }
+
+  // Push encoded items to the dynamic field at field index
+  function pushToDynamicField(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 dynamicFieldIndex,
+    bytes calldata dataToPush
+  ) public virtual {
+    StoreCore.pushToDynamicField(tableId, keyTuple, dynamicFieldIndex, dataToPush);
+  }
+
+  // Pop byte length from the dynamic field at field index
+  function popFromDynamicField(
+    ResourceId tableId,
+    bytes32[] calldata keyTuple,
+    uint8 dynamicFieldIndex,
+    uint256 byteLengthToPop
+  ) public virtual {
+    StoreCore.popFromDynamicField(tableId, keyTuple, dynamicFieldIndex, byteLengthToPop);
+  }
+
+  // Set full record (including full dynamic data)
+  function deleteRecord(ResourceId tableId, bytes32[] memory keyTuple) public virtual {
+    StoreCore.deleteRecord(tableId, keyTuple);
+  }
+
+  function registerTable(
+    ResourceId tableId,
+    FieldLayout fieldLayout,
+    Schema keySchema,
+    Schema valueSchema,
+    string[] calldata keyNames,
+    string[] calldata fieldNames
+  ) public virtual {
+    StoreCore.registerTable(tableId, fieldLayout, keySchema, valueSchema, keyNames, fieldNames);
+  }
+
+  // Register hook to be called when a record or field is set or deleted
+  function registerStoreHook(ResourceId tableId, IStoreHook hookAddress, uint8 enabledHooksBitmap) public virtual {
+    StoreCore.registerStoreHook(tableId, hookAddress, enabledHooksBitmap);
+  }
+
+  // Unregister hook to be called when a record or field is set or deleted
+  function unregisterStoreHook(ResourceId tableId, IStoreHook hookAddress) public virtual {
+    StoreCore.unregisterStoreHook(tableId, hookAddress);
+  }
+}
