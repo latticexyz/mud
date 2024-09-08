359a1e616de7bb2407b57cb0fb0cbb5713f50ae3
Kevin Ingersoll <kingersoll@gmail.com>
2024-09-08 12:37:57 +0100

    fixes

--- examples/custom-world/src/CustomWorld.sol
+++ examples/custom-world/src/CustomWorld.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.24;
 import { World } from "@latticexyz/world/src/World.sol";
 
 contract CustomWorld is World {
-  function helloWorld() view returns (string) {
+  function helloWorld() public pure returns (string memory) {
     return "Hello world!";
   }
 }
