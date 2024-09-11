cae88661a443b9b3b7689f80162f94a539150d54
Ori Pomerantz <qbzzt1@gmail.com>
2024-09-11 16:47:15 -0500

    Add a "you don't need this" to the replicating onchain state guide as requested by @ludns in #3140.
    Also, move it down in the guide list.

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -1,9 +1,13 @@
 export default {
-  "replicating-onchain-state": "Replicating onchain state",
   "hello-world": "Hello World",
   "extending-a-world": "Extending a World",
   "adding-delegation": "Adding Delegation",
   "modules": "Writing MUD Modules",
   emojimon: "Emojimon",
   testing: "Testing",
+  "---": {
+    title: "", // no title renders as a line
+    type: "separator",
+  },  
+  "replicating-onchain-state": "Replicating onchain state",
 };
