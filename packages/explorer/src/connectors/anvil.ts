439559d77382ceca035e3d26b643bea7ff5fb5c1
karooolis <hello@karolisram.com>
2024-09-11 18:03:04 +0100

    cleanup anvil connector

--- packages/explorer/src/connectors/anvil.ts
+++ packages/explorer/src/connectors/anvil.ts
@@ -1,4 +1,4 @@
-import { EIP1193RequestFn, Transport, WalletRpcSchema, getAddress, http } from "viem";
+import { EIP1193RequestFn, Transport, WalletRpcSchema, http } from "viem";
 import { Account, privateKeyToAccount } from "viem/accounts";
 import { anvil as anvilChain } from "viem/chains";
 import { createConnector } from "wagmi";
@@ -32,7 +32,7 @@ export function anvil({ id, name, accounts }: AnvilConnectorOptions) {
   type Provider = ReturnType<Transport<"http", unknown, EIP1193RequestFn<WalletRpcSchema>>>;
 
   let connected = false;
-  return createConnector<Provider>((config) => ({
+  return createConnector<Provider>(() => ({
     id,
     name,
     type: "anvil",
@@ -62,20 +62,8 @@ export function anvil({ id, name, accounts }: AnvilConnectorOptions) {
       const accounts = await this.getAccounts();
       return !!accounts.length;
     },
-    async onAccountsChanged() {
-      if (accounts.length === 0) this.onDisconnect();
-      else
-        config.emitter.emit("change", {
-          accounts: accounts.map((x) => getAddress(x.address)),
-        });
-    },
-    async onDisconnect() {
-      config.emitter.emit("disconnect");
-      connected = false;
-    },
-    onChainChanged(chain) {
-      const chainId = Number(chain);
-      config.emitter.emit("change", { chainId });
-    },
+    async onAccountsChanged() {},
+    async onDisconnect() {},
+    onChainChanged() {},
   }));
 }
