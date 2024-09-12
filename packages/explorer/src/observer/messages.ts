784e5a98e679388ad6bc941cd1bc9b6486cf276d
Kevin Ingersoll <kingersoll@gmail.com>
2024-09-12 14:55:40 +0000

    feat(explorer): write observer (#3169)
    Co-authored-by: Karolis Ramanauskas <hello@karolisram.com>

---
+++ packages/explorer/src/observer/messages.ts
@@ -0,0 +1,37 @@
+import { Address, Hash } from "viem";
+import { ReceiptSummary } from "./common";
+
+export type Messages = {
+  ping: {};
+  write: {
+    writeId: string;
+    address: Address;
+    functionSignature: string;
+    args: unknown[];
+  };
+  "write:result": PromiseSettledResult<Hash> & {
+    writeId: string;
+  };
+  waitForTransactionReceipt: {
+    writeId: string;
+  };
+  "waitForTransactionReceipt:result": PromiseSettledResult<ReceiptSummary> & {
+    writeId: string;
+  };
+  waitForStateChange: {
+    writeId: string;
+  };
+  "waitForStateChange:result": PromiseSettledResult<ReceiptSummary> & {
+    writeId: string;
+  };
+};
+
+export type MessageType = keyof Messages;
+export type Message<messageType extends MessageType = MessageType> = {
+  [k in MessageType]: Omit<Messages[k], "type" | "time"> & { type: k; time: number };
+}[messageType];
+
+export type EmitMessage = <const messageType extends MessageType>(
+  type: messageType,
+  data: Messages[messageType],
+) => void;
