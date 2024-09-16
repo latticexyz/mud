fafc6d4846cecd35d909d5cba4c4defafa84f826
karooolis <hello@karolisram.com>
2024-09-16 14:53:33 +0300

    memoize wagmi config

--- packages/explorer/src/app/[chainName]/Providers.tsx
+++ packages/explorer/src/app/[chainName]/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { WagmiProvider, createConfig, http } from "wagmi";
 import { injected, metaMask, safe } from "wagmi/connectors";
-import { ReactNode } from "react";
+import { ReactNode, useMemo } from "react";
 import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -13,23 +13,25 @@ const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   const chain = useChain();
-  const wagmiConfig = createConfig({
-    chains: [chain],
-    connectors: [
-      injected(),
-      metaMask({
-        dappMetadata: {
-          name: "World Explorer",
-        },
-      }),
-      safe(),
-      ...getDefaultAnvilConnectors(chain.id),
-    ],
-    transports: {
-      [chain.id]: http(),
-    },
-    ssr: true,
-  });
+  const wagmiConfig = useMemo(() => {
+    return createConfig({
+      chains: [chain],
+      connectors: [
+        injected(),
+        metaMask({
+          dappMetadata: {
+            name: "World Explorer",
+          },
+        }),
+        safe(),
+        ...getDefaultAnvilConnectors(chain.id),
+      ],
+      transports: {
+        [chain.id]: http(),
+      },
+      ssr: true,
+    });
+  }, [chain]);
 
   return (
     <WagmiProvider config={wagmiConfig}>
