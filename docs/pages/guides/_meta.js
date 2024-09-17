1f3f0979d981ffe7816b3c3d8d4b4af15e78b599
ludens <ludens@lattice.xyz>
2024-09-17 17:38:26 +1200

    Apply suggestions from code review
    Co-authored-by: Kevin Ingersoll <kingersoll@gmail.com>

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -5,9 +5,5 @@ export default {
   "modules": "Writing MUD Modules",
   emojimon: "Emojimon",
   testing: "Testing",
-  "---": {
-    title: "", // no title renders as a line
-    type: "separator",
-  },  
   "replicating-onchain-state": "Replicating onchain state",
 };
