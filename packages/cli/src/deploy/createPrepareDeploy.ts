0bce659d32fe0fa5a6420f171eb39032d5c9cd47
alvrs <alvarius@lattice.xyz>
2024-09-19 13:23:47 +0100

    default prepareDeploy param

--- packages/cli/src/deploy/createPrepareDeploy.ts
+++ packages/cli/src/deploy/createPrepareDeploy.ts
@@ -7,8 +7,17 @@ export function createPrepareDeploy(
   bytecodeWithPlaceholders: Hex,
   placeholders: readonly LibraryPlaceholder[],
 ): DeterministicContract["prepareDeploy"] {
-  return function prepareDeploy(deployer: Address, libraryMap: LibraryMap) {
+  return function prepareDeploy(deployer: Address, libraryMap?: LibraryMap) {
     let bytecode = bytecodeWithPlaceholders;
+
+    if (placeholders.length === 0) {
+      return { bytecode, address: getCreate2Address({ from: deployer, bytecode, salt }) };
+    }
+
+    if (!libraryMap) {
+      throw new Error("Libraries must be provided if there are placeholders");
+    }
+
     for (const placeholder of placeholders) {
       const address = libraryMap.getAddress({ name: placeholder.name, path: placeholder.path, deployer });
       bytecode = spliceHex(bytecode, placeholder.start, placeholder.length, address);
