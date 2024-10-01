cb8215944e28af09b852b61960cf32e1744b4068
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-01 16:55:37 +0100

    pick another port

--- packages/store/ts/test/common.ts
+++ packages/store/ts/test/common.ts
@@ -1,7 +1,7 @@
 import { createTestClient, http } from "viem";
 
 export const anvilHost = "127.0.0.1";
-export const anvilPort = 8555;
+export const anvilPort = 8556;
 
 // ID of the current test worker. Used by the `@viem/anvil` proxy server.
 export const poolId = Number(process.env.VITEST_POOL_ID ?? 1);
