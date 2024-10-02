b7f57447b5a4e2e9dcb60711340e80415181d27e
alvrs <alvarius@lattice.xyz>
2024-10-02 11:49:32 +0100

    change error message

--- packages/store-sync/src/fromEventSource.ts
+++ packages/store-sync/src/fromEventSource.ts
@@ -4,7 +4,7 @@ export function fromEventSource<T>(url: string | URL): Observable<MessageEvent<T
   return new Observable<MessageEvent>((subscriber) => {
     const eventSource = new EventSource(url);
     eventSource.onmessage = (ev): void => subscriber.next(ev);
-    eventSource.onerror = (): void => subscriber.error(new Error("Unexpected error in event source: " + url));
+    eventSource.onerror = (): void => subscriber.error(new Error("Unexpected error in event source " + url));
     return () => eventSource.close();
   });
 }
