42cf12b1bbcdb9e0f5c749a8559d84c86f0f146f
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-06 13:41:56 +0100

    no wait on

--- test-setup/startAnvil.ts
+++ test-setup/startAnvil.ts
@@ -1,16 +1,5 @@
 import { startProxy as startAnvilProxy } from "@viem/anvil";
 import { anvilHost, anvilPort } from "./common";
-import { execa } from "execa";
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-console.log("building mock game");
-await execa("pnpm", ["run", "build"], {
-  cwd: `${__dirname}/../test/mock-game-contracts`,
-});
 
 console.log("starting anvil proxy");
 await startAnvilProxy({ host: anvilHost, port: anvilPort });
