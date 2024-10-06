c46145e98b1d7e1842bd21fd8a498630de58a073
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-06 10:59:41 +0100

    Revert "wip moving tests into its own package"
    This reverts commit 8e319e6dde441a778f6d3c3521e88ee519d14777.

--- packages/store/ts/exports/internal.ts
+++ packages/store/ts/exports/internal.ts
@@ -1,4 +1,3 @@
 export * from "../common";
 export * from "../getStoreLogs";
 export * from "../flattenStoreLogs";
-export * from "../storeLog";
--- test/store/README.md
+++
@@ -1,1 +0,0 @@
-Some of our tests in `store` package depend on other packages that would cause a circular dependency, so we've moved them here.
