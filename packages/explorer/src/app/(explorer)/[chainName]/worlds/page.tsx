665b880b6f193efe0929a068edf711b9febeec88
karooolis <hello@karolisram.com>
2024-10-07 11:35:48 +0300

    wip

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/page.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/page.tsx
@@ -1,61 +1,24 @@
-"use client";
+import { Address } from "viem";
+import { WorldsForm } from "./WorldsForm";
 
-import { useRouter } from "next/navigation";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "../../../../components/ui/Button";
-import { Form, FormControl, FormField, FormItem } from "../../../../components/ui/Form";
-import { Input } from "../../../../components/ui/Input";
+type ApiResponse = {
+  items: {
+    address: {
+      hash: Address;
+    };
+  }[];
+};
 
-const formSchema = z.object({
-  worldAddress: z.string().min(1, {
-    message: "World address is required.",
-  }),
-});
-
-export default function WorldsPage({ params }: Props) {
-  const router = useRouter();
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      worldAddress: "",
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    router.push(`/${params.chainName}/worlds/${values.worldAddress}`);
+async function fetchWorlds(): Promise<Address[]> {
+  const response = await fetch("https://explorer.redstone.xyz/api/v2/mud/worlds");
+  if (!response.ok) {
+    throw new Error("Failed to fetch worlds");
   }
+  const data: ApiResponse = await response.json();
+  return data.items.map((world) => world.address.hash);
+}
 
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-4">
-      <h1 className="mb-6 font-mono text-5xl font-bold uppercase">Worlds Explorer</h1>
-
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full max-w-md space-y-6">
-          <FormField
-            control={form.control}
-            name="worldAddress"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input placeholder="Enter world address" {...field} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-
-          <div className="flex w-full items-center gap-x-2">
-            <Button className="flex-1" variant="default">
-              Enter the world
-            </Button>
-            <Button className="flex-1" variant="secondary">
-              I&apos;m feeling lucky
-            </Button>
-          </div>
-        </form>
-      </Form>
-    </div>
-  );
+export default async function WorldsPage({ params }: { params: { chainName: string } }) {
+  const worlds = await fetchWorlds();
+  return <WorldsForm worlds={worlds} />;
 }
