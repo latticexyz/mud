e97d2eb1ddca6a3d102d90593ed23237f091d456
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-16 12:33:30 +0100

    update tsup config after merge

--- packages/entrykit/tsup.config.ts
+++ packages/entrykit/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 // import packageJson from "./package.json";
 
-export default defineConfig({
+export default defineConfig((opts) => ({
   outDir: "dist/tsup",
   entry: {
     index: "src/exports/index.ts",
@@ -9,13 +9,17 @@ export default defineConfig({
   },
   target: "esnext",
   format: ["esm"],
-  dts: !process.env.TSUP_SKIP_DTS,
   sourcemap: true,
-  clean: true,
   minify: true,
   // Because we're injecting CSS via shadow DOM, we'll disable style injection and load CSS as a base64 string.
   // TODO: figure out how to do this conditionally for only specific imports?
   injectStyle: false,
   loader: { ".css": "text" },
-  // external: Object.keys(packageJson.dependencies),
-});
+  // don't generate DTS during watch mode because it's slow
+  // we're likely using TS source in this mode anyway
+  dts: !opts.watch,
+  // don't clean during watch mode to avoid removing
+  // previously-built DTS files, which other build tasks
+  // depend on
+  clean: !opts.watch,
+}));
