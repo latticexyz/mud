61070d6e22722ee35ddd8a388763d7c289f42fcc
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-17 00:36:16 +0100

    almost works

--- packages/store/src/StoreSimulator.sol
+++ packages/store/src/StoreSimulator.sol
@@ -1,33 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
-import { IStore } from "./IStore.sol";
 import { Create2 } from "./vendor/Create2.sol";
+import { IStore } from "./IStore.sol";
+import { EncodedLengths } from "./EncodedLengths.sol";
+import { ResourceId } from "./ResourceId.sol";
+import { StoreCore } from "./StoreCore.sol";
 
 contract StoreProxy {
   address public store;
+  bytes[] private calls;
 
   constructor(address _store) {
     store = _store;
   }
 
+  function _calls() public view returns (bytes[] memory) {
+    return calls;
+  }
+
   fallback() external payable {
+    calls.push(msg.data);
     (bool success, bytes memory result) = store.call(msg.data);
-    require(success, "Forwarding call to Store failed");
+    if (!success) {
+      assembly {
+        let returndata_size := mload(result)
+        revert(add(result, 0x20), returndata_size)
+      }
+    }
     assembly {
       return(add(result, 0x20), mload(result))
     }
   }
 
   receive() external payable {
-    // Forward ETH to target if necessary
-    (bool success, ) = store.call{ value: msg.value }("");
-    require(success, "Forwarding ETH to Store failed");
+    (bool success, bytes memory result) = store.call{ value: msg.value }("");
+    if (!success) {
+      assembly {
+        let returndata_size := mload(result)
+        revert(add(result, 0x20), returndata_size)
+      }
+    }
   }
 }
 
 contract StoreSimulator {
-  error CallResult(bool success, bytes data);
+  error CallResult(bool success, bytes data, bytes[] calls);
 
   function getProxyAddress(address store) public view returns (address) {
     return Create2.computeAddress(0, keccak256(_getBytecode(store)));
@@ -36,7 +54,7 @@ contract StoreSimulator {
   function call(address store, bytes calldata callData) external {
     address proxy = Create2.deploy(0, 0, _getBytecode(store));
     (bool success, bytes memory data) = proxy.call(callData);
-    revert CallResult(success, data);
+    revert CallResult(success, data, StoreProxy(payable(proxy))._calls());
   }
 
   function _getBytecode(address store) internal pure returns (bytes memory) {
--- packages/store/ts/storeSimulator.test.ts
+++ packages/store/ts/storeSimulator.test.ts
@@ -2,15 +2,29 @@
 import { beforeAll, beforeEach, describe, it } from "vitest";
 import { snapshotAnvilState, testClient } from "../../../test-setup/common";
 import { deployMockGame, worldAbi } from "../../../test-setup/mockGame";
-import { Address, Hex, encodeFunctionData, stringToHex } from "viem";
+import {
+  Address,
+  ContractFunctionRevertedError,
+  DecodeErrorResultReturnType,
+  Hex,
+  SimulateContractErrorType,
+  decodeErrorResult,
+  decodeFunctionResult,
+  encodeFunctionData,
+  keccak256,
+  pad,
+  parseAbi,
+  stringToHex,
+  toBytes,
+} from "viem";
 import storeSimulatorArtifact from "../out/StoreSimulator.sol/StoreSimulator.json";
-import storeSimulatorAbi from "../out/StoreSimulator.sol/StoreSimulator.abi.json";
-import { simulateContract } from "viem/actions";
+import { readContract, simulateContract } from "viem/actions";
 
 const storeSimulator = stringToHex("StoreSimulator", { size: 20 });
 
 describe("storeSimulator", async () => {
   let worldAddress: Address;
+  let proxyAddress: Address;
 
   beforeAll(async () => {
     const resetAnvilState = await snapshotAnvilState();
@@ -19,6 +33,14 @@ describe("storeSimulator", async () => {
       address: storeSimulator,
       bytecode: storeSimulatorArtifact.deployedBytecode.object as Hex,
     });
+    proxyAddress = await readContract(testClient, {
+      address: storeSimulator,
+      abi: storeSimulatorAbi,
+      functionName: "getProxyAddress",
+      args: [worldAddress],
+    });
+    console.log("world address", worldAddress);
+    console.log("proxy address", proxyAddress);
     return resetAnvilState;
   });
   beforeEach(snapshotAnvilState);
@@ -35,8 +57,129 @@ describe("storeSimulator", async () => {
       abi: storeSimulatorAbi,
       functionName: "call",
       args: [worldAddress, callData],
+      // account: worldAddress,
+      stateOverride: [
+        {
+          address: worldAddress,
+          stateDiff: [
+            {
+              slot: keccak256(toBytes("mud.store.storage.StoreSwitch")),
+              value: pad(proxyAddress, { size: 32 }),
+            },
+          ],
+        },
+      ],
+    }).catch((e) => {
+      const error = e as SimulateContractErrorType;
+      // throw error;
+      if (error.name === "ContractFunctionExecutionError") {
+        if (error.cause instanceof ContractFunctionRevertedError) {
+          if (error.cause.data) {
+            const callResult = error.cause.data as DecodeErrorResultReturnType<storeSimulatorAbi>;
+            if (callResult.errorName === "CallResult") {
+              const [success, data, calls] = callResult.args;
+              console.log("got calls", calls);
+              if (success) {
+                return decodeFunctionResult({ abi: worldAbi, data });
+              } else {
+                return decodeErrorResult({ abi: worldAbi, data });
+              }
+            }
+          }
+        }
+      }
+      throw error;
     });
 
     console.log("got result", result);
+    console.dir(result, { depth: null });
   });
 });
+
+const storeSimulatorAbi = [
+  {
+    type: "function",
+    name: "call",
+    inputs: [
+      {
+        name: "store",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "callData",
+        type: "bytes",
+        internalType: "bytes",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "getProxyAddress",
+    inputs: [
+      {
+        name: "store",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "error",
+    name: "CallResult",
+    inputs: [
+      {
+        name: "success",
+        type: "bool",
+        internalType: "bool",
+      },
+      {
+        name: "data",
+        type: "bytes",
+        internalType: "bytes",
+      },
+      {
+        name: "calls",
+        type: "bytes[]",
+        internalType: "bytes[]",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "Create2EmptyBytecode",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "FailedDeployment",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "InsufficientBalance",
+    inputs: [
+      {
+        name: "balance",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "needed",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+  },
+] as const;
+type storeSimulatorAbi = typeof storeSimulatorAbi;
