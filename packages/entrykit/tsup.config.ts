d844186c513bd7ac27ee861fcfd815acb208fbad
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-16 13:26:22 +0100

    move shared deps to peer deps

--- packages/entrykit/tsup.config.ts
+++ packages/entrykit/tsup.config.ts
@@ -15,9 +15,7 @@ export default defineConfig((opts) => ({
   // TODO: figure out how to do this conditionally for only specific imports?
   injectStyle: false,
   loader: { ".css": "text" },
-  // don't generate DTS during watch mode because it's slow
-  // we're likely using TS source in this mode anyway
-  dts: !opts.watch,
+  dts: true,
   // don't clean during watch mode to avoid removing
   // previously-built DTS files, which other build tasks
   // depend on
