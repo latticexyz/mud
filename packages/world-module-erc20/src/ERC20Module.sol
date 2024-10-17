d6a335238791950ef6c83d37f3ae7d014958c653
Vicente Dragicevic <vdrg@protonmail.ch>
2024-10-17 16:38:52 -0300

    Move ERC20Registry registration to an external contract to decrease module contract size

--- packages/world-module-erc20/src/ERC20Module.sol
+++ packages/world-module-erc20/src/ERC20Module.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.24;
 
 import { ResourceIds } from "@latticexyz/store/src/codegen/tables/ResourceIds.sol";
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
-import { RESOURCE_TABLE } from "@latticexyz/store/src/storeResourceTypes.sol";
 
 import { Module } from "@latticexyz/world/src/Module.sol";
 import { WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
@@ -17,6 +16,8 @@ import { ModuleConstants } from "./Constants.sol";
 contract ERC20Module is Module {
   error ERC20Module_InvalidNamespace(bytes14 namespace);
   error ERC20Module_NamespaceAlreadyExists(bytes14 namespace);
+
+  ERC20RegistryLib public immutable registryLib = new ERC20RegistryLib();
 
   function install(bytes memory encodedArgs) public {
     // TODO: we should probably check just for namespace, not for all args
@@ -35,25 +36,39 @@ contract ERC20Module is Module {
 
     IBaseWorld world = IBaseWorld(_world());
 
-    ResourceId erc20RegistryTableId = ModuleConstants.registryTableId();
-    if (!ResourceIds.getExists(erc20RegistryTableId)) {
-      world.registerNamespace(ModuleConstants.namespaceId());
-      ERC20Registry.register(erc20RegistryTableId);
-    }
-
     ERC20WithWorld token = new ERC20WithWorld(world, namespace, name, symbol);
 
     // Grant access to the token so it can write to tables after transferring ownership
     world.grantAccess(namespaceId, address(token));
 
-    // Register the ERC20 in the ERC20Registry
-    ERC20Registry.set(erc20RegistryTableId, namespaceId, address(token));
-
     // The token should have transferred the namespace ownership to this module in its constructor
     world.transferOwnership(namespaceId, _msgSender());
+
+    registryLib.delegateRegister(world, namespaceId, address(token));
   }
 
   function installRoot(bytes memory) public pure {
     revert Module_RootInstallNotSupported();
   }
 }
+
+contract ERC20RegistryLib {
+  function register(IBaseWorld world, ResourceId namespaceId, address token) public {
+    ResourceId erc20RegistryTableId = ModuleConstants.registryTableId();
+    if (!ResourceIds.getExists(erc20RegistryTableId)) {
+      world.registerNamespace(ModuleConstants.namespaceId());
+      ERC20Registry.register(erc20RegistryTableId);
+    }
+    // Register the ERC20 in the ERC20Registry
+    ERC20Registry.set(erc20RegistryTableId, namespaceId, address(token));
+  }
+}
+
+function delegateRegister(ERC20RegistryLib lib, IBaseWorld world, ResourceId namespaceId, address token) {
+  (bool success, bytes memory returnData) = address(lib).delegatecall(
+    abi.encodeCall(ERC20RegistryLib.register, (world, namespaceId, token))
+  );
+  if (!success) revertWithBytes(returnData);
+}
+
+using { delegateRegister } for ERC20RegistryLib;
