9ba07bd9252be098f077f086e4abc66c70beec57
V <vdrg@protonmail.ch>
2024-10-18 09:09:41 -0300

    Update packages/world-module-erc20/src/Context.sol
    Co-authored-by: alvarius <alvarius@lattice.xyz>

--- packages/world-module-erc20/src/Context.sol
+++ packages/world-module-erc20/src/Context.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.24;
 
 // @dev Provides information about the current execution context
 // We only use it in these contracts in case we want to extend it in the future
