8f06167bd9cb8acd7b1c241820d675f12a1a636e
karooolis <hello@karolisram.com>
2024-10-21 10:06:17 +0300

    use query columns from ast

--- packages/explorer/src/app/(explorer)/queries/useTableDataQuery.ts
+++ packages/explorer/src/app/(explorer)/queries/useTableDataQuery.ts
@@ -1,4 +1,5 @@
 import { useParams } from "next/navigation";
+import { Parser } from "node-sql-parser";
 import { Hex } from "viem";
 import { Table } from "@latticexyz/config";
 import { useQuery } from "@tanstack/react-query";
@@ -10,6 +11,8 @@ type Props = {
   table: Table | undefined;
   query: string | undefined;
 };
+
+const parser = new Parser();
 
 export type TableData = {
   columns: string[];
@@ -47,14 +50,9 @@ export function useTableDataQuery({ table, query }: Props) {
     select: (data: DozerResponse) => {
       if (!table || !data?.result?.[0]) return;
 
-      const schemaKeys = Object.keys(table.schema);
+      const parsedQuery = parser.astify(query);
+      const columnKeys = parsedQuery.columns.map((column) => column.expr.column);
       const result = data.result[0];
-      const columnKeys = result[0]
-        .map((columnKey) => {
-          const schemaKey = schemaKeys.find((schemaKey) => schemaKey.toLowerCase() === columnKey);
-          return schemaKey || columnKey;
-        })
-        .filter((key) => schemaKeys.includes(key));
       const rows = result.slice(1).map((row) => Object.fromEntries(columnKeys.map((key, index) => [key, row[index]])));
 
       return {
