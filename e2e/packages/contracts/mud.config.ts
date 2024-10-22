d1d4376c363862e2be3ff8ef840f8e92a9bce758
Vicente Dragicevic <vdrg@protonmail.ch>
2024-10-22 11:21:43 -0300

    Add e2e module test package

--- e2e/packages/contracts/mud.config.ts
+++ e2e/packages/contracts/mud.config.ts
@@ -1,29 +1,5 @@
 import { defineWorld } from "@latticexyz/world";
-// import { defineERC20Config } from "@latticexyz/world-module-erc20";
-// import { encodeAbiParameters, stringToHex } from "viem";
-//
-// const erc20ModuleArgs = encodeAbiParameters(
-//   [
-//     { type: "bytes14" },
-//     {
-//       type: "tuple",
-//       components: [{ type: "uint8" }, { type: "string" }, { type: "string" }],
-//     },
-//   ],
-//   [stringToHex("MyToken", { size: 14 }), [18, "Worthless Token", "WT"]],
-// );
-//
-// const erc721ModuleArgs = encodeAbiParameters(
-//   [
-//     { type: "bytes14" },
-//     {
-//       type: "tuple",
-//       components: [{ type: "string" }, { type: "string" }, { type: "string" }],
-//     },
-//   ],
-//   [stringToHex("MyNFT", { size: 14 }), ["No Valuable Token", "NVT", "http://www.example.com/base/uri/goes/here"]],
-// );
-//
+
 export default defineWorld({
   tables: {
     Number: {
@@ -95,35 +71,5 @@ export default defineWorld({
         "@latticexyz/world-modules/out/Unstable_CallWithSignatureModule.sol/Unstable_CallWithSignatureModule.json",
       root: true,
     },
-    // {
-    //   artifactPath: "@latticexyz/world-modules/out/PuppetModule.sol/PuppetModule.json",
-    //   root: false,
-    //   args: [],
-    // },
-    // {
-    //   artifactPath: "@latticexyz/world-modules/out/ERC20Module.sol/ERC20Module.json",
-    //   root: false,
-    //   args: [
-    //     {
-    //       type: "bytes",
-    //       value: erc20ModuleArgs,
-    //     },
-    //   ],
-    // },
-    // {
-    //   artifactPath: "@latticexyz/world-modules/out/ERC721Module.sol/ERC721Module.json",
-    //   root: false,
-    //   args: [
-    //     {
-    //       type: "bytes",
-    //       value: erc721ModuleArgs,
-    //     },
-    //   ],
-    // },
-    // defineERC20Config({
-    //   namespace: "erc20Namespace",
-    //   name: "MyToken",
-    //   symbol: "MTK",
-    // }),
   ],
 });
