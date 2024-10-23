a8f871a90230a492125d203599d4499653eccd05
Kevin Ingersoll <kingersoll@gmail.com>
2024-10-23 10:59:34 +0100

    remove logs

--- packages/store-sync/src/createStoreSync.ts
+++ packages/store-sync/src/createStoreSync.ts
@@ -240,7 +240,6 @@ export async function createStoreSync({
           return fromEventSource<string>(url);
         }),
         map((messageEvent) => {
-          console.log("got message event", messageEvent);
           const data = JSON.parse(messageEvent.data);
           if (!isLogsApiResponse(data)) {
             throw new Error("Received unexpected from indexer:" + messageEvent.data);
@@ -291,8 +290,6 @@ export async function createStoreSync({
     tap(async ({ logs, blockNumber }) => {
       debug("stored", logs.length, "logs for block", blockNumber);
       lastBlockNumberProcessed = blockNumber;
-
-      console.log({ caughtUp, startBlock, latestBlockNumber });
 
       if (!caughtUp && startBlock != null && latestBlockNumber != null) {
         if (lastBlockNumberProcessed < latestBlockNumber) {
