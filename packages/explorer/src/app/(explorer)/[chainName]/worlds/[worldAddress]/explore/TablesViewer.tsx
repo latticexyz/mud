bf1f103831771c1f5cbc58a6a5827476859bd540
karooolis <hello@karolisram.com>
2024-10-24 09:46:23 +0300

    only show data if no error

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/explore/TablesViewer.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/explore/TablesViewer.tsx
@@ -125,22 +125,23 @@ export function TablesViewer({ table, query }: { table?: TableType; query?: stri
           <div className="relative w-full overflow-auto">
             <Table>
               <TableHeader>
-                {reactTable.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <TableHead key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(header.column.columnDef.header, header.getContext())}
-                        </TableHead>
-                      );
-                    })}
-                  </TableRow>
-                ))}
+                {!isError &&
+                  reactTable.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                      {headerGroup.headers.map((header) => {
+                        return (
+                          <TableHead key={header.id}>
+                            {header.isPlaceholder
+                              ? null
+                              : flexRender(header.column.columnDef.header, header.getContext())}
+                          </TableHead>
+                        );
+                      })}
+                    </TableRow>
+                  ))}
               </TableHeader>
               <TableBody>
-                {reactTable.getRowModel().rows?.length ? (
+                {!isError && reactTable.getRowModel().rows?.length ? (
                   reactTable.getRowModel().rows.map((row) => (
                     <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
                       {row.getVisibleCells().map((cell) => (
