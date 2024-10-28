11a9ce39bf38358c29d1ebb52b20f0cfdff6ec9d
karooolis <hello@karolisram.com>
2024-10-28 12:12:44 +0200

    undo dts flag

--- packages/explorer/tsup.config.ts
+++ packages/explorer/tsup.config.ts
@@ -9,7 +9,7 @@ export default defineConfig((opts) => ({
   minify: false,
   // don't generate DTS during watch mode because it's slow
   // we're likely using TS source in this mode anyway
-  dts: false, // !opts.watch,
+  dts: !opts.watch,
   // don't clean during watch mode to avoid removing
   // previously-built DTS files, which other build tasks
   // depend on
