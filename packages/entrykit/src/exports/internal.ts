1dfd769f4547d1cf0ed74c992f8b63500a7d1948
Kevin Ingersoll <kingersoll@gmail.com>
2024-11-01 14:34:36 +0000

    remove wiresaw export

--- packages/entrykit/src/exports/internal.ts
+++ packages/entrykit/src/exports/internal.ts
@@ -17,6 +17,5 @@ export * from "../passkey/passkeyWallet";
 export * from "../passkey/passkeyConnector";
 export * from "../smart-account/toCoinbaseSmartAccount";
 export * from "../smart-account/isCoinbaseSmartAccount";
-export * from "../wiresaw";
 export * from "../getConnectors";
 export * from "../getWallets";
