ec145c7bbeae364c568ff3c5dad268d6f2740140
karooolis <hello@karolisram.com>
2024-11-07 11:20:56 +0700

    handle request quota limit

--- docs/app/api/stargazers/route.ts
+++ docs/app/api/stargazers/route.ts
@@ -9,34 +9,39 @@ const octokit = new Octokit();
 // };
 
 export async function GET() {
-  const {
-    data: { stargazers_count: totalStars },
-  } = await octokit.request("GET /repos/{owner}/{repo}", {
-    owner: "latticexyz",
-    repo: "mud",
-  });
+  try {
+    const {
+      data: { stargazers_count: totalStars },
+    } = await octokit.request("GET /repos/{owner}/{repo}", {
+      owner: "latticexyz",
+      repo: "mud",
+    });
 
-  // TODO: bring back?
-  // const totalPages = Math.ceil(totalStars / 100);
-  // let allStargazers: Stargazer[] = [];
-  // for (let page = 0; page < totalPages; page++) {
-  //   const { data: stargazers } = await octokit.request("GET /repos/{owner}/{repo}/stargazers", {
-  //     owner: "latticexyz",
-  //     repo: "mud",
-  //     headers: {
-  //       accept: "application/vnd.github.star+json",
-  //     },
-  //     per_page: 100,
-  //     page: page + 1,
-  //   });
+    // TODO: bring back?
+    // const totalPages = Math.ceil(totalStars / 100);
+    // let allStargazers: Stargazer[] = [];
+    // for (let page = 0; page < totalPages; page++) {
+    //   const { data: stargazers } = await octokit.request("GET /repos/{owner}/{repo}/stargazers", {
+    //     owner: "latticexyz",
+    //     repo: "mud",
+    //     headers: {
+    //       accept: "application/vnd.github.star+json",
+    //     },
+    //     per_page: 100,
+    //     page: page + 1,
+    //   });
 
-  //   const stargazersWithTotal = stargazers.map((star, index) => ({
-  //     timestamp: new Date(star.starred_at),
-  //     total: allStargazers.length + index + 1,
-  //   }));
+    //   const stargazersWithTotal = stargazers.map((star, index) => ({
+    //     timestamp: new Date(star.starred_at),
+    //     total: allStargazers.length + index + 1,
+    //   }));
 
-  //   allStargazers = [...allStargazers, ...stargazersWithTotal];
-  // }
+    //   allStargazers = [...allStargazers, ...stargazersWithTotal];
+    // }
 
-  return Response.json(totalStars);
+    return Response.json(totalStars);
+  } catch (error) {
+    console.error(error);
+    return Response.json(0);
+  }
 }
