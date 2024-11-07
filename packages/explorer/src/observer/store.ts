cb633c405b73c39da54517d10ce0e4060fbc5e3b
alvrs <alvarius@lattice.xyz>
2024-11-07 14:05:56 +0700

    feat: buffer transactions

--- packages/explorer/src/observer/store.ts
+++ packages/explorer/src/observer/store.ts
@@ -31,37 +31,61 @@ export const store = createStore<State>(() => ({
 }));
 
 debug("listening for relayed messages", relayChannelName);
+const messages: MessageEvent<Message>[] = [];
 const channel = new BroadcastChannel(relayChannelName);
-channel.addEventListener("message", ({ data }: MessageEvent<Message>) => {
-  if (data.type === "ping") return;
-  store.setState((state) => {
-    const write = data.type === "write" ? ({ ...data, events: [] } satisfies Write) : state.writes[data.writeId];
-    if (!write) return state;
+channel.addEventListener("message", (message: MessageEvent<Message>) => {
+  if (message.data.type === "ping") return;
+  messages.push(message);
+});
 
-    let hash = write.hash;
-    if (data.type === "waitForTransactionReceipt") {
-      hash = data.hash;
-    } else if (
-      data.type === "waitForUserOperationReceipt:result" &&
-      isPromiseFulfilled<
-        Messages["waitForUserOperationReceipt:result"] extends PromiseSettledResult<infer T> ? T : never
-      >(data)
-    ) {
-      hash = data.value.receipt.transactionHash;
+function processMessages(): void {
+  if (messages.length === 0) return;
+
+  store.setState((state) => {
+    let updated = state;
+
+    for (const { data } of messages) {
+      if (data.type === "ping") continue;
+
+      const write = data.type === "write" ? ({ ...data, events: [] } satisfies Write) : state.writes[data.writeId];
+      if (!write) continue;
+
+      let hash = write.hash;
+      if (data.type === "waitForTransactionReceipt") {
+        hash = data.hash;
+      } else if (
+        data.type === "waitForUserOperationReceipt:result" &&
+        isPromiseFulfilled<
+          Messages["waitForUserOperationReceipt:result"] extends PromiseSettledResult<infer T> ? T : never
+        >(data)
+      ) {
+        hash = data.value.receipt.transactionHash;
+      }
+
+      console.log("processMessages", data.type, data.writeId, hash);
+
+      updated = {
+        ...updated,
+        writes: {
+          ...updated.writes,
+          [data.writeId]: {
+            ...write,
+            type: data.type,
+            hash,
+            userOpHash: data.type === "waitForUserOperationReceipt" ? data.userOpHash : write.userOpHash,
+            events: [...write.events, data],
+          },
+        },
+      };
     }
 
-    return {
-      ...state,
-      writes: {
-        ...state.writes,
-        [data.writeId]: {
-          ...write,
-          type: data.type,
-          hash,
-          userOpHash: data.type === "waitForUserOperationReceipt" ? data.userOpHash : write.userOpHash,
-          events: [...write.events, data],
-        },
-      },
-    };
+    // Clear messages after processing
+    messages.length = 0;
+    console.log("updated", updated);
+    return updated;
+    // return state;
   });
-});
+}
+
+const processInterval = 100;
+setInterval(processMessages, processInterval);
