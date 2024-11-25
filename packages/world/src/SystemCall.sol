1d0094e789dce605888ae6d3279a67d868a7f4e2
Vicente Dragicevic <3252614+vdrg@users.noreply.github.com>
2024-11-25 11:45:40 -0300

    Add more context to the comment about staticcalls not supported for root systems

--- packages/world/src/SystemCall.sol
+++ packages/world/src/SystemCall.sol
@@ -94,7 +94,8 @@ library SystemCall {
     // Check if the system exists
     if (systemAddress == address(0)) revert IWorldErrors.World_ResourceNotFound(systemId, systemId.toString());
 
-    // Staticcalls are not supported for root systems
+    // Staticcalls are not supported for root systems yet, as it would require a runtime check
+    // that we are in the context of a staticcall
     if (systemId.getNamespace() == ROOT_NAMESPACE) revert IWorldErrors.World_InvalidNamespace(ROOT_NAMESPACE);
 
     // Allow access if the system is public or the caller has access to the namespace or name
