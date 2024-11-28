ad02d17b97ac3032b213f38351481bbf1a82b04a
V <vdrg@protonmail.ch>
2024-11-28 14:28:14 -0300

    test(world-module-erc20): rename ERC20BaseTest.sol to ERC20BaseTest.t.sol (#3347)

--- packages/world-module-erc20/test/ERC20Pausable.t.sol
+++ packages/world-module-erc20/test/ERC20Pausable.t.sol
@@ -12,7 +12,7 @@ import { IERC20Errors } from "../src/interfaces/IERC20Errors.sol";
 import { IERC20Events } from "../src/interfaces/IERC20Events.sol";
 import { MUDERC20 } from "../src/experimental/MUDERC20.sol";
 import { Pausable, ERC20Pausable } from "../src/experimental/ERC20Pausable.sol";
-import { MockERC20Base, MockERC20WithInternalStore, MockERC20WithWorld, ERC20BehaviorTest, ERC20WithInternalStoreBehaviorTest, ERC20WithWorldBehaviorTest } from "./ERC20BaseTest.sol";
+import { MockERC20Base, MockERC20WithInternalStore, MockERC20WithWorld, ERC20BehaviorTest, ERC20WithInternalStoreBehaviorTest, ERC20WithWorldBehaviorTest } from "./ERC20BaseTest.t.sol";
 
 // Mock to include mint and burn functions
 abstract contract MockERC20Pausable is MUDERC20, ERC20Pausable {
