b0b42a9e0cca0e31fb31d1e00b78c3015b6f5663
Karolis Ramanauskas <hello@karolisram.com>
2024-12-13 09:13:32 +0200

    feat(explorer): export table data (#3380)

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/explore/TablesViewer.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/explore/TablesViewer.tsx
@@ -20,6 +20,7 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from ".
 import { cn } from "../../../../../../utils";
 import { TData, TDataRow, useTableDataQuery } from "../../../../queries/useTableDataQuery";
 import { EditableTableCell } from "./EditableTableCell";
+import { ExportButton } from "./ExportButton";
 import { typeSortingFn } from "./utils/typeSortingFn";
 
 const initialSortingState: SortingState = [];
@@ -105,6 +106,8 @@ export function TablesViewer({ table, query }: { table?: TableType; query?: stri
           className="max-w-sm rounded border px-2 py-1"
           disabled={!tableData}
         />
+
+        <ExportButton tableData={tableData} isLoading={isLoading} />
       </div>
 
       <div
