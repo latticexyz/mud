5d3b3477fb544fe1bce2ffd65f70c6f71d339e54
alvrs <alvarius@lattice.xyz>
2025-01-07 15:08:43 +0000

    add package boilerplate

---
+++ packages/paymaster/README.md
@@ -0,0 +1,3 @@
+# Paymaster Contracts
+
+> :warning: **Important note: these contracts have not been audited yet, so any production use is discouraged for now.**
---
+++ packages/paymaster/test/GenerousPaymaster.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.24;
+
+import { Test } from "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
+
+contract GenerousPaymasterTest is Test {}
