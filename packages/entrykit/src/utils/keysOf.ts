ebc6de39e93c85ec78497f7f6067ef221de93a9d
Kevin Ingersoll <kingersoll@gmail.com>
2025-01-09 11:49:02 +0000

    rm unused stuff

--- packages/entrykit/src/utils/keysOf.ts
+++
@@ -1,7 +0,0 @@
-import { unionToTuple } from "@ark/util";
-
-export type keysOf<T extends object> = unionToTuple<keyof T>;
-
-export function keysOf<T extends object>(value: T): keysOf<T> {
-  return Object.keys(value) as never;
-}
--- packages/entrykit/src/utils/valuesOf.ts
+++
@@ -1,7 +0,0 @@
-import { unionToTuple } from "@ark/util";
-
-export type valuesOf<T extends object> = unionToTuple<T[keyof T]>;
-
-export function valuesOf<T extends object>(value: T): valuesOf<T> {
-  return Object.values(value) as never;
-}
