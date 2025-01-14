1f44a99fe1d21fe14d523ec4764e81a0c7f1ec6c
Kevin Ingersoll <kingersoll@gmail.com>
2025-01-14 14:05:16 +0000

    pass through all sync options

--- templates/react/packages/client/src/mud/SyncProvider.tsx
+++ templates/react/packages/client/src/mud/SyncProvider.tsx
@@ -1,24 +1,21 @@
-import { createContext, ReactNode, useContext, useEffect } from "react";
+import { ReactNode, createContext, useContext, useEffect } from "react";
 import { useConfig } from "wagmi";
 import { getClient } from "wagmi/actions";
 import { useQuery } from "@tanstack/react-query";
-import { SyncAdapter, SyncResult } from "@latticexyz/store-sync";
-import { Address } from "viem";
+import { SyncAdapter, SyncOptions, SyncResult } from "@latticexyz/store-sync";
 
 /** @internal */
 export const SyncContext = createContext<{
   sync?: SyncResult;
 } | null>(null);
 
-export type Props = {
+export type Props = Omit<SyncOptions, "publicClient"> & {
   chainId: number;
-  address: Address;
-  startBlock?: bigint;
   adapter: SyncAdapter;
   children: ReactNode;
 };
 
-export function SyncProvider({ chainId, address, startBlock, adapter, children }: Props) {
+export function SyncProvider({ chainId, adapter, children, ...syncOptions }: Props) {
   const existingValue = useContext(SyncContext);
   if (existingValue != null) {
     throw new Error("A `SyncProvider` cannot be nested inside another.");
@@ -34,7 +31,7 @@ export function SyncProvider({ chainId, address, startBlock, adapter, children }
         throw new Error(`Unable to retrieve Viem client for chain ${chainId}.`);
       }
 
-      return adapter({ publicClient: client, address, startBlock });
+      return adapter({ publicClient: client, ...syncOptions });
     },
     staleTime: Infinity,
     refetchOnMount: false,
