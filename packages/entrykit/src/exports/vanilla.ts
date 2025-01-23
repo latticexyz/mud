b0e8763272e501ed3012b490eb40b64da20393a9
Kevin Ingersoll <kingersoll@gmail.com>
2025-01-23 21:19:31 +0000

    add vanilla build

--- packages/entrykit/src/exports/vanilla.ts
+++ packages/entrykit/src/exports/vanilla.ts
@@ -1,6 +1,6 @@
-export { mount, type MountOptions } from "../bundle/mount";
-export { mountButton } from "../bundle/mountButton";
+export { mount, type MountOptions } from "../vanilla/mount";
+export { mountButton } from "../vanilla/mountButton";
 
 // TODO: export readonly variant?
 // TODO: upgrade zustand so we can get the readonly variant
-export { store, type Store } from "../bundle/store";
+export { store, type Store } from "../vanilla/store";
--- packages/entrykit/tsup.config.ts
+++ packages/entrykit/tsup.config.ts
@@ -1,20 +1,34 @@
-import { defineConfig } from "tsup";
-// import packageJson from "./package.json";
+import { Options, defineConfig } from "tsup";
 
-export default defineConfig((opts) => ({
-  outDir: "dist/tsup",
-  entry: ["src/exports/index.ts", "src/exports/internal.ts", "src/bin/deploy-local-prereqs.ts"],
-  target: "esnext",
-  format: ["esm"],
-  sourcemap: true,
-  minify: true,
-  // Because we're injecting CSS via shadow DOM, we'll disable style injection and load CSS as a base64 string.
-  // TODO: figure out how to do this conditionally for only specific imports?
-  injectStyle: false,
-  loader: { ".css": "text" },
-  dts: true,
-  // don't clean during watch mode to avoid removing
-  // previously-built DTS files, which other build tasks
-  // depend on
-  clean: !opts.watch,
-}));
+export default defineConfig((opts) => {
+  const commonConfig: Options = {
+    target: "esnext",
+    format: ["esm"],
+    sourcemap: true,
+    minify: true,
+    loader: { ".css": "text" },
+    // don't generate DTS during watch mode because it's slow
+    // we're likely using TS source in this mode anyway
+    dts: !opts.watch,
+    // don't clean during watch mode to avoid removing
+    // previously-built DTS files, which other build tasks
+    // depend on
+    clean: !opts.watch,
+
+    // Because we're injecting CSS via shadow DOM, we'll disable style injection and load CSS as a base64 string.
+    // TODO: figure out how to do this conditionally for only specific imports?
+    injectStyle: false,
+  };
+
+  return [
+    {
+      ...commonConfig,
+      entry: ["src/exports/index.ts", "src/exports/internal.ts", "src/bin/deploy-local-prereqs.ts"],
+    },
+    {
+      ...commonConfig,
+      entry: ["src/exports/vanilla.ts"],
+      noExternal: ["react", "react-dom", "@tanstack/react-query"],
+    },
+  ];
+});
