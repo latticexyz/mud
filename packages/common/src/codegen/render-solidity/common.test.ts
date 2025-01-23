27ca5d1c1dd71d350aacf0bb2e8ef71f876cf564
JamesLefrere <jlefrere@gmail.com>
2025-01-23 15:59:29 +0100

    chore: standardise dev dependencies
    - Replace common workspace package devDependencies (where possible without major modifications) with the workspace root's devDependencies
    - Update some error message snapshots for workspace vitest version

--- packages/common/src/codegen/render-solidity/common.test.ts
+++ packages/common/src/codegen/render-solidity/common.test.ts
@@ -62,8 +62,8 @@ describe("renderTableId", () => {
   it("returns Solidity code to compute table ID", () => {
     expect(renderTableId({ namespace: "somewhere", name: "Player", offchainOnly: false })).toMatchInlineSnapshot(`
       "
-          // Hex below is the result of \`WorldResourceIdLib.encode({ namespace: \\"somewhere\\", name: \\"Player\\", typeId: RESOURCE_TABLE });\`
-          ResourceId constant _tableId = ResourceId.wrap(0x7462736f6d6577686572650000000000506c6179657200000000000000000000);
+          // Hex below is the result of \`WorldResourceIdLib.encode({ namespace: "somewhere", name: "Player", typeId: RESOURCE_TABLE });\`
+    ResourceId constant _tableId = ResourceId.wrap(0x7462736f6d6577686572650000000000506c6179657200000000000000000000);
         "
     `);
   });
@@ -71,7 +71,7 @@ describe("renderTableId", () => {
   it("returns Solidity code to compute offchain table ID", () => {
     expect(renderTableId({ namespace: "somewhere", name: "Player", offchainOnly: true })).toMatchInlineSnapshot(`
       "
-          // Hex below is the result of \`WorldResourceIdLib.encode({ namespace: \\"somewhere\\", name: \\"Player\\", typeId: RESOURCE_OFFCHAIN_TABLE });\`
+          // Hex below is the result of \`WorldResourceIdLib.encode({ namespace: "somewhere", name: "Player", typeId: RESOURCE_OFFCHAIN_TABLE });\`
           ResourceId constant _tableId = ResourceId.wrap(0x6f74736f6d6577686572650000000000506c6179657200000000000000000000);
         "
     `);
