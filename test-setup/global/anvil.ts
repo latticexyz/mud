90aac1d4acce19ac592d47a090732dd11c1c3e7a
Kevin Ingersoll <kingersoll@gmail.com>
2025-01-31 14:44:22 +0000

    test: move anvil to vitest global setup hook (#3526)

---
+++ test-setup/global/anvil.ts
@@ -0,0 +1,21 @@
+import { startProxy as startAnvilProxy } from "@viem/anvil";
+import { anvilHost, anvilPort } from "../common";
+import { execa } from "execa";
+
+// ensure anvil dies
+// TODO: maybe don't need this once we move to prool
+process.on("SIGINT", () => process.exit());
+process.on("SIGTERM", () => process.exit());
+process.on("SIGQUIT", () => process.exit());
+
+export default async () => {
+  console.log("building mock game contracts");
+  await execa("pnpm", ["build"], {
+    cwd: `${__dirname}/../../test/mock-game-contracts`,
+    stdout: ["inherit"],
+    stderr: ["inherit"],
+  });
+
+  console.log("starting anvil proxy");
+  return startAnvilProxy({ host: anvilHost, port: anvilPort });
+};
