1e23474f74d706ee8ab411de5104d1e7847870e0
vdrg <vdrg@protonmail.ch>
2025-02-06 12:38:00 -0300

    Remove internal store related tests, update readme

--- packages/world-module-erc20/src/examples/ERC20PausableBurnable.sol
+++ packages/world-module-erc20/src/examples/ERC20PausableBurnable.sol
@@ -5,20 +5,19 @@ pragma solidity >=0.8.24;
 
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
-import { WithWorld } from "@latticexyz/store-consumer/src/experimental/WithWorld.sol";
-import { Context } from "@latticexyz/store-consumer/src/experimental/Context.sol";
+import { WorldConsumer } from "@latticexyz/world-consumer/src/experimental/WorldConsumer.sol";
 
 import { ERC20Pausable } from "../experimental/ERC20Pausable.sol";
 import { ERC20Burnable } from "../experimental/ERC20Burnable.sol";
 import { MUDERC20 } from "../experimental/MUDERC20.sol";
 
-contract ERC20WithWorld is WithWorld, MUDERC20, ERC20Pausable, ERC20Burnable {
+contract ERC20PausableBurnable is MUDERC20, ERC20Pausable, ERC20Burnable {
   constructor(
     IBaseWorld world,
     bytes14 namespace,
     string memory name,
     string memory symbol
-  ) WithWorld(world, namespace, true) MUDERC20(name, symbol) {
+  ) WorldConsumer(world, namespace, true) MUDERC20(name, symbol) {
     // Transfer namespace ownership to the creator
     world.transferOwnership(getNamespaceId(), _msgSender());
   }
@@ -36,10 +35,6 @@ contract ERC20WithWorld is WithWorld, MUDERC20, ERC20Pausable, ERC20Burnable {
   }
 
   // The following functions are overrides required by Solidity.
-
-  function _msgSender() public view override(Context, WithWorld) returns (address sender) {
-    return WithWorld._msgSender();
-  }
 
   function _update(address from, address to, uint256 value) internal override(MUDERC20, ERC20Pausable) {
     super._update(from, to, value);
