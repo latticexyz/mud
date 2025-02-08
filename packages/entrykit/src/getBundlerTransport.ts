9de00030969dff4beb772b7d8ff2101c9a46dec3
Kevin Ingersoll <kingersoll@gmail.com>
2025-02-08 23:53:52 +0000

    reuse client transport in local bundler

--- packages/entrykit/src/getBundlerTransport.ts
+++ packages/entrykit/src/getBundlerTransport.ts
@@ -1,25 +1,25 @@
 import { transactionQueue } from "@latticexyz/common/actions";
-import { Chain, createClient, fallback, http, keccak256, stringToHex, webSocket } from "viem";
+import { Chain, Client, Transport, createClient, http, keccak256, stringToHex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { userOpExecutor } from "./quarry/transports/userOpExecutor";
 
-export function getBundlerTransport(chain: Chain) {
-  const bundlerHttpUrl = chain.rpcUrls.bundler?.http[0];
+export function getBundlerTransport(client: Client<Transport, Chain>) {
+  const bundlerHttpUrl = client.chain.rpcUrls.bundler?.http[0];
   // TODO: bundler websocket
   const bundlerTransport = bundlerHttpUrl
     ? http(bundlerHttpUrl)
-    : chain.id === 31337
+    : client.chain.id === 31337
       ? userOpExecutor({
           executor: createClient({
-            chain,
-            transport: fallback([webSocket(), http()]),
+            chain: client.chain,
+            transport: () => ({ config: client.transport, request: client.request }),
             account: privateKeyToAccount(keccak256(stringToHex("local user op executor"))),
             pollingInterval: 10,
           }).extend(transactionQueue()),
         })
       : null;
   if (!bundlerTransport) {
-    throw new Error(`Chain ${chain.id} config did not include a bundler RPC URL.`);
+    throw new Error(`Chain ${client.chain.id} config did not include a bundler RPC URL.`);
   }
   return bundlerTransport;
 }
