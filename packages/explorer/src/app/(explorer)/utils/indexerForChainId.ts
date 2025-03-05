8ad26ff241b5e4f65588855993d887b8eeea4fd1
karooolis <hello@karolisram.com>
2025-03-05 18:15:31 +0200

    undo redundant changes

--- packages/explorer/src/app/(explorer)/utils/indexerForChainId.ts
+++ packages/explorer/src/app/(explorer)/utils/indexerForChainId.ts
@@ -1,15 +1,15 @@
-import { validateChainId } from "../../../common";
+import { anvil } from "viem/chains";
+import { MUDChain } from "@latticexyz/common/chains";
+import { chainIdToName, supportedChains, validateChainId } from "../../../common";
 
 export function indexerForChainId(chainId: number): { type: "sqlite" | "hosted"; url: string } {
   validateChainId(chainId);
 
-  return { type: "hosted", url: new URL("/q", "http://0.0.0.0:8000").toString() };
+  if (chainId === anvil.id) {
+    return { type: "sqlite", url: "/api/sqlite-indexer" };
+  }
 
-  // if (chainId === anvil.id) {
-  //   return { type: "sqlite", url: "/api/sqlite-indexer" };
-  // }
-
-  // const chainName = chainIdToName[chainId];
-  // const chain = supportedChains[chainName] as MUDChain;
-  // return { type: "hosted", url: new URL("/q", chain.indexerUrl).toString() };
+  const chainName = chainIdToName[chainId];
+  const chain = supportedChains[chainName] as MUDChain;
+  return { type: "hosted", url: new URL("/q", chain.indexerUrl).toString() };
 }
