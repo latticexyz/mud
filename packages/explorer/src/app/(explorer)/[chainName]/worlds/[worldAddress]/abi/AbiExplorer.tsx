91610d760dd98f10783fd460e0a43d69ee1a57c0
karooolis <hello@karolisram.com>
2025-03-21 23:18:23 +0200

    add abi title

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/abi/AbiExplorer.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/abi/AbiExplorer.tsx
@@ -15,9 +15,12 @@ export function AbiExplorer() {
   }
 
   return (
-    <pre className="text-md relative mb-4 rounded border border-white/20 p-3 text-sm">
-      <JsonView src={data?.abi} theme="a11y" />
-      <CopyButton value={JSON.stringify(data?.abi, null, 2)} className="absolute right-1.5 top-1.5" />
-    </pre>
+    <div className="space-y-4">
+      <h4 className="font-semibold uppercase">ABI</h4>
+      <pre className="text-md relative mb-4 rounded border border-white/20 p-3 text-sm">
+        <JsonView src={data?.abi} theme="a11y" />
+        <CopyButton value={JSON.stringify(data?.abi, null, 2)} className="absolute right-1.5 top-1.5" />
+      </pre>
+    </div>
   );
 }
