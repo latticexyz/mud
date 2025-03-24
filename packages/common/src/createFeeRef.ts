c312a1fe798d7a8216ccfa8f10ae8bb00defcedb
alvarius <alvarius@lattice.xyz>
2025-03-24 13:23:13 +0100

    add internal export for fee ref

--- packages/common/src/createFeeRef.ts
+++ packages/common/src/createFeeRef.ts
@@ -9,22 +9,21 @@ export type CreateFeeRefOptions = {
 };
 
 export type FeeRef = {
-  fees: EstimateFeesPerGasReturnType | {};
+  fees: EstimateFeesPerGasReturnType;
   lastUpdatedTimestamp: number;
 };
 
 /** Update fee values once every `refreshInterval` instead of right before every request */
 export async function createFeeRef({ client, args, refreshInterval }: CreateFeeRefOptions): Promise<FeeRef> {
-  const feeRef: FeeRef = { fees: {}, lastUpdatedTimestamp: 0 };
+  const feeRef: FeeRef = { fees: {} as never, lastUpdatedTimestamp: 0 };
+  await updateFees();
+  setInterval(updateFees, refreshInterval);
 
   async function updateFees(): Promise<void> {
     const fees = await getAction(client, estimateFeesPerGas, "estimateFeesPerGas")(args);
     feeRef.fees = fees;
     feeRef.lastUpdatedTimestamp = Date.now();
   }
-
-  setInterval(updateFees, refreshInterval);
-  await updateFees();
 
   return feeRef;
 }
