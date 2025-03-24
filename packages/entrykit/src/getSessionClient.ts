d8985fc51bca6b0fee6dcacd9c440ee738a73a64
alvarius <alvarius@lattice.xyz>
2025-03-24 14:25:12 +0100

    use existing fee estimation logic

--- packages/entrykit/src/getSessionClient.ts
+++ packages/entrykit/src/getSessionClient.ts
@@ -1,11 +1,10 @@
-import { Account, Address, Chain, Client, FeeValuesEIP1559, LocalAccount, RpcSchema, Transport } from "viem";
+import { Account, Address, Chain, Client, LocalAccount, RpcSchema, Transport } from "viem";
 import { smartAccountActions } from "permissionless";
 import { callFrom } from "@latticexyz/world/internal";
 import { createBundlerClient } from "./createBundlerClient";
 import { SessionClient } from "./common";
 import { SmartAccount } from "viem/account-abstraction";
 import { getBundlerTransport } from "./getBundlerTransport";
-import { internal_getFeeRef } from "@latticexyz/common";
 
 export async function getSessionClient({
   userAddress,
@@ -27,20 +26,6 @@ export async function getSessionClient({
     transport: getBundlerTransport(client.chain),
     client,
     account: sessionAccount,
-    userOperation: {
-      estimateFeesPerGas: async ({ userOperation }) => {
-        const { fees } = await internal_getFeeRef({ client, refreshInterval: 5000 });
-
-        if (fees.maxFeePerGas == null || fees.maxPriorityFeePerGas == null) {
-          throw new Error("Unexpected undefined fee per gas");
-        }
-
-        return {
-          maxFeePerGas: userOperation.maxFeePerGas ?? fees.maxFeePerGas,
-          maxPriorityFeePerGas: userOperation.maxPriorityFeePerGas ?? fees.maxPriorityFeePerGas,
-        } satisfies FeeValuesEIP1559;
-      },
-    },
   });
 
   const sessionClient = bundlerClient
