2d78f79451f93bc2bb2030e1f812735eceff47e2
karooolis <hello@karolisram.com>
2025-04-17 15:23:44 +0300

    use local indexer to fetch worlds

--- packages/explorer/src/app/(explorer)/api/sqlite-indexer/worlds/route.ts
+++ packages/explorer/src/app/(explorer)/api/sqlite-indexer/worlds/route.ts
@@ -1,24 +1,41 @@
-import { Address } from "viem";
-import { getDatabase } from "../../utils/getDatabase";
+import { anvil } from "viem/chains";
+import { indexerForChainId } from "../../../utils/indexerForChainId";
 
 export const dynamic = "force-dynamic";
 
-type Row = {
-  address: Address;
-};
-
-type SqliteTable = Row[] | undefined;
-
 export async function GET() {
   try {
-    const db = getDatabase();
-    const data = (await db?.prepare("SELECT DISTINCT address FROM __mudStoreTables").all()) as SqliteTable;
-    const items = data?.map((row) => ({
+    const indexer = indexerForChainId(anvil.id);
+    const response = await fetch(indexer.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([
+        {
+          query: "SELECT DISTINCT address FROM __mudStoreTables",
+        },
+      ]),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const result = data.result[0];
+    if (!result || !Array.isArray(result) || result.length < 2) {
+      return Response.json({ items: [] });
+    }
+
+    const rows = result.slice(1);
+    const items = rows.map((row: string[]) => ({
       address: {
-        hash: row.address,
+        hash: row[0],
       },
     }));
-    return Response.json({ items: items || [] });
+
+    return Response.json({ items });
   } catch (error: unknown) {
     const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
     return Response.json({ error: errorMessage }, { status: 400 });
