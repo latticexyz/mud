59940480dbde2e4dc2a9b26731f4ca3d08097c5b
dk1a <dk1a@protonmail.com>
2025-04-18 15:02:27 +0300

    self review

--- e2e/packages/contracts/src/systems/AddressPayableArraySystem.sol
+++ e2e/packages/contracts/src/systems/AddressPayableArraySystem.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.24;
 
 import { System } from "@latticexyz/world/src/System.sol";
-import { Vector } from "../codegen/index.sol";
 
 contract AddressPayableArraySystem is System {
   function getAddressPayableArray(address payable[] memory array) public pure returns (address payable[] memory) {
