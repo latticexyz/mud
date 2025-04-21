27317c7302b9a20fcd0d01812a81fd937aec733e
dk1a <dk1a@protonmail.com>
2025-04-21 16:46:41 +0300

    chore(world-modules): fix ERC721 comment typos

--- packages/world-modules/src/modules/erc721-puppet/IERC721Mintable.sol
+++ packages/world-modules/src/modules/erc721-puppet/IERC721Mintable.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
+// OpenZeppelin Contracts (last updated v5.0.0) (token/ERC721/ERC721.sol)
 pragma solidity >=0.8.24;
 
 import { IERC721 } from "./IERC721.sol";
