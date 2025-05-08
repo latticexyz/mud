ca896861b524b6e8757a7f33258c0f633625ab19
karooolis <hello@karolisram.com>
2025-05-08 18:59:55 +0300

    undo playground chains common setup

--- packages/entrykit/playground/common.ts
+++ packages/entrykit/playground/common.ts
@@ -1,12 +1,9 @@
-import { pyrope } from "@latticexyz/common/chains";
 import { Hex } from "viem";
-import { anvil, redstone } from "viem/chains";
+import { anvil } from "viem/chains";
 
 const testWorlds = {
   // TODO: get this from somewhere else, like playground deploy output
   [anvil.id]: "0x60e7e3caed67b9d2cca14519b6cd7700a7d4ee66",
-  [redstone.id]: "0xf75b1b7bdb6932e487c4aa8d210f4a682abeacf0",
-  [pyrope.id]: "0xEb62A70B481a031985365154924D5dfd318260D5",
 } as Partial<Record<string, Hex>>;
 
 const searchParams = new URLSearchParams(window.location.search);
