6e8925d7e4a9ad24aaf4c66bc1659c7cc75c5e3a
karooolis <hello@karolisram.com>
2025-05-08 18:51:58 +0300

    undo playground wagmi config

--- packages/entrykit/playground/wagmiConfig.ts
+++ packages/entrykit/playground/wagmiConfig.ts
@@ -1,50 +1,34 @@
 import { Chain, http } from "viem";
-import { sepolia, baseSepolia } from "viem/chains";
+import { anvil } from "viem/chains";
 import { createWagmiConfig } from "../src/createWagmiConfig";
 import { chainId } from "./common";
-import { pyrope } from "@latticexyz/common/chains";
 
 const chains = [
   {
-    ...pyrope,
+    ...anvil,
     rpcUrls: {
-      ...pyrope.rpcUrls,
+      ...anvil.rpcUrls,
+      // bundler: {
+      //   http: ["http://127.0.0.1:4337"],
+      // },
+      // TODO: automatically grant allowance in anvil instead of requiring the service
       // quarryPassIssuer: {
-      // http: ["https://rpc.pyropechain.com"],
+      //   http: ["http://127.0.0.1:3003/rpc"],
       // },
     },
-    // ...anvil,
-    // rpcUrls: {
-    //   ...anvil.rpcUrls,
-    // bundler: {
-    //   http: ["http://127.0.0.1:4337"],
-    // },
-    // TODO: automatically grant allowance in anvil instead of requiring the service
-    // quarryPassIssuer: {
-    //   http: ["http://127.0.0.1:3003/rpc"],
-    // },
-    // },
     contracts: {
-      quarryPaymaster: {
-        address: "0xEe87F5F13A3e3dFb82A0e7f41ef09eC4e7c8E0C1", // without 0.1 ETH limit
-      },
       // quarryPaymaster: {
       //   address: "0x8D8b6b8414E1e3DcfD4168561b9be6bD3bF6eC4B",
       // },
-      // paymaster: {
-      //   address: "0xf03E61E7421c43D9068Ca562882E98d1be0a6b6e",
-      // },
+      paymaster: {
+        address: "0xf03E61E7421c43D9068Ca562882E98d1be0a6b6e",
+      },
     },
   },
-  sepolia,
-  baseSepolia,
 ] as const satisfies Chain[];
 
 const transports = {
-  // [anvil.id]: http(),
-  [pyrope.id]: http(),
-  [sepolia.id]: http(),
-  [baseSepolia.id]: http(),
+  [anvil.id]: http(),
 } as const;
 
 export const wagmiConfig = createWagmiConfig({
@@ -54,6 +38,6 @@ export const wagmiConfig = createWagmiConfig({
   chains,
   transports,
   pollingInterval: {
-    // [anvil.id]: 500,
+    [anvil.id]: 500,
   },
 });
