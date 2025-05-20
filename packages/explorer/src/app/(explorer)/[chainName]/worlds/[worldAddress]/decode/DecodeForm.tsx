f8dcfe42aedc8f8a64a03d5bc3bdfe29eff537c7
alvarius <alvarius@lattice.xyz>
2025-05-20 11:47:42 +0200

    fall back to 4byte registry

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/decode/DecodeForm.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/decode/DecodeForm.tsx
@@ -3,7 +3,7 @@
 import { AbiFunction, AbiItem, Hex, toFunctionSelector } from "viem";
 import { formatAbiItem } from "viem/utils";
 import * as z from "zod";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import "react18-json-view/src/dark.css";
 import "react18-json-view/src/style.css";
 import { useForm } from "react-hook-form";
@@ -30,6 +30,7 @@ import { getErrorSelector } from "./getErrorSelector";
 type AbiError = AbiItem & { type: "error" };
 
 type ResourceItem = { type: "resource"; id: string; resource: Resource };
+type SelectorDatabase = { type: "remote"; selectors: string[] };
 
 const formSchema = z.object({
   encodedData: z.string().min(1).optional(),
@@ -46,12 +47,25 @@ function isAbiError(item: AbiItem): item is AbiItem & { type: "error" } {
 export function DecodeForm() {
   const { data: worldData, isLoading: isWorldAbiLoading } = useWorldAbiQuery();
   const { data: systemData, isLoading: isSystemAbisLoading } = useSystemAbisQuery();
-  const [decoded, setDecoded] = useState<AbiFunction | AbiError | ResourceItem>();
+  const [decoded, setDecoded] = useState<AbiFunction | AbiError | ResourceItem | SelectorDatabase>();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
+  const results = useMemo(() => {
+    if (!decoded) return [];
+    if (decoded.type === "resource") {
+      return [{ type: decoded.resource.type, label: resourceToLabel(decoded.resource) }];
+    }
+    if (decoded.type === "function" || decoded.type === "error") {
+      return [{ type: decoded.type, label: formatAbiItem(decoded) }];
+    }
+    if (decoded.type === "remote") {
+      return decoded.selectors.map((selector) => ({ type: "signature", label: selector }));
+    }
+    return [];
+  }, [decoded]);
 
-  function onSubmit({ encodedData }: z.infer<typeof formSchema>) {
+  async function onSubmit({ encodedData }: z.infer<typeof formSchema>) {
     if (!encodedData) return;
 
     const worldAbi = worldData?.abi || [];
@@ -72,11 +86,27 @@ export function DecodeForm() {
       return;
     }
 
-    // Attempt to decode as table
     try {
+      // Attempt to decode as table
       const resource = hexToResource(encodedData as Hex);
       if (resource) {
         setDecoded({ type: "resource", id: encodedData, resource });
+      }
+    } catch {
+      // ignore error
+    }
+
+    const selector = encodedData.substring(0, 10);
+
+    // Attempt to find in 4byte database
+    try {
+      const response = await fetch(`https://www.4byte.directory/api/v1/signatures/?hex_signature=${selector}`);
+      const data = await response.json();
+      if (data.results.length > 0) {
+        setDecoded({
+          type: "remote",
+          selectors: data.results.map((result: { text_signature: string }) => result.text_signature),
+        });
       }
     } catch {
       // ignore error
@@ -111,14 +141,14 @@ export function DecodeForm() {
               "border-red-400 bg-red-100": !decoded,
             })}
           >
-            {decoded ? (
-              <>
-                <span className="mr-2 text-sm opacity-50">
-                  {decoded.type === "resource" ? decoded.resource.type : decoded.type}
-                </span>
-                <span>{decoded.type === "resource" ? resourceToLabel(decoded.resource) : formatAbiItem(decoded)}</span>
-                <CopyButton value={JSON.stringify(decoded, null, 2)} className="absolute right-1.5 top-1.5" />
-              </>
+            {results.length > 0 ? (
+              results.map(({ type, label }) => (
+                <>
+                  <span className="mr-2 text-sm opacity-50">{type}</span>
+                  <span>{label}</span>
+                  <CopyButton value={JSON.stringify(decoded, null, 2)} className="absolute right-1.5 top-1.5" />
+                </>
+              ))
             ) : (
               <span className="text-red-700">No matching function or error found for this selector</span>
             )}
