4a21c60fe55fdc34eb4f8b294ceab9ebb5c69389
alvarius <alvarius@lattice.xyz>
2025-05-21 14:21:02 +0200

    fall back to regular rpc methods if wiresaw is down

--- packages/common/src/transports/wiresaw.ts
+++ packages/common/src/transports/wiresaw.ts
@@ -27,85 +27,107 @@ export function wiresaw<const wiresawTransport extends Transport>(
       ...rest,
       // TODO: type `request` so we don't have to cast
       async request(req): ReturnType<EIP1193RequestFn> {
-        if (req.method === "eth_chainId") {
-          if (chainId != null) return chainId;
-          if (transport.fallbackEth) {
-            const { request: fallbackRequest } = transport.fallbackEth(opts);
-            return (chainId = await fallbackRequest(req));
+        try {
+          if (req.method === "eth_chainId") {
+            if (chainId != null) return chainId;
+            if (transport.fallbackEth) {
+              const { request: fallbackRequest } = transport.fallbackEth(opts);
+              return (chainId = await fallbackRequest(req));
+            }
+            return (chainId = await originalRequest(req));
           }
-          return (chainId = await originalRequest(req));
-        }
 
-        if (req.method === "eth_estimateGas") {
-          return originalRequest({ ...req, method: "wiresaw_estimateGas" });
-        }
+          if (req.method === "eth_estimateGas") {
+            return await originalRequest({ ...req, method: "wiresaw_estimateGas" });
+          }
 
-        if (req.method === "eth_call") {
-          return originalRequest({ ...req, method: "wiresaw_call" });
-        }
+          if (req.method === "eth_call") {
+            return await originalRequest({ ...req, method: "wiresaw_call" });
+          }
 
-        if (req.method === "eth_getTransactionCount") {
-          return originalRequest({ ...req, method: "wiresaw_getTransactionCount" });
-        }
+          if (req.method === "eth_getTransactionCount") {
+            return await originalRequest({ ...req, method: "wiresaw_getTransactionCount" });
+          }
 
-        if (req.method === "eth_getTransactionReceipt") {
-          return getTransactionReceipt((req.params as [Hex])[0]);
-        }
+          if (req.method === "eth_getTransactionReceipt") {
+            return await getTransactionReceipt((req.params as [Hex])[0]);
+          }
 
-        if (req.method === "eth_sendUserOperation") {
-          const { userOpHash, txHash } = (await originalRequest({
-            ...req,
-            method: "wiresaw_sendUserOperation",
-          })) as WiresawSendUserOperationResult;
-          transactionHashes[userOpHash] = txHash;
-          return userOpHash;
-        }
+          if (req.method === "eth_sendUserOperation") {
+            const { userOpHash, txHash } = (await originalRequest({
+              ...req,
+              method: "wiresaw_sendUserOperation",
+            })) as WiresawSendUserOperationResult;
+            transactionHashes[userOpHash] = txHash;
+            return userOpHash;
+          }
 
-        if (req.method === "eth_getUserOperationReceipt") {
-          const userOpHash = (req.params as [Hex])[0];
-          const knownTransactionHash = transactionHashes[userOpHash];
-          if (knownTransactionHash) {
-            const transactionReceipt = await getTransactionReceipt(knownTransactionHash);
-            if (transactionReceipt) {
-              return getUserOperationReceipt(userOpHash, transactionReceipt);
+          if (req.method === "eth_getUserOperationReceipt") {
+            const userOpHash = (req.params as [Hex])[0];
+            const knownTransactionHash = transactionHashes[userOpHash];
+            if (knownTransactionHash) {
+              const transactionReceipt = await getTransactionReceipt(knownTransactionHash);
+              if (transactionReceipt) {
+                return getUserOperationReceipt(userOpHash, transactionReceipt);
+              }
+            }
+            if (transport.fallbackBundler) {
+              const { request: fallbackRequest } = transport.fallbackBundler(opts);
+              return await fallbackRequest(req);
             }
           }
-          if (transport.fallbackBundler) {
-            const { request: fallbackRequest } = transport.fallbackBundler(opts);
-            return fallbackRequest(req);
-          }
-        }
 
-        if (req.method === "eth_estimateUserOperationGas") {
-          try {
-            return await estimateUserOperationGas({
-              request: originalRequest,
-              params: req.params as never,
-            });
-          } catch (e) {
-            console.warn("[wiresaw] estimating user operation gas failed, falling back to bundler", e);
+          if (req.method === "eth_estimateUserOperationGas") {
+            try {
+              return await estimateUserOperationGas({
+                request: originalRequest,
+                params: req.params as never,
+              });
+            } catch (e) {
+              console.warn("[wiresaw] estimating user operation gas failed, falling back to bundler", e);
+            }
+
+            if (transport.fallbackBundler) {
+              const { request: fallbackRequest } = transport.fallbackBundler(opts);
+              return await fallbackRequest(req);
+            }
           }
 
-          if (transport.fallbackBundler) {
-            const { request: fallbackRequest } = transport.fallbackBundler(opts);
-            return fallbackRequest(req);
+          // Fallback to regular RPC for methods that don't require wiresaw
+          if (
+            req.method === "eth_blockNumber" ||
+            req.method === "eth_getBlockByNumber" ||
+            req.method === "eth_maxPriorityFeePerGas"
+          ) {
+            if (transport.fallbackEth) {
+              const { request: fallbackRequest } = transport.fallbackEth(opts);
+              return await fallbackRequest(req);
+            }
+            return await originalRequest(req);
           }
-        }
 
-        // Fallback to regular RPC for methods that don't require wiresaw
-        if (
-          req.method === "eth_blockNumber" ||
-          req.method === "eth_getBlockByNumber" ||
-          req.method === "eth_maxPriorityFeePerGas"
-        ) {
+          return await originalRequest(req);
+        } catch (e) {
+          console.warn("[wiresaw] request error", e);
+          const bundlerMethods = [
+            "eth_estimateUserOperationGas",
+            "eth_sendUserOperation",
+            "eth_getUserOperationReceipt",
+          ];
+          if (bundlerMethods.includes(req.method)) {
+            if (transport.fallbackBundler) {
+              const { request: fallbackRequest } = transport.fallbackBundler(opts);
+              console.warn("[wiresaw] falling back to bundler rpc", req);
+              return fallbackRequest(req);
+            }
+          }
           if (transport.fallbackEth) {
             const { request: fallbackRequest } = transport.fallbackEth(opts);
+            console.warn("[wiresaw] falling back to eth rpc", req);
             return fallbackRequest(req);
           }
-          return originalRequest(req);
+          throw e;
         }
-
-        return originalRequest(req);
 
         async function getTransactionReceipt(hash: Hex): Promise<RpcTransactionReceipt | undefined> {
           // Return cached receipt if available
