bc95ea094386f235971f1c5b98a34f41c161999d
Kevin Ingersoll <kingersoll@gmail.com>
2025-05-30 11:27:32 +0200

    feat(entrykit): fork walletconnect connector (#3725)

--- packages/entrykit/src/createWagmiConfig.ts
+++ packages/entrykit/src/createWagmiConfig.ts
@@ -1,6 +1,8 @@
 import { Chain, Transport } from "viem";
-import { Config, CreateConfigParameters, createConfig } from "wagmi";
+import { Config, CreateConfigParameters, CreateConnectorFn, createConfig } from "wagmi";
 import { getDefaultConfig } from "connectkit";
+import { injected, coinbaseWallet, safe } from "wagmi/connectors";
+import { walletConnect } from "./connectors/walletConnect";
 
 export type CreateWagmiConfigOptions<
   chains extends readonly [Chain, ...Chain[]] = readonly [Chain, ...Chain[]],
@@ -21,7 +23,36 @@ export function createWagmiConfig<
   const chains extends readonly [Chain, ...Chain[]],
   transports extends Record<chains[number]["id"], Transport>,
 >(config: CreateWagmiConfigOptions<chains, transports>): Config<chains, transports> {
-  // TODO: wallets so we can extend with eventual passkey wallet
+  // TODO: remove connectors and use ConnectKit's default once https://github.com/wevm/wagmi/pull/4691 lands
+  const connectors: CreateConnectorFn[] = [];
+
+  // If we're in an iframe, include the SafeConnector
+  const shouldUseSafeConnector = !(typeof window === "undefined") && window?.parent !== window;
+  if (shouldUseSafeConnector) {
+    connectors.push(
+      safe({
+        allowedDomains: [/gnosis-safe.io$/, /app.safe.global$/],
+      }),
+    );
+  }
+
+  connectors.push(
+    injected({ target: "metaMask" }),
+    coinbaseWallet({
+      appName: config.appName,
+      overrideIsMetaMask: false,
+    }),
+  );
+
+  if (config.walletConnectProjectId) {
+    connectors.push(
+      walletConnect({
+        showQrModal: false,
+        projectId: config.walletConnectProjectId,
+      }),
+    );
+  }
+
   const configParams = getDefaultConfig({
     chains: config.chains,
     transports: config.transports,
@@ -29,6 +60,8 @@ export function createWagmiConfig<
     appName: config.appName,
     walletConnectProjectId: config.walletConnectProjectId,
     enableFamily: false,
+    connectors,
   });
+
   return createConfig(configParams) as never;
 }
