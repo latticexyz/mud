e9c80e3097d3a01a04a8935ed2db556c1175ba9d
karooolis <hello@karolisram.com>
2025-06-02 16:06:14 +0300

    add collapsible system functions content

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/SystemContent.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/SystemContent.tsx
@@ -1,4 +1,8 @@
+import { ChevronsUpDown } from "lucide-react";
 import { AbiFunction, AbiItem, Hex, toFunctionHash } from "viem";
+import { useState } from "react";
+import { Button } from "../../../../../../../components/ui/Button";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../../../../../../../components/ui/Collapsible";
 import { FunctionField } from "./FunctionField";
 
 type System = {
@@ -25,24 +29,37 @@ export function SystemContent({
   isNamespace,
   initialFunctionHash,
 }: SystemContentProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   if (isNamespace && systems) {
     return (
       <div>
-        <h4 className="mt-4 text-4xl font-semibold opacity-50">{name}</h4>
-        {systems.map((system) => (
-          <div key={system.systemId}>
-            <h4 className="my-4 text-2xl font-semibold">{system.name}</h4>
-            {system.functions.map((abi: AbiFunction) => (
-              <FunctionField
-                key={toFunctionHash(abi)}
-                systemId={system.systemId as Hex}
-                worldAbi={worldAbi}
-                functionAbi={abi}
-                isSelected={initialFunctionHash === toFunctionHash(abi)}
-              />
+        <Collapsible open={isExpanded} onOpenChange={setIsExpanded}>
+          <CollapsibleTrigger asChild>
+            <div className="group flex w-full cursor-pointer items-center justify-between">
+              <h4 className="mt-4 text-2xl font-semibold">{name}</h4>
+              <Button size="sm" variant="ghost">
+                <ChevronsUpDown className="h-4 w-4" />
+              </Button>
+            </div>
+          </CollapsibleTrigger>
+          <CollapsibleContent>
+            {systems.map((system) => (
+              <div key={system.systemId}>
+                <h4 className="my-4 text-xl font-semibold opacity-70">{system.name}</h4>
+                {system.functions.map((abi: AbiFunction) => (
+                  <FunctionField
+                    key={toFunctionHash(abi)}
+                    systemId={system.systemId as Hex}
+                    worldAbi={worldAbi}
+                    functionAbi={abi}
+                    isSelected={initialFunctionHash === toFunctionHash(abi)}
+                  />
+                ))}
+              </div>
             ))}
-          </div>
-        ))}
+          </CollapsibleContent>
+        </Collapsible>
       </div>
     );
   }
@@ -50,15 +67,26 @@ export function SystemContent({
   if (functions) {
     return (
       <div>
-        <h4 className="my-4 text-2xl font-semibold">{name}</h4>
-        {functions.map((abi: AbiFunction) => (
-          <FunctionField
-            key={toFunctionHash(abi)}
-            worldAbi={worldAbi}
-            functionAbi={abi}
-            isSelected={initialFunctionHash === toFunctionHash(abi)}
-          />
-        ))}
+        <Collapsible open={isExpanded} onOpenChange={setIsExpanded}>
+          <CollapsibleTrigger asChild>
+            <div className="group flex w-full cursor-pointer items-center justify-between">
+              <h4 className="my-4 text-2xl font-semibold">{name}</h4>
+              <Button size="sm" variant="ghost">
+                <ChevronsUpDown className="h-4 w-4" />
+              </Button>
+            </div>
+          </CollapsibleTrigger>
+          <CollapsibleContent>
+            {functions.map((abi: AbiFunction) => (
+              <FunctionField
+                key={toFunctionHash(abi)}
+                worldAbi={worldAbi}
+                functionAbi={abi}
+                isSelected={initialFunctionHash === toFunctionHash(abi)}
+              />
+            ))}
+          </CollapsibleContent>
+        </Collapsible>
       </div>
     );
   }
