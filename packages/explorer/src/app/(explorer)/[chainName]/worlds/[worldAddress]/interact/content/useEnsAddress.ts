9620c3a0ffda83a61b0ec9deb65f91cc70b2d664
Karolis Ramanauskas <hello@karolisram.com>
2025-06-12 00:16:26 +0300

    fix(explorer): crashing interact tab (#3736)

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/useEnsAddress.ts
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/useEnsAddress.ts
@@ -2,7 +2,7 @@ import { createPublicClient, http } from "viem";
 import { getEnsAddress } from "viem/actions";
 import { mainnet } from "viem/chains";
 import { normalize } from "viem/ens";
-import { useQuery } from "@tanstack/react-query";
+import { skipToken, useQuery } from "@tanstack/react-query";
 
 const mainnetClient = createPublicClient({
   chain: mainnet,
@@ -10,26 +10,29 @@ const mainnetClient = createPublicClient({
 });
 
 function isValidEnsName(name: string): boolean {
-  return name.includes(".");
+  return /\w\.\w/.test(name);
 }
 
 // This is a workaround implementation because wagmi's useEnsAddress hook
 // requires configuring mainnet in the wagmi config.
-export function useEnsAddress(name: string) {
+export function useEnsAddress(name: string | undefined) {
   return useQuery({
     queryKey: ["ensAddress", name],
-    queryFn: async () => {
-      const normalizedName = normalize(name);
-      const address = await getEnsAddress(mainnetClient, {
-        name: normalizedName,
-      });
+    queryFn:
+      !!name && isValidEnsName(name)
+        ? async () => {
+            const normalizedName = normalize(name);
+            const address = await getEnsAddress(mainnetClient, {
+              name: normalizedName,
+            });
 
-      if (!address) {
-        throw new Error("Invalid ENS name");
-      }
+            if (!address) {
+              throw new Error("Invalid ENS name");
+            }
 
-      return address;
-    },
-    enabled: isValidEnsName(name),
+            return address;
+          }
+        : skipToken,
+    retry: 1,
   });
 }
