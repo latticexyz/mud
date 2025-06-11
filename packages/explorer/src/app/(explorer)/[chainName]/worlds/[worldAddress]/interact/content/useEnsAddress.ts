dea1db8791da61d8ecbc93045a83568f97a55c88
karooolis <hello@karolisram.com>
2025-06-11 23:52:47 +0300

    use skipToken, remove enabled

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/useEnsAddress.ts
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/useEnsAddress.ts
@@ -2,37 +2,42 @@ import { createPublicClient, http } from "viem";
 import { getEnsAddress } from "viem/actions";
 import { mainnet } from "viem/chains";
 import { normalize } from "viem/ens";
-import { useQuery } from "@tanstack/react-query";
+import { skipToken, useQuery } from "@tanstack/react-query";
 
 const mainnetClient = createPublicClient({
   chain: mainnet,
   transport: http(),
 });
 
-function isValidEnsName(name: string | undefined): boolean {
-  if (!name || !name.includes(".")) return false;
+function isValidEnsName(name: string): boolean {
+  if (!name.includes(".")) {
+    return false;
+  }
+
   const parts = name.split(".");
   return parts.length >= 2 && !!parts[1] && parts[1].length > 0;
 }
 
 // This is a workaround implementation because wagmi's useEnsAddress hook
 // requires configuring mainnet in the wagmi config.
-export function useEnsAddress(name: string) {
+export function useEnsAddress(name: string | undefined) {
   return useQuery({
     queryKey: ["ensAddress", name],
-    queryFn: async () => {
-      const normalizedName = normalize(name);
-      const address = await getEnsAddress(mainnetClient, {
-        name: normalizedName,
-      });
+    queryFn:
+      !!name && isValidEnsName(name)
+        ? async () => {
+            const normalizedName = normalize(name);
+            const address = await getEnsAddress(mainnetClient, {
+              name: normalizedName,
+            });
 
-      if (!address) {
-        throw new Error("Invalid ENS name");
-      }
+            if (!address) {
+              throw new Error("Invalid ENS name");
+            }
 
-      return address;
-    },
+            return address;
+          }
+        : skipToken,
     retry: 1,
-    enabled: isValidEnsName(name),
   });
 }
