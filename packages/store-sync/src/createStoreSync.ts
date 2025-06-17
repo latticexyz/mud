8b34b50f4fac86ead1454a15c1fb3e308e6745ff
alvarius <alvarius@lattice.xyz>
2025-06-17 16:08:38 +0200

    move explainer comment to createStoreSync

--- packages/store-sync/src/createStoreSync.ts
+++ packages/store-sync/src/createStoreSync.ts
@@ -49,6 +49,29 @@ import { getChainId, getTransactionReceipt } from "viem/actions";
 import packageJson from "../package.json";
 import { createPendingBlockStream } from "./createPendingBlockStream";
 
+/**
+ * High level approach to syncing state with `createStoreSync`
+ *
+ * If pending logs are not available:
+ * - Initialize snapshot
+ * - Initialize log stream from latest block
+ *   - Catch up logs between snapshot and latest block
+ *   - Attempt to stream logs from indexer
+ *   - On failure, fallback to streaming logs from RPC
+ * - Release initial, catchup and ongoing stream to subscribers
+ *
+ * If pending logs are available:
+ * - Initialize from snapshot
+ * - Open a pending log stream
+ *   - On error recreate the stream
+ * - Open a fallback log stream (indexer or RPC)
+ * - Catch up logs between snapshot and latest block
+ * - Cache processed log indices from pending logs stream
+ * - On every new block from the fallback logs stream
+ *   - Verify that all logs have already been processed in the pending logs stream
+ *   - If missing logs are found, release the missing logs to subscribers and recreate the pending logs stream
+ */
+
 const debug = parentDebug.extend("createStoreSync");
 
 const defaultFilters: SyncFilter[] = internalTableIds.map((tableId) => ({ tableId }));
