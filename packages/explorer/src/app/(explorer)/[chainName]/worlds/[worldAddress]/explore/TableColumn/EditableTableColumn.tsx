85b40974e4f2f8520486aaafa4e4c658e2f93051
karooolis <hello@karolisram.com>
2025-06-18 16:12:59 +0300

    readonly if not connected

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/explore/TableColumn/EditableTableColumn.tsx
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/explore/TableColumn/EditableTableColumn.tsx
@@ -1,4 +1,5 @@
 import { Hex } from "viem";
+import { useAccount } from "wagmi";
 import { AbiType, Table } from "@latticexyz/config";
 import { TDataRow } from "../../../../../queries/useTableDataQuery";
 import { BooleanField } from "./BooleanField";
@@ -14,6 +15,8 @@ type Props = {
 };
 
 export function EditableTableColumn({ name, value, fieldType, tableConfig, keyTuple, blockHeight = 0 }: Props) {
+  const { isConnected } = useAccount();
+
   if (fieldType === "bool") {
     return (
       <BooleanField
@@ -22,6 +25,7 @@ export function EditableTableColumn({ name, value, fieldType, tableConfig, keyTu
         tableConfig={tableConfig}
         keyTuple={keyTuple}
         blockHeight={blockHeight}
+        isReadOnly={!isConnected}
       />
     );
   }
@@ -33,6 +37,7 @@ export function EditableTableColumn({ name, value, fieldType, tableConfig, keyTu
       tableConfig={tableConfig}
       keyTuple={keyTuple}
       blockHeight={blockHeight}
+      isReadOnly={!isConnected}
     />
   );
 }
