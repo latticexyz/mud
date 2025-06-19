0f935240e89e51b2b8ccf3bf03aa65a0624c9cf3
karooolis <hello@karolisram.com>
2025-06-19 23:37:20 +0300

    return arg if undefined

--- packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/encodeFunctionArgs.ts
+++ packages/explorer/src/app/(explorer)/[chainName]/worlds/[worldAddress]/interact/content/encodeFunctionArgs.ts
@@ -3,7 +3,7 @@ import { AbiFunction } from "viem";
 export function encodeFunctionArgs(args: unknown[], inputs: AbiFunction): unknown[] {
   const encodedArgs = args.map((arg, i) => {
     const input = inputs.inputs[i];
-    if (!input) return arg;
+    if (!input || !arg) return arg;
 
     if (input.type.includes("[]")) return JSON.parse(arg as string);
     if (input.type === "tuple") return JSON.parse(arg as string);
