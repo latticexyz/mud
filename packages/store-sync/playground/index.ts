7e27b978b12b0a936492df274e85a430097a5bcd
alvarius <alvarius@lattice.xyz>
2025-06-23 13:19:59 +0200

    add wiresaw to playground

--- packages/store-sync/playground/index.ts
+++ packages/store-sync/playground/index.ts
@@ -1,5 +1,18 @@
-import { redstone } from "@latticexyz/common/chains";
+import { redstone as redstoneBase } from "@latticexyz/common/chains";
 import { createStoreSync } from "../src";
+import { createPublicClient, http } from "viem";
+
+const redstone = {
+  ...redstoneBase,
+  rpcUrls: {
+    ...redstoneBase.rpcUrls,
+    wiresaw: { webSocket: ["wss://wiresaw.redstonechain.com"], http: ["https://wiresaw.redstonechain.com"] },
+  },
+};
+
+const client = createPublicClient({ chain: redstone, transport: http() });
+const latestBlock = await client.getBlockNumber();
+console.log("latestBlock", latestBlock);
 
 const result = await createStoreSync({
   internal_clientOptions: {
@@ -7,12 +20,16 @@ const result = await createStoreSync({
     validateBlockRange: true,
   },
   address: "0x253eb85B3C953bFE3827CC14a151262482E7189C",
-  startBlock: 18756337n,
+  initialBlockLogs: {
+    blockNumber: latestBlock - 1000n,
+    logs: [],
+  },
   enableHydrationChunking: false,
   storageAdapter: async (block) => {
-    console.log("storageAdapter", block);
+    console.log("storageAdapter", { blockNumber: block.blockNumber, logs: block.logs.length });
   },
 });
 
 // Start sync
 result.storedBlockLogs$.subscribe();
+result.latestBlockNumber$.subscribe((blockNumber) => console.log("latestBlockNumber", blockNumber));
