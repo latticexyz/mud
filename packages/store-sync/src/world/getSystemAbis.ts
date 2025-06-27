a7ce36bd855c91c1a0e7a515c6ed7422ef59973d
Kevin Ingersoll <kingersoll@gmail.com>
2025-06-27 16:42:01 +0100

    feat(store-sync): include registered system functions in ABI (#3776)

--- packages/store-sync/src/world/getSystemAbis.ts
+++ packages/store-sync/src/world/getSystemAbis.ts
@@ -1,6 +1,11 @@
-import { Client, Abi, Address, Hex, hexToString, parseAbi, stringToHex } from "viem";
+import { Client, Abi, Address, Hex, hexToString, parseAbi, stringToHex, toFunctionSelector } from "viem";
 import metadataConfig from "@latticexyz/world-module-metadata/mud.config";
 import { getRecords } from "../getRecords";
+import { getFunctions } from "./getFunctions";
+import { groupBy } from "@latticexyz/common/utils";
+import { functionSignatureToAbiItem } from "./functionSignatureToAbiItem";
+import { hexToResource } from "@latticexyz/common";
+import { isAbiFunction } from "./common";
 
 export async function getSystemAbis({
   client,
@@ -19,29 +24,60 @@ export async function getSystemAbis({
   readonly indexerUrl?: string;
   readonly chainId?: number;
 }): Promise<{ readonly [systemId: Hex]: Abi }> {
-  const { records } = await getRecords({
-    table: metadataConfig.tables.metadata__ResourceTag,
-    worldAddress,
-    chainId,
-    indexerUrl,
-    client,
-    fromBlock,
-    toBlock,
-  });
+  const [worldFunctions, { records: metadataRecords }] = await Promise.all([
+    getFunctions({
+      client,
+      worldAddress,
+      fromBlock,
+      toBlock,
+      indexerUrl,
+      chainId,
+    }),
+    getRecords({
+      table: metadataConfig.tables.metadata__ResourceTag,
+      worldAddress,
+      chainId,
+      indexerUrl,
+      client,
+      fromBlock,
+      toBlock,
+    }),
+  ]);
 
-  const abis = Object.fromEntries([
-    ...(systemIds?.map((id) => [id, [] as Abi] as const) || []),
-    ...records
+  const systemAbis = Object.fromEntries(
+    metadataRecords
       .filter(({ resource, tag }) => {
         const isAbiTag = tag === stringToHex("abi", { size: 32 });
-        const matchesSystemId = systemIds ? systemIds.includes(resource) : true;
+        const matchesSystemId = systemIds ? systemIds.includes(resource) : hexToResource(resource).type === "system";
         return isAbiTag && matchesSystemId;
       })
       .map(({ resource, value }) => {
         const abi = value === "0x" ? [] : parseAbi(hexToString(value).split("\n"));
         return [resource, abi] as const;
       }),
-  ]);
+  );
+
+  const systemFunctions = Object.fromEntries(
+    Array.from(
+      groupBy(
+        worldFunctions.filter(({ systemId }) => (systemIds ? systemIds.includes(systemId) : true)),
+        ({ systemId }) => systemId,
+      ).entries(),
+    ).map(([systemId, functions]) => {
+      return [systemId, functions.map((func) => functionSignatureToAbiItem(func.systemFunctionSignature))];
+    }),
+  );
+
+  const ids = Array.from(new Set(systemIds ?? [...Object.keys(systemAbis), ...Object.keys(systemFunctions)]));
+
+  const abis = Object.fromEntries(
+    ids.map((systemId) => {
+      const abi = systemAbis[systemId] ?? [];
+      const abiSelectors = new Set(abi.filter(isAbiFunction).map(toFunctionSelector));
+      const functions = systemFunctions[systemId] ?? [];
+      return [systemId, [...abi, ...functions.filter((item) => !abiSelectors.has(toFunctionSelector(item)))]];
+    }),
+  );
 
   return abis;
 }
--- packages/store-sync/src/world/test/mocks.ts
+++ packages/store-sync/src/world/test/mocks.ts
@@ -1,8 +1,9 @@
+import { resourceToHex } from "@latticexyz/common";
 import { stringToHex } from "viem";
 
-export const mockSystem1Id = stringToHex("system1", { size: 32 });
-export const mockSystem2Id = stringToHex("system2", { size: 32 });
-export const mockSystem3Id = stringToHex("system3", { size: 32 });
+export const mockSystem1Id = resourceToHex({ type: "system", namespace: "test", name: "system1" });
+export const mockSystem2Id = resourceToHex({ type: "system", namespace: "test", name: "system2" });
+export const mockSystem3Id = resourceToHex({ type: "system", namespace: "test", name: "system3" });
 
 export const mockSystem1Fn = "function systemFn(uint256 arg1, bool arg2) pure returns (uint256)";
 export const mockSystem2Fn = "function systemFn2(uint256 arg1, bool arg2) pure returns (uint256)";
@@ -27,7 +28,7 @@ export const mockMetadata = [
     value: stringToHex("System 1"),
   },
   {
-    resource: stringToHex("world", { size: 32 }),
+    resource: mockSystem1Fn,
     tag: stringToHex("worldAbi", { size: 32 }),
     value: stringToHex(`${mockWorldFn}\n${mockError}`),
   },
