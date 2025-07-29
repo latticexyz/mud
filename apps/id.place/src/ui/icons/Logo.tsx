a4d45381d54a997ecaceb668e483201bac2ce71b
Kevin Ingersoll <kingersoll@gmail.com>
2025-07-29 19:42:05 +0900

    update logo/icon throughout

--- apps/id.place/src/ui/icons/Logo.tsx
+++ apps/id.place/src/ui/icons/Logo.tsx
@@ -4,7 +4,7 @@ import { IconSVG, Props } from "./IconSVG";
 
 export function Logo(props: Props) {
   return (
-    <IconSVG viewBox="0 0 45 40" shapeRendering="crispEdges" {...props}>
+    <IconSVG viewBox="0 0 45 40" {...props}>
       <path
         // eslint-disable-next-line max-len
         d="M5 0H0v5h5V0ZM5 5H0v5h5V5ZM5 10H0v5h5v-5ZM5 15H0v5h5v-5ZM5 20H0v5h5v-5ZM5 25H0v5h5v-5ZM5 30H0v5h5v-5ZM5 35H0v5h5v-5ZM10 35H5v5h5v-5ZM15 35h-5v5h5v-5ZM20 35h-5v5h5v-5ZM25 35h-5v5h5v-5ZM30 35h-5v5h5v-5ZM40 30h-5v5h5v-5ZM40 35h-5v5h5v-5ZM35 35h-5v5h5v-5ZM40 5h-5v5h5V5ZM10 0H5v5h5V0ZM15 0h-5v5h5V0ZM20 0h-5v5h5V0ZM25 0h-5v5h5V0ZM30 0h-5v5h5V0ZM35 0h-5v5h5V0ZM40 0h-5v5h5V0Z"
--- packages/id/src/porto/connector.ts
+++ packages/id/src/porto/connector.ts
@@ -269,11 +269,26 @@ export function isPortoConnector(connector: Connector): connector is PortoConnec
 
 /* eslint-disable max-len */
 const icon = `
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" shape-rendering="crispEdges">
-    <rect width="100%" height="100%" fill="white" />
-    <svg x="4" y="4" width="24" height="24" viewBox="0 0 24 24" fill="black" shape-rendering="crispEdges">
-      <path d="M2 3H0v18h24V3H2zm20 2v14H2V5h20zM10 7H6v4h4V7zm-6 6h8v4H4v-4zm16-6h-6v2h6V7zm-6 4h6v2h-6v-2zm6 4h-6v2h6v-2z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60">
+    <rect width="100%" height="100%" fill="#e0e7ff" />
+
+    <svg
+      x="5"
+      y="5"
+      width="50"
+      height="50"
+      viewBox="-5 0 50 40"
+    >
+      <path
+        d="M5 0H0v5h5V0ZM5 5H0v5h5V5ZM5 10H0v5h5v-5ZM5 15H0v5h5v-5ZM5 20H0v5h5v-5ZM5 25H0v5h5v-5ZM5 30H0v5h5v-5ZM5 35H0v5h5v-5ZM10 35H5v5h5v-5ZM15 35h-5v5h5v-5ZM20 35h-5v5h5v-5ZM25 35h-5v5h5v-5ZM30 35h-5v5h5v-5ZM40 30h-5v5h5v-5ZM40 35h-5v5h5v-5ZM35 35h-5v5h5v-5ZM40 5h-5v5h5V5ZM10 0H5v5h5V0ZM15 0h-5v5h5V0ZM20 0h-5v5h5V0ZM25 0h-5v5h5V0ZM30 0h-5v5h5V0ZM35 0h-5v5h5V0ZM40 0h-5v5h5V0Z"
+        fill="#000"
+      />
+      <path
+        d="M30 10h-5v5h5v-5ZM30 15h-5v5h5v-5ZM30 20h-5v5h5v-5ZM30 25h-5v5h5v-5ZM35 10h-5v5h5v-5ZM35 25h-5v5h5v-5ZM45 10h-5v5h5v-5ZM45 15h-5v5h5v-5ZM45 20h-5v5h5v-5ZM45 25h-5v5h5v-5ZM40 10h-5v5h5v-5ZM40 25h-5v5h5v-5Z"
+        fill="#4f39f6"
+      />
     </svg>
+
   </svg>
 `;
 /* eslint-enable max-len */
