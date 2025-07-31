b4f280446deadc16342a842d81fe0169004a0aeb
Gustavo Giráldez <ggiraldez@manas.tech>
2025-07-31 11:17:45 -0400

    Revert change in `parseSystem` return type and fix finding the system base

--- packages/common/src/codegen/utils/parseSystem.ts
+++ packages/common/src/codegen/utils/parseSystem.ts
@@ -3,30 +3,40 @@ import { findContractNodeSlang } from "./findContractNode";
 import { findSymbolImport } from "./findSymbolImport";
 import { Parser } from "@nomicfoundation/slang/parser";
 import { LanguageFacts } from "@nomicfoundation/slang/utils";
-import { Query } from "@nomicfoundation/slang/cst";
+import { assertNonterminalNode, Query } from "@nomicfoundation/slang/cst";
+import { ContractDefinition } from "@nomicfoundation/slang/ast";
 
 const baseSystemName = "System";
 const baseSystemPath = "@latticexyz/world/src/System.sol";
 
-export function parseSystem(source: string, contractName: string): boolean {
+export function parseSystem(
+  source: string,
+  contractName: string,
+): undefined | { contractType: "contract" | "abstract" } {
   const parser = Parser.create(LanguageFacts.latestVersion());
   const root = parser.parseFileContents(source).createTreeCursor();
 
-  const contract = findContractNodeSlang(root, contractName);
-  if (!contract) return false;
+  const contractCursor = findContractNodeSlang(root, contractName);
+  if (!contractCursor) return;
+
+  assertNonterminalNode(contractCursor.node);
+  const contract = new ContractDefinition(contractCursor.node);
+  const contractType = contract.abstractKeyword ? "abstract" : "contract";
 
   // if using the System suffix, assume its a system
-  if (contractName.endsWith("System") && contractName !== baseSystemName) return true;
+  if (contractName.endsWith("System") && contractName !== baseSystemName) return { contractType };
 
-  for (const _ of contract.query([
+  for (const _ of contractCursor.query([
     Query.create(`
       [InheritanceType
-        type_name: ["${baseSystemName}"]
+        type_name: [_ ["${baseSystemName}"]]
       ]
     `),
   ])) {
-    return findSymbolImport(parse(source), baseSystemName)?.path === baseSystemPath;
+    if (findSymbolImport(parse(source), baseSystemName)?.path === baseSystemPath) {
+      return { contractType };
+    }
   }
 
-  return false;
+  return;
 }
